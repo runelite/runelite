eee5f72d4e50742a2df3fc076163798c5ca717b6
Adam <Adam@anope.org>
2015-10-17 20:19:37 -0400

    Add disabled test to dump cache

--- src/test/java/net/runelite/cache/fs/StoreLoadTest.java
+++ src/test/java/net/runelite/cache/fs/StoreLoadTest.java
@@ -1,5 +1,6 @@
 package net.runelite.cache.fs;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import org.junit.Test;
 
@@ -11,5 +12,37 @@ public class StoreLoadTest
 		Store store = new Store(new java.io.File("d:/rs/07/cache"));//c:/rs/cache"));
 		store.load();
 		System.out.println(store);
+	}
+
+	//@Test
+	public void unpackStore() throws IOException
+	{
+		java.io.File base = new java.io.File("d:/rs/07/cache");
+		try (Store store = new Store(base))
+		{
+			store.load();
+
+			for (Index i : store.getIndexes())
+			{
+				java.io.File ifile = new java.io.File(base, "" + i.getId());
+				ifile.mkdir();
+
+				for (Archive a : i.getArchives())
+				{
+					java.io.File afile = new java.io.File(ifile, "" + a.getArchiveId());
+					afile.mkdir();
+
+					for (File f : a.getFiles())
+					{
+						java.io.File ffile = new java.io.File(afile, "" + f.getFileId());
+						try (FileOutputStream fout = new FileOutputStream(ffile))
+						{
+							if (f.getContents() != null)
+								fout.write(f.getContents());
+						}
+					}
+				}
+			}
+		}
 	}
 }
