3c5fcc018fbafb5154e8b24f32add52f45918b66
Adam <Adam@anope.org>
2015-11-11 20:22:46 -0500

    Add basic failing graph test

--- src/test/java/net/runelite/deob/ClassGroupFactory.java
+++ src/test/java/net/runelite/deob/ClassGroupFactory.java
@@ -9,42 +9,64 @@ import net.runelite.deob.signature.Type;
 
 public class ClassGroupFactory
 {
+	private static void addVoidMethod(Methods methods, String name)
+	{
+		Method method = new Method(methods, name, new Signature("()V"));
+		method.setStatic();
+		methods.addMethod(method);
+
+		Attributes methodAttributes = method.getAttributes();
+
+		Code code = new Code(methodAttributes);
+		methodAttributes.addAttribute(code);
+
+		Instructions ins = code.getInstructions();
+		ins.addInstruction(new VReturn(ins));
+	}
+
 	public static ClassGroup generateGroup()
 	{
 		ClassGroup group = new ClassGroup();
-		
+
 		ClassFile cf = new ClassFile(group);
 		cf.setName("test");
 		cf.setSuperName("java/lang/Object");
 		group.addClass(cf);
-		
+
 		Fields fields = cf.getFields();
 		Field field = new Field(fields, "field", new Type("I"));
 		field.setStatic();
 		fields.addField(field);
-		
+
 		Methods methods = cf.getMethods();
 		Method method = new Method(methods, "func", new Signature("()V"));
 		method.setStatic();
 		methods.addMethod(method);
-		
+
 		Attributes methodAttributes = method.getAttributes();
-		
+
 		Code code = new Code(methodAttributes);
 		methodAttributes.addAttribute(code);
+
+		{
+			method = new Method(methods, "func2", new Signature("(III)V"));
+			method.setStatic();
+			methods.addMethod(method);
+
+			methodAttributes = method.getAttributes();
+
+			code = new Code(methodAttributes);
+			methodAttributes.addAttribute(code);
+
+			Instructions ins = code.getInstructions();
+			ins.addInstruction(new VReturn(ins));
+		}
 		
-		method = new Method(methods, "func2", new Signature("(III)V"));
-		method.setStatic();
-		methods.addMethod(method);
-		
-		methodAttributes = method.getAttributes();
-		
-		code = new Code(methodAttributes);
-		methodAttributes.addAttribute(code);
-		
-		Instructions ins = code.getInstructions();
-		ins.addInstruction(new VReturn(ins));
-		
+		addVoidMethod(methods, "void1");
+		addVoidMethod(methods, "void2");
+		addVoidMethod(methods, "void3");
+		addVoidMethod(methods, "void4");
+
 		return group;
 	}
 }
