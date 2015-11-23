3d2e3d6b601cbdedc3c5373d053fc7a314ac3e94
Adam <Adam@anope.org>
2015-11-22 18:54:34 -0600

    Add retention/targets to annotations

--- src/main/java/net/runelite/mapping/ObfuscatedName.java
+++ src/main/java/net/runelite/mapping/ObfuscatedName.java
@@ -1,5 +1,12 @@
 package net.runelite.mapping;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
 public @interface ObfuscatedName
 {
 	String value();
