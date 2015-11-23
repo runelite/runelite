ef8c8f6fb8bbd632ca152258a3934a6870850ab7
Adam <Adam@anope.org>
2015-11-22 19:41:34 -0600

    Add export, import, and implements annotation

---
+++ src/main/java/net/runelite/mapping/Import.java
@@ -0,0 +1,13 @@
+package net.runelite.mapping;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Import
+{
+	String value();
+}
