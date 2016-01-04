02a404130658ef80da55ef877ed084ce81ecdc00
Adam <Adam@anope.org>
2016-01-03 20:33:57 -0500

    Need to split aeq from ieq

--- src/main/java/net/runelite/deob/attributes/code/instructions/IfNull.java
+++ src/main/java/net/runelite/deob/attributes/code/instructions/IfNull.java
@@ -2,6 +2,8 @@ package net.runelite.deob.attributes.code.instructions;
 
 import net.runelite.deob.attributes.code.InstructionType;
 import net.runelite.deob.attributes.code.Instructions;
+import net.runelite.deob.execution.InstructionContext;
+import net.runelite.deob.execution.StackContext;
 
 public class IfNull extends If0
 {
@@ -10,4 +12,27 @@ public class IfNull extends If0
 		super(instructions, type, pc);
 	}
 
+	@Override
+	public boolean isSame(InstructionContext thisIc, InstructionContext otherIc)
+	{
+		if (super.isSame(thisIc, otherIc))
+			return true;
+		
+		if (otherIc.getInstruction() instanceof IfCmpEq)
+		{
+			StackContext s1 = otherIc.getPops().get(0),
+				s2 = otherIc.getPops().get(1);
+			
+			if (s1.getPushed().getInstruction() instanceof AConstNull)
+			{
+				return true;
+			}
+			if (s2.getPushed().getInstruction() instanceof AConstNull)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
