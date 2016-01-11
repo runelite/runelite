a58393f6c7737484e02283c0a190a2970fa58469
Adam <Adam@anope.org>
2016-01-11 08:44:49 -0500

    Another method

--- src/main/java/net/runelite/deob/attributes/code/instructions/IfACmpEq.java
+++ src/main/java/net/runelite/deob/attributes/code/instructions/IfACmpEq.java
@@ -2,11 +2,37 @@ package net.runelite.deob.attributes.code.instructions;
 
 import net.runelite.deob.attributes.code.InstructionType;
 import net.runelite.deob.attributes.code.Instructions;
+import net.runelite.deob.execution.InstructionContext;
+import net.runelite.deob.execution.StackContext;
 
 public class IfACmpEq extends If
 {
 	public IfACmpEq(Instructions instructions, InstructionType type, int pc)
 	{
 		super(instructions, type, pc);
+	}
+	
+	@Override
+	public boolean isSame(InstructionContext thisIc, InstructionContext otherIc)
+	{
+		if (super.isSame(thisIc, otherIc))
+			return true;
+		
+		if (otherIc.getInstruction() instanceof IfNull)
+		{
+			StackContext s1 = thisIc.getPops().get(0),
+				s2 = thisIc.getPops().get(1);
+			
+			if (s1.getPushed().getInstruction() instanceof AConstNull)
+			{
+				return true;
+			}
+			if (s2.getPushed().getInstruction() instanceof AConstNull)
+			{
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
