99c51051fe9475718673e7d2af849ed9834475f7
Adam <Adam@anope.org>
2016-01-13 10:31:33 -0500

    Began thinking about this branch thing

--- src/main/java/net/runelite/deob/attributes/code/instructions/IfCmpGt.java
+++ src/main/java/net/runelite/deob/attributes/code/instructions/IfCmpGt.java
@@ -2,6 +2,8 @@ package net.runelite.deob.attributes.code.instructions;
 
 import net.runelite.deob.attributes.code.InstructionType;
 import net.runelite.deob.attributes.code.Instructions;
+import net.runelite.deob.deobfuscators.rename.ParallelExecutorMapping;
+import net.runelite.deob.execution.InstructionContext;
 
 public class IfCmpGt extends If
 {
@@ -10,4 +12,31 @@ public class IfCmpGt extends If
 		super(instructions, type, pc);
 	}
 
+	@Override
+	public boolean isSame(InstructionContext thisIc, InstructionContext otherIc)
+	{
+		if (super.isSame(thisIc, otherIc))
+			return true;
+		
+		if (otherIc.getInstruction() instanceof IfCmpLe)
+		{
+			return true;
+			 //this is equal, but the branching is different
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public void map(ParallelExecutorMapping mapping, InstructionContext ctx, InstructionContext other)
+	{
+		if (other.getInstruction() instanceof IfCmpLe)
+		{
+			
+		}
+		else
+		{
+			//super.ma
+		}
+	}
 }
