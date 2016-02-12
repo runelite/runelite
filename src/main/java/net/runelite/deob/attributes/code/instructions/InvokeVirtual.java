466da0bcb3dfbb89eabc3e3afd4ba3bbd34e9821
Adam <Adam@anope.org>
2016-02-12 15:11:15 -0500

    Map invoke arguments

--- src/main/java/net/runelite/deob/attributes/code/instructions/InvokeVirtual.java
+++ src/main/java/net/runelite/deob/attributes/code/instructions/InvokeVirtual.java
@@ -24,6 +24,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import net.runelite.deob.Field;
+import net.runelite.deob.attributes.code.instruction.types.GetFieldInstruction;
 import net.runelite.deob.deobfuscators.rename.MappingExecutorUtil;
 import net.runelite.deob.deobfuscators.rename.ParallelExecutorMapping;
 import net.runelite.deob.execution.Execution;
@@ -193,6 +195,32 @@ public class InvokeVirtual extends Instruction implements InvokeInstruction
 			assert m1.getMethods().getClassFile().getName().equals(m2.getMethods().getClassFile().getName());
 			
 			mapping.map(m1, m2);
+		}
+		
+		/* map arguments */
+		assert ctx.getPops().size() == other.getPops().size();
+		
+		for (int i = 0; i < ctx.getPops().size(); ++i)
+		{
+			StackContext s1 = ctx.getPops().get(i),
+				s2 = other.getPops().get(i);
+			
+			InstructionContext base1 = MappingExecutorUtil.resolve(s1.getPushed(), s1);
+			InstructionContext base2 = MappingExecutorUtil.resolve(s2.getPushed(), s2);
+			
+			if (base1.getInstruction() instanceof GetFieldInstruction && base2.getInstruction() instanceof GetFieldInstruction)
+			{
+				GetFieldInstruction gf1 = (GetFieldInstruction) base1.getInstruction(),
+					gf2 = (GetFieldInstruction) base2.getInstruction();
+				
+				Field f1 = gf1.getMyField(),
+					f2 = gf2.getMyField();
+				
+				if (f1 != null && f2 != null)
+				{
+					mapping.map(f1, f2);
+				}
+			}
 		}
 	}
 	
