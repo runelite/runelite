f64fd20d58f5b7f174c80c432a7c89c4af4c2f1d
Adam <Adam@anope.org>
2016-03-10 23:26:43 -0500

    Also constructor

--- src/main/java/net/runelite/mapping/ObfuscatedSignature.java
+++ src/main/java/net/runelite/mapping/ObfuscatedSignature.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface ObfuscatedSignature
 {
 	String signature();
