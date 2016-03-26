13ce8cb5a0ceab45177e8392e181d620d8a2ce48
Adam <Adam@anope.org>
2016-03-26 17:22:00 -0400

    Remove ismappable which has never worked

--- src/test/java/net/runelite/deob/deobfuscators/rename/MapTest.java
+++ src/test/java/net/runelite/deob/deobfuscators/rename/MapTest.java
@@ -5,23 +5,10 @@ import java.io.IOException;
 import net.runelite.asm.ClassGroup;
 import net.runelite.asm.Method;
 import net.runelite.deob.util.JarUtil;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class MapTest
 {
-	@Test
-	public void testMappable() throws IOException
-	{
-		ClassGroup group1 = JarUtil.loadJar(new File("d:/rs/07/adamin1.jar"));
-		ClassGroup group2 = JarUtil.loadJar(new File("d:/rs/07/adamin2.jar"));
-		
-		Assert.assertTrue(MappingExecutorUtil.isMappable(
-			group1.findClass("class99").findMethod("method2220"),
-			group2.findClass("class99").findMethod("method2149")
-		));
-	}
-	
 	@Test
 	public void test() throws IOException
 	{
