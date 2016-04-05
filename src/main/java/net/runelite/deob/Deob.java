21ded198c4dd98cf3eef0095b41e04a10db45153
Adam <Adam@anope.org>
2016-04-04 22:25:13 -0400

    XXX wip save. I think this might be due to calculating max stack incorrectly due to long/double.

--- src/main/java/net/runelite/deob/Deob.java
+++ src/main/java/net/runelite/deob/Deob.java
@@ -23,88 +23,67 @@ import net.runelite.deob.util.JarUtil;
 public class Deob
 {
 	public static void main(String[] args) throws IOException
-	{
-		//merge(); if(true) return;
-		
+	{	
 		long start = System.currentTimeMillis();
 		
 		ClassGroup group = JarUtil.loadJar(new File(args[0]));
 		
-		run(group, new RenameUnique());
-
-		// remove except RuntimeException
-		run(group, new RuntimeExceptions());
-
-		// remove unused methods
-		run(group, new UnreachedCode());
-		run(group, new UnusedMethods());
-
-		// remove illegal state exceptions, frees up some parameters
-		run(group, new IllegalStateExceptions());
-
-		// remove constant logically dead parameters
-		run(group, new ConstantParameter());
-
-		// remove unhit blocks
-		run(group, new UnreachedCode());
-		run(group, new UnusedMethods());
+//		run(group, new RenameUnique());
+//
+//		// remove except RuntimeException
+//		run(group, new RuntimeExceptions());
+//
+//		// remove unused methods
+//		run(group, new UnreachedCode());
+//		run(group, new UnusedMethods());
+//
+//		// remove illegal state exceptions, frees up some parameters
+//		run(group, new IllegalStateExceptions());
+//
+//		// remove constant logically dead parameters
+//		run(group, new ConstantParameter());
+//
+//		// remove unhit blocks
+//		run(group, new UnreachedCode());
+//		run(group, new UnusedMethods());
 
 		// remove unused parameters
 		run(group, new UnusedParameters());
-
-		// remove unused fields
-		run(group, new UnusedFields());
-
-		// remove unused methods, again?
-		run(group, new UnusedMethods());
-
-//		run(group, new MethodInliner());
-//		run(group, new UnusedMethods()); // inliner might leave unused methods
-
-//		// broken because rename was removed
-//		//run(group, new MethodMover());
-
-		run(group, new FieldInliner());
-
-//		// XXX this is broken because when moving clinit around, some fields can depend on other fields
-//		// (like multianewarray)
-//		//new FieldMover().run(group);
-
-		run(group, new UnusedClass());
-	
-		ModArith mod = new ModArith();
-		mod.run(group);
-
-		int last = -1, cur;
-		while ((cur = mod.runOnce()) > 0)
-		{
-			new MultiplicationDeobfuscator().run(group);
-
-			new MultiplyOneDeobfuscator().run(group);
-
-			new MultiplyZeroDeobfuscator().run(group);
-
-			if (last == cur)
-				break;
-
-			last = cur;
-		}
-
-		mod.annotateEncryption();
+//
+//		// remove unused fields
+//		run(group, new UnusedFields());
+//
+//		// remove unused methods, again?
+//		run(group, new UnusedMethods());
+//
+//		run(group, new FieldInliner());
+//
+//		run(group, new UnusedClass());
+//
+//		ModArith mod = new ModArith();
+//		mod.run(group);
+//
+//		int last = -1, cur;
+//		while ((cur = mod.runOnce()) > 0)
+//		{
+//			new MultiplicationDeobfuscator().run(group);
+//
+//			new MultiplyOneDeobfuscator().run(group);
+//
+//			new MultiplyZeroDeobfuscator().run(group);
+//
+//			if (last == cur)
+//				break;
+//
+//			last = cur;
+//		}
+//
+//		mod.annotateEncryption();
 
 		JarUtil.saveJar(group, new File(args[1]));
 		
 		long end = System.currentTimeMillis();
 		System.out.println("Done in " + ((end - start) / 1000L) + "s");
-	}
-	
-	private static void merge() throws IOException
-	{
-		ClassGroup group1 = JarUtil.loadJar(new File("d:/rs/07/adamin1.jar")),
-			group2 = JarUtil.loadJar(new File("d:/rs/07/adamin2.jar"));
-//		
-//		Rename2 rename = new Rename2();
-//		rename.run(group1, group2);
 	}
 	
 	public static boolean isObfuscated(String name)
