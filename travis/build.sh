830870f7d1b293f17adf7a6d68e4becfd683f6d5
Adam <Adam@anope.org>
2016-04-30 17:21:54 -0400

    Travis

---
+++ travis/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	ssh-keyscan -t rsa $RUNELITE_BACKEND > ~/.ssh/known_hosts 2>/dev/null
+
+	openssl aes-256-cbc -d -in travis/runelite.key.enc -out ~/.ssh/runelite -k $SECRET_KEY
+	chmod 600 ~/.ssh/runelite
+
+	set -o pipefail
+	mvn clean deploy --settings travis/settings.xml | sed "s/$RUNELITE_BACKEND/REDACTED/g"
+	exit
+else
+	set -o pipefail
+	mvn clean verify --settings travis/settings.xml | sed "s/$RUNELITE_BACKEND/REDACTED/g"
+	exit
+fi
