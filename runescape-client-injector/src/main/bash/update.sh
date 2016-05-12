33be756073c31493d24d250b853cada54a39126f
Adam <Adam@anope.org>
2016-05-12 18:09:10 -0400

    Don't download vanilla client everytime instead fetch the specific version from maven

--- runescape-client-injector/src/main/bash/update.sh
+++ runescape-client-injector/src/main/bash/update.sh
@@ -2,37 +2,26 @@
 
 JAVA_ARGS="-ea -Xmx512m"
 
-DEOB_PATH=`echo $DEOB_PATH | sed 's/\.jar$/-jar-with-dependencies.jar/'`
-
 echo RS client $RS_CLIENT_PATH
 echo Deobfuscator at $DEOB_PATH
+echo Vanilla at $VANILLA_PATH
 
 JAV_CONFIG=/tmp/jav_config.ws
 VANILLA=/tmp/vanilla.jar
 VANILLA_INJECTED=/tmp/vanilla_injected.jar
 
-curl -L oldschool.runescape.com/jav_config.ws > $JAV_CONFIG
-
-CODEBASE=$(grep codebase $JAV_CONFIG | cut -d'=' -f2)
-INITIAL_JAR=$(grep initial_jar $JAV_CONFIG | cut -d'=' -f2)
-JAR_URL=$CODEBASE$INITIAL_JAR
-
-echo Downloading vanilla client from $JAR_URL
-
-rm -f $VANILLA
-wget $JAR_URL -O $VANILLA
-
 # get version of vanilla
-VANILLA_VER=$(java -cp $DEOB_PATH net.runelite.deob.clientver.ClientVersionMain $VANILLA)
+VANILLA_VER=$(java -cp $DEOB_PATH net.runelite.deob.clientver.ClientVersionMain $VANILLA_PATH)
 echo "Vanilla client version $VANILLA_VER"
 
 # inject vanilla client.
 rm -f $VANILLA_INJECTED
-java $JAVA_ARGS -cp $DEOB_PATH net.runelite.deob.updater.UpdateInject $RS_CLIENT_PATH $VANILLA $VANILLA_INJECTED
+java $JAVA_ARGS -cp $DEOB_PATH net.runelite.deob.updater.UpdateInject $RS_CLIENT_PATH $VANILLA_PATH $VANILLA_INJECTED
 if [ $? -ne 0 ] ; then
         exit 1
 fi
 
+# Unpack so maven can pick this up as a built artifact
 rm -rf /tmp/runelite-work
 mkdir /tmp/runelite-work
 cd /tmp/runelite-work
