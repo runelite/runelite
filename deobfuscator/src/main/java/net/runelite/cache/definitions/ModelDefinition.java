d96f4e3585ed790d2d2bb827e0e6a21d58faecfd
Adam <Adam@anope.org>
2016-05-28 11:36:18 -0400

    Add model dumper and basic model viewer

---
+++ deobfuscator/src/main/java/net/runelite/cache/definitions/ModelDefinition.java
@@ -0,0 +1,45 @@
+package net.runelite.cache.definitions;
+
+public class ModelDefinition
+{
+	public short[] texTriangleX;
+	public int[] vertexX;
+	public byte[] faceRenderPriorities;
+	public int[] vertexY;
+	public int triangleFaceCount;
+	public int[] trianglePointsX;
+	public int[] vertexSkins;
+	public int[] trianglePointsZ;
+	public int anInt2562;
+	public int[] trianglePointsY;
+	public byte[] faceAlphas;
+	public short aShort2565;
+	public byte[] faceRenderType;
+	public short[] faceTextures;
+	public byte priority;
+	public int anInt2569;
+	public byte[] textureRenderTypes;
+	public short[] texTriangleY;
+	public short[] texTriangleZ;
+	public short[] aShortArray2574;
+	public short[] aShortArray2575;
+	public short[] aShortArray2577;
+	public short[] aShortArray2578;
+	boolean aBool2579;
+	public byte[] aByteArray2580;
+	public byte[] textureCoords;
+	public int[] triangleSkinValues;
+	public int[][] anIntArrayArray2583;
+	public int[][] anIntArrayArray2584;
+	public short[] aShortArray2586;
+	public short aShort2589;
+	public short[] faceColor;
+	public int shadowIntensity;
+	public int anInt2592;
+	public int anInt2593;
+	public int[] vertexZ;
+	public int anInt2595;
+	public int vertexCount = 0;
+	public short[] texturePrimaryColor;
+
+}
