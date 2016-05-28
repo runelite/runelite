a503ddcbcf90b3d6bb1ab4cdfd95c630a8b81e12
Adam <Adam@anope.org>
2016-05-28 19:18:40 -0400

    Script definition and dumping

--- deobfuscator/src/main/java/net/runelite/cache/definitions/loaders/ScriptLoader.java
+++ deobfuscator/src/main/java/net/runelite/cache/definitions/loaders/ScriptLoader.java
@@ -27,61 +27,59 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package net.runelite.cache.definitions.loaders;
 
-package net.runelite.cache;
+import net.runelite.cache.definitions.ScriptDefinition;
+import net.runelite.cache.io.InputStream;
 
-
-import com.google.common.io.Files;
-import com.google.gson.Gson;
-import java.io.IOException;
-import net.runelite.cache.definitions.loaders.ModelLoader;
-import net.runelite.cache.fs.Archive;
-import net.runelite.cache.fs.File;
-import net.runelite.cache.fs.Index;
-import net.runelite.cache.fs.Store;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ModelDumperTest
+public class ScriptLoader
 {
-	private static final Logger logger = LoggerFactory.getLogger(ModelDumperTest.class);
-
-	private Gson gson = new Gson();
-
-	@Rule
-	public TemporaryFolder folder = StoreLocation.getTemporaryFolder();
-
-	@Test
-	public void test() throws IOException
+	public ScriptDefinition load(int id, byte[] b)
 	{
-		java.io.File modelDir = folder.newFolder("models");
-		int count = 0;
+		ScriptDefinition def = new ScriptDefinition();
+		InputStream in = new InputStream(b);
 
-		try (Store store = new Store(StoreLocation.LOCATION))
+		in.setOffset(in.getLength() - 12);
+		int paramCount = in.readInt();
+		int localIntCount = in.readUnsignedShort();
+		int localStringCount = in.readUnsignedShort();
+		int anInt2269 = in.readUnsignedShort();
+		int anInt2276 = in.readUnsignedShort();
+
+		def.setLocalIntCount(localIntCount);
+		def.setLocalStringCount(localStringCount);
+		def.setAnInt2269(anInt2269);
+		def.setAnInt2276(anInt2276);
+
+		in.setOffset(0);
+		in.readStringOrNull();
+
+		int[] instructions = new int[paramCount];
+		int[] intOperands = new int[paramCount];
+		String[] aStringArray2272 = new String[paramCount];
+
+		def.setInstructions(instructions);
+		def.setIntOperands(intOperands);
+		def.setaStringArray2272(aStringArray2272);
+
+		int var3;
+		for (int var6 = 0; in.getOffset() < in.getLength() - 12; instructions[var6++] = var3)
 		{
-			store.load();
-
-			Index index = store.getIndex(IndexType.MODELS);
-
-			for (Archive archive : index.getArchives())
+			var3 = in.readUnsignedShort();
+			if (var3 == 3)
 			{
-				assert archive.getFiles().size() == 1;
-
-				File file = archive.getFiles().get(0);
-				byte[] contents = file.getContents();
-
-				ModelLoader loader = new ModelLoader();
-				loader.load(contents);
-
-				Files.write(contents, new java.io.File(modelDir, archive.getArchiveId() + ".model"));
-				//Files.write(gson.toJson(loader), new java.io.File(modelDir, archive.getArchiveId() + ".json"), Charset.defaultCharset());
-				++count;
+				aStringArray2272[var6] = in.readString();
+			}
+			else if (var3 < 100 && 21 != var3 && 38 != var3 && 39 != var3)
+			{
+				intOperands[var6] = in.readInt();
+			}
+			else
+			{
+				intOperands[var6] = in.readUnsignedByte();
 			}
 		}
 
-		logger.info("Dumped {} models to {}", count, modelDir);
+		return def;
 	}
 }
