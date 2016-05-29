748fe1302526fbb4f7e781395fa093067f1c4709
Adam <Adam@anope.org>
2016-05-28 20:08:44 -0400

    This is no longer necessary here

--- deobfuscator/src/test/java/net/runelite/deob/TemporyFolderLocation.java
+++ deobfuscator/src/test/java/net/runelite/deob/TemporyFolderLocation.java
@@ -31,7 +31,6 @@
 package net.runelite.deob;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,19 +39,8 @@ public class TemporyFolderLocation
 {
 	private static final Logger logger = LoggerFactory.getLogger(TemporyFolderLocation.class);
 
-	public static File LOCATION;
-
 	static
 	{
-		try
-		{
-			LOCATION = new File(TemporyFolderLocation.class.getResource("/cache").toURI());
-		}
-		catch (URISyntaxException ex)
-		{
-			logger.error(null, ex);
-		}
-
 		File tmp = new File("d:/temp");
 		if (tmp.exists() || tmp.mkdir())
 			System.setProperty("java.io.tmpdir", "d:/temp");
