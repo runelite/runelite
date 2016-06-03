ad2bf9055839cd790ffd20137af9650bd091b21d
Adam <Adam@anope.org>
2016-06-03 14:13:28 -0400

    Add cache downloader client

---
+++ cache/src/main/java/net/runelite/cache/downloader/FileResult.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by Adam <Adam@sigterm.info>
+ * 4. Neither the name of the Adam <Adam@sigterm.info> nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Adam <Adam@sigterm.info> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Adam <Adam@sigterm.info> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.runelite.cache.downloader;
+
+public class FileResult
+{
+	private final int index;
+	private final int fileId;
+	private final byte[] contents;
+	private final int revision;
+	private final int crc;
+	private final byte[] whirlpool;
+	private final int compression; // compression method used by archive data
+
+	public FileResult(int index, int fileId, byte[] contents, int revision, int crc, byte[] whirlpool, int compression)
+	{
+		this.index = index;
+		this.fileId = fileId;
+		this.contents = contents;
+		this.revision = revision;
+		this.crc = crc;
+		this.whirlpool = whirlpool;
+		this.compression = compression;
+	}
+
+	public int getIndex()
+	{
+		return index;
+	}
+
+	public int getFileId()
+	{
+		return fileId;
+	}
+
+	public byte[] getContents()
+	{
+		return contents;
+	}
+
+	public int getRevision()
+	{
+		return revision;
+	}
+
+	public int getCrc()
+	{
+		return crc;
+	}
+
+	public byte[] getWhirlpool()
+	{
+		return whirlpool;
+	}
+
+	public int getCompression()
+	{
+		return compression;
+	}
+}
