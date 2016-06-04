c60f70a8ad0d2bd147b33910cfc9ed51f9ffddda
Adam <Adam@anope.org>
2016-06-04 18:40:53 -0400

    Remove unused map from execution mapper

--- deobfuscator/src/main/java/net/runelite/deob/deobfuscators/mapping/ExecutionMapper.java
+++ deobfuscator/src/main/java/net/runelite/deob/deobfuscators/mapping/ExecutionMapper.java
@@ -31,8 +31,6 @@
 package net.runelite.deob.deobfuscators.mapping;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import net.runelite.asm.Method;
 
 public class ExecutionMapper
@@ -41,7 +39,6 @@ public class ExecutionMapper
 	
 	private Method method1;
 	private Collection<Method> methods2;
-	private Map<Method, ParallelExecutorMapping> mappings = new HashMap<>();
 
 	public ExecutionMapper(Method method1, Collection<Method> methods2)
 	{
@@ -57,7 +54,6 @@ public class ExecutionMapper
 		for (Method m : methods2)
 		{
 			ParallelExecutorMapping mapping = MappingExecutorUtil.map(method1, m);
-			mappings.put(m, mapping);
 
 			if (highest == null || mapping.same > highest.same)
 			{
