24754772f636cdc47feaa7ebf94986f7969d4381
Adam <Adam@anope.org>
2016-06-09 19:24:03 -0400

    Load pixels directly into sprite definition, not sure what these offsets are

--- cache/src/test/java/net/runelite/cache/TitleDumper.java
+++ cache/src/test/java/net/runelite/cache/TitleDumper.java
@@ -28,32 +28,25 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package net.runelite.cache.loaders;
+package net.runelite.cache;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.List;
-import javax.imageio.ImageIO;
+import java.nio.file.Files;
 import net.runelite.cache.IndexType;
 import net.runelite.cache.StoreLocation;
-import net.runelite.cache.definitions.SpriteDefinition;
-import net.runelite.cache.definitions.loaders.SpriteLoader;
 import net.runelite.cache.fs.Archive;
 import net.runelite.cache.fs.File;
 import net.runelite.cache.fs.Index;
 import net.runelite.cache.fs.Store;
-import net.runelite.cache.io.InputStream;
-import net.runelite.cache.renderable.RGBSprite;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SpriteLoaderTest
+public class TitleDumper
 {
-	private static final Logger logger = LoggerFactory.getLogger(SpriteLoaderTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(TitleDumper.class);
 
 	@Rule
 	public TemporaryFolder folder = StoreLocation.getTemporaryFolder();
@@ -62,50 +55,19 @@ public class SpriteLoaderTest
 	public void extract() throws IOException
 	{
 		java.io.File base = StoreLocation.LOCATION,
-			outDir = folder.newFolder();
-
-		int count = 0;
+			outFile = folder.newFile();
 
 		try (Store store = new Store(base))
 		{
 			store.load();
-			
-			Index index = store.getIndex(IndexType.SPRITES);
-			
-			for (Archive a : index.getArchives())
-			{
-				List<File> files = a.getFiles();
-				
-				Assert.assertEquals(1, files.size());
-				
-				File file = files.get(0);
-				byte[] contents = file.getContents();
-				
-				SpriteLoader loader = new SpriteLoader();
-				loader.load(new InputStream(contents));
-				
-				SpriteDefinition[] defs = loader.getSprites();
-				
-				for (int i = 0; i < defs.length; ++i)
-				{
-					RGBSprite sp = RGBSprite.fromSpriteDefinition(defs[i]);
 
-					// I don't know why this happens
-					if (sp.getSpriteHeight() <= 0 || sp.getSpriteWidth() <= 0)
-						continue;
+			Index index = store.getIndex(IndexType.BINARY);
+			Archive a = index.findArchiveByName("title.jpg");
+			File file = a.getFiles().get(0);
 
-					BufferedImage image = sp.getBufferedImage();
-					java.io.File targ = new java.io.File(outDir, a.getArchiveId() + "-" + i + ".png");
-					targ.mkdirs();
-					ImageIO.write(image, "png", targ);
-
-					++count;
-				}
-			}
+			Files.write(outFile.toPath(), file.getContents());
 		}
 
-		Assert.assertTrue(count > 3000);
-
-		logger.info("Dumped {} sprites to {}", count, outDir);
+		logger.info("Dumped to {}", outFile);
 	}
 }
