abf29d0929370442f50027a7b3baadc49fad0de0
Adam <Adam@anope.org>
2016-11-26 12:08:48 -0500

    Move transformer package, don't know why this was here

--- deobfuscator/src/main/java/net/runelite/deob/deobfuscators/transformers/GetPathTransformer.java
+++ deobfuscator/src/main/java/net/runelite/deob/deobfuscators/transformers/GetPathTransformer.java
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.deob.clientver.transformers;
+package net.runelite.deob.deobfuscators.transformers;
 
 import net.runelite.asm.ClassFile;
 import net.runelite.asm.ClassGroup;
