runelite/runelite

commit 4514cd7f9538be97fca3196909d3fbb0247cef01

Add support for generating setters

deobfuscator/src/main/java/net/runelite/deob/injection/Inject.java
@@ -58,6 +58,7 @@ import net.runelite.asm.attributes.code.instructions.SiPush;
 import net.runelite.asm.pool.Class;
 import net.runelite.asm.signature.Signature;
 import net.runelite.asm.signature.Type;
+import net.runelite.deob.deobfuscators.arithmetic.DMath;
 import net.runelite.mapping.Import;
 import net.runelite.rs.api.Client;
 import org.slf4j.Logger;
@@ -79,6 +80,8 @@ public class Inject
 	private static final java.lang.Class<?> clientClass = Client.class;
 	
 	private final InjectHook hooks = new InjectHook(this);
+
+	private final InjectSetter setters = new InjectSetter(this);
 
 	// deobfuscated contains exports etc to apply to vanilla
 	private final ClassGroup deobfuscated, vanilla;
@@ -118,7 +121,12 @@ public class Inject
 		return sig;
 	}
 
-	private Type classToType(java.lang.Class<?> c)
+	/**
+	 * Convert a java.lang.Class to a Type
+	 * @param c
+	 * @return
+	 */
+	public Type classToType(java.lang.Class<?> c)
 	{
 		int dimms = 0;
 		while (c.isArray())
@@ -207,8 +215,14 @@ public class Inject
 
 				if (an == null || an.find(EXPORT) == null)
 					continue; // not an exported field
-				
-				String exportedName = an.find(EXPORT).getElement().getString();
+
+				Annotation exportAnnotation = an.find(EXPORT);
+				String exportedName = exportAnnotation.getElement().getString();
+
+				boolean isSetter = false;
+				if (exportAnnotation.getElements().size() == 2)
+					isSetter = (boolean) exportAnnotation.getElements().get(1).getValue();
+
 				obfuscatedName = an.find(OBFUSCATED_NAME).getElement().getString();
 				
 				Annotation getterAnnotation = an.find(OBFUSCATED_GETTER);
@@ -234,8 +248,19 @@ public class Inject
 					logger.warn("Non static exported field {} on non exported interface", exportedName);
 					continue;
 				}
+
+				if (isSetter)
+				{
+					Number setter = null;
+					if (getter != null)
+					{
+						setter = DMath.modInverse(getter); // inverse getter to get the setter
+					}
+
+					setters.injectSetter(targetClass, targetApiClass, otherf, exportedName, setter);
+				}
 				
-				java.lang.reflect.Method apiMethod = findImportMethodOnApi(targetApiClass, exportedName);
+				java.lang.reflect.Method apiMethod = findImportMethodOnApi(targetApiClass, exportedName, false);
 				if (apiMethod == null)
 				{
 					logger.info("Unable to find import method on api class {} with imported name {}, not injecting getter", targetApiClass, exportedName);
@@ -301,7 +326,7 @@ public class Inject
 					continue;
 				}
 				
-				java.lang.reflect.Method apiMethod = findImportMethodOnApi(targetClassJava, exportedName); // api method to invoke 'otherm'
+				java.lang.reflect.Method apiMethod = findImportMethodOnApi(targetClassJava, exportedName, false); // api method to invoke 'otherm'
 				if (apiMethod == null)
 				{
 					logger.info("Unable to find api method on {} with imported name {}, not injecting invoker", targetClassJava, exportedName);
@@ -350,13 +375,13 @@ public class Inject
 		return apiClass;
 	}
 
-	private java.lang.reflect.Method findImportMethodOnApi(java.lang.Class<?> clazz, String name)
+	public java.lang.reflect.Method findImportMethodOnApi(java.lang.Class<?> clazz, String name, boolean setter)
 	{
 		for (java.lang.reflect.Method method : clazz.getMethods())
 		{
 			Import i = method.getAnnotation(Import.class);
 			
-			if (i == null || !name.equals(i.value()))
+			if (i == null || !name.equals(i.value()) || i.setter() != setter)
 				continue;
 			
 			return method;
@@ -504,36 +529,12 @@ public class Inject
 		{
 			Type type = deobfuscatedMethod.getDescriptor().getTypeOfArg(i);
 
-			if (type.getArrayDims() > 0 || !type.isPrimitive())
-			{
-				ins.add(new ALoad(instructions, index++));
-			}
+			Instruction loadInstruction = createLoadForTypeIndex(instructions, type, index);
+
+			if (loadInstruction instanceof DLoad || loadInstruction instanceof LLoad)
+				index += 2;
 			else
-			{
-				switch (type.getType())
-				{
-					case "B":
-					case "C":
-					case "I":
-					case "S":
-					case "Z":
-						ins.add(new ILoad(instructions, index++));
-						break;
-					case "D":
-						ins.add(new DLoad(instructions, index++));
-						++index; // takes two slots
-						break;
-					case "F":
-						ins.add(new FLoad(instructions, index++));
-						break;
-					case "J":
-						ins.add(new LLoad(instructions, index++));
-						++index;
-						break;
-					default:
-						throw new RuntimeException("Unknown type");
-				}
-			}
+				index += 1;
 		}
 
 		if (lastGarbageArgumentType != null)
@@ -612,6 +613,40 @@ public class Inject
 		ins.add(new Return(instructions, returnType));
 
 		clazz.getMethods().addMethod(invokerMethodSignature);
+	}
+
+	/**
+	 * create a load instruction for a variable of type from a given index
+	 *
+	 * @param instructions
+	 * @param type
+	 * @param index
+	 * @return
+	 */
+	public Instruction createLoadForTypeIndex(Instructions instructions, Type type, int index)
+	{
+		if (type.getArrayDims() > 0 || !type.isPrimitive())
+		{
+			return new ALoad(instructions, index).makeSpecific();
+		}
+
+		switch (type.getType())
+		{
+			case "B":
+			case "C":
+			case "I":
+			case "S":
+			case "Z":
+				return new ILoad(instructions, index).makeSpecific();
+			case "D":
+				return new DLoad(instructions, index).makeSpecific();
+			case "F":
+				return new FLoad(instructions, index).makeSpecific();
+			case "J":
+				return new LLoad(instructions, index).makeSpecific();
+			default:
+				throw new RuntimeException("Unknown type");
+		}
 	}
 
 	public ClassGroup getDeobfuscated()

deobfuscator/src/main/java/net/runelite/asm/attributes/code/instructions/PutField.java
@@ -54,6 +54,13 @@ public class PutField extends Instruction implements SetFieldInstruction
 		super(instructions, type);
 	}
 
+	public PutField(Instructions instructions, net.runelite.asm.Field field)
+	{
+		super(instructions, InstructionType.PUTFIELD);
+		this.field = field.getPoolField();
+		this.myField = field;
+	}
+
 	@Override
 	public String toString()
 	{

deobfuscator/src/main/java/net/runelite/asm/attributes/code/instructions/PutStatic.java
@@ -52,6 +52,13 @@ public class PutStatic extends Instruction implements SetFieldInstruction
 	{
 		super(instructions, type);
 	}
+
+	public PutStatic(Instructions instructions, net.runelite.asm.Field field)
+	{
+		super(instructions, InstructionType.PUTSTATIC);
+		this.field = field.getPoolField();
+		this.myField = field;
+	}
 	
 	@Override
 	public String toString()

deobfuscator/src/main/java/net/runelite/deob/injection/InjectSetter.java
@@ -1,0 +1,139 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.deob.injection;
+
+import java.util.List;
+import net.runelite.asm.ClassFile;
+import net.runelite.asm.Field;
+import net.runelite.asm.Method;
+import net.runelite.asm.attributes.Code;
+import net.runelite.asm.attributes.code.Instruction;
+import net.runelite.asm.attributes.code.Instructions;
+import net.runelite.asm.attributes.code.instructions.ALoad_0;
+import net.runelite.asm.attributes.code.instructions.IMul;
+import net.runelite.asm.attributes.code.instructions.LDC2_W;
+import net.runelite.asm.attributes.code.instructions.LDC_W;
+import net.runelite.asm.attributes.code.instructions.LMul;
+import net.runelite.asm.attributes.code.instructions.PutField;
+import net.runelite.asm.attributes.code.instructions.PutStatic;
+import net.runelite.asm.attributes.code.instructions.VReturn;
+import net.runelite.asm.signature.Signature;
+import net.runelite.asm.signature.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InjectSetter
+{
+	private static final Logger logger = LoggerFactory.getLogger(InjectSetter.class);
+
+	private final Inject inject;
+
+	public InjectSetter(Inject inject)
+	{
+		this.inject = inject;
+	}
+
+	/**
+	 * inject a setter into the vanilla classgroup
+	 *
+	 * @param targetClass Class where to inject the setter (field's class,
+	 * or client)
+	 * @param targetApiClass API targetClass implements, which may have the
+	 * setter declared
+	 * @param field Field of vanilla that will be set
+	 * @param exportedName exported name of field
+	 * @param setter
+	 */
+	public void injectSetter(ClassFile targetClass, Class<?> targetApiClass, Field field, String exportedName, Number setter)
+	{
+		java.lang.reflect.Method method = inject.findImportMethodOnApi(targetApiClass, exportedName, true);
+		if (method == null)
+		{
+			logger.warn("Setter injection for field {} but an API method was not found on {}", exportedName, targetApiClass);
+			return;
+		}
+
+		if (method.getParameterCount() != 1)
+		{
+			logger.warn("Setter {} with not parameter count != 1?", exportedName);
+			return;
+		}
+
+		logger.info("Injecting setter for {} on {}", exportedName, targetApiClass);
+
+		assert targetClass.findMethod(method.getName()) == null;
+		assert field.isStatic() || field.getFields().getClassFile() == targetClass;
+
+		Signature sig = new Signature();
+		sig.setTypeOfReturnValue(new Type("V"));
+		sig.addArg(new Type(inject.classToType(method.getParameterTypes()[0])));
+
+		Method setterMethod = new Method(targetClass.getMethods(), method.getName(), sig);
+		setterMethod.setAccessFlags(Method.ACC_PUBLIC);
+		targetClass.getMethods().addMethod(setterMethod);
+
+		Code code = new Code(setterMethod);
+		setterMethod.setCode(code);
+
+		Instructions instructions = code.getInstructions();
+		List<Instruction> ins = instructions.getInstructions();
+
+		// load this
+		if (!field.isStatic())
+		{
+			ins.add(new ALoad_0(instructions));
+		}
+
+		// load argument
+		ins.add(inject.createLoadForTypeIndex(instructions, sig.getTypeOfArg(0), 1));
+
+		if (setter != null)
+		{
+			assert setter instanceof Integer || setter instanceof Long;
+
+			if (setter instanceof Integer)
+			{
+				ins.add(new LDC_W(instructions, (int) setter));
+				ins.add(new IMul(instructions));
+			}
+			else
+			{
+				ins.add(new LDC2_W(instructions, (long) setter));
+				ins.add(new LMul(instructions));
+			}
+		}
+
+		if (field.isStatic())
+		{
+			ins.add(new PutStatic(instructions, field));
+		}
+		else
+		{
+			ins.add(new PutField(instructions, field));
+		}
+
+		ins.add(new VReturn(instructions));
+	}
+}

runescape-api/src/main/java/net/runelite/mapping/Export.java
@@ -35,4 +35,6 @@ import java.lang.annotation.Target;
 public @interface Export
 {
 	String value();
+
+	boolean setter() default false;
 }

runescape-api/src/main/java/net/runelite/mapping/Import.java
@@ -35,4 +35,6 @@ import java.lang.annotation.Target;
 public @interface Import
 {
 	String value();
+
+	boolean setter() default false;
 }

runescape-api/src/main/java/net/runelite/rs/api/Client.java
@@ -119,6 +119,9 @@ public interface Client extends GameEngine
 	@Import("username")
 	String getUsername();
 
+	@Import(value = "username", setter = true)
+	void setUsername(String username);
+
 	@Import("menuActions")
 	String[] getMenuActions();
 
@@ -148,8 +151,6 @@ public interface Client extends GameEngine
 
 	@Import("rootInterface")
 	int getRootInterface();
-
-	//void setUsername(String var1);
 
 	@Import("sendGameMessage")
 	void sendGameMessage(int var1, String var2, String var3);

runescape-client/src/main/java/class41.java
@@ -86,7 +86,7 @@ public class class41 {
    @Export("loginMessage3")
    static String loginMessage3;
    @ObfuscatedName("as")
-   @Export("username")
+   @Export(value = "username", setter = true)
    static String username;
    @ObfuscatedName("ar")
    @Export("loginMessage2")

commit 2729f8a47b56352a8382b5e3a9380182b86bc6e8

injector: add missing ins.add() accidentally broken in 4514cd7f9538be97fca3196909d3fbb0247cef01

deobfuscator/src/main/java/net/runelite/deob/injection/Inject.java
@@ -530,6 +530,7 @@ public class Inject
 			Type type = deobfuscatedMethod.getDescriptor().getTypeOfArg(i);
 
 			Instruction loadInstruction = createLoadForTypeIndex(instructions, type, index);
+			ins.add(loadInstruction);
 
 			if (loadInstruction instanceof DLoad || loadInstruction instanceof LLoad)
 				index += 2;