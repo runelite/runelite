03d4d76cc67b7285876c6c9dcc0a901712ed621e
Adam <Adam@anope.org>
2017-02-26 12:41:47 -0500

    cache: fix 5309

--- cache/src/main/java/net/runelite/cache/script/Instructions.java
+++ cache/src/main/java/net/runelite/cache/script/Instructions.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 public class Instructions
 {
-	private static Map<Integer, Instruction> instructions = new HashMap<>();
+	private static final Map<Integer, Instruction> instructions = new HashMap<>();
 
 	public static void init()
 	{
@@ -404,7 +404,7 @@ public class Instructions
 		add(Opcodes.GET_ISRESIZED, 0, 1);
 		add(Opcodes.SET_ISRESIZED, 1, 0);
 		add(5308, 0, 1);
-		add(5308, 1, 0);
+		add(5309, 1, 0);
 		// 5400-5600
 		add(5504, 2, 0);
 		add(5505, 0, 1);
