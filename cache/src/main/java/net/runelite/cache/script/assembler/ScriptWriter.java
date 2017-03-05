3462dd678539781bc4ccad858b5bed2c26f5b3b5
Adam <Adam@anope.org>
2017-03-05 16:28:39 -0500

    cache: add assembler and split scriptdumper test into disassembler/disassemblertest

---
+++ cache/src/main/java/net/runelite/cache/script/assembler/ScriptWriter.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.cache.script.assembler;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.runelite.cache.definitions.ScriptDefinition;
+import net.runelite.cache.script.Instruction;
+import net.runelite.cache.script.Instructions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScriptWriter extends rs2asmBaseListener
+{
+	private static final Logger logger = LoggerFactory.getLogger(ScriptWriter.class);
+
+	private final LabelVisitor labelVisitor;
+
+	private int pos;
+	private List<Integer> opcodes = new ArrayList<>();
+	private List<Integer> iops = new ArrayList<>();
+	private List<String> sops = new ArrayList<>();
+
+	public ScriptWriter(LabelVisitor labelVisitor)
+	{
+		this.labelVisitor = labelVisitor;
+	}
+
+	@Override
+	public void exitInstruction(rs2asmParser.InstructionContext ctx)
+	{
+		++pos;
+	}
+
+	@Override
+	public void enterName_string(rs2asmParser.Name_stringContext ctx)
+	{
+		String text = ctx.getText();
+		Instruction i = Instructions.find(text);
+		if (i == null)
+		{
+			logger.warn("Unknown instruction {}", text);
+			throw new RuntimeException("Unknown instruction " + text);
+		}
+
+		int opcode = i.getOpcode();
+		addOpcode(opcode);
+	}
+
+	@Override
+	public void enterName_opcode(rs2asmParser.Name_opcodeContext ctx)
+	{
+		String text = ctx.getText();
+		int opcode = Integer.parseInt(text);
+		addOpcode(opcode);
+	}
+
+	private void addOpcode(int opcode)
+	{
+		assert opcodes.size() == pos;
+		assert iops.size() == pos;
+		assert sops.size() == pos;
+
+		opcodes.add(opcode);
+		iops.add(null);
+		sops.add(null);
+	}
+
+	@Override
+	public void enterOperand_int(rs2asmParser.Operand_intContext ctx)
+	{
+		String text = ctx.getText();
+		int value = Integer.parseInt(text);
+		iops.set(pos, value);
+	}
+
+	@Override
+	public void enterOperand_qstring(rs2asmParser.Operand_qstringContext ctx)
+	{
+		String text = ctx.getText();
+		text = text.substring(1, text.length() - 1);
+		sops.set(pos, text);
+	}
+
+	@Override
+	public void enterOperand_label(rs2asmParser.Operand_labelContext ctx)
+	{
+		String text = ctx.getText();
+		Integer instruction = labelVisitor.getInstructionForLabel(text);
+		if (instruction == null)
+		{
+			throw new RuntimeException("reference to unknown label " + text);
+		}
+
+		int target = instruction - pos - 1; // -1 to go to the instruction prior
+		iops.set(pos, target);
+	}
+
+	public ScriptDefinition buildScript()
+	{
+		ScriptDefinition script = new ScriptDefinition();
+		script.setInstructions(opcodes.stream().mapToInt(Integer::valueOf).toArray());
+		script.setIntOperands(iops.stream()
+			.map(i -> i == null ? 0 : i)
+			.mapToInt(Integer::valueOf)
+			.toArray());
+		script.setStringOperands(sops.toArray(new String[0]));
+		return script;
+	}
+}
---
+++ cache/src/main/java/net/runelite/cache/script/disassembler/Disassembler.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.cache.script.disassembler;
+
+import java.io.IOException;
+import net.runelite.cache.definitions.ScriptDefinition;
+import net.runelite.cache.script.Instruction;
+import net.runelite.cache.script.Instructions;
+import net.runelite.cache.script.Opcodes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Disassembler
+{
+	private static final Logger logger = LoggerFactory.getLogger(Disassembler.class);
+
+	private boolean isJump(int opcode)
+	{
+		switch (opcode)
+		{
+			case Opcodes.JUMP:
+			case Opcodes.IF_ICMPEQ:
+			case Opcodes.IF_ICMPGE:
+			case Opcodes.IF_ICMPGT:
+			case Opcodes.IF_ICMPLE:
+			case Opcodes.IF_ICMPLT:
+			case Opcodes.IF_ICMPNE:
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	private boolean[] needLabel(ScriptDefinition script)
+	{
+		int[] instructions = script.getInstructions();
+		int[] iop = script.getIntOperands();
+		boolean[] jumped = new boolean[instructions.length];
+
+		for (int i = 0; i < instructions.length; ++i)
+		{
+			int opcode = instructions[i];
+
+			if (!isJump(opcode))
+			{
+				continue;
+			}
+
+			// + 1 because the jumps go to the instructions prior to the
+			// one you really want, because the pc is incremented on the
+			// next loop
+			int to = i + iop[i] + 1;
+			assert to >= 0 && to < instructions.length;
+
+			jumped[to] = true;
+		}
+
+		return jumped;
+	}
+
+	public String disassemble(ScriptDefinition script) throws IOException
+	{
+		StringBuilder writer = new StringBuilder();
+
+		int[] instructions = script.getInstructions();
+		int[] iops = script.getIntOperands();
+		String[] sops = script.getStringOperands();
+
+		assert iops.length == instructions.length;
+		assert sops.length == instructions.length;
+
+		boolean[] jumps = needLabel(script);
+
+		for (int i = 0; i < instructions.length; ++i)
+		{
+			int opcode = instructions[i];
+			int iop = iops[i];
+			String sop = sops[i];
+
+			Instruction ins = Instructions.find(opcode);
+			if (ins == null)
+			{
+				logger.warn("Unknown instruction {} in script {}", opcode, script.getId());
+			}
+
+			if (jumps[i])
+			{
+				// something jumps here
+				writer.append("LABEL").append(i).append(":\n");
+			}
+
+			String name;
+			if (ins != null && ins.getName() != null)
+			{
+				name = ins.getName();
+			}
+			else
+			{
+				name = String.format("%03d", opcode);
+			}
+
+			writer.append(String.format("   %-22s", name));
+
+			if (iop != 0 || opcode == Opcodes.LOAD_INT)
+			{
+				if (isJump(opcode))
+				{
+					writer.append(" LABEL").append(i + iop + 1);
+				}
+				else
+				{
+					writer.append(" ").append(iop);
+				}
+			}
+
+			if (sop != null)
+			{
+				writer.append(" \"").append(sop).append("\"");
+			}
+			writer.append("\n");
+		}
+
+		return writer.toString();
+	}
+}
