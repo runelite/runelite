693452a0a7ec46608272a253cacdb0015873c7d8
Adam <Adam@anope.org>
2017-03-11 14:42:30 -0500

    Try this again, with less special characters in the password..

--- scripts/deploy-vanilla.sh
+++ scripts/deploy-vanilla.sh
@@ -4,6 +4,6 @@ if [ "$#" -ne 2 ] ; then
 	exit 1
 fi
 
-echo "Deploying vanilla version $1 ($2) to $RUNELITE_REPOSITORY_URL"
+echo "Deploying vanilla version $1 ($2) to repo.runelite.net"
 
-mvn deploy:deploy-file -DgroupId=net.runelite.rs -DartifactId=vanilla -Dversion=$1 -Dfile=$2 -DrepositoryId=runelite -Durl=$RUNELITE_REPOSITORY_URL
+mvn deploy:deploy-file -DgroupId=net.runelite.rs -DartifactId=vanilla -Dversion=$1 -Dfile=$2 -DrepositoryId=runelite -Durl=dav:http://repo.runelite.net
