b80d6b6ca52661395ad72a7e8303bedcd24d2515
Abel Briggs <abelbriggs1@hotmail.com>
2017-04-16 15:06:07 -0500

    Added implementation for an idle notifier plugin, animation hook, and thread pool (#23)
    Added implementation for an idle notifier plugin and animation hook

---
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016-2017, Abel Briggs
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.api;
+
+// Note: This class is not complete: these animations were manually gathered
+// through getAnimation(). Please add animations as you happen to use them.
+public final class AnimationID
+{
+	public static final int IDLE = -1;
+	public static final int WOODCUTTING_BRONZE = 879;
+	public static final int WOODCUTTING_IRON = 877;
+	public static final int WOODCUTTING_STEEL = 875;
+	public static final int WOODCUTTING_BLACK = 873;
+	public static final int WOODCUTTING_MITHRIL = 871;
+	public static final int WOODCUTTING_ADAMANT = 869;
+	public static final int WOODCUTTING_RUNE = 867;
+	public static final int WOODCUTTING_DRAGON = 2846;
+	public static final int WOODCUTTING_INFERNAL = 0; // unknown - placeholder
+	public static final int CONSUMING = 829; // consuming consumables
+	public static final int FIREMAKING = 733;
+	public static final int COOKING_FIRE = 897;
+	public static final int COOKING_RANGE = 896;
+	public static final int FLETCHING_BOW_CUTTING = 1248;
+	public static final int FLETCHING_BOW_STRINGING = 6684; // ids are based on bow - incomplete!
+	public static final int GEM_CUTTING_OPAL = 890;
+	public static final int GEM_CUTTING_JADE = 891;
+	public static final int GEM_CUTTING_REDTOPAZ = 892;
+	public static final int GEM_CUTTING_SAPPHIRE = 888;
+	public static final int GEM_CUTTING_EMERALD = 889;
+	public static final int GEM_CUTTING_RUBY = 887;
+	public static final int GEM_CUTTING_DIAMOND = 886;
+	public static final int CRAFTING_LEATHER = 1249; // unknown if the anim is the same for all leathers
+	public static final int SMITHING_SMELTING = 899;
+	public static final int SMITHING_CANNONBALL = 827; //cball smithing uses this and SMITHING_SMELTING
+	public static final int SMITHING_ANVIL = 898;
+	public static final int FISHING_NET = 621;
+	public static final int FISHING_POLE_INITIAL = 622; //initial swing of pole
+	public static final int FISHING_POLE_CAST = 623; // pole is in the water
+	public static final int FISHING_CAGE = 619;
+	public static final int FISHING_HARPOON = 618;
+	public static final int MINING_BRONZE_PICKAXE = 625;
+	public static final int MINING_IRON_PICKAXE = 626;
+	public static final int MINING_STEEL_PICKAXE = 627;
+	public static final int MINING_BLACK_PICKAXE = 3873;
+	public static final int MINING_MITHRIL_PICKAXE = 629;
+	public static final int MINING_ADAMANT_PICKAXE = 628;
+	public static final int MINING_RUNE_PICKAXE = 624;
+	public static final int MINING_DRAGON_PICKAXE = 7139;
+	public static final int MINING_INFERNAL_PICKAXE = 0; //placeholder, unknown
+	public static final int MINING_MOTHERLODE_BRONZE = 6753;
+	public static final int MINING_MOTHERLODE_IRON = 6754;
+	public static final int MINING_MOTHERLODE_STEEL = 6755;
+	public static final int MINING_MOTHERLODE_BLACK = 3866;
+	public static final int MINING_MOTHERLODE_MITHRIL = 6757;
+	public static final int MINING_MOTHERLODE_ADAMANT = 6756;
+	public static final int MINING_MOTHERLODE_RUNE = 6752;
+	public static final int MINING_MOTHERLODE_DRAGON = 6758;
+	public static final int MINING_MOTHERLODE_INFERNAL = 0; // placeholder, unknown
+	public static final int HERBLORE_POTIONMAKING = 363; //used for both herb and secondary
+	public static final int MAGIC_CHARGING_ORBS = 726;
+}
