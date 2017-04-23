9024417b7743b12c1258a814e70e50b5d551698d
Adam <Adam@anope.org>
2017-04-22 20:50:00 -0400

    cache: document more handshake responses

--- cache/src/main/java/net/runelite/cache/downloader/requests/HelloHandshake.java
+++ cache/src/main/java/net/runelite/cache/downloader/requests/HelloHandshake.java
@@ -22,13 +22,40 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package net.runelite.cache.downloader.requests;
 
 public class HelloHandshake
 {
 	public static final int RESPONSE_OK = 0;
+	public static final int ACCOUNT_DISABLED = 4;
+	public static final int ACCOUNT_ONLINE = 5;
 	public static final int RESPONSE_OUTDATED = 6;
+	public static final int WORLD_FULL = 7;
+	public static final int SERVER_OFFLINE = 8;
+	public static final int LIMITED_EXCEEDED = 9;
+	public static final int BAD_SESSION_ID = 10;
+	public static final int ACCOUNT_HIJACK = 11;
+	public static final int MEMBERS_WORLD = 12;
+	public static final int COULD_NOT_COMPLETE_LOGIN = 13;
+	public static final int SERVER_BEING_UPDATED = 14;
+	public static final int TOO_MANY_ATTEMPTS = 16;
+	public static final int MEMBERS_ONLY_AREA = 17;
+	public static final int ACCOUNT_LOCKED = 18;
+	public static final int CLOSED_BETA = 19;
+	public static final int INVALID_LOGINSERVER = 20;
+	public static final int MALFORMED_PACKET = 22;
+	public static final int NO_REPLY_FROM_LOGINSERVER = 23;
+	public static final int ERR_LOADING_PROFILE = 24;
+	public static final int UNEXPECTED_LOGINSERVER_RESPONSE = 25;
+	public static final int IP_BANNED = 26;
+	public static final int SERVICE_UNAVAILABLE = 27;
+	public static final int NO_DISPLAY_NAME = 31;
+	public static final int BILLING_ERROR = 32;
+	public static final int ACCOUNT_INACCESSABLE = 37;
+	public static final int VOTE_TO_PLAY = 38;
+	public static final int NOT_ELIGIBLE = 55;
+	public static final int NEED_AUTHENTICATOR = 56;
+	public static final int AUTHENTICATOR_CODE_WRONG = 57;
 
 	private byte type = 15; // handshake type
 	private int revision;
@@ -42,6 +69,7 @@ public class HelloHandshake
 	{
 		this.revision = revision;
 	}
+
 	public byte getType()
 	{
 		return type;
