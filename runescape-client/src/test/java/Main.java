f76522eb03baf0b6003a1cccd898cd8dd882c4a1
Adam <Adam@anope.org>
2017-05-25 19:42:47 -0400

    runescape-client: update Main to set client size, which is now required

--- runescape-client/src/test/java/Main.java
+++ runescape-client/src/test/java/Main.java
@@ -4,14 +4,23 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame
 {
+	private static final int WIDTH = 765;
+	private static final int HEIGHT = 503;
+
 	public Main() throws IOException
 	{
 		this.setSize(800, 600);
 		Client client = new Client();
+
 		ConfigLoader config = new ConfigLoader();
 		config.fetch();
+
 		client.setStub(new RSStub(config, client));
+
 		this.add(client);
+
+		client.setSize(WIDTH, HEIGHT);
+
 		client.init();
 		client.start();
 		this.setVisible(true);
