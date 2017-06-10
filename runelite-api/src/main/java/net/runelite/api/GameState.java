6f241ceea57a64b7cbb8daf7b4402e9a94aa2da5
Adam <Adam@anope.org>
2017-06-09 23:17:51 -0400

    checkstyle: require tabs for indenting

--- runelite-api/src/main/java/net/runelite/api/GameState.java
+++ runelite-api/src/main/java/net/runelite/api/GameState.java
@@ -22,31 +22,34 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package net.runelite.api;
 
 public enum GameState
 {
-   UNKNOWN(-1),
-   STARTING(0),
-   LOGIN_SCREEN(10),
-   LOGGING_IN(20),
-   LOADING(25),
-   LOGGED_IN(30),
-   HOPPING(45);
+	UNKNOWN(-1),
+	STARTING(0),
+	LOGIN_SCREEN(10),
+	LOGGING_IN(20),
+	LOADING(25),
+	LOGGED_IN(30),
+	HOPPING(45);
 
-   private final int state;
+	private final int state;
 
-   GameState(int state)
-   {
-	   this.state = state;
-   }
+	GameState(int state)
+	{
+		this.state = state;
+	}
 
-   public static GameState of(int state)
-   {
-	   for (GameState gs : GameState.values())
-		   if (gs.state == state)
-			   return gs;
-	   return UNKNOWN;
-   }
+	public static GameState of(int state)
+	{
+		for (GameState gs : GameState.values())
+		{
+			if (gs.state == state)
+			{
+				return gs;
+			}
+		}
+		return UNKNOWN;
+	}
 }
