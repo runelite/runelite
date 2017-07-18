7e19d9df2146d24e25a0bae94eb235e321b6eeab
l2- <xanderhermans@hotmail.com>
2017-07-18 02:31:00 +0200

    Added high alch values to price command

--- runelite-client/src/main/java/net/runelite/client/plugins/pricecommands/PriceCommands.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pricecommands/PriceCommands.java
@@ -40,6 +40,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.http.api.item.ItemClient;
 import net.runelite.http.api.item.ItemPrice;
 import net.runelite.http.api.item.SearchResult;
+import net.runelite.rs.api.ItemComposition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,6 +53,8 @@ public class PriceCommands extends Plugin
 	private final ItemClient itemClient = new ItemClient();
 	private final RuneLite runelite = RuneLite.getRunelite();
 	private final Client client = RuneLite.getClient();
+
+	private static final float HIGH_ALCHEMY_CONSTANT = 0.6f;
 
 	@Override
 	protected void startUp() throws Exception
@@ -139,8 +142,14 @@ public class PriceCommands extends Plugin
 				return;
 			}
 
-			int cost = itemPrice.getPrice();
-			String response = "Price of " + result.getItems().get(0).getName() + ": GE average " + String.format("%,d", cost);
+			ItemComposition itemComposition = client.getItemDefinition(itemId);
+			int HaPrice = -1;
+			if (itemComposition != null)
+			{
+				HaPrice = Math.round(itemComposition.getPrice() * HIGH_ALCHEMY_CONSTANT);
+			}
+			int GePrice = itemPrice.getPrice();
+			String response = "Price of " + result.getItems().get(0).getName() + ": GE average " + String.format("%,d", GePrice) + " HA value " + String.format("%,d", HaPrice);
 
 			logger.debug("Setting response {}", response);
 
