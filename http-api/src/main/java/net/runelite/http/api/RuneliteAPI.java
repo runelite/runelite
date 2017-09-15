d91b779981ece36520ff96d2884137d6770d32b5
Julian Nowaczek <jnowaczek@users.noreply.github.com>
2017-09-14 19:58:40 -0500

    Replace HiscorePanel skill icons with smaller versions from the official hiscore website

--- http-api/src/main/java/net/runelite/http/api/RuneliteAPI.java
+++ http-api/src/main/java/net/runelite/http/api/RuneliteAPI.java
@@ -42,8 +42,8 @@ public class RuneliteAPI
 	public static final OkHttpClient CLIENT = new OkHttpClient();
 	public static final Gson GSON = new Gson();
 
-	private static final String BASE = "https://api.runelite.net/runelite-";
-	private static final String WSBASE = "wss://api.runelite.net/runelite-";
+	private static final String BASE = "http://localhost:8080/runelite-";
+	private static final String WSBASE = "wss://localhost:8080/runelite-";
 	private static final Properties properties = new Properties();
 	private static String version;
 	private static int rsVersion;
