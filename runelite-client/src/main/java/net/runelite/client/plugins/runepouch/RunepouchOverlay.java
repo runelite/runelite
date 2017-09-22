9e6040139c05ff170ef97e2ab7c858a9caa48407
Tyler Hardy <tylerthardy@gmail.com>
2017-09-22 05:20:06 -0500

    License change

--- runelite-client/src/main/java/net/runelite/client/plugins/runepouch/RunepouchOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/runepouch/RunepouchOverlay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * Copyright (c) 2017, Tyler <http://github.com/tylerthardy>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
