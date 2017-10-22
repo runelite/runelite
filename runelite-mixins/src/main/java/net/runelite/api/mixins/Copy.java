367bd1e1ecafc37cb9208f4f6e231a8e2ea54a54
Adam <Adam@anope.org>
2017-10-22 10:57:54 -0400

    Add Documented annotation and javadoc to new mixin annotations

--- runelite-mixins/src/main/java/net/runelite/api/mixins/Copy.java
+++ runelite-mixins/src/main/java/net/runelite/api/mixins/Copy.java
@@ -24,13 +24,19 @@
  */
 package net.runelite.api.mixins;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates a copy of the specified method should be copied into
+ * the target class.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Documented
 public @interface Copy
 {
 	/**
