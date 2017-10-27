1244c0cb80fe26258053e0920ec6a388624e7941
Adam <Adam@anope.org>
2017-10-26 20:50:20 -0400

    Remove rsmv modelviewer [ci skip]

--- model-viewer-rsmv/src/main/java/net/runelite/rsmv/class16.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.rsmv;
-
-
-final class class16 {
-   int field83;
-   int field84;
-   int field85;
-   int field86;
-}
--- model-viewer-rsmv/src/main/java/net/runelite/rsmv/class18.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.rsmv;
-
-
-public class class18 {
-   class16[] field89;
-   public int field90 = 1000;
-}
