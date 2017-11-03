000d3ffdb202cc42da6e6ecc31151fb67c5554a5
Tyler Hardy <tylerthardy@gmail.com>
2017-11-03 19:02:05 -0400

    Add slayer plugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/slayer/Slayer.java
@@ -0,0 +1,339 @@
+/*
+ * Copyright (c) 2017, Tyler <https://github.com/tylerthardy>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.slayer;
+
+import com.google.common.eventbus.Subscribe;
+import java.awt.image.BufferedImage;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.ItemID;
+import static net.runelite.api.Skill.SLAYER;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.RuneLite;
+import net.runelite.client.events.ChatMessage;
+import net.runelite.client.events.ExperienceChanged;
+import net.runelite.client.events.GameStateChanged;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.task.Schedule;
+import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@PluginDescriptor(
+	name = "Slayer plugin"
+)
+public class Slayer extends Plugin
+{
+	private static final Logger logger = LoggerFactory.getLogger(Slayer.class);
+
+	//Chat messages
+	private static final Pattern CHAT_GEM_PROGRESS_MESSAGE = Pattern.compile("You're assigned to kill (.*); only (\\d*) more to go\\.");
+	private static final String CHAT_GEM_COMPLETE_MESSAGE = "You need something new to hunt.";
+	private static final Pattern CHAT_COMPLETE_MESSAGE = Pattern.compile("[\\d]+(?:,[\\d]+)?");
+	private static final String CHAT_CANCEL_MESSAGE = "Your task has been cancelled.";
+
+	//NPC messages
+	private static final Pattern NPC_ASSIGN_MESSAGE = Pattern.compile(".*Your new task is to kill (\\d*) (.*)\\.");
+	private static final Pattern NPC_CURRENT_MESSAGE = Pattern.compile("You're still hunting (.*), you have (\\d*) to go\\..*");
+
+	//Reward UI
+	private static final Pattern REWARD_POINTS = Pattern.compile("Reward points: (\\d*)");
+
+	private final InfoBoxManager infoBoxManager = RuneLite.getRunelite().getInfoBoxManager();
+	private final SlayerConfig config = RuneLite.getRunelite().getConfigManager().getConfig(SlayerConfig.class);
+	private final SlayerOverlay overlay = new SlayerOverlay(this);
+	private final Client client = RuneLite.getClient();
+
+	private String taskName;
+	private int amount;
+	private TaskCounter counter;
+	private int streak;
+	private int points;
+	private int cachedXp;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+
+	}
+
+	@Subscribe
+	public void onGameStateChange(GameStateChanged event)
+	{
+		if (!config.enabled())
+		{
+			return;
+		}
+
+		switch (event.getGameState())
+		{
+			case HOPPING:
+			case LOGGING_IN:
+				cachedXp = 0;
+				taskName = "";
+				amount = 0;
+				break;
+			case LOGGED_IN:
+				if (config.amount() != -1 && !config.taskName().isEmpty())
+				{
+					setTask(config.taskName(), config.amount());
+				}
+				break;
+		}
+	}
+
+	private void save()
+	{
+		config.amount(amount);
+		config.taskName(taskName);
+		config.points(points);
+		config.streak(streak);
+	}
+
+	@Schedule(
+		period = 600,
+		unit = ChronoUnit.MILLIS
+	)
+	public void scheduledChecks()
+	{
+		if (!config.enabled() || client == null)
+		{
+			return;
+		}
+
+		Widget NPCDialog = client.getWidget(WidgetInfo.DIALOG_NPC_TEXT);
+		if (NPCDialog != null)
+		{
+			String NPCText = NPCDialog.getText().replaceAll("<[^>]*>", " "); //remove color and linebreaks
+			Matcher mAssign = NPC_ASSIGN_MESSAGE.matcher(NPCText); //number, name
+			Matcher mCurrent = NPC_CURRENT_MESSAGE.matcher(NPCText); //name, number
+			boolean found1 = mAssign.find();
+			boolean found2 = mCurrent.find();
+			if (!found1 && !found2)
+			{
+				return;
+			}
+			String taskName = found1 ? mAssign.group(2) : mCurrent.group(1);
+			int amount = Integer.parseInt(found1 ? mAssign.group(1) : mCurrent.group(2));
+
+			setTask(taskName, amount);
+		}
+
+		Widget rewardsBarWidget = client.getWidget(WidgetInfo.SLAYER_REWARDS_TOPBAR);
+		if (rewardsBarWidget != null)
+		{
+			for (Widget w : rewardsBarWidget.getDynamicChildren())
+			{
+				Matcher mPoints = REWARD_POINTS.matcher(w.getText());
+				if (mPoints.find())
+				{
+					points = Integer.parseInt(mPoints.group(1));
+					break;
+				}
+			}
+		}
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		if (!config.enabled() || event.getType() != ChatMessageType.SERVER)
+		{
+			return;
+		}
+
+		String chatMsg = event.getMessage().replaceAll("<[^>]*>", ""); //remove color and linebreaks
+		if (chatMsg.endsWith("; return to a Slayer master."))
+		{
+			Matcher mComplete = CHAT_COMPLETE_MESSAGE.matcher(chatMsg);
+
+			List<String> matches = new ArrayList<>();
+			while (mComplete.find())
+			{
+				matches.add(mComplete.group(0));
+			}
+
+			switch (matches.size())
+			{
+				case 0:
+					streak = 1;
+					break;
+				case 1:
+					streak = Integer.parseInt(matches.get(0));
+					break;
+				case 3:
+					streak = Integer.parseInt(matches.get(0));
+					points = Integer.parseInt(matches.get(2).replaceAll(",", ""));
+					break;
+				default:
+					logger.warn("Unreachable default case for message ending in '; return to Slayer master'");
+			}
+			setTask("", 0);
+			return;
+		}
+
+		if (chatMsg.equals(CHAT_GEM_COMPLETE_MESSAGE) || chatMsg.equals(CHAT_CANCEL_MESSAGE))
+		{
+			setTask("", 0);
+			return;
+		}
+
+		Matcher mProgress = CHAT_GEM_PROGRESS_MESSAGE.matcher(chatMsg);
+		if (!mProgress.find())
+		{
+			return;
+		}
+		String taskName = mProgress.group(1);
+		int amount = Integer.parseInt(mProgress.group(2));
+
+		setTask(taskName, amount);
+	}
+
+	@Subscribe
+	public void onExperienceChanged(ExperienceChanged event)
+	{
+		if (!config.enabled() || event.getSkill() != SLAYER)
+		{
+			return;
+		}
+
+		if (cachedXp == 0)
+		{
+			// this is the initial xp sent on login
+			cachedXp = client.getSkillExperience(SLAYER);
+			return;
+		}
+
+		killedOne();
+	}
+
+	private void killedOne()
+	{
+		amount--;
+		save();
+		if (!config.showInfobox())
+		{
+			return;
+		}
+		counter.setText(String.valueOf(amount));
+	}
+
+	private void setTask(String name, int amt)
+	{
+		taskName = name.toLowerCase();
+		amount = amt;
+		save();
+
+		infoBoxManager.removeIf(t -> t instanceof TaskCounter);
+
+		if (taskName.isEmpty() || !config.showInfobox())
+		{
+			return;
+		}
+
+		Task task = Task.getTask(taskName);
+		if (task == null)
+		{
+			logger.warn("No slayer task for {} in the Task database", taskName);
+		}
+		BufferedImage taskImg = task != null ? task.getImage() : ItemManager.getImage(ItemID.ENCHANTED_GEM);
+		counter = new TaskCounter(taskImg, amount);
+		counter.setTooltip(capsString(taskName));
+
+		infoBoxManager.addInfoBox(counter);
+	}
+
+	//Getters
+	public SlayerConfig getConfig()
+	{
+		return config;
+	}
+
+	@Override
+	public SlayerOverlay getOverlay()
+	{
+		return overlay;
+	}
+
+	public String getTaskName()
+	{
+		return taskName;
+	}
+
+	void setTaskName(String taskName)
+	{
+		this.taskName = taskName;
+	}
+
+	public int getAmount()
+	{
+		return amount;
+	}
+
+	void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+
+	public int getStreak()
+	{
+		return streak;
+	}
+
+	void setStreak(int streak)
+	{
+		this.streak = streak;
+	}
+
+	public int getPoints()
+	{
+		return points;
+	}
+
+	void setPoints(int points)
+	{
+		this.points = points;
+	}
+
+	//Utils
+	private String capsString(String str)
+	{
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
+	}
+}
---
+++ runelite-client/src/test/java/net/runelite/client/plugins/slayer/SlayerTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.slayer;
+
+import static net.runelite.api.ChatMessageType.SERVER;
+import net.runelite.client.RuneLite;
+import net.runelite.client.events.ChatMessage;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SlayerTest
+{
+	private static final String TASK_ONE = "You've completed one task; return to a Slayer master.";
+	private static final String TASK_COMPLETE_NO_POINTS = "<col=ef1020>You've completed 3 tasks; return to a Slayer master.</col>";
+	private static final String TASK_POINTS = "You've completed 9 tasks and received 0 points, giving you a total of 18,000; return to a Slayer master.";
+
+	private static final String TASK_COMPLETE = "You need something new to hunt.";
+	private static final String TASK_CANCELED = "Your task has been cancelled.";
+
+	@Mock(answer = RETURNS_DEEP_STUBS)
+	private RuneLite runeLite;
+
+	@Mock
+	private SlayerConfig slayerConfig;
+
+	private Slayer slayerPlugin;
+
+	@Before
+	public void before()
+	{
+		RuneLite.setRunelite(runeLite);
+		when(runeLite.getConfigManager().getConfig(Matchers.any(Class.class))).thenReturn(slayerConfig);
+		when(slayerConfig.enabled()).thenReturn(true);
+
+		slayerPlugin = new Slayer();
+	}
+
+	@Test
+	public void testOneTask()
+	{
+		ChatMessage chatMessageEvent = new ChatMessage(SERVER, "Perterter", TASK_ONE, null);
+		slayerPlugin.onChatMessage(chatMessageEvent);
+
+		assertEquals(1, slayerPlugin.getStreak());
+		assertEquals("", slayerPlugin.getTaskName());
+		assertEquals(0, slayerPlugin.getAmount());
+	}
+
+	@Test
+	public void testNoPoints()
+	{
+		ChatMessage chatMessageEvent = new ChatMessage(SERVER, "Perterter", TASK_COMPLETE_NO_POINTS, null);
+		slayerPlugin.onChatMessage(chatMessageEvent);
+
+		assertEquals(3, slayerPlugin.getStreak());
+		assertEquals("", slayerPlugin.getTaskName());
+		assertEquals(0, slayerPlugin.getAmount());
+	}
+
+	@Test
+	public void testPoints()
+	{
+		ChatMessage chatMessageEvent = new ChatMessage(SERVER, "Perterter", TASK_POINTS, null);
+		slayerPlugin.onChatMessage(chatMessageEvent);
+
+		assertEquals(9, slayerPlugin.getStreak());
+		assertEquals("", slayerPlugin.getTaskName());
+		assertEquals(0, slayerPlugin.getAmount());
+		assertEquals(18_000, slayerPlugin.getPoints());
+	}
+
+	@Test
+	public void testComplete()
+	{
+		slayerPlugin.setTaskName("cows");
+		slayerPlugin.setAmount(42);
+
+		ChatMessage chatMessageEvent = new ChatMessage(SERVER, "Perterter", TASK_COMPLETE, null);
+		slayerPlugin.onChatMessage(chatMessageEvent);
+
+		assertEquals("", slayerPlugin.getTaskName());
+		assertEquals(0, slayerPlugin.getAmount());
+	}
+
+	@Test
+	public void testCancelled()
+	{
+		slayerPlugin.setTaskName("cows");
+		slayerPlugin.setAmount(42);
+
+		ChatMessage chatMessageEvent = new ChatMessage(SERVER, "Perterter", TASK_CANCELED, null);
+		slayerPlugin.onChatMessage(chatMessageEvent);
+
+		assertEquals("", slayerPlugin.getTaskName());
+		assertEquals(0, slayerPlugin.getAmount());
+	}
+}
