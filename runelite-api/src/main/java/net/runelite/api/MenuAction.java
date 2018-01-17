a6be288254f506862939965a28d5096158e47d3d
Tyler Hardy <tylerthardy@gmail.com>
2018-01-17 01:16:19 -0600

    Generalize and add MenuActions

--- runelite-api/src/main/java/net/runelite/api/MenuAction.java
+++ runelite-api/src/main/java/net/runelite/api/MenuAction.java
@@ -27,114 +27,216 @@ package net.runelite.api;
 public enum MenuAction
 {
 	/**
+	 * Menu action for using an item in your inventory on a tile object (GameObject or GroundObject).
+	 */
+	ITEM_USE_ON_GAME_OBJECT(1),
+	/**
+	 * Menu action for casting a spell on a tile object (GameObject or GroundObject).
+	 */
+	SPELL_CAST_ON_GAME_OBJECT(2),
+	/**
+	 * First menu action for a game object.
+	 */
+	GAME_OBJECT_FIRST_OPTION(3),
+	/**
+	 * Second menu action for a game object.
+	 */
+	GAME_OBJECT_SECOND_OPTION(4),
+	/**
+	 * Third menu action for a game object.
+	 */
+	GAME_OBJECT_THIRD_OPTION(5),
+	/**
+	 * Fourth menu action for a game object.
+	 */
+	GAME_OBJECT_FOURTH_OPTION(6),
+	/**
+	 * Fifth menu action for a game object.
+	 */
+	GAME_OBJECT_FIFTH_OPTION(1001),
+
+	/**
+	 * Menu action for using an item in your inventory on an NPC.
+	 */
+	ITEM_USE_ON_NPC(7),
+	/**
+	 * Menu action for casting a spell on an NPC.
+	 */
+	SPELL_CAST_ON_NPC(8),
+	/**
+	 * First menu action for an NPC.
+	 */
+	NPC_FIRST_OPTION(9),
+	/**
+	 * Second menu action for an NPC.
+	 */
+	NPC_SECOND_OPTION(10),
+	/**
+	 * Third menu action for an NPC.
+	 */
+	NPC_THIRD_OPTION(11),
+	/**
+	 * Fourth menu action for an NPC.
+	 */
+	NPC_FOURTH_OPTION(12),
+	/**
+	 * Fifth menu action for an NPC.
+	 */
+	NPC_FIFTH_OPTION(13),
+
+	/**
+	 * Menu action for using an item on a player.
+	 */
+	ITEM_USE_ON_PLAYER(14),
+	/**
+	 * Menu action for casting a spell on a player.
+	 */
+	SPELL_CAST_ON_PLAYER(15),
+
+	/**
+	 * Menu action for using an item on an item on the ground.
+	 */
+	ITEM_USE_ON_GROUND_ITEM(16),
+	/**
+	 * Menu action for casting a spell on an item on the ground.
+	 */
+	SPELL_CAST_ON_GROUND_ITEM(17),
+	/**
+	 * First menu action for an item on the ground.
+	 */
+	GROUND_ITEM_FIRST_OPTION(18),
+	/**
+	 * Second menu action for an item on the ground.
+	 */
+	GROUND_ITEM_SECOND_OPTION(19),
+	/**
+	 * Third menu action for an item on the ground.
+	 */
+	GROUND_ITEM_THIRD_OPTION(20),
+	/**
+	 * Fourth menu action for an item on the ground.
+	 */
+	GROUND_ITEM_FOURTH_OPTION(21),
+	/**
+	 * Fifth menu action for an item on the ground.
+	 */
+	GROUND_ITEM_FIFTH_OPTION(22),
+
+	/**
+	 * Menu action for walking.
+	 */
+	WALK(23),
+
+	/**
+	 * Interaction with widget (type 1).
+	 */
+	WIDGET_TYPE_1(24),
+	/**
+	 * Interaction with widget (type 2).
+	 */
+	WIDGET_TYPE_2(25),
+	/**
+	 * Interaction with widget (type 3).
+	 */
+	WIDGET_TYPE_3(26),
+	/**
+	 * Interaction with widget (type 4).
+	 */
+	WIDGET_TYPE_4(28),
+	/**
+	 * Interaction with widget (type 5).
+	 */
+	WIDGET_TYPE_5(29),
+	/**
+	 * Interaction with widget (type 6).
+	 */
+	WIDGET_TYPE_6(30),
+	/**
+	 * Menu action when using an item on another item inside a widget (inventory).
+	 */
+	ITEM_USE_ON_WIDGET_ITEM(31),
+	/**
+	 * Menu action when using an item on a widget.
+	 */
+	ITEM_USE_ON_WIDGET(32),
+
+	/**
+	 * First menu action for an item.
+	 */
+	ITEM_FIRST_OPTION(33),
+	/**
+	 * Second menu action for an item.
+	 */
+	ITEM_SECOND_OPTION(34),
+	/**
+	 * Third menu action for an item.
+	 */
+	ITEM_THIRD_OPTION(35),
+	/**
+	 * Fourth menu action for an item.
+	 */
+	ITEM_FOURTH_OPTION(36),
+	/**
+	 * Fifth menu action for an item.
+	 */
+	ITEM_FIFTH_OPTION(37),
+	/**
+	 * Menu action to drop an item (identical to ITEM_FIFTH_OPTION).
+	 */
+	ITEM_DROP(37),
+	/**
+	 * Menu action to use an item.
+	 */
+	ITEM_USE(38),
+
+	/**
+	 * First menu action for a widget.
+	 */
+	WIDGET_FIRST_OPTION(39),
+	/**
+	 * Second menu action for a widget.
+	 */
+	WIDGET_SECOND_OPTION(40),
+	/**
+	 * Third menu action for a widget.
+	 */
+	WIDGET_THIRD_OPTION(41),
+	/**
+	 * Fourth menu action for a widget.
+	 */
+	WIDGET_FOURTH_OPTION(42),
+	/**
+	 * Fifth menu action for a widget.
+	 */
+	WIDGET_FIFTH_OPTION(43),
+
+	/**
+	 * Default menu action for a widget.
+	 */
+	WIDGET_DEFAULT(57),
+
+	/**
 	 * Menu action triggered by examining an object.
 	 */
 	EXAMINE_OBJECT(1002),
-	
 	/**
 	 * Menu action triggered by examining an NPC.
 	 */
 	EXAMINE_NPC(1003),
-	
 	/**
 	 * Menu action triggered by examining item on ground.
 	 */
 	EXAMINE_ITEM_GROUND(1004),
-	
 	/**
 	 * Menu action triggered by examining item in inventory.
 	 */
 	EXAMINE_ITEM(1005),
-	
 	/**
 	 * Menu action triggered by either examining item in bank, examining
 	 * item in inventory while having bank open, or examining equipped item.
 	 */
 	EXAMINE_ITEM_BANK_EQ(1007),
-	
-	/**
-	 * Menu action triggered by using an item in inventory on an item thats
-	 * on the ground.
-	 */
-	USE_ON_ITEM_GROUND(1),
-	
-	/**
-	 * Menu action triggered when checking a trap while hunting.
-	 */
-	CHECK(3),
-	
-	/**
-	 * Menu action triggered when checking a trap while hunting.
-	 */
-	DISMANTLE(3),
-	
-	/**
-	 * Menu action triggered by using an item in inventory on an NPC.
-	 */
-	USE_ON_NPC(7),
-	
-	/**
-	 * Menu action triggered when clicking "Dismiss" on a random event NPC.
-	 */
-	DISSMISS(13),
-	
-	/**
-	 * Menu action triggered by using an item in inventory on another
-	 * player.
-	 */
-	USE_ON_PLAYER(14),
-	
-	/**
-	 * Menu action triggered when laying a trap that is already on the
-	 * ground.
-	 */
-	LAY_GROUND(21),
-	
-	/**
-	 * Menu action triggered when clicking "CLICK HERE TO PLAY" just after
-	 * login.
-	 */
-	PLAY_RUNESCAPE(24),
-	
-	/**
-	 * Menu action triggered by using an item in your inventory on another
-	 * item in your inventory.
-	 */
-	USE_ON_ITEM_INV(31),
-	
-	/**
-	 * Menu action triggered when laying a trap that is in your inventory.
-	 */
-	LAY_INV(33),
-	
-	/**
-	 * Menu action triggered when cleaning a herb.
-	 */
-	CLEAN(33),
-	
-	/**
-	 * Menu action triggered when clicking on a wieldable item in your
-	 * inventory.
-	 */
-	WIELD(34),
-	
-	/**
-	 * Menu action triggered when clicking on a wieldable item in your
-	 * inventory.
-	 */
-	WEAR(34),
-	
-	/**
-	 * Menu action triggered when dropping an item from your inventory.
-	 */
-	DROP(37),
-	
-	/**
-	 * Menu action triggered clicking on an item in your inventory to "Use"
-	 * it, and select it to use it on something else.
-	 */
-	USE(38),
-	
+
 	/**
 	 * Menu action injected by runelite for its menu items.
 	 */
