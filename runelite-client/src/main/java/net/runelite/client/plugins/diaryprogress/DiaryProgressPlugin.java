3becf865bf69f222cd4fe7d05827eae37158b287
oplosthee <omniihd@gmail.com>
2018-01-29 16:55:25 +0100

    Fix Karamja diary displaying incorrect progress

--- runelite-client/src/main/java/net/runelite/client/plugins/diaryprogress/DiaryProgressPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/diaryprogress/DiaryProgressPlugin.java
@@ -75,7 +75,8 @@ public class DiaryProgressPlugin extends Plugin
 			{
 				for (Varbits varbits : entry.getVarbits())
 				{
-					if (client.getSetting(varbits) == 1)
+					int value = client.getSetting(varbits);
+					if ((entry != DiaryEntry.KARAMJA && value == 1) || value == 2)
 					{
 						progress.append(STAGE_FINISHED_STRING);
 					}
