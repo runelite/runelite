43c2dc35ecba6d9ec0efc58057d7ec915964b094
SomeoneWithAnInternetConnection <34518321+SomeoneWithAnInternetConnection@users.noreply.github.com>
2018-01-30 20:31:06 -0500

    Add method to get the clickable area of a TileObject
     getClickbox returns an AWT Area object which corresponds to the
     clickable area of a GameObject, GroundObject, DecorativeObject or
     WallObject.
    
     getClickbox for ItemLayers isn't implemented, since I haven't looked
     into how clickabilty works for items on the ground in the client.
    
     The actual calculation is done by Perspective::getClickbox. I really
     hope there's a nicer place to put it.

--- runelite-mixins/src/main/java/net/runelite/mixins/RSWallObjectMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSWallObjectMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, Adam <Adam@sigterm.info>
+ * Copyright (c) 2018, SomeoneWithAnInternetConnection
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,9 +22,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package net.runelite.mixins;
 
-import java.awt.Polygon;
 import java.awt.geom.Area;
 import net.runelite.api.Client;
 import net.runelite.api.Model;
@@ -32,54 +32,73 @@ import net.runelite.api.Perspective;
 import net.runelite.api.Renderable;
 import net.runelite.api.mixins.Inject;
 import net.runelite.api.mixins.Mixin;
-import net.runelite.rs.api.RSDecorativeObject;
+import net.runelite.rs.api.RSWallObject;
 
-@Mixin(RSDecorativeObject.class)
-public abstract class RSDecorativeObjectMixin implements RSDecorativeObject
+@Mixin(RSWallObject.class)
+public abstract class RSWallObjectMixin implements RSWallObject
 {
 	@Inject
-	private Model getModel()
+	private Model getModelA()
 	{
-		Renderable renderable = getRenderable();
+		Renderable renderable = getRenderable1();
 		if (renderable == null)
 		{
 			return null;
 		}
 
-		Model model;
-
 		if (renderable instanceof Model)
 		{
-			model = (Model) renderable;
+			return (Model) renderable;
 		}
 		else
 		{
-			model = renderable.getModel();
+			return renderable.getModel();
+		}
+	}
+
+	@Inject
+	private Model getModelB()
+	{
+		Renderable renderable = getRenderable2();
+		if (renderable == null)
+		{
+			return null;
 		}
 
-		return model;
+		if (renderable instanceof Model)
+		{
+			return (Model) renderable;
+		}
+		else
+		{
+			return renderable.getModel();
+		}
 	}
 
 	@Inject
 	@Override
 	public Area getClickbox(Client client)
 	{
-		return Perspective.getClickbox(client, getModel(), getOrientation(), getX(), getY());
-	}
+		Area clickbox = new Area();
 
-	@Inject
-	@Override
-	public Polygon getConvexHull()
-	{
+		Area clickboxA = Perspective.getClickbox(client, getModelA(), getOrientationA(), getX(), getY());
+		Area clickboxB = Perspective.getClickbox(client, getModelB(), getOrientationB(), getX(), getY());
 
-
-		Model model = getModel();
-
-		if (model == null)
+		if (clickboxA == null && clickboxB == null)
 		{
 			return null;
 		}
 
-		return getConvexHull(model, getOrientation());
+		if (clickboxA != null)
+		{
+			clickbox.add(clickboxA);
+		}
+
+		if (clickboxB != null)
+		{
+			clickbox.add(clickboxB);
+		}
+
+		return clickbox;
 	}
 }
