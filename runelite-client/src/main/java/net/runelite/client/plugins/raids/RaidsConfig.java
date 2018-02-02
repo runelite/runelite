14fe5d61d1d8ce78293ef0491c0580ab92753b2f
Tomas Slusny <slusnucky@gmail.com>
2018-02-02 11:40:57 +0100

    Fix indent
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsConfig.java
@@ -5,16 +5,16 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup(
-		keyName = "raids",
-		name = "Raids",
-		description = "Configuration for the raids plugin"
+	keyName = "raids",
+	name = "Raids",
+	description = "Configuration for the raids plugin"
 )
 public interface RaidsConfig extends Config
 {
 	@ConfigItem(
-			keyName = "enabled",
-			name = "Enable",
-			description = "Configures whether or not the raids plugin is enabled"
+		keyName = "enabled",
+		name = "Enable",
+		description = "Configures whether or not the raids plugin is enabled"
 	)
 	default boolean enabled()
 	{
@@ -22,9 +22,9 @@ public interface RaidsConfig extends Config
 	}
 
 	@ConfigItem(
-			keyName = "pointsMessage",
-			name = "Display points in chatbox after raid",
-			description = "Display a message with total points, individual points and percentage at the end of a raid"
+		keyName = "pointsMessage",
+		name = "Display points in chatbox after raid",
+		description = "Display a message with total points, individual points and percentage at the end of a raid"
 	)
 	default boolean pointsMessage()
 	{
