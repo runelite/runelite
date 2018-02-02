dd83913708b87d9ab0e36f4f33189573006f1e23
SomeoneWithAnInternetConnection <34518321+SomeoneWithAnInternetConnection@users.noreply.github.com>
2018-02-01 19:15:27 -0500

    Simplify Triangle and Vertex with lombok

--- runelite-api/src/main/java/net/runelite/api/model/Vertex.java
+++ runelite-api/src/main/java/net/runelite/api/model/Vertex.java
@@ -24,8 +24,10 @@
  */
 package net.runelite.api.model;
 
+import lombok.Value;
 import net.runelite.api.Perspective;
 
+@Value
 public class Vertex
 {
 	private final int x;
@@ -37,64 +39,6 @@ public class Vertex
 		this.x = x;
 		this.y = y;
 		this.z = z;
-	}
-
-	@Override
-	public String toString()
-	{
-		return "Vertex{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
-	}
-
-	@Override
-	public int hashCode()
-	{
-		int hash = 7;
-		hash = 67 * hash + this.x;
-		hash = 67 * hash + this.y;
-		hash = 67 * hash + this.z;
-		return hash;
-	}
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (obj == null)
-		{
-			return false;
-		}
-		if (getClass() != obj.getClass())
-		{
-			return false;
-		}
-		final Vertex other = (Vertex) obj;
-		if (this.x != other.x)
-		{
-			return false;
-		}
-		if (this.y != other.y)
-		{
-			return false;
-		}
-		if (this.z != other.z)
-		{
-			return false;
-		}
-		return true;
-	}
-
-	public int getX()
-	{
-		return x;
-	}
-
-	public int getY()
-	{
-		return y;
-	}
-
-	public int getZ()
-	{
-		return z;
 	}
 
 	/**
