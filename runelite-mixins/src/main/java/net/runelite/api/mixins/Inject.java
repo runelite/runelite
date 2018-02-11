d8e4fe13c513387395952572b452f4ff34ad8f53
Max Weber <mii7303@gmail.com>
2018-02-11 14:06:37 -0700

    injector: Allow injection of <init> and <clinit>

--- runelite-mixins/src/main/java/net/runelite/api/mixins/Inject.java
+++ runelite-mixins/src/main/java/net/runelite/api/mixins/Inject.java
@@ -30,7 +30,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR})
 public @interface Inject
 {
 
