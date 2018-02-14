48f9a4d66c3ab2ff9af41733fe580cb61fb5b954
Kamiel <k.vanflierenburg@outlook.com>
2018-02-14 16:46:46 +0100

    Remove unused import

--- runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
@@ -48,7 +48,6 @@ import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
