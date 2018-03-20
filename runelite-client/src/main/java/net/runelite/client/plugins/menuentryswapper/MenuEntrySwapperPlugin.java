b5a56b31ae0e8478da788c29d02fbe96c75afbf3
XrioBtw <33559295+XrioBtw@users.noreply.github.com>
2018-03-20 23:29:10 +0100

    Add net and harpoon swap

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -103,6 +103,10 @@ public class MenuEntrySwapperPlugin extends Plugin
 		{
 			swap("harpoon", option, target, true);
 		}
+		else if (config.swapHarpoon() && option.equals("net"))
+		{
+			swap("harpoon", option, target, true);
+		}
 		else if (config.swapHarpoon() && option.equals("big net"))
 		{
 			swap("harpoon", option, target, true);
