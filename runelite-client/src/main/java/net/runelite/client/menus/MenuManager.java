8ce7f2a5fa2b2ddef6597ba378e200eb97beb64c
Pim <pim_bor4@hotmail.com>
2018-03-24 14:42:22 +0100

    Revert "Merge branch 'poh-burner-timer' of https://github.com/Pim112/runelite into poh-burner-timer"
    This reverts commit 3cdf332fee5df6a3854c0fa7ee9789c6c51a9e1a, reversing
    changes made to 689bebca16da3ac67afd573e1a61be28ff0d3e46.

--- runelite-client/src/main/java/net/runelite/client/menus/MenuManager.java
+++ runelite-client/src/main/java/net/runelite/client/menus/MenuManager.java
@@ -93,7 +93,7 @@ public class MenuManager
 
 	public void removeNpcMenuOption(String option)
 	{
-		npcMenuOptions.remove(option);
+		npcMenuOptions.remove(npcMenuOptions);
 
 		// remove this option from all npc compositions
 		Client client = clientProvider.get();
