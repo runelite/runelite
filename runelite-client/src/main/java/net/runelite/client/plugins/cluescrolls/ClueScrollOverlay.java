14aa57e126393bdd6a38ce3c7cbe2a15bb9b8f0c
Lotto <prozet@live.se>
2018-03-26 17:25:58 +0200

    runelite-client: revamp clue scroll plugin

--- runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/ClueScrollOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/ClueScrollOverlay.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016-2017, Seth <Sethtroll3@gmail.com>
+ * Copyright (c) 2018, Lotto <https://github.com/devLotto>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,72 +23,43 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
 package net.runelite.client.plugins.cluescrolls;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.time.Duration;
-import java.time.Instant;
 import javax.inject.Inject;
-import net.runelite.api.Client;
-import net.runelite.api.ItemComposition;
+import net.runelite.client.plugins.cluescrolls.clues.ClueScroll;
 import net.runelite.client.ui.overlay.Overlay;
-import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.PanelComponent;
 
 public class ClueScrollOverlay extends Overlay
 {
-	private static final Duration WAIT_DURATION = Duration.ofMinutes(4);
+	public static final Color TITLED_CONTENT_COLOR = new Color(190, 190, 190);
 
-	private final Client client;
+	private final ClueScrollPlugin plugin;
 	private final PanelComponent panelComponent = new PanelComponent();
 
-	ClueScroll clue;
-	Instant clueTimeout;
-
 	@Inject
-	public ClueScrollOverlay(Client client)
+	public ClueScrollOverlay(ClueScrollPlugin plugin)
 	{
-		setPosition(OverlayPosition.TOP_LEFT);
-		this.client = client;
+		this.plugin = plugin;
 	}
 
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		ClueScroll clue = plugin.getClue();
+
 		if (clue == null)
 		{
 			return null;
 		}
 
-		if (clueTimeout == null || Instant.now().compareTo(clueTimeout.plus(WAIT_DURATION)) >= 0)
-		{
-			return null;
-		}
-
 		panelComponent.getLines().clear();
-		panelComponent.setTitle("Required items");
 
-		if (clue.getIds().length == 0)
-		{
-			panelComponent.getLines().add(new PanelComponent.Line("None", ""));
-		}
-		else
-		{
-			for (int id : clue.getIds())
-			{
-				ItemComposition clueItem = client.getItemDefinition(id);
-
-				if (clueItem == null)
-				{
-					continue;
-				}
-
-				panelComponent.getLines().add(new PanelComponent.Line(clueItem.getName(), ""));
-			}
-		}
+		clue.makeOverlayHint(panelComponent, plugin);
 
 		return panelComponent.render(graphics);
 	}
