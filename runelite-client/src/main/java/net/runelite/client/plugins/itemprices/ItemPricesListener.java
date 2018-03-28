a3457fab22b80cf485609851e289ab5b2408b445
RobinDson <robindavidsson@outlook.com>
2018-03-28 19:50:47 +0200

    Updated ItemPrices with a liastener for it to be enabled when ALT is pushed. This was requested on issue #1094. Resolves #1094

--- runelite-client/src/main/java/net/runelite/client/plugins/itemprices/ItemPricesListener.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemprices/ItemPricesListener.java
@@ -8,35 +8,35 @@ import java.awt.event.KeyEvent;
 
 public class ItemPricesListener implements KeyListener
 {
-    @Inject
-    KeyManager keyManager;
+	@Inject
+	KeyManager keyManager;
 
-    @Inject
-    private ItemPricesPlugin plugin;
+	@Inject
+	private ItemPricesPlugin plugin;
 
-    @Override
-    public void keyTyped(KeyEvent e)
-    {
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
 
-    }
+	}
 
-    //enables overlay on HOTKEY press (ALT)
-    @Override
-    public void keyPressed(KeyEvent e)
-    {
-        if (e.getKeyCode() == KeyEvent.VK_ALT)
-        {
-            plugin.showPrices = true;
-        }
-    }
+	//enables overlay on HOTKEY press (ALT)
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_ALT)
+		{
+			plugin.showPrices = true;
+		}
+	}
 
-    //disables overlay on HOTKEY release (ALT)
-    @Override
-    public void keyReleased(KeyEvent e)
-    {
-        if (e.getKeyCode() == KeyEvent.VK_ALT)
-        {
-            plugin.showPrices = false;
-        }
-    }
+	//disables overlay on HOTKEY release (ALT)
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_ALT)
+		{
+			plugin.showPrices = false;
+		}
+	}
 }
