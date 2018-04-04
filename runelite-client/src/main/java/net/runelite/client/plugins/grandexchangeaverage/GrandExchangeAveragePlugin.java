6570471138dd79d2210feb1bc9a61e2c43c2f47e
Denis I. Blazevic <denicevap@gmail.com>
2018-04-04 19:23:43 +0200

    Plugin show up in plugin list

--- runelite-client/src/main/java/net/runelite/client/plugins/grandexchangeaverage/GrandExchangeAveragePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grandexchangeaverage/GrandExchangeAveragePlugin.java
@@ -1,17 +1,22 @@
 package net.runelite.client.plugins.grandexchangeaverage;
 
 import com.google.common.eventbus.Subscribe;
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.grandexchangeaverage.GrandExchangeOfferText;
 import net.runelite.client.plugins.grandexchangeaverage.GrandExchangeOfferTitle;
 import javax.inject.Inject;
 
+@Slf4j
 @PluginDescriptor(name = "Grand Exchange Average")
-public class GrandExchangeAveragePlugin {
+public class GrandExchangeAveragePlugin extends Plugin {
     @Inject
     private Client client;
 
@@ -21,6 +26,11 @@ public class GrandExchangeAveragePlugin {
     @Inject
     private GrandExchangeOfferText offerText;
 
+    @Provides
+    GrandExchangeAverageConfig getConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(GrandExchangeAverageConfig.class);
+    }
 
     @Subscribe
     public void onGameTick(GameTick event)
@@ -32,9 +42,10 @@ public class GrandExchangeAveragePlugin {
             return;
         }
 
-        offerTitle.save();
+        log.debug("Found Offer Title!");
+        //offerTitle.save();
 
-        int averagePrice = getGEAveragePrice(offerTitle);
+        //int averagePrice = getGEAveragePrice(offerTitle);
 
     }
 }
