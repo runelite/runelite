58950ed2b005e1e01b6c436dbb2e4d96aef30743
Adam <Adam@anope.org>
2018-04-05 19:22:11 -0400

    runescape-client: item comp int1 -> team

--- runescape-client/src/main/java/class47.java
+++ runescape-client/src/main/java/class47.java
@@ -661,7 +661,7 @@ public class class47 {
             var1.groundActions = null;
             var1.inventoryActions = null;
             var1.shiftClickIndex = -1;
-            var1.int1 = 0;
+            var1.team = 0;
             if(var1.field3671 != null) {
                boolean var3 = false;
 
