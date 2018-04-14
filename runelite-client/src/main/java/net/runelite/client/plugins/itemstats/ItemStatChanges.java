c3543245539b25b8b1eb3703544ed2ca9da83962
Adam <Adam@anope.org>
2018-04-13 23:02:06 -0400

    item stats: fix sara brew

--- runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
@@ -36,6 +36,7 @@ import static net.runelite.client.plugins.itemstats.Builders.*;
 import static net.runelite.client.plugins.itemstats.stats.Stats.*;
 import net.runelite.client.plugins.itemstats.food.Anglerfish;
 import net.runelite.client.plugins.itemstats.potions.PrayerPotion;
+import net.runelite.client.plugins.itemstats.potions.SaradominBrew;
 
 @Singleton
 @Slf4j
@@ -124,7 +125,7 @@ public class ItemStatChanges
 		add(boost(MAGIC, 3), MAGIC_ESSENCE1, MAGIC_ESSENCE2, MAGIC_ESSENCE3, MAGIC_ESSENCE4);
 		add(combo(3, boost(ATTACK, perc(.15, 5)), boost(STRENGTH, perc(.15, 5)), boost(DEFENCE, perc(.15, 5))), SUPER_COMBAT_POTION1, SUPER_COMBAT_POTION2, SUPER_COMBAT_POTION3, SUPER_COMBAT_POTION4);
 		add(combo(3, boost(ATTACK, perc(.20, 2)), boost(STRENGTH, perc(.12, 2)), heal(PRAYER, perc(.10, 0)), heal(DEFENCE, perc(.10, -2)), heal(HITPOINTS, perc(.12, 0))), ZAMORAK_BREW1, ZAMORAK_BREW2, ZAMORAK_BREW3, ZAMORAK_BREW4);
-		add(combo(2, boost(HITPOINTS, perc(.15, 2)), boost(DEFENCE, perc(.20, 2)), heal(ATTACK, perc(.10, -2)), heal(RANGED, perc(.10, -2)), heal(MAGIC, perc(.10, -2))), SARADOMIN_BREW1, SARADOMIN_BREW2, SARADOMIN_BREW3, SARADOMIN_BREW4);
+		add(new SaradominBrew(0.15, 0.2, 0.1, 2), SARADOMIN_BREW1, SARADOMIN_BREW2, SARADOMIN_BREW3, SARADOMIN_BREW4);
 		add(boost(RANGED, perc(.15, 5)), SUPER_RANGING_1, SUPER_RANGING_2, SUPER_RANGING_3, SUPER_RANGING_4);
 		add(boost(MAGIC, perc(.15, 5)), SUPER_MAGIC_POTION_1, SUPER_MAGIC_POTION_2, SUPER_MAGIC_POTION_3, SUPER_MAGIC_POTION_4);
 
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemstats/potions/SaradominBrew.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2016-2018, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.itemstats.potions;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import net.runelite.api.Client;
+import static net.runelite.client.plugins.itemstats.Builders.perc;
+import net.runelite.client.plugins.itemstats.Effect;
+import net.runelite.client.plugins.itemstats.SimpleStatBoost;
+import net.runelite.client.plugins.itemstats.BoostedStatBoost;
+import net.runelite.client.plugins.itemstats.stats.Stat;
+import net.runelite.client.plugins.itemstats.StatChange;
+import static net.runelite.client.plugins.itemstats.stats.Stats.*;
+import net.runelite.client.plugins.itemstats.StatsChanges;
+
+@RequiredArgsConstructor
+public class SaradominBrew implements Effect
+{
+	private static final Stat[] saradominBrewStats = new Stat[]
+	{
+		ATTACK, STRENGTH, RANGED, MAGIC
+	};
+
+	private final double percH; //percentage heal
+	private final double percD; //percentage defence boost
+	private final double percSD; //percentage stat drain
+	private final int delta;
+
+	@Override
+	public StatsChanges calculate(Client client)
+	{
+		StatsChanges changes = new StatsChanges(0);
+		SimpleStatBoost hitpoints = new SimpleStatBoost(HITPOINTS, true, perc(percH, delta));
+		SimpleStatBoost defence = new SimpleStatBoost(DEFENCE, true, perc(percD, delta));
+		BoostedStatBoost calc = new BoostedStatBoost(null, false, perc(percSD, -delta));
+		changes.setStatChanges(Stream.concat(
+			Stream.of(hitpoints.effect(client)),
+			Stream.concat(
+			Stream.of(defence.effect(client)),
+			Stream.of(saradominBrewStats)
+				.filter(stat -> 1 < stat.getValue(client))
+				.map(stat ->
+				{
+					calc.setStat(stat);
+					return calc.effect(client);
+				})
+			)
+		).toArray(StatChange[]::new));
+		changes.setPositivity(Stream.of(changes.getStatChanges())
+			.map(sc -> sc.getPositivity())
+			.max(Comparator.comparing(Enum::ordinal)).get());
+		return changes;
+	}
+}
