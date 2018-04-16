360992a06bc76a587255f86babba153cac346fd7
JonnoUK <jonathan@taylorhq.com>
2018-04-16 10:16:44 +0100

    Correcting formatting

--- runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangeSearchPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangeSearchPanel.java
@@ -212,14 +212,15 @@ class GrandExchangeSearchPanel extends JPanel
 
 
 			// Remove searching label after search is complete if item found
-            if (ITEMS_LIST.size() > 0)
-            {
-                showSearchString(null);
-            }
-            else
-            {
-                showSearchString("No result found.");
-            }
+
+			if (ITEMS_LIST.size() > 0)
+			{
+				showSearchString(null);
+			}
+			else
+			{
+				showSearchString("No result found.");
+			}
 			ITEMS_LIST.clear();
 
 		});
