a8f65155a959e40236293768b6780520c3022997
SoyChai <soychai@tuta.io>
2018-04-16 12:58:40 +1000

    api: Add stats tab to WidgetID and WidgetInfo

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -24,6 +24,7 @@
  */
 package net.runelite.api.widgets;
 
+@SuppressWarnings("WeakerAccess")
 public class WidgetID
 {
 	public static final int LOGOUT_PANEL_ID = 182;
@@ -80,6 +81,7 @@ public class WidgetID
 	public static final int RAIDS_REWARD_GROUP_ID = 539;
 	public static final int EXPERIENCE_TRACKER_GROUP_ID = 122;
 	public static final int TITHE_FARM_GROUP_ID = 241;
+	public static final int STATS_GROUP_ID = 320;
 
 	static class WorldMap
 	{
@@ -394,5 +396,34 @@ public class WidgetID
 	{
 		static final int WIDGET = 1;
 		static final int BOTTOM_BAR = 14;
+	}
+
+	static class Stats
+	{
+		static final int ATTACK = 1;
+		static final int STRENGTH = 2;
+		static final int DEFENCE = 3;
+		static final int RANGED = 4;
+		static final int PRAYER = 5;
+		static final int MAGIC = 6;
+		static final int RUNECRAFT = 7;
+		static final int CONSTRUCTION = 8;
+		static final int HITPOINTS = 9;
+		static final int AGILITY = 10;
+		static final int HERBLORE = 11;
+		static final int THIEVING = 12;
+		static final int CRAFTING = 13;
+		static final int FLETCHING = 14;
+		static final int SLAYER = 15;
+		static final int HUNTER = 16;
+		static final int MINING = 17;
+		static final int SMITHING = 18;
+		static final int FISHING = 19;
+		static final int COOKING = 20;
+		static final int FIREMAKING = 21;
+		static final int WOODCUTTING = 22;
+		static final int FARMING = 23;
+		static final int TOTAL_LEVEL = 24;
+		static final int TOOLTIP = 28;
 	}
 }
