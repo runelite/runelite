470b553dde719bbc50391e4ebbfc369110fdb0a8
Grant Dellar <grant.dellar@live.com>
2018-04-24 23:01:32 +1200

    Remove config & fix equals check
    Remove config

--- runelite-client/src/main/java/net/runelite/client/plugins/osrswikia/OSRSWikiaPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/osrswikia/OSRSWikiaPlugin.java
@@ -1,43 +1,36 @@
 package net.runelite.client.plugins.osrswikia;
 
 import com.google.common.eventbus.Subscribe;
-import com.google.inject.Provides;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.api.events.CommandExecuted;
 import net.runelite.client.util.LinkBrowser;
 
-import javax.inject.Inject;
-
 @PluginDescriptor(
-        name = "OSRS Wikia",
-        enabledByDefault = false
+	name = "OSRS Wikia",
+	enabledByDefault = false
 )
-public class OSRSWikiaPlugin extends Plugin {
-    private final String BASEURI = "http://oldschoolrunescape.wikia.com/wiki";
-    
-    @Inject
-    private OSRSWikiaConfig config;
-    
-    @Provides
-    OSRSWikiaConfig getConfig(ConfigManager configManager) {
-        return configManager.getConfig(OSRSWikiaConfig.class);
-    }
-    
-    @Subscribe
-    public void onCommand(CommandExecuted commandExecuted) {
-        if(!commandExecuted.getCommand().equals("wiki") || !config.allowWikiCommand())
-            return;
-    
-        String browseURI = BASEURI;
-        String searchQuery = String.join("+", commandExecuted.getArguments());
-    
-        // empty query after ::wiki should just take user to osrs wikia home
-        if(searchQuery.trim().length() > 0) {
-            browseURI += "/Special:Search?query=" + searchQuery;
-        }
-    
-        LinkBrowser.browse(browseURI);
-    }
+public class OSRSWikiaPlugin extends Plugin
+{
+	private final String BASEURI = "http://oldschoolrunescape.wikia.com/wiki";
+
+	@Subscribe
+	public void onCommand(CommandExecuted commandExecuted)
+	{
+		if (!commandExecuted.getCommand().toLowerCase().equals("wiki"))
+		{
+			return;
+		}
+
+		String browseURI = BASEURI;
+		String searchQuery = String.join("+", commandExecuted.getArguments());
+
+		// empty query after ::wiki should just take user to osrs wikia home
+		if (searchQuery.trim().length() > 0)
+		{
+			browseURI += "/Special:Search?query=" + searchQuery;
+		}
+
+		LinkBrowser.browse(browseURI);
+	}
 }
