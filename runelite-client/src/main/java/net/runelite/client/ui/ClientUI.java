d438fa7e45085f299067d51a27adaec206799d55
Tomas Slusny <slusnucky@gmail.com>
2018-04-27 22:47:44 +0200

    Remove unused imports in ClientUI
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
+++ runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
@@ -38,8 +38,6 @@ import java.awt.Graphics;
 import java.awt.LayoutManager;
 import java.awt.Rectangle;
 import java.awt.TrayIcon;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.annotation.Nullable;
