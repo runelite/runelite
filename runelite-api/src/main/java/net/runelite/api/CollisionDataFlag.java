4b46be6eacaa4796ca11f518e2194e965d0bbd37
WooxSolo <twoxsolo123@gmail.com>
2018-04-28 11:31:07 -0400

    runelite-api: export collision data

--- runelite-api/src/main/java/net/runelite/api/CollisionDataFlag.java
+++ runelite-api/src/main/java/net/runelite/api/CollisionDataFlag.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, Adam <Adam@sigterm.info>
+ * Copyright (c) 2018, Woox <https://github.com/wooxsolo>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,13 +22,30 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.rs.api;
+package net.runelite.api;
 
-import net.runelite.api.CollisionData;
-import net.runelite.mapping.Import;
-
-public interface RSCollisionData extends CollisionData
+public final class CollisionDataFlag
 {
-	@Import("flags")
-	int[][] getFlags();
+	public static final int BLOCK_MOVEMENT_NORTH_WEST = 0x1;
+	public static final int BLOCK_MOVEMENT_NORTH = 0x2;
+	public static final int BLOCK_MOVEMENT_NORTH_EAST = 0x4;
+	public static final int BLOCK_MOVEMENT_EAST = 0x8;
+	public static final int BLOCK_MOVEMENT_SOUTH_EAST = 0x10;
+	public static final int BLOCK_MOVEMENT_SOUTH = 0x20;
+	public static final int BLOCK_MOVEMENT_SOUTH_WEST = 0x40;
+	public static final int BLOCK_MOVEMENT_WEST = 0x80;
+
+	public static final int BLOCK_MOVEMENT_OBJECT = 0x100;
+	public static final int BLOCK_MOVEMENT_FLOOR_DECORATION = 0x40000;
+	public static final int BLOCK_MOVEMENT_FLOOR = 0x200000; // Eg. water
+	public static final int BLOCK_MOVEMENT_FULL =
+		BLOCK_MOVEMENT_OBJECT |
+			BLOCK_MOVEMENT_FLOOR_DECORATION |
+			BLOCK_MOVEMENT_FLOOR;
+
+	public static final int BLOCK_LINE_OF_SIGHT_NORTH = BLOCK_MOVEMENT_NORTH << 9; // 0x400
+	public static final int BLOCK_LINE_OF_SIGHT_EAST = BLOCK_MOVEMENT_EAST << 9; // 0x1000
+	public static final int BLOCK_LINE_OF_SIGHT_SOUTH = BLOCK_MOVEMENT_SOUTH << 9; // 0x4000
+	public static final int BLOCK_LINE_OF_SIGHT_WEST = BLOCK_MOVEMENT_WEST << 9; // 0x10000
+	public static final int BLOCK_LINE_OF_SIGHT_FULL = 0x20000;
 }
