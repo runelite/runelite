4c2e12d7922465fba1cd7d3b415d41d5b0fc817d
Fryslan <pietjetse@outlook.com>
2018-04-29 13:43:02 +0200

    Changed add tabs instead of spaces in style

--- runelite-client/src/main/java/net/runelite/client/plugins/cat/CatPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cat/CatPlugin.java
@@ -1,4 +1,3 @@
-
 package net.runelite.client.plugins.cat;
 
 import com.google.common.eventbus.Subscribe;
@@ -15,38 +14,37 @@ import java.util.Arrays;
 import java.util.List;
 
 @PluginDescriptor(
-        name = "Cat Information"
+		name = "Cat Information"
 )
 
 @Slf4j
 public class CatPlugin extends Plugin
 {
 
-    @Inject
-    private Notifier notifier;
+	private final String CAT_ATTENTION_MESSAGE_1 = "Your kitten wants attention.";
+	private final String CAT_ATTENTION_MESSAGE_2 = "Your kitten really wants attention.";
+	private final String CAT_HUNGRY_MESSAGE_1 = "Your kitten is hungry.";
+	private final String CAT_HUNGRY_MESSAGE_2 = "Your kitten is very hungry.";
+	private final String CAT_RAN_AWAY_MESSAGE = "Your kitten got lonely and ran off.";
 
-    private final String CAT_ATTENTION_MESSAGE_1 = "Your kitten wants attention.";
-    private final String CAT_ATTENTION_MESSAGE_2 = "Your kitten really wants attention.";
-    private final String CAT_HUNGRY_MESSAGE_1 = "Your kitten is hungry.";
-    private final String CAT_HUNGRY_MESSAGE_2 = "Your kitten is very hungry.";
-    private final String CAT_RAN_AWAY_MESSAGE = "Your kitten got lonely and ran off.";
+	@Inject
+	private Notifier notifier;
 
-    private String[] messages = {CAT_ATTENTION_MESSAGE_1, CAT_ATTENTION_MESSAGE_2, CAT_HUNGRY_MESSAGE_1, CAT_HUNGRY_MESSAGE_2, CAT_RAN_AWAY_MESSAGE};
+	private String[] messages = {CAT_ATTENTION_MESSAGE_1, CAT_ATTENTION_MESSAGE_2, CAT_HUNGRY_MESSAGE_1, CAT_HUNGRY_MESSAGE_2, CAT_RAN_AWAY_MESSAGE};
 
-    @Subscribe
-    public void onChatMessage(ChatMessage event)
-    {
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
 
-        if (event.getType() == ChatMessageType.SERVER)
-        {
-            String message = Text.removeTags(event.getMessage());
-            List<String> m = Arrays.asList(messages);
+		if (event.getType() == ChatMessageType.SERVER)
+		{
+			String message = Text.removeTags(event.getMessage());
+			List<String> m = Arrays.asList(messages);
 
-            if (m.contains(message))
-            {
-                notifier.notify(message);
-            }
-        }
-    }
-
+			if (m.contains(message))
+			{
+				notifier.notify(message);
+			}
+		}
+	}
 }
