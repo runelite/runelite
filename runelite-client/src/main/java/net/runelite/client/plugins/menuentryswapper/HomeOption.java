df3f00613515ff91112a8cb5546dca19365b0f23
Samuel Beresford <seb1g13@soton.ac.uk>
2018-05-01 14:42:02 +0100

    Change HomeOption Enum to UPPERCASE format and override toString Add license header to new files (HomeOption.java)

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/HomeOption.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/HomeOption.java
@@ -1,9 +1,46 @@
+/*
+ * Copyright (c) 2018, Sam "Berry" Beresford <seb1g13@soton.ac.uk>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.menuentryswapper;
 
 public enum HomeOption
 {
-	Home,
-	Friends,
-	Build,
-	Enter
+	HOME,
+	FRIENDS,
+	BUILD,
+	ENTER;
+
+	@Override
+	public String toString()
+	{
+		switch (this)
+		{
+			case HOME: return "Home";
+			case FRIENDS: return "Friends";
+			case BUILD: return "Build";
+			case ENTER: return "Enter";
+			default: throw new IllegalArgumentException();
+		}
+	}
 }
