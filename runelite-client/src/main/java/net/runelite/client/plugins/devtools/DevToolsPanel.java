bbeb61cd3ec0e0f07378b73d001193eeb1905162
Max Weber <mii7303@gmail.com>
2018-05-03 17:18:29 -0600

    devtools: Add Notifier button

--- runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPanel.java
@@ -27,29 +27,33 @@ package net.runelite.client.plugins.devtools;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.TrayIcon;
 import javax.inject.Inject;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import net.runelite.api.Client;
 import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.Notifier;
 import net.runelite.client.ui.PluginPanel;
 
 public class DevToolsPanel extends PluginPanel
 {
 	private final Client client;
+	private final Notifier notifier;
 	private final DevToolsPlugin plugin;
 
 	private final WidgetInspector widgetInspector;
 	private final VarInspector varInspector;
 
 	@Inject
-	public DevToolsPanel(Client client, DevToolsPlugin plugin, WidgetInspector widgetInspector, VarInspector varInspector)
+	public DevToolsPanel(Client client, DevToolsPlugin plugin, WidgetInspector widgetInspector, VarInspector varInspector, Notifier notifier)
 	{
 		super();
 		this.client = client;
 		this.plugin = plugin;
 		this.widgetInspector = widgetInspector;
 		this.varInspector = varInspector;
+		this.notifier = notifier;
 
 		setBackground(ColorScheme.DARK_GRAY_COLOR);
 
@@ -247,6 +251,13 @@ public class DevToolsPanel extends PluginPanel
 			plugin.toggleExamineInfo();
 		});
 		container.add(examineInfoBtn);
+
+		final JButton notificationBtn = new JButton("Notification");
+		notificationBtn.addActionListener(e ->
+		{
+			notifier.notify("Wow!", TrayIcon.MessageType.ERROR);
+		});
+		container.add(notificationBtn);
 
 		return container;
 	}
