5652fa6389c1690b326734c7c9e1866d1da560b1
Daron Arakelian <seallproducts@gmail.com>
2018-05-02 22:22:00 -0400

    Initial commit of blowpipe scale tracker plugin.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/blowpipetracker/BlowPipeTrackerPlugin.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2018, Sir Girion <https://github.com/darakelian>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.blowpipetracker;
+
+import com.google.common.eventbus.Subscribe;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Actor;
+import net.runelite.api.AnimationID;
+import net.runelite.api.Client;
+import net.runelite.api.events.AnimationChanged;
+import net.runelite.api.kit.KitType;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+		name = "Blowpipe Tracker"
+)
+@Slf4j
+public class BlowPipeTrackerPlugin extends Plugin
+{
+
+    @Inject
+    private Client client;
+
+    @Subscribe
+    public void onAnimationChanged(AnimationChanged animationChanged)
+    {
+        Actor actor = animationChanged.getActor();
+
+        if (actor == null)
+        	return;
+
+        if (actor != client.getLocalPlayer())
+            return;
+
+        if (actor.getAnimation() != AnimationID.BLOWPIPE_ATTACK_ANIMATION)
+			return;
+
+		AttractorDefinition attractorDefinition = getAttractorForPlayer();
+        if (attractorDefinition == null)
+        	return;
+
+
+    }
+
+    private AttractorDefinition getAttractorForPlayer()
+	{
+		int attractorEquippedId = client.getLocalPlayer().getPlayerComposition().getEquipmentId(KitType.CAPE);
+
+		return AttractorDefinition.getAttractorById(attractorEquippedId);
+	}
+}
