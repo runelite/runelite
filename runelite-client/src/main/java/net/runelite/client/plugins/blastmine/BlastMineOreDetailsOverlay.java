6ce5debd989a12f1c5251a539bbc6b49caca55a0
Tim <pandahgorawr@gmail.com>
2018-05-04 21:42:59 +0200

    curly bois

--- runelite-client/src/main/java/net/runelite/client/plugins/blastmine/BlastMineOreDetailsOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/blastmine/BlastMineOreDetailsOverlay.java
@@ -18,7 +18,8 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.HashMap;
 
-class BlastMineOreDetailsOverlay extends Overlay {
+class BlastMineOreDetailsOverlay extends Overlay
+{
 	private final static int COAL = ItemID.COAL;
 	private final static int GOLD_ORE = ItemID.GOLD_ORE;
 	private final static int MITHRIL_ORE = ItemID.MITHRIL_ORE;
@@ -33,7 +34,8 @@ class BlastMineOreDetailsOverlay extends Overlay {
 	private final HashMap<Integer, Integer> orePrices = new HashMap<Integer, Integer>();
 
 	@Inject
-	BlastMineOreDetailsOverlay(Client client, BlastMinePluginConfig config, ItemManager itemManager) {
+	BlastMineOreDetailsOverlay(Client client, BlastMinePluginConfig config, ItemManager itemManager)
+	{
 		setPosition(OverlayPosition.TOP_LEFT);
 		this.client = client;
 		this.config = config;
@@ -41,31 +43,37 @@ class BlastMineOreDetailsOverlay extends Overlay {
 	}
 
 	@Override
-	public Dimension render(Graphics2D graphics) {
+	public Dimension render(Graphics2D graphics)
+	{
 
 		Widget blastMineWidget = client.getWidget(WidgetInfo.BLAST_MINE);
 		panelComponent.getLines().clear();
 
-		if (orePrices.size() == 0) {
+		if (orePrices.size() == 0)
+		{
 			this.getOrePrices();
 		}
 
-		if (blastMineWidget != null) {
+		if (blastMineWidget != null)
+		{
 			blastMineWidget.setHidden(true);
-			if (config.showSackDetails()) {
+			if (config.showSackDetails())
+			{
 				panelComponent.getLines().add(new PanelComponent.Line("XP: ", StackFormatter.formatNumber(CalculateXP()) + " xp"));
 				panelComponent.getLines().add(new PanelComponent.Line("GP: ", StackFormatter.formatNumber(CalculatePrice()) + " gp"));
 			}
 		}
 
-		if (panelComponent.getLines().size() > 0) {
+		if (panelComponent.getLines().size() > 0)
+		{
 			return panelComponent.render(graphics);
 		}
 
 		return null;
 	}
 
-	private int CalculateXP() {
+	private int CalculateXP()
+	{
 		return client.getVar(Varbits.BLAST_MINE_COAL) * 30 +
 				client.getVar(Varbits.BLAST_MINE_GOLD) * 60 +
 				client.getVar(Varbits.BLAST_MINE_MITHRIL) * 110 +
@@ -73,7 +81,8 @@ class BlastMineOreDetailsOverlay extends Overlay {
 				client.getVar(Varbits.BLAST_MINE_RUNITE) * 240;
 	}
 
-	private int CalculatePrice() {
+	private int CalculatePrice()
+	{
 		return client.getVar(Varbits.BLAST_MINE_COAL) * orePrices.get(COAL) +
 				client.getVar(Varbits.BLAST_MINE_GOLD) * orePrices.get(GOLD_ORE) +
 				client.getVar(Varbits.BLAST_MINE_MITHRIL) * orePrices.get(MITHRIL_ORE) +
@@ -81,7 +90,8 @@ class BlastMineOreDetailsOverlay extends Overlay {
 				client.getVar(Varbits.BLAST_MINE_RUNITE) * orePrices.get(RUNITE_ORE);
 	}
 
-	private void getOrePrices() {
+	private void getOrePrices()
+	{
 		orePrices.put(COAL, this.getItemPrice(COAL));
 		orePrices.put(GOLD_ORE, this.getItemPrice(GOLD_ORE));
 		orePrices.put(MITHRIL_ORE, this.getItemPrice(MITHRIL_ORE));
@@ -89,16 +99,20 @@ class BlastMineOreDetailsOverlay extends Overlay {
 		orePrices.put(RUNITE_ORE, this.getItemPrice(RUNITE_ORE));
 	}
 
-	private int getItemPrice(int itemId) {
+	private int getItemPrice(int itemId)
+	{
 		// convert to unnoted id
 		ItemComposition composition = this.itemManager.getItemComposition(itemId);
 		final boolean note = composition.getNote() != -1;
 		final int id = note ? composition.getLinkedNoteId() : composition.getId();
 
 		ItemPrice itemPrice;
-		try {
+		try
+		{
 			itemPrice = this.itemManager.getItemPrice(id);
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			return 0;
 		}
 
