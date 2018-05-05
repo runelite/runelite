b9d5e74a041d1f7d6cccbde5a0a70efc4abe1ade
RobinDson <robindavidsson@outlook.com>
2018-05-05 23:05:38 +0200

    Small fix

--- runelite-client/src/main/java/net/runelite/client/plugins/timers/TimersPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timers/TimersPlugin.java
@@ -43,9 +43,41 @@ import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIDOTEPLUS;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIDOTEPLUSPLUS;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIFIRE;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIPOISON;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIVENOM;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIVENOMPLUSPOISON;
+import static net.runelite.client.plugins.timers.GameTimer.ANTIVENOMPLUSVENOM;
+import static net.runelite.client.plugins.timers.GameTimer.BIND;
+import static net.runelite.client.plugins.timers.GameTimer.CANNON;
+import static net.runelite.client.plugins.timers.GameTimer.ENTANGLE;
+import static net.runelite.client.plugins.timers.GameTimer.EXANTIFIRE;
+import static net.runelite.client.plugins.timers.GameTimer.EXSUPERANTIFIRE;
+import static net.runelite.client.plugins.timers.GameTimer.FULLTB;
+import static net.runelite.client.plugins.timers.GameTimer.GOD_WARS_ALTAR;
+import static net.runelite.client.plugins.timers.GameTimer.HALFBIND;
+import static net.runelite.client.plugins.timers.GameTimer.HALFENTANGLE;
+import static net.runelite.client.plugins.timers.GameTimer.HALFSNARE;
+import static net.runelite.client.plugins.timers.GameTimer.HALFTB;
+import static net.runelite.client.plugins.timers.GameTimer.ICEBARRAGE;
+import static net.runelite.client.plugins.timers.GameTimer.ICEBLITZ;
+import static net.runelite.client.plugins.timers.GameTimer.ICEBURST;
+import static net.runelite.client.plugins.timers.GameTimer.ICERUSH;
+import static net.runelite.client.plugins.timers.GameTimer.IMBUEDHEART;
+import static net.runelite.client.plugins.timers.GameTimer.MAGICIMBUE;
+import static net.runelite.client.plugins.timers.GameTimer.OVERLOAD;
+import static net.runelite.client.plugins.timers.GameTimer.OVERLOAD_RAID;
+import static net.runelite.client.plugins.timers.GameTimer.PRAYER_ENHANCE;
+import static net.runelite.client.plugins.timers.GameTimer.SANFEW;
+import static net.runelite.client.plugins.timers.GameTimer.SNARE;
+import static net.runelite.client.plugins.timers.GameTimer.STAMINA;
+import static net.runelite.client.plugins.timers.GameTimer.SUPERANTIFIRE;
+import static net.runelite.client.plugins.timers.GameTimer.SUPERANTIPOISON;
+import static net.runelite.client.plugins.timers.GameTimer.VENGEANCE;
+import static net.runelite.client.plugins.timers.GameTimer.VENGEANCEOTHER;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
-
-import static net.runelite.client.plugins.timers.GameTimer.*;
 
 @PluginDescriptor(
 	name = "Timers"
