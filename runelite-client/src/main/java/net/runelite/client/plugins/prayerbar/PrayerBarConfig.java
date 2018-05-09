6ae71e965a5993f9fd651189dfae3221ad07bde1
Chdata <chdata7@gmail.com>
2018-05-09 14:09:13 -0500

    Begin work on Prayer Bar plugin.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerbar/PrayerBarConfig.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2018 Chdata
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.prayerbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup(
+	keyName = "prayerbar",
+	name = "Prayer Bar",
+	description = "Configuration for the overhead prayer bar"
+)
+public interface PrayerBarConfig extends Config
+{
+	@ConfigItem(
+		keyName = "prayerBarFgColor",
+		name = "Prayer Bar FG Color",
+		description = "Color of the overhead prayer bar",
+		position = 1
+	)
+	default Color getPrayerBarColor()
+	{
+		return Color.cyan; //new Color(0x67, 0xD4, 0xA9) // Prayer Potion color
+	}
+
+	@ConfigItem(
+			keyName = "prayerBarBgColor",
+			name = "Prayer Bar BG Color",
+			description = "Color of the background of the overhead prayer bar",
+			position = 2
+	)
+	default Color getPrayerBarBackgroundColor()
+	{
+		return Color.white;
+	}
+
+	@ConfigItem(
+			keyName = "prayerBarBgAlpha",
+			name = "Prayer Bar BG Alpha",
+			description = "Translucency of the background of the overhead prayer bar",
+			position = 3
+	)
+	default int getPrayerBarBackgroundAlpha()
+	{
+		return 255;
+	}
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerbar/PrayerBarOverlay.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2018, Chdata
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.prayerbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import net.runelite.api.Client;
+import net.runelite.api.Perspective;
+import net.runelite.api.Player;
+import net.runelite.api.Point;
+import net.runelite.api.Skill;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.client.ui.overlay.*;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+@Singleton
+public class PrayerBarOverlay extends Overlay
+{
+	private final Client client;
+	private Player localPlayer;
+	//private boolean prayersActive = false;
+	private final PrayerBarConfig config;
+	private Dimension prayerBarSize = new Dimension(30, 5); // 30x5 is the size of health bars
+	//private static final Logger logger = LoggerFactory.getLogger(PrayerBarOverlay.class);
+
+	@Inject
+	public PrayerBarOverlay(PrayerBarConfig config, Client client)
+	{
+		this.config = config;
+		this.client = client;
+
+		setPosition(OverlayPosition.DYNAMIC);
+		setPriority(OverlayPriority.MED);
+		setLayer(OverlayLayer.UNDER_WIDGETS);
+	}
+
+	/*public void onTick()
+	{
+		//prayersActive = isAnyPrayerActive();
+	}*/
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		localPlayer = client.getLocalPlayer();
+		if (localPlayer == null)
+		{
+			return null;
+		}
+
+		return renderPrayerBar(graphics, localPlayer);
+	}
+
+	private Dimension renderPrayerBar(Graphics2D graphics, Player localPlayer)
+	{
+		Color backgroundColor = new Color(
+				config.getPrayerBarBackgroundColor().getRed(),
+				config.getPrayerBarBackgroundColor().getGreen(),
+				config.getPrayerBarBackgroundColor().getBlue(),
+				Math.min(config.getPrayerBarBackgroundAlpha(), 255)
+		);
+
+		int height = localPlayer.getLogicalHeight() + 15;
+		LocalPoint localLocation = localPlayer.getLocalLocation();
+		Point canvasPoint = Perspective.worldToCanvas(client, localLocation.getX(), localLocation.getY(), client.getPlane(), height);
+
+		//Draw bar
+		int barX = canvasPoint.getX() - 15; // (-15, 3) is to line it up with your healthbar, 1 pixel beneath it.
+		int barY = canvasPoint.getY() + 3;
+
+		int barWidth = prayerBarSize.width;
+		int barHeight = prayerBarSize.height;
+
+		float ratio = (float) client.getBoostedSkillLevel(Skill.PRAYER) / (float) client.getRealSkillLevel(Skill.PRAYER);
+
+		int progressFill = (int) ((float) barWidth * ratio);
+
+		graphics.setColor(backgroundColor);
+		graphics.fillRect(barX, barY, barWidth, barHeight);
+		graphics.setColor(config.getPrayerBarColor());
+		graphics.fillRect(barX, barY, progressFill, barHeight);
+
+		return new Dimension(barWidth, barHeight);
+	}
+
+	/*private boolean isAnyPrayerActive()
+	{
+		for (Prayer pray : Prayer.values())//Check if any prayers are active
+		{
+			if (client.isPrayerActive(pray))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}*/
+}
