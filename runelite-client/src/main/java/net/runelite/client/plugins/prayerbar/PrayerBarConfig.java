169906713e0032535d2d27f77e397a8bc8dab532
Chdata <chdata7@gmail.com>
2018-05-10 10:23:41 -0500

    Remove unused import.

--- runelite-client/src/main/java/net/runelite/client/plugins/prayerbar/PrayerBarConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerbar/PrayerBarConfig.java
@@ -25,7 +25,6 @@
 package net.runelite.client.plugins.prayerbar;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
