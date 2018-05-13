44d51fedcbd1d33841f76c29e44a67578d445987
Henrik Lindström <henke96@bredband.net>
2018-05-13 02:22:41 +0200

    Took care of special cases in the reverse health calculation

--- runelite-client/src/main/java/net/runelite/client/plugins/opponentinfo/OpponentInfoOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/opponentinfo/OpponentInfoOverlay.java
@@ -159,13 +159,28 @@ class OpponentInfoOverlay extends Overlay
 					// This is the reverse of the calculation of healthRatio done by the server
 					// which is: healthRatio = 1 + (healthScale - 1) * health / maxHealth (if health > 0, 0 otherwise)
 					// It's able to recover the exact health if maxHealth <= healthScale.
-					int minHealth = (lastMaxHealth * (lastRatio - 1) - 1) / (lastHealthScale - 1) + 1;
-					int maxHealth = (lastMaxHealth * lastRatio - 1) / (lastHealthScale - 1);
-					if (maxHealth > lastMaxHealth)
+					int minHealth = 1;
+					int maxHealth;
+					if (lastHealthScale > 1)
 					{
+						if (lastRatio > 1)
+						{
+							// This doesn't apply if healthRatio = 1, because of the special case in the server calculation that
+							// health = 0 forces healthRatio = 0 instead of the expected healthRatio = 1
+							minHealth = (lastMaxHealth * (lastRatio - 1) + lastHealthScale - 2) / (lastHealthScale - 1);
+						}
+						maxHealth = (lastMaxHealth * lastRatio - 1) / (lastHealthScale - 1);
+						if (maxHealth > lastMaxHealth) {
+							maxHealth = lastMaxHealth;
+						}
+					}
+					else
+					{
+						// If healthScale is 1, healthRatio will always be 1 unless health = 0
+						// so we know nothing about the upper limit except that it can't be higher than maxHealth
 						maxHealth = lastMaxHealth;
 					}
-					// Take the average of min and max possible healts
+					// Take the average of min and max possible healths
 					health = (minHealth + maxHealth + 1) / 2;
 				}
 
