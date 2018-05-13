aa35b2c2180a1930f3f33f9c980d1ad0c4115071
Adam <Adam@anope.org>
2018-05-13 08:42:00 -0400

    http-service: lombok item api

--- http-api/src/main/java/net/runelite/http/api/item/Item.java
+++ http-api/src/main/java/net/runelite/http/api/item/Item.java
@@ -24,50 +24,13 @@
  */
 package net.runelite.http.api.item;
 
+import lombok.Data;
+
+@Data
 public class Item
 {
 	private int id;
 	private String name;
 	private String description;
 	private ItemType type;
-
-	public int getId()
-	{
-		return id;
-	}
-
-	public void setId(int id)
-	{
-		this.id = id;
-	}
-
-	public String getName()
-	{
-		return name;
-	}
-
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-
-	public String getDescription()
-	{
-		return description;
-	}
-
-	public void setDescription(String description)
-	{
-		this.description = description;
-	}
-
-	public ItemType getType()
-	{
-		return type;
-	}
-
-	public void setType(ItemType type)
-	{
-		this.type = type;
-	}
 }
