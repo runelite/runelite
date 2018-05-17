36a774dd143f8ae38fae3ed47de865815d2ac1f8
Jan <janie177@hotmail.com>
2018-05-17 16:39:57 +0200

    Added MenuEntrySwap option for grimy herbs. This is useful for farming when making herbs noted.

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -446,6 +446,10 @@ public class MenuEntrySwapperPlugin extends Plugin
 		{
 			swap("use", option, target, true);
 		}
+		else if (config.swapHerbs() && option.equals("clean"))
+		{
+			swap("use", option, target, true);
+		}
 	}
 
 	@Subscribe
