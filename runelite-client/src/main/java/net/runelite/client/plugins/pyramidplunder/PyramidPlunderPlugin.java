1b3068092aaff1534c74230b1f6e30c1d550e89f
basseleski <39378518+basseleski@users.noreply.github.com>
2018-05-17 17:46:44 +0200

    Pyramid plunder plugin
    Changed spaces to tabs

--- runelite-client/src/main/java/net/runelite/client/plugins/pyramidplunder/PyramidPlunderPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pyramidplunder/PyramidPlunderPlugin.java
@@ -15,58 +15,58 @@ import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
 import net.runelite.client.ui.overlay.infobox.Timer;
 
 @PluginDescriptor(
-        name = "Pyramid plunder",
-        enabledByDefault = false
+		name = "Pyramid plunder",
+		enabledByDefault = false
 )
 
 public class PyramidPlunderPlugin extends Plugin
 {
-    @Inject
-    private Client client;
-    @Inject
-    private InfoBoxManager infoBoxManager;
-    private boolean startingPlunder;
-    private Timer plunderTimer;
-    private SkillIconManager skillIconManager = new SkillIconManager();
-    private BufferedImage icon = skillIconManager.getSkillImage(Skill.THIEVING);
+	@Inject
+	private Client client;
+	@Inject
+	private InfoBoxManager infoBoxManager;
+	private boolean startingPlunder;
+	private Timer plunderTimer;
+	private SkillIconManager skillIconManager = new SkillIconManager();
+	private BufferedImage icon = skillIconManager.getSkillImage(Skill.THIEVING);
 
-    @Subscribe
-    public void onMenuOptionClicked(MenuOptionClicked event)
-    {
-        if (event.getMenuOption().matches("Start-minigame"))
-        {
-            for (NPC list: client.getNpcs())
-            {
-                if (list.getName().matches("Guardian mummy"))
-                {
-                    startingPlunder = true;
-                    break;
-                }
-            }
-        }
-        else
-        {
-            startingPlunder = false;
-        }
-    }
-    @Subscribe
-    public void onGameStateChanged(GameStateChanged event)
-    {
-        if (event.getGameState() == GameState.LOADING)
-        {
-            if (startingPlunder)
-            {
-                plunderTimer = new Timer(5, ChronoUnit.MINUTES, icon, this);
-                infoBoxManager.addInfoBox(plunderTimer);
-                startingPlunder = false;
-            }
-            else
-            {
-                if(infoBoxManager.getInfoBoxes().contains(plunderTimer))
-                {
-                    infoBoxManager.removeInfoBox(plunderTimer);
-                }
-            }
-        }
-    }
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		if (event.getMenuOption().matches("Start-minigame"))
+		{
+			for (NPC list: client.getNpcs())
+			{
+				if (list.getName().matches("Guardian mummy"))
+				{
+					startingPlunder = true;
+					break;
+				}
+			}
+		}
+		else
+		{
+			startingPlunder = false;
+		}
+	}
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState() == GameState.LOADING)
+		{
+			if (startingPlunder)
+			{
+				plunderTimer = new Timer(5, ChronoUnit.MINUTES, icon, this);
+				infoBoxManager.addInfoBox(plunderTimer);
+				startingPlunder = false;
+			}
+			else
+			{
+				if(infoBoxManager.getInfoBoxes().contains(plunderTimer))
+				{
+					infoBoxManager.removeInfoBox(plunderTimer);
+				}
+			}
+		}
+	}
 }
