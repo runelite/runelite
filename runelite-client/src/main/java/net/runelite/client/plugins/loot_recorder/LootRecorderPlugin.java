19d65cc36a65ac1d04a99816ed581c1a9c6343f3
Michael Goodwin <29030969+TheStonedTurtle@users.noreply.github.com>
2018-05-17 17:41:21 -0700

    Nice.

--- runelite-client/src/main/java/net/runelite/client/plugins/loot_recorder/LootRecorderPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loot_recorder/LootRecorderPlugin.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.loot-recorder;
+package net.runelite.client.plugins.loot_recorder;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ObjectArrays;
