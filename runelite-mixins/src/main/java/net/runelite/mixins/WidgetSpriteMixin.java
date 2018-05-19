ffaefda355aa599bd597b42a35c98d2ad6b5757d
Adam <Adam@anope.org>
2018-05-19 19:06:18 -0400

    runelite-api: add sprite override api

---
+++ runelite-mixins/src/main/java/net/runelite/mixins/WidgetSpriteMixin.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2018, Lotto <https://github.com/devLotto>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.mixins;
+
+import java.util.Map;
+import net.runelite.api.SpritePixels;
+import net.runelite.api.mixins.Copy;
+import net.runelite.api.mixins.Mixin;
+import net.runelite.api.mixins.Replace;
+import net.runelite.api.mixins.Shadow;
+import net.runelite.rs.api.RSSpritePixels;
+import net.runelite.rs.api.RSWidget;
+
+@Mixin(RSWidget.class)
+public abstract class WidgetSpriteMixin implements RSWidget
+{
+	@Shadow("widgetSpriteOverrides")
+	private static Map<Integer, SpritePixels> widgetSpriteOverrides;
+
+	@Copy("getWidgetSprite")
+	public RSSpritePixels rs$getWidgetSprite(boolean var1)
+	{
+		throw new RuntimeException();
+	}
+
+	@Replace("getWidgetSprite")
+	public RSSpritePixels rl$getWidgetSprite(boolean var1)
+	{
+		if (getSpriteId() != -1 && widgetSpriteOverrides != null)
+		{
+			SpritePixels sprite = widgetSpriteOverrides.get(getId());
+
+			if (sprite != null)
+			{
+				return (RSSpritePixels) sprite;
+			}
+		}
+
+		return rs$getWidgetSprite(var1);
+	}
+}
