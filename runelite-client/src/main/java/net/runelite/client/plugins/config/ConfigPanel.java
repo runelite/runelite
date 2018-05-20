69b8f809fd5cc0b5ab2203f2951a93a2e9a5b9ab
Ruben <chickenzrage@gmail.com>
2018-05-21 00:51:33 +0100

    Add mouse hover changes to various ui elements
    This commit adds the following with the main aim of improving the overall ui experience:
    InfoPanel: changes the panels to act more like buttons, they highlight on hover and the cursor changes to a pointer (since they are hyperlinks).
    NewsFeedPanel: changes the cursor to a pointer on hover to indicate a hyperlink
    HighscorePanel: changes the buttons below the search bar to highlight on hover to help indicate a clickable element.
    FarmingPanel: same as above expect for the patch buttons
    GE Panel: same as above but for the offer and search button
    ConfigPanel: Changes the cog color on hover to help indicate clickable element.

--- runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
@@ -37,6 +37,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.Map;
@@ -80,6 +81,7 @@ import net.runelite.client.ui.DynamicGridLayout;
 import net.runelite.client.ui.PluginPanel;
 import net.runelite.client.ui.components.ComboBoxListRenderer;
 import net.runelite.client.ui.components.IconTextField;
+import net.runelite.client.util.SwingUtil;
 
 @Slf4j
 public class ConfigPanel extends PluginPanel
@@ -88,6 +90,7 @@ public class ConfigPanel extends PluginPanel
 	private static final int SPINNER_FIELD_WIDTH = 6;
 
 	private static final ImageIcon CONFIG_ICON;
+	private static final ImageIcon CONFIG_ICON_HOVER;
 	private static final ImageIcon ON_SWITCHER;
 	private static final ImageIcon OFF_SWITCHER;
 	private static final ImageIcon SEARCH;
@@ -98,7 +101,9 @@ public class ConfigPanel extends PluginPanel
 		{
 			synchronized (ImageIO.class)
 			{
-				CONFIG_ICON = new ImageIcon(ImageIO.read(ConfigPanel.class.getResourceAsStream("config_edit_icon.png")));
+				BufferedImage configIcon = ImageIO.read(ConfigPanel.class.getResourceAsStream("config_edit_icon.png"));
+				CONFIG_ICON = new ImageIcon(configIcon);
+				CONFIG_ICON_HOVER = new ImageIcon(SwingUtil.grayscaleOffset(configIcon, -100));
 				ON_SWITCHER = new ImageIcon(ImageIO.read(ConfigPanel.class.getResourceAsStream("switchers/on.png")));
 				OFF_SWITCHER = new ImageIcon(ImageIO.read(ConfigPanel.class.getResourceAsStream("switchers/off.png")));
 				SEARCH = new ImageIcon(ImageIO.read(IconTextField.class.getResourceAsStream("search.png")));
@@ -246,7 +251,20 @@ public class ConfigPanel extends PluginPanel
 					@Override
 					public void mousePressed(MouseEvent mouseEvent)
 					{
+						editConfigButton.setIcon(CONFIG_ICON);
 						openGroupConfigPanel(config, configDescriptor, configManager);
+					}
+
+					@Override
+					public void mouseEntered(MouseEvent e)
+					{
+						editConfigButton.setIcon(CONFIG_ICON_HOVER);
+					}
+
+					@Override
+					public void mouseExited(MouseEvent e)
+					{
+						editConfigButton.setIcon(CONFIG_ICON);
 					}
 				});
 				editConfigButton.setVisible(true);
