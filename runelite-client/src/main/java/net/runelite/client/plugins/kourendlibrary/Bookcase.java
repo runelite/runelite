244787994fcd0a9f406c27c458c2b0721712c2b6
trimbe <benno.tremback@gmail.com>
2018-05-20 06:58:17 -0400

    Fix bounds for west bookcases in Kourend library (#2875)

--- runelite-client/src/main/java/net/runelite/client/plugins/kourendlibrary/Bookcase.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/kourendlibrary/Bookcase.java
@@ -79,7 +79,7 @@ class Bookcase
 		StringBuilder b = new StringBuilder();
 
 		boolean north = location.getY() > 3815;
-		boolean west = location.getX() < 1625;
+		boolean west = location.getX() < 1627;
 		if (north && west)
 		{
 			b.append("Northwest");
