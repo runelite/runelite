c864687f784026781b350841c03c682e19a6770f
TzZek <tzzek@protonmail.com>
2018-05-23 14:59:21 -0400

    fix codestyle for xpDropPlugin

--- runelite-client/src/main/java/net/runelite/client/plugins/experiencedrop/XpDropPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/experiencedrop/XpDropPlugin.java
@@ -41,7 +41,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @PluginDescriptor(
-		name = "XP Drop"
+	name = "XP Drop"
 )
 public class XpDropPlugin extends Plugin
 {
@@ -89,7 +89,7 @@ public class XpDropPlugin extends Plugin
 
 		String text = widget.getText();
 		final IntStream spriteIDs =
-				Arrays.stream(widget.getParent().getDynamicChildren()).mapToInt(Widget::getSpriteId);
+			Arrays.stream(widget.getParent().getDynamicChildren()).mapToInt(Widget::getSpriteId);
 
 		if (text != null)
 		{
@@ -99,8 +99,8 @@ public class XpDropPlugin extends Plugin
 			{
 				case MELEE:
 					if (spriteIDs.anyMatch(id ->
-							id == SpriteID.SKILL_ATTACK || id == SpriteID.SKILL_STRENGTH || id == SpriteID.SKILL_DEFENCE
-									|| id == SpriteID.SKILL_HITPOINTS))
+						id == SpriteID.SKILL_ATTACK || id == SpriteID.SKILL_STRENGTH || id == SpriteID.SKILL_DEFENCE
+							|| id == SpriteID.SKILL_HITPOINTS))
 					{
 						color = config.getMeleePrayerColor().getRGB();
 					}
