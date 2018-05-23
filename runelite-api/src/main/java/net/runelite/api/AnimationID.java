d7b092e970b750de1f8d139a685bb4b85e9da372
iguaan <stuffaa00@gmail.com>
2018-05-23 19:13:41 +0300

    idlenotifier: add support for gilded altar

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -113,6 +113,7 @@ public final class AnimationID
 	public static final int HERBLORE_POTIONMAKING = 363; //used for both herb and secondary
 	public static final int MAGIC_CHARGING_ORBS = 726;
 	public static final int BURYING_BONES = 827;
+	public static final int USING_GILDED_ALTAR = 3705;
 	public static final int LOOKING_INTO = 832;
 	public static final int DIG = 830;
 	public static final int VENGEANCE_OTHER = 4411;
