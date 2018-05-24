09eb27de7951b1c60a588ff86036df5a5732e75c
Michael Goodwin <29030969+TheStonedTurtle@users.noreply.github.com>
2018-05-23 23:21:48 -0700

    UniqueItem activites
    unique items shared between activites like Godsword shards

--- runelite-client/src/main/java/net/runelite/client/plugins/lootrecorder/UniqueItem.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/lootrecorder/UniqueItem.java
@@ -118,31 +118,55 @@ public enum UniqueItem
 	ARMADYL_CHESTPLATE("Armadyl chestplate", ItemID.ARMADYL_CHESTPLATE, "Armadyl", "Uniques", 0),
 	ARMADYL_CHAINSKIRT("Armadyl chainskirt", ItemID.ARMADYL_CHAINSKIRT, "Armadyl", "Uniques", 0),
 	ARMADYL_HILT("Armadyl hilt", ItemID.ARMADYL_HILT, "Armadyl", "Uniques", 0),
-	PET_KREEARRA("Pet Kree'arra", ItemID.PET_KREEARRA, "Armadyl", "Pet", 1),
+	PET_KREEARRA("Pet Kree'arra", ItemID.PET_KREEARRA, "Armadyl", "Pet", -1),
 	// General Graardor (Bandos)
 	BANDOS_CHESTPLATE("Bandos chestplate", ItemID.BANDOS_CHESTPLATE, "Bandos", "Uniques", 0),
 	BANDOS_TASSETS("Bandos tassets", ItemID.BANDOS_TASSETS, "Bandos", "Uniques", 0),
 	BANDOS_BOOTS("Bandos boots", ItemID.BANDOS_BOOTS, "Bandos", "Uniques", 0),
 	BANDOS_HILT("Bandos hilt", ItemID.BANDOS_HILT, "Bandos", "Uniques", 0),
-	PET_GENERAL_GRAARDOR("Pet general graardor", ItemID.PET_GENERAL_GRAARDOR, "Bandos", "Pet", 1),
+	PET_GENERAL_GRAARDOR("Pet general graardor", ItemID.PET_GENERAL_GRAARDOR, "Bandos", "Pet", -1),
 	// Command Zilyana (Saradomin)
 	SARADOMIN_SWORD("Saradomin sword", ItemID.SARADOMIN_SWORD, "Saradomin", "Uniques", 0),
 	ARMADYL_CROSSBOW("Armadyl crossbow", ItemID.ARMADYL_CROSSBOW, "Saradomin", "Uniques", 0),
 	SARADOMINS_LIGHT("Saradomins light", ItemID.SARADOMINS_LIGHT, "Saradomin", "Uniques", 0),
 	SARADOMIN_HILT("Saradomin hilt", ItemID.SARADOMIN_HILT, "Saradomin", "Uniques", 0),
-	PET_ZILYANA("Pet zilyana", ItemID.PET_ZILYANA, "Saradomin", "Pet", 1),
+	PET_ZILYANA("Pet zilyana", ItemID.PET_ZILYANA, "Saradomin", "Pet", -1),
 	// K'ril Tsutsaroth (Zammy)
 	STEAM_BATTLESTAFF("Steam battlestaff", ItemID.STEAM_BATTLESTAFF, "Zammy", "Uniques", 0),
 	STAFF_OF_THE_DEAD("Staff of the dead", ItemID.STAFF_OF_THE_DEAD, "Zammy", "Uniques", 0),
 	ZAMORAKIAN_SPEAR("Zamorakian spear", ItemID.ZAMORAKIAN_SPEAR, "Zammy", "Uniques", 0),
 	ZAMORAK_HILT("Zamorak hilt", ItemID.ZAMORAK_HILT, "Zammy", "Uniques", 0),
-	PET_KRIL_TSUTSAROTH("Pet k'ril tsutsaroth", ItemID.PET_KRIL_TSUTSAROTH, "Zammy", "Pet", 1);
+	PET_KRIL_TSUTSAROTH("Pet k'ril tsutsaroth", ItemID.PET_KRIL_TSUTSAROTH, "Zammy", "Pet", -1),
 
+
+	// Shared "Uniques" (Dropped by multiple activities)
+	GODSWORD_SHARD_1("Godsword shard 1", ItemID.GODSWORD_SHARD_1, "Armadyl", "Bandos", "Saradomin", "Zammy"),
+	GODSWORD_SHARD_2("Godsword shard 2", ItemID.GODSWORD_SHARD_2, "Armadyl", "Bandos", "Saradomin", "Zammy"),
+	GODSWORD_SHARD_3("Godsword shard 3", ItemID.GODSWORD_SHARD_3, "Armadyl", "Bandos", "Saradomin", "Zammy");
+
+	UniqueItem(String n, int id, String a, String set, int position)
+	{
+		this.name = n;
+		this.itemID = id;
+		this.activities = new String[]{a};
+		this.setName = set;
+		this.position = position;
+
+	}
+
+	UniqueItem(String n, int id, String... activities)
+	{
+		this.name = n;
+		this.itemID = id;
+		this.activities = activities;
+		this.setName = "Shared";
+		this.position = -1;
+	}
 
 
 	private final String name;
 	private final int itemID;
-	private final String activity;
+	private final String[] activities;
 	private final String setName;
 	private final int position;
 
@@ -175,7 +199,12 @@ public enum UniqueItem
 		Map<String, ArrayList<UniqueItem>> byName = new HashMap<>();
 		for (UniqueItem item : values())
 		{
-			byName.computeIfAbsent(item.getActivity().toUpperCase(), e -> new ArrayList<UniqueItem>()).add(item);
+			String[] activities = item.getActivities();
+			for (String activity : activities)
+			{
+				byName.computeIfAbsent(activity.toUpperCase(), e -> new ArrayList<UniqueItem>()).add(item);
+			}
+
 		}
 
 		return byName;
