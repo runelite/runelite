dc79096f488ad9db70a46e50810ed98d75550a92
GodsWithin <39600642+GodsWithin@users.noreply.github.com>
2018-05-30 23:01:12 +0200

    Update SkillCalculator.java

--- runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/SkillCalculator.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/SkillCalculator.java
@@ -154,7 +154,7 @@ class SkillCalculator extends JPanel
 
 		for (UIActionSlot slot : combinedActionSlots)
 		{
-      xp += slot.getValue();
+      			xp += slot.getValue();
 		}
 			
 		if (neededXP > 0)
@@ -234,7 +234,7 @@ class SkillCalculator extends JPanel
 					}
 
 					if (slot.isSelected())
-          {
+          				{
 						combinedActionSlots.remove(slot);
 					}
 					else
@@ -344,5 +344,5 @@ class SkillCalculator extends JPanel
 		xpFactor -= lastBonus;
 		calculate();
 	}
-  
+	
 }
