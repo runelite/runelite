07af3ec0d81710162517479a97a421bec459c34e
Hydrox6 <ikada@protonmail.ch>
2018-06-01 01:20:15 +0100

    [devtools] Add Chat Inspector

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/devtools/ChatInspector.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2018 Hydrox6 <ikada@protonmail.ch>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.devtools;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Inject;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.events.SetMessage;
+import net.runelite.client.ui.ClientUI;
+import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.DynamicGridLayout;
+
+@Slf4j
+class ChatInspector extends JFrame
+{
+	private final static int MAX_LOG_ENTRIES = 10_000;
+
+	private final EventBus eventBus;
+
+	private final JPanel tracker = new JPanel();
+
+	@Inject
+	ChatInspector(EventBus eventBus)
+	{
+		this.eventBus = eventBus;
+
+		setTitle("RuneLite Chat Inspector");
+		setIconImage(ClientUI.ICON);
+
+		setLayout(new BorderLayout());
+
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter()
+		{
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				close();
+			}
+		});
+
+		tracker.setLayout(new DynamicGridLayout(0, 1, 0, 3));
+
+		final JPanel trackerWrapper = new JPanel();
+		trackerWrapper.setLayout(new BorderLayout());
+		trackerWrapper.add(tracker, BorderLayout.NORTH);
+
+		final JScrollPane trackerScroller = new JScrollPane(trackerWrapper);
+		trackerScroller.setPreferredSize(new Dimension(400, 400));
+
+		final JScrollBar vertical = trackerScroller.getVerticalScrollBar();
+		vertical.addAdjustmentListener(new AdjustmentListener()
+		{
+			int lastMaximum = actualMax();
+
+			private int actualMax()
+			{
+				return vertical.getMaximum() - vertical.getModel().getExtent();
+			}
+
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e)
+			{
+				if (vertical.getValue() >= lastMaximum)
+				{
+					vertical.setValue(actualMax());
+				}
+				lastMaximum = actualMax();
+			}
+		});
+
+		add(trackerScroller, BorderLayout.CENTER);
+
+		pack();
+	}
+
+	private void addChatLog(ChatMessageType type, String channel, String name, String message)
+	{
+		SwingUtilities.invokeLater(() ->
+		{
+			tracker.add(new JLabel(String.format("%s, %s", type.toString(), channel)));
+			JLabel last = new JLabel(String.format("%s: %s", name, message));
+			last.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, ColorScheme.LIGHT_GRAY_COLOR));
+			tracker.add(last);
+
+			// Cull very old stuff
+			for (; tracker.getComponentCount() > MAX_LOG_ENTRIES; )
+			{
+				tracker.remove(0);
+			}
+
+			tracker.revalidate();
+		});
+	}
+
+	@Subscribe
+	public void onSetMessage(SetMessage event)
+	{
+		addChatLog(event.getType(), event.getSender(), event.getName(), event.getValue());
+	}
+
+	public void open()
+	{
+		eventBus.register(this);
+		setVisible(true);
+		toFront();
+		repaint();
+	}
+
+	public void close()
+	{
+		tracker.removeAll();
+		eventBus.unregister(this);
+		setVisible(false);
+	}
+}
