c76c10c20c1a7b6fa6cebe3c82ae07f77df0de33
Adam <Adam@anope.org>
2018-06-03 16:13:18 -0400

    death indicator: use animation to detect death
    It is possible the player can move after their hp reaches 0, which makes
    the player death event not always take place at the location of death

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -45,6 +45,7 @@ public final class AnimationID
 	public static final int WOODCUTTING_3A_AXE = 7264;
 	public static final int CONSUMING = 829; // consuming consumables
 	public static final int FIREMAKING = 733;
+	public static final int DEATH = 836;
 	public static final int COOKING_FIRE = 897;
 	public static final int COOKING_RANGE = 896;
 	public static final int COOKING_WINE = 7529;
