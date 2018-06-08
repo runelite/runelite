56ae94e30f7cba2509667f7712a3193a75e3a97a
Ian O'Neill <iano2k4@hotmail.com>
2018-06-08 22:50:16 +0100

    Added methods to create the sound files. More sound effect files renaming.

--- cache/src/main/java/net/runelite/cache/definitions/sound/AudioEnvelopeDefinition.java
+++ cache/src/main/java/net/runelite/cache/definitions/sound/AudioEnvelopeDefinition.java
@@ -26,17 +26,17 @@ package net.runelite.cache.definitions.sound;
 
 public class AudioEnvelopeDefinition
 {
-	public int field1085;
+	public int segments = 2;
 	public int[] durations = new int[2];
-	public int form;
+	public int[] phases = new int[2];
 	public int start;
 	public int end;
-	public int[] phases = new int[2];
-	public int field1091;
-	public int segments = 2;
-	public int field1093;
-	public int field1094;
-	public int field1095;
+	public int form;
+	public int ticks;
+	public int phaseIndex;
+	public int step;
+	public int amplitude;
+	public int max;
 
 	public AudioEnvelopeDefinition()
 	{
@@ -44,5 +44,38 @@ public class AudioEnvelopeDefinition
 		this.durations[1] = '\uffff';
 		this.phases[0] = 0;
 		this.phases[1] = '\uffff';
+	}
+
+	public final int step(int var1)
+	{
+		if (this.max >= this.ticks)
+		{
+			this.amplitude = this.phases[this.phaseIndex++] << 15;
+
+			if (this.phaseIndex >= this.segments)
+			{
+				this.phaseIndex = this.segments - 1;
+			}
+
+			this.ticks = (int) ((double) this.durations[this.phaseIndex] / 65536.0 * (double) var1);
+
+			if (this.ticks > this.max)
+			{
+				this.step = ((this.phases[this.phaseIndex] << 15) - this.amplitude) / (this.ticks - this.max);
+			}
+		}
+		this.amplitude += this.step;
+		++this.max;
+
+		return this.amplitude - this.step >> 15;
+	}
+
+	public final void reset()
+	{
+		this.ticks = 0;
+		this.phaseIndex = 0;
+		this.step = 0;
+		this.amplitude = 0;
+		this.max = 0;
 	}
 }
