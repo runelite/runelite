597dd125cde3e031e461bbc5ecf2d82d5a553133
Nathen <nathen.sample@gmail.com>
2018-06-20 00:05:55 +0100

    Rectifying style

--- runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleTimer.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleTimer.java
@@ -7,7 +7,8 @@ import java.awt.image.BufferedImage;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
-class IdleTimer extends Timer {
+class IdleTimer extends Timer
+{
     IdleTimer(BufferedImage infoImage, Plugin plugin, Duration seconds){
         super(seconds.toMillis(), ChronoUnit.MILLIS, infoImage, plugin);
     }
