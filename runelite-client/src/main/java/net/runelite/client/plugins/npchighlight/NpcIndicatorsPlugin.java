5b1b781b4cb46ce6fb9645dc9844d4d173d57faf
Adam <Adam@anope.org>
2018-06-21 17:16:36 -0400

    Revert "Prevent NPC tagging from interrupting actions"
    This reverts commit a2fea6d33f5c328a961c90285e363677c61e9af3.

--- runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
@@ -242,8 +242,6 @@ public class NpcIndicatorsPlugin extends Plugin
 	{
 		if (click.getMenuOption().equals(TAG))
 		{
-			click.consume();
-
 			final int id = click.getId();
 			final boolean removed = npcTags.remove(id);
 			final NPC[] cachedNPCs = client.getCachedNPCs();
