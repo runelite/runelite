6cda099ce3469f4d04a4f257213a8eb8af66fc21
Tomas Slusny <slusnucky@gmail.com>
2018-06-30 03:21:30 +0200

    Revert "Add ironman status and AccountType to API"
    This reverts commit 865bae6c2d30a7d9a0d94e76cd86175308b829c5.

--- runelite-api/src/main/java/net/runelite/api/Varbits.java
+++ runelite-api/src/main/java/net/runelite/api/Varbits.java
@@ -58,11 +58,6 @@ public enum Varbits
 	RUNE_POUCH_AMOUNT3(1626),
 
 	/**
-	 * Ironman status. See {@link AccountType}
-	 */
-	IRONMAN_STATUS(1777),
-
-	/**
 	 * Prayers
 	 */
 	PRAYER_THICK_SKIN(4104),
