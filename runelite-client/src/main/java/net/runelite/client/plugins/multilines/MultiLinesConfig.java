92fbb327d9add50a9645d311e3b7545716e094a5
Mattias Cederlund <mattec92@hotmail.com>
2018-07-02 14:21:09 +0200

    File header and cleanup.

--- runelite-client/src/main/java/net/runelite/client/plugins/multilines/MultiLinesConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/multilines/MultiLinesConfig.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2018, Mattias Cederlund <https://github.com/mattec92>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.multilines;
 
 import java.awt.Color;
@@ -16,7 +40,8 @@ public interface MultiLinesConfig extends Config
 	@ConfigItem(
 		keyName = "singleColor",
 		name = "Single color",
-		description = "Color of single combat side of border")
+		description = "Color of single combat side of border"
+	)
 	default Color singleColor()
 	{
 		return Color.GREEN;
@@ -25,7 +50,8 @@ public interface MultiLinesConfig extends Config
 	@ConfigItem(
 		keyName = "multiColor",
 		name = "Multi color",
-		description = "Color of multi combat side of border")
+		description = "Color of multi combat side of border"
+	)
 	default Color multiColor()
 	{
 		return Color.RED;
