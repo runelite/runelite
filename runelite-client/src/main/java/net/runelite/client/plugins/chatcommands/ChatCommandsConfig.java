f038e706ab58691a01a101d3e1a95f70d6061a59
Juul Damen <juul.damen@student.fontys.nl>
2018-07-25 01:08:25 +0200

    Fixed checkstyle error

--- runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsConfig.java
@@ -70,7 +70,10 @@ public interface ChatCommandsConfig extends Config
 			name = "Killcount Command",
 			description = "Configures whether the Killcount command is enabled"
 	)
-	default boolean killcount() { return true; }
+	default boolean killcount()
+	{
+		return true;
+	}
 
 	@ConfigItem(
 		position = 4,
