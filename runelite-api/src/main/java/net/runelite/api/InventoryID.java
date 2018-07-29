2202ccc37f27b61b3c2fe6678117f0a492a3f588
Michael Goodwin <29030969+TheStonedTurtle@users.noreply.github.com>
2018-07-29 21:20:39 +0200

    Add Chambers Of Xeric and TOB inventories
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- runelite-api/src/main/java/net/runelite/api/InventoryID.java
+++ runelite-api/src/main/java/net/runelite/api/InventoryID.java
@@ -48,7 +48,15 @@ public enum InventoryID
 	/**
 	 * Barrows reward chest inventory.
 	 */
-	BARROWS_REWARD(141);
+	BARROWS_REWARD(141),
+	/**
+	 * Chambers of Xeric chest inventory.
+	 */
+	CHAMBERS_OF_XERIC_CHEST(581),
+	/**
+	 * Theater of Blood reward chest inventory (Raids 2)
+	 */
+	THEATRE_OF_BLOOD_CHEST(10);
 
 	private final int id;
 
