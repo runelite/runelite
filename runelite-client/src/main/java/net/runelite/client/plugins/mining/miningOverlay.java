6f59ba5ad373ec2d7ec21b652e4c136d01934ca4
DarkSlaayer <41709823+DarkSlaayer@users.noreply.github.com>
2018-07-29 17:20:59 -0500

    Shows mining locations respawn times on mining rocks

--- runelite-client/src/main/java/net/runelite/client/plugins/mining/miningOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mining/miningOverlay.java
@@ -12,49 +12,49 @@ import java.awt.*;
 
 public class miningOverlay extends Overlay {
 
-    private final Client client;
-    private final miningPlugin plugin;
-    private final PanelComponent panelComponent = new PanelComponent();
+	private final Client client;
+	private final miningPlugin plugin;
+	private final PanelComponent panelComponent = new PanelComponent();
 
 
-    @Inject
-    private miningOverlay(Client client, miningPlugin plugin)
-    {
-        setPosition(OverlayPosition.TOP_LEFT);
-        this.client = client;
-        this.plugin = plugin;
-    }
+	@Inject
+	private miningOverlay(Client client, miningPlugin plugin)
+	{
+		setPosition(OverlayPosition.TOP_LEFT);
+		this.client = client;
+		this.plugin = plugin;
+	}
 
-    @Override
-    public Dimension render(Graphics2D graphics)
-    {
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
 
-        if(!miningPlugin.menuInt)
-        {
-            return null;
-        }
-        panelComponent.getChildren().clear();
-        if ((System.currentTimeMillis() - plugin.getTimeLastMined()) / 1000 < plugin.getDisplaySinceLastMined()) {
+		if(!miningPlugin.menuInt)
+		{
+			return null;
+		}
+		panelComponent.getChildren().clear();
+		if ((System.currentTimeMillis() - plugin.getTimeLastMined()) / 1000 < plugin.getDisplaySinceLastMined()) {
 
-            panelComponent.getChildren().add(TitleComponent.builder()
-                    .preferredLocation(new Point(40,200))
-                    .text("Mining")
-                    .build());
-
-
-            panelComponent.getChildren().add(LineComponent.builder()
-                    .left("Ores mined:")
-                    .right(Integer.toString(plugin.getOresMined()))
-                    .build());
-
-            return panelComponent.render(graphics);
-
-        } else {
-            return null;
-        }
+			panelComponent.getChildren().add(TitleComponent.builder()
+					.preferredLocation(new Point(40,200))
+					.text("Mining")
+					.build());
 
 
+			panelComponent.getChildren().add(LineComponent.builder()
+					.left("Ores mined:")
+					.right(Integer.toString(plugin.getOresMined()))
+					.build());
 
-    }
-    
+			return panelComponent.render(graphics);
+
+		} else {
+			return null;
+		}
+
+
+
+	}
+
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/mining/miningRockOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mining/miningRockOverlay.java
@@ -17,93 +17,94 @@ import java.util.Map;
 public class miningRockOverlay extends Overlay {
 
 
-    private final Client client;
+	private final Client client;
 
-    private final miningPlugin plugin;
+	private final miningPlugin plugin;
 
-    private final PanelComponent panelComponent = new PanelComponent();
+	private final PanelComponent panelComponent = new PanelComponent();
 
-    private boolean panelUp = false;
+	private boolean panelUp = false;
 
-    @Inject
-    ItemManager itemManager;
+	@Inject
+	ItemManager itemManager;
 
-    @Inject
-    miningRockOverlay(Client client, miningPlugin a) {
-        this.client = client;
-        this.plugin = a;
-        setLayer(OverlayLayer.ABOVE_SCENE);
-        setPosition(OverlayPosition.DYNAMIC);
-    }
-
-
-    @Override
-    public Dimension render(Graphics2D graphics) {
+	@Inject
+	miningRockOverlay(Client client, miningPlugin a) {
+		this.client = client;
+		this.plugin = a;
+		setLayer(OverlayLayer.ABOVE_SCENE);
+		setPosition(OverlayPosition.DYNAMIC);
+	}
 
 
-        if (plugin.getRockObjects() != null && plugin.getRockObjects().size() > 0) {
-
-            for (GameObject rock : plugin.getRockObjects()) {
-                if (plugin.getRockObjects() != null) {
-                    if (rock != null) {
-                        if (rock.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()) < 12) {
-                            if (rock.getCanvasLocation() != null && Rock.isRock(rock.getId())) {
-                                    OverlayUtil.renderTextLocation(graphics, new Point(rock.getCanvasLocation().getX(), rock.getCanvasLocation().getY() - 40), Rock.getName(rock) , Color.green);
-                                     OverlayUtil.renderImageLocation(graphics, rock.getCanvasLocation(), itemManager.getImage(1275));
-                                }
-                            }
-                        }
-
-                    }
-
-                }
-        }
-
-        if (plugin.getDespawnedRockObjects() != null) {
-
-            for (GameObject rock : plugin.getDespawnedRockObjects()) {
-                if (plugin.getDespawnedRockObjects() != null && rock != null) {
-                    if (rock.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()) < 12) {
-
-                        if (rock.getCanvasLocation() != null) {
-                            Long respawnTime = null;
-
-                            Point rockLoc = new Point(rock.getWorldLocation().getX(), rock.getWorldLocation().getY());
-                            for (Map.Entry<Point, Long> timers : plugin.getDespawnedRockObjectsTimers().entrySet()) {
-
-                                if (timers.getKey().getX() == rockLoc.getX() && timers.getKey().getY() == rockLoc.getY()) {
-
-                                     respawnTime =  System.currentTimeMillis() - timers.getValue();
-
-                                    break;
-                                }
-                            }
-                            if (respawnTime != null) {
-                                Long editedRespawnTime = ((Rock.getRespawnTime(rock.getId()) - respawnTime) / 1000);
-                                OverlayUtil.renderTextLocation(graphics
-                                        , rock.getCanvasLocation()
-                                        , Rock.getName(rock) +
-                                                " respawning in: " +
-                                                (editedRespawnTime)
-                                        , new Color(51,204,255));
-                                if (respawnTime >= Rock.getRespawnTime(rock.getId())) {
-                                    plugin.getDespawnedRockObjectsTimers().remove(rockLoc);
-                                }
-                            }
-                        }
-                    }
+	@Override
+	public Dimension render(Graphics2D graphics) {
 
 
-                }
+		if (plugin.getRockObjects() != null && plugin.getRockObjects().size() > 0) {
 
-            }
-        }
+			for (GameObject rock : plugin.getRockObjects()) {
+				if (plugin.getRockObjects() != null) {
+					if (rock != null) {
+						if (rock.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()) < 12) {
+							if (rock.getCanvasLocation() != null && Rock.isRock(rock.getId())) {
+									OverlayUtil.renderTextLocation(graphics, new Point(rock.getCanvasLocation().getX(), rock.getCanvasLocation().getY() - 40), Rock.getName(rock) , Color.green);
+									 OverlayUtil.renderImageLocation(graphics, rock.getCanvasLocation(), itemManager.getImage(1275));
+								}
+							}
+						}
+
+					}
+
+				}
+		}
+
+		if (plugin.getDespawnedRockObjects() != null) {
+
+			for (GameObject rock : plugin.getDespawnedRockObjects()) {
+				if (plugin.getDespawnedRockObjects() != null && rock != null) {
+					if (rock.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()) < 12) {
+
+						if (rock.getCanvasLocation() != null) {
+							Long respawnTime = null;
+
+							Point rockLoc = new Point(rock.getWorldLocation().getX(), rock.getWorldLocation().getY());
+							for (Map.Entry<Point, Long> timers : plugin.getDespawnedRockObjectsTimers().entrySet()) {
+
+								if (timers.getKey().getX() == rockLoc.getX() && timers.getKey().getY() == rockLoc.getY()) {
+
+									 respawnTime =  System.currentTimeMillis() - timers.getValue();
+
+									break;
+								}
+							}
+							if (respawnTime != null)
+							{
+								Long editedRespawnTime = ((Rock.getRespawnTime(rock.getId()) - respawnTime) / 1000);
+								OverlayUtil.renderTextLocation(graphics
+										, rock.getCanvasLocation()
+										, Rock.getName(rock) +
+												" respawning in: " +
+												(editedRespawnTime)
+										, new Color(51,204,255));
+								if (respawnTime >= Rock.getRespawnTime(rock.getId())) {
+									plugin.getDespawnedRockObjectsTimers().remove(rockLoc);
+								}
+							}
+						}
+					}
+
+
+				}
+
+			}
+		}
 
 
 
 
-        return null;
-    }
+		return null;
+	}
 
 
 
