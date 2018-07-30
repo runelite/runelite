af4bc0242ffc5029cfe693eefac733951f7cca8d
Jordan Atwood <jordan.atwood423@gmail.com>
2018-07-30 12:24:52 -0700

    achievement diary plugin: Fix Belladonna farming req

--- runelite-client/src/main/java/net/runelite/client/plugins/achievementdiary/diaries/LumbridgeDiaryRequirement.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/achievementdiary/diaries/LumbridgeDiaryRequirement.java
@@ -82,7 +82,7 @@ public class LumbridgeDiaryRequirement extends GenericDiaryRequirement
 		add("Travel from Lumbridge to Edgeville on a Waka Canoe.",
 			new Requirement(Skill.FARMING, 63));
 		add("Pick some Belladonna from the farming patch at Draynor Manor.",
-			new Requirement(Skill.FARMING, 68));
+			new Requirement(Skill.FARMING, 63));
 		add("Light your mining helmet in the Lumbridge castle basement.",
 			new Requirement(Skill.FIREMAKING, 65));
 		add("Recharge your prayer at Clan Wars with Smite activated.",
