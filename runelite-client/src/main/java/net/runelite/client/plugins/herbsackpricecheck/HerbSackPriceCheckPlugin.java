7522919abbabbe856512430086f3e7374698bcf3
Aquivers <jo.twitch0@gmail.com>
2018-08-05 23:02:16 +0200

    Remove wildcard import

--- runelite-client/src/main/java/net/runelite/client/plugins/herbsackpricecheck/HerbSackPriceCheckPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/herbsackpricecheck/HerbSackPriceCheckPlugin.java
@@ -26,7 +26,8 @@ package net.runelite.client.plugins.herbsackpricecheck;
 
 import com.google.common.eventbus.Subscribe;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
+import net.runelite.api.Client;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.MenuOptionClicked;
@@ -79,7 +80,7 @@ public class HerbSackPriceCheckPlugin extends Plugin
 		}
 		gettingHerbs = true;
 	}
-	
+
 	@Subscribe
 	private void onChatMessage(ChatMessage message)
 	{
