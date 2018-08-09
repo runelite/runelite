4ac1dc2c57350960adc8fe83cbca818a7c92fecb
DWhite72 <whitedylan7@gmail.com>
2018-08-09 05:00:20 -0500

    Supplies Tracker

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/suppliestracker/SuppliesTrackRow.java
@@ -0,0 +1,43 @@
+package net.runelite.client.plugins.suppliestracker;
+
+
+import net.runelite.client.game.AsyncBufferedImage;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+
+class SuppliesTrackRow extends JPanel
+{
+
+	int quantity;
+	long totalCost;
+	int itemId;
+
+	SuppliesTrackRow(AsyncBufferedImage icon, String name, int quant, long cost, int id)
+	{
+		//Set variables
+		quantity = quant;
+		totalCost = cost;
+		itemId = id;
+
+		//build components
+		setLayout(new BorderLayout());
+		JLabel iconLabel = new JLabel();
+		icon.addTo(iconLabel);
+		String rowText = "  x " + quant + "   (" + String.format("%,d", cost) + "gp)";
+		JLabel label = new JLabel(rowText);
+		iconLabel.setToolTipText(buildToolTip(name, quant, cost));
+
+		//add components
+		add(label, BorderLayout.CENTER);
+		add(iconLabel, BorderLayout.LINE_START);
+	}
+
+	private String buildToolTip(String name, int quantity, long cost)
+	{
+		return name + " x " + quantity + " (" + String.format("%,d", cost) + "gp)";
+	}
+
+}
