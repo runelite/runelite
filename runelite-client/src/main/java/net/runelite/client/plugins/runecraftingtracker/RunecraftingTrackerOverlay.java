6121901339d815bf1e108578d4cb05970da66246
Henning <henning.ber@gmail.com>
2018-08-09 16:37:53 +0200

    license headers added

--- runelite-client/src/main/java/net/runelite/client/plugins/runecraftingtracker/RunecraftingTrackerOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/runecraftingtracker/RunecraftingTrackerOverlay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Seth <Sethtroll3@gmail.com>
+ * Copyright (c) 2018, Henning Berge <henning@henning.tech>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
