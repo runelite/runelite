4b36e73fe83feb05ffe9d812d7fa236e0d3b7b43
Jason <jbassos@live.com>
2018-08-14 03:35:12 -0400

    Vorkath overlay

--- runelite-client/src/main/java/net/runelite/client/plugins/vorkath/VorkathOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/vorkath/VorkathOverlay.java
@@ -1,5 +1,6 @@
 package net.runelite.client.plugins.vorkath;
 
+import net.runelite.api.Client;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayPriority;
@@ -13,27 +14,27 @@ public class VorkathOverlay extends Overlay {
 
     private final VorkathPlugin plugin;
 
+    private final Client client;
+
     private final PanelComponent panelComponent = new PanelComponent();
 
     @Inject
-    private VorkathOverlay(VorkathPlugin plugin)
-    {
+    private VorkathOverlay(VorkathPlugin plugin, Client client) {
         setPosition(OverlayPosition.TOP_LEFT);
         setPriority(OverlayPriority.HIGHEST);
         this.plugin = plugin;
+        this.client = client;
     }
 
     @Override
     public Dimension render(Graphics2D graphics) {
-        panelComponent.getChildren().clear();
-        panelComponent.getChildren().add(LineComponent.builder()
-                .left("Attack Count")
-                .right(Integer.toString(plugin.getAttackCount()))
-                .build());
-        panelComponent.getChildren().add(LineComponent.builder()
-                .left("Animation ID")
-                .right(Integer.toString(plugin.getAnimationID()))
-                .build());
+        if (client.isInInstancedRegion()) {
+            panelComponent.getChildren().clear();
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Attack Count")
+                    .right(Integer.toString(plugin.getAttackCount()))
+                    .build());
+        }
         return panelComponent.render(graphics);
     }
 }
