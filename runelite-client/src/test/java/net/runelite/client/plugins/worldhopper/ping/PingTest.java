b722e222b9eaad7cafbcf486eac8d63cc794def5
Richard Brown <richardjbrown1@gmail.com>
2018-08-24 14:51:36 -0600

    Platform check for PingTest
    It seems that travis Cl was failing the PingTest because it is running on a linux system.
    Added  a OSType check to break the test if it is not running on a linux system.

--- runelite-client/src/test/java/net/runelite/client/plugins/worldhopper/ping/PingTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/worldhopper/ping/PingTest.java
@@ -4,27 +4,34 @@ import com.sun.jna.Memory;
 import com.sun.jna.Pointer;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import net.runelite.client.util.OSType;
 import org.junit.Test;
 
 public class PingTest
 {
-    @Test
-    public void test() throws UnknownHostException
-    {
-        IPHlpAPI ipHlpAPI = IPHlpAPI.INSTANCE;
-        long ptr = ipHlpAPI.IcmpCreateFile();
-        System.out.println("ptr " + ptr);
-        InetAddress inetAddress = InetAddress.getByName("104.131.36.154");
-        byte[] address = inetAddress.getAddress();
-        String dataStr = "RuneLitePing";
-        int dataLength = dataStr.length()+1;
-        Pointer data = new Memory(dataLength);
-        data.setString(0L, dataStr);
-        IcmpEchoReply icmpEchoReply = new IcmpEchoReply(new Memory(IcmpEchoReply.SIZE + dataLength));
-        int packed = (address[0] & 0xff) | ((address[1] & 0xff)<<8) | ((address[2] & 0xff)<<16) | ((address[3] & 0xff)<<24);
-        int ret = ipHlpAPI.IcmpSendEcho(ptr, packed, data, (short)(dataLength),0L, icmpEchoReply, IcmpEchoReply.SIZE + dataLength, 1000);
-        System.out.println("RET " + ret);
-        System.out.println("RTT " + icmpEchoReply.roundTripTime.longValue());
-        ipHlpAPI.IcmpCloseHandle(ptr);
-    }
+	@Test
+	public void test() throws UnknownHostException
+	{
+		if (OSType.getOSType() != OSType.Windows)
+		{
+			return;
+		}
+		
+		IPHlpAPI ipHlpAPI = IPHlpAPI.INSTANCE;
+		long ptr = ipHlpAPI.IcmpCreateFile();
+		System.out.println("ptr " + ptr);
+		InetAddress inetAddress = InetAddress.getByName("104.131.36.154");
+		byte[] address = inetAddress.getAddress();
+		String dataStr = "RuneLitePing";
+		int dataLength = dataStr.length()+1;
+		Pointer data = new Memory(dataLength);
+		data.setString(0L, dataStr);
+		IcmpEchoReply icmpEchoReply = new IcmpEchoReply(new Memory(IcmpEchoReply.SIZE + dataLength));
+		int packed = (address[0] & 0xff) | ((address[1] & 0xff)<<8) | ((address[2] & 0xff)<<16) | ((address[3] & 0xff)<<24);
+		int ret = ipHlpAPI.IcmpSendEcho(ptr, packed, data, (short)(dataLength),0L, icmpEchoReply, IcmpEchoReply.SIZE + dataLength, 1000);
+		System.out.println("RET " + ret);
+		System.out.println("RTT " + icmpEchoReply.roundTripTime.longValue());
+		ipHlpAPI.IcmpCloseHandle(ptr);
+	}
 }
