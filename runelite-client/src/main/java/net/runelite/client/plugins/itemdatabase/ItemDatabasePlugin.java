6a6fb0399382aca6d64d43e78523fad5dbdf45a1
CC007 <Coolcat_the_best2@hotmail.com>
2018-09-01 23:28:11 +0200

    First commit of Item Database

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemdatabase/ItemDatabasePlugin.java
@@ -0,0 +1,14 @@
+package net.runelite.client.plugins.itemdatabase;
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+	name = "Item Database",
+	description = "Search for items and get all the information about these items.",
+	tags = {"item", "recipe", "info"},
+	enabledByDefault = false
+)
+public class ItemDatabasePlugin extends Plugin
+{
+}
