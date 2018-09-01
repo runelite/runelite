2e398f1837759afb3cec96f33faac6096515ee93
killje <killje@github.com>
2018-09-02 00:53:41 +0200

    Side panel created

--- runelite-client/src/main/java/net/runelite/client/plugins/itemdatabase/ItemDatabasePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemdatabase/ItemDatabasePlugin.java
@@ -1,7 +1,38 @@
+/*
+ * Copyright (c) 2018 Patrick Beuks (killje) <patrick.beuks@gmail.com>
+ * Copyright (c) 2018 CC007 <Coolcat_the_best2@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.itemdatabase;
 
+import javax.inject.Inject;
+import net.runelite.api.ItemID;
+import net.runelite.client.game.AsyncBufferedImage;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
 
 @PluginDescriptor(
 	name = "Item Database",
@@ -11,4 +42,30 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class ItemDatabasePlugin extends Plugin
 {
+	
+	@Inject
+	private ClientToolbar clientToolbar;
+	
+	@Inject
+	private ItemManager itemManager;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		
+		ItemDatabasePanel panel = new ItemDatabasePanel();
+		
+		AsyncBufferedImage icon = itemManager.getImage(ItemID.CLOCKWORK_BOOK);
+		
+		NavigationButton navButton = NavigationButton.builder()
+			.tooltip("Item Database")
+			.icon(icon)
+			.panel(panel)
+			.priority(4)
+			.build();
+
+		clientToolbar.addNavigation(navButton);
+	}
+	
+	
 }
