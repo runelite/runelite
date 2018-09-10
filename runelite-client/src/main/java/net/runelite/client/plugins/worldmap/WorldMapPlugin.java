e11459a48cfe6bb995d10517df76fb98220e8b0b
johnhamilto <john.hamilton@wustl.edu>
2018-09-09 19:37:15 -0500

    this one's for you

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/WorldMapPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/WorldMapPlugin.java
@@ -32,15 +32,20 @@ import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import net.runelite.api.Client;
 import net.runelite.api.Experience;
+import net.runelite.api.GameState;
 import net.runelite.api.Skill;
-import net.runelite.api.Varbits;
 import net.runelite.api.events.ConfigChanged;
 import net.runelite.api.events.ExperienceChanged;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.worldmap.WorldMapPointManager;
 import net.runelite.client.util.ImageUtil;
+
+import static net.runelite.api.widgets.WidgetID.QUEST_COMPLETED_GROUP_ID;
 
 @PluginDescriptor(
 	name = "World Map",
@@ -52,6 +57,7 @@ public class WorldMapPlugin extends Plugin
 	static final BufferedImage BLANK_ICON;
 	static final BufferedImage FAIRY_TRAVEL_ICON;
 	static final BufferedImage NOPE_ICON;
+	static final BufferedImage HIGHLIGHT_ICON;
 
 	static final String CONFIG_KEY = "worldmap";
 	static final String CONFIG_KEY_FAIRY_RING_TOOLTIPS = "fairyRingTooltips";
@@ -82,10 +88,17 @@ public class WorldMapPlugin extends Plugin
 		NOPE_ICON = new BufferedImage(iconBufferSize, iconBufferSize, BufferedImage.TYPE_INT_ARGB);
 		final BufferedImage nopeImage = ImageUtil.getResourceStreamFromClass(WorldMapPlugin.class, "nope_icon.png");
 		NOPE_ICON.getGraphics().drawImage(nopeImage, 1, 1, null);
+
+		HIGHLIGHT_ICON = new BufferedImage(iconBufferSize, iconBufferSize, BufferedImage.TYPE_INT_ARGB);
+		final BufferedImage highlightImage = ImageUtil.getResourceStreamFromClass(WorldMapPlugin.class, "quest_highlight_icon.png");
+		HIGHLIGHT_ICON.getGraphics().drawImage(highlightImage, 1, 1, null);
 	}
 
 	@Inject
 	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
 
 	@Inject
 	private WorldMapConfig config;
@@ -137,18 +150,17 @@ public class WorldMapPlugin extends Plugin
 					}
 					break;
 				case CONFIG_KEY_QUEST_START_TOOLTIPS:
+				case CONFIG_KEY_QUEST_START_ICON:
+					worldMapPointManager.removeIf(QuestStartPoint.class::isInstance);
+
 					if (config.questStartTooltips())
 					{
+						System.out.println("configs");
 						Arrays.stream(QuestStartLocation.values())
-							.map(value -> new QuestStartPoint(value, BLANK_ICON))
+							.map(value -> new QuestStartPoint(value, config.questStartIcon() && !value.isComplete() ? HIGHLIGHT_ICON : BLANK_ICON))
 							.forEach(worldMapPointManager::add);
 					}
-					else
-					{
-						worldMapPointManager.removeIf(QuestStartPoint.class::isInstance);
-					}
-				case CONFIG_KEY_QUEST_START_ICON:
-					// TODO
+					break;
 				case CONFIG_KEY_NORMAL_TELEPORT_ICON:
 				case CONFIG_KEY_ANCIENT_TELEPORT_ICON:
 				case CONFIG_KEY_LUNAR_TELEPORT_ICON:
@@ -223,6 +235,46 @@ public class WorldMapPlugin extends Plugin
 					.map(value -> new AgilityShortcutPoint(value, config.agilityShortcutLevelIcon() && value.getLevelReq() > agilityLevel ? NOPE_ICON : BLANK_ICON))
 					.forEach(worldMapPointManager::add);
 			}
+		}
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState() == GameState.LOGGED_IN)
+		{
+			clientThread.invokeLater(() ->
+			{
+				for (QuestStartLocation qsL : QuestStartLocation.values())
+				{
+					boolean complete = true;
+					for (int[] quest : qsL.getQuests())
+					{
+						client.runScript(quest[0], quest[1]);
+						if (client.getIntStack()[client.getIntStackSize() - 1] != 2)
+						{
+							complete = false;
+							System.out.println(qsL.name() + "not complete");
+						}
+					}
+					qsL.setComplete(complete);
+				}
+
+				worldMapPointManager.removeIf(QuestStartPoint.class::isInstance);
+				Arrays.stream(QuestStartLocation.values()).map(value -> new QuestStartPoint(value, config.questStartIcon() && !value.isComplete() ? HIGHLIGHT_ICON : BLANK_ICON)).forEach(worldMapPointManager::add);
+			});
+		}
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded event)
+	{
+		if (event.getGroupId() == QUEST_COMPLETED_GROUP_ID && config.questStartIcon())
+		{
+			worldMapPointManager.removeIf(QuestStartPoint.class::isInstance);
+			Arrays.stream(QuestStartLocation.values())
+				.map(value -> new QuestStartPoint(value, config.questStartIcon() && !value.isComplete() ? HIGHLIGHT_ICON : BLANK_ICON))
+				.forEach(worldMapPointManager::add);
 		}
 	}
 
