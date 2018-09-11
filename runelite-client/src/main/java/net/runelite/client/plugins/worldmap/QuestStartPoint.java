50ab567f3a051274fe94b48dfca4273485a0491f
johnhamilto <john.hamilton@wustl.edu>
2018-09-11 03:26:32 -0500

    requested changes3

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/QuestStartPoint.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/QuestStartPoint.java
@@ -26,7 +26,6 @@
 package net.runelite.client.plugins.worldmap;
 
 import net.runelite.client.ui.overlay.worldmap.WorldMapPoint;
-
 import java.awt.image.BufferedImage;
 
 public class QuestStartPoint extends WorldMapPoint
