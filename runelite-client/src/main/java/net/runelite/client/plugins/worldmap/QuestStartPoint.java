4224c6671ec3bcb9bd5e14012284169509a8a1b8
johnhamilto <john.hamilton@wustl.edu>
2018-09-11 03:25:01 -0500

    requested changes

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/QuestStartPoint.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/QuestStartPoint.java
@@ -25,7 +25,6 @@
  */
 package net.runelite.client.plugins.worldmap;
 
-import lombok.Getter;
 import net.runelite.client.ui.overlay.worldmap.WorldMapPoint;
 
 import java.awt.image.BufferedImage;
@@ -33,14 +32,10 @@ import java.awt.image.BufferedImage;
 public class QuestStartPoint extends WorldMapPoint
 {
 
-	@Getter
-	private QuestStartLocation data;
-
 	QuestStartPoint(QuestStartLocation data, BufferedImage icon)
 	{
 		super(data.getLocation(), icon);
 
-		this.data = data;
 		setTooltip(data.getTooltip());
 	}
 
