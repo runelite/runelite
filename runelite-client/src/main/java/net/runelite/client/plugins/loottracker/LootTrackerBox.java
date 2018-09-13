4dd8322121e190cca16a22f0a865ecf721b36223
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2018-09-13 01:03:50 +0100

    loot tracker: add view for grouping loot by npc/event

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerBox.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerBox.java
@@ -29,6 +29,10 @@ import com.google.common.base.Strings;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.Nullable;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -39,14 +43,26 @@ import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.util.StackFormatter;
 
-@Getter
 class LootTrackerBox extends JPanel
 {
 	private static final int ITEMS_PER_ROW = 5;
-	private final long totalPrice;
+	private final JPanel itemContainer = new JPanel();
+	private final JLabel priceLabel = new JLabel();
+	private final JLabel subTitleLabel = new JLabel();
+	private final ItemManager itemManager;
+	private final String id;
 
-	LootTrackerBox(final ItemManager itemManager, final String title, final String subTitle, final LootTrackerItemEntry[] items)
+	@Getter
+	private final List<LootTrackerRecord> records = new ArrayList<>();
+
+	@Getter
+	private long totalPrice;
+
+	LootTrackerBox(final ItemManager itemManager, final String id, @Nullable final String subtitle)
 	{
+		this.id = id;
+		this.itemManager = itemManager;
+
 		setLayout(new BorderLayout(0, 1));
 		setBorder(new EmptyBorder(5, 0, 0, 0));
 
@@ -54,43 +70,140 @@ class LootTrackerBox extends JPanel
 		logTitle.setBorder(new EmptyBorder(7, 7, 7, 7));
 		logTitle.setBackground(ColorScheme.DARKER_GRAY_COLOR.darker());
 
-		final JLabel titleLabel = new JLabel(title);
+		final JLabel titleLabel = new JLabel(id);
 		titleLabel.setFont(FontManager.getRunescapeSmallFont());
 		titleLabel.setForeground(Color.WHITE);
 
 		logTitle.add(titleLabel, BorderLayout.WEST);
 
 		// If we have subtitle, add it
-		if (!Strings.isNullOrEmpty(subTitle))
+		if (!Strings.isNullOrEmpty(subtitle))
 		{
-			final JLabel subTitleLabel = new JLabel(subTitle);
+			subTitleLabel.setText(subtitle);
 			subTitleLabel.setFont(FontManager.getRunescapeSmallFont());
 			subTitleLabel.setForeground(ColorScheme.LIGHT_GRAY_COLOR);
 			logTitle.add(subTitleLabel, BorderLayout.CENTER);
 		}
 
-		totalPrice = calculatePrice(items);
+		priceLabel.setFont(FontManager.getRunescapeSmallFont());
+		priceLabel.setForeground(ColorScheme.LIGHT_GRAY_COLOR);
+		logTitle.add(priceLabel, BorderLayout.EAST);
 
-		if (totalPrice > 0)
+		add(logTitle, BorderLayout.NORTH);
+		add(itemContainer, BorderLayout.CENTER);
+	}
+
+	int getTotalKills()
+	{
+		return records.size();
+	}
+
+	/**
+	 * Checks if this box matches specified record
+	 * @param record loot record
+	 * @return true if match is made
+	 */
+	boolean matches(final LootTrackerRecord record)
+	{
+		return record.getTitle().equals(id);
+	}
+
+	/**
+	 * Checks if this box matches specified id
+	 * @param id other record id
+	 * @return true if match is made
+	 */
+	boolean matches(final String id)
+	{
+		if (id == null)
 		{
-			final JLabel priceLabel = new JLabel(StackFormatter.quantityToStackSize(totalPrice) + " gp");
-			priceLabel.setFont(FontManager.getRunescapeSmallFont());
-			priceLabel.setForeground(ColorScheme.LIGHT_GRAY_COLOR);
-			logTitle.add(priceLabel, BorderLayout.EAST);
+			return true;
 		}
 
+		return this.id.equals(id);
+	}
+
+	/**
+	 * Adds an record's data into a loot box.
+	 * This will add new items to the list, re-calculating price and kill count.
+	 */
+	void combine(final LootTrackerRecord record)
+	{
+		if (!matches(record))
+		{
+			throw new IllegalArgumentException(record.toString());
+		}
+
+		records.add(record);
+		buildItems();
+
+		priceLabel.setText(StackFormatter.quantityToStackSize(totalPrice) + " gp");
+		if (records.size() > 1)
+		{
+			subTitleLabel.setText("x " + records.size());
+		}
+
+		repaint();
+	}
+
+	/**
+	 * This method creates stacked items from the item list, calculates total price and then
+	 * displays all the items in the UI.
+	 */
+	private void buildItems()
+	{
+		final List<LootTrackerItem> allItems = new ArrayList<>();
+		final List<LootTrackerItem> items = new ArrayList<>();
+		totalPrice = 0;
+
+		for (LootTrackerRecord records : records)
+		{
+			allItems.addAll(Arrays.asList(records.getItems()));
+		}
+
+		for (final LootTrackerItem entry : allItems)
+		{
+			totalPrice += entry.getPrice();
+
+			int quantity = 0;
+			for (final LootTrackerItem i : items)
+			{
+				if (i.getId() == entry.getId())
+				{
+					quantity = i.getQuantity();
+					items.remove(i);
+					break;
+				}
+			}
+			if (quantity > 0)
+			{
+				int newQuantity = entry.getQuantity() + quantity;
+				long pricePerItem = entry.getPrice() == 0 ? 0 : (entry.getPrice() / entry.getQuantity());
+
+				items.add(new LootTrackerItem(entry.getId(), entry.getName(), newQuantity, pricePerItem * newQuantity));
+			}
+			else
+			{
+				items.add(entry);
+			}
+		}
+
+		items.sort((i1, i2) -> Long.compare(i2.getPrice(), i1.getPrice()));
+
 		// Calculates how many rows need to be display to fit all items
-		final int rowSize = ((items.length % ITEMS_PER_ROW == 0) ? 0 : 1) + items.length / ITEMS_PER_ROW;
-		final JPanel itemContainer = new JPanel(new GridLayout(rowSize, ITEMS_PER_ROW, 1, 1));
+		final int rowSize = ((items.size() % ITEMS_PER_ROW == 0) ? 0 : 1) + items.size() / ITEMS_PER_ROW;
+
+		itemContainer.removeAll();
+		itemContainer.setLayout(new GridLayout(rowSize, ITEMS_PER_ROW, 1, 1));
 
 		for (int i = 0; i < rowSize * ITEMS_PER_ROW; i++)
 		{
 			final JPanel slotContainer = new JPanel();
 			slotContainer.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 
-			if (i < items.length)
+			if (i < items.size())
 			{
-				final LootTrackerItemEntry item = items[i];
+				final LootTrackerItem item = items.get(i);
 				final JLabel imageLabel = new JLabel();
 				imageLabel.setToolTipText(buildToolTip(item));
 				imageLabel.setVerticalAlignment(SwingConstants.CENTER);
@@ -102,26 +215,14 @@ class LootTrackerBox extends JPanel
 			itemContainer.add(slotContainer);
 		}
 
-		add(logTitle, BorderLayout.NORTH);
-		add(itemContainer, BorderLayout.CENTER);
+		itemContainer.repaint();
 	}
 
-	private String buildToolTip(LootTrackerItemEntry item)
+	private static String buildToolTip(LootTrackerItem item)
 	{
 		final String name = item.getName();
 		final int quantity = item.getQuantity();
 		final long price = item.getPrice();
-
 		return name + " x " + quantity + " (" + StackFormatter.quantityToStackSize(price) + ")";
-	}
-
-	private static long calculatePrice(final LootTrackerItemEntry[] itemStacks)
-	{
-		long total = 0;
-		for (LootTrackerItemEntry itemStack : itemStacks)
-		{
-			total += itemStack.getPrice();
-		}
-		return total;
 	}
 }
