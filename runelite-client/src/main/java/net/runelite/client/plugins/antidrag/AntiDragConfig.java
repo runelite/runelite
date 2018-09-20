2f7c174167b1daeebb8f0b83ab12285f183e0c8d
Redeb <https://github.com/Redeb>
2018-09-20 13:33:12 +0100

    Reverting anti-drag changes

--- runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragConfig.java
@@ -28,17 +28,35 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("antiDrag")
+@ConfigGroup(AntiDragPlugin.CONFIG_GROUP)
 public interface AntiDragConfig extends Config
 {
 	@ConfigItem(
-		keyName = "dragDelay",
-		name = "Drag Delay",
-		description = "Configures the inventory drag delay in client ticks (20ms)",
-		position = 1
+	  keyName = "dragDelay",
+	  name = "Drag Delay",
+	  description = "Configures the inventory drag delay in client ticks (20ms)",
+	  position = 1
 	)
 	default int dragDelay()
 	{
 		return 600 / 20; // one game tick
+	}
+
+	@ConfigItem(
+	  keyName = "dragDelay",
+	  name = "",
+	  description = ""
+	)
+	void dragDelay(int delay);
+
+	@ConfigItem(
+	  keyName = "onShiftOnly",
+	  name = "On Shift Only",
+	  description = "Configures whether to only adjust the delay while holding shift",
+	  position = 2
+	)
+	default boolean onShiftOnly()
+	{
+		return true;
 	}
 }
