3b805b11c7410cd5be6b917364524bb447c3862e
Xandayn <xandayn@gmail.com>
2018-09-20 22:01:17 -0700

    [PlayerInfo Plugin] Fixed some formatting issues

--- runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerInfoConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerInfoConfig.java
@@ -9,80 +9,80 @@ import java.awt.*;
 @ConfigGroup("playerinfo")
 public interface PlayerInfoConfig extends Config
 {
-    @ConfigItem(
-            keyName = "enableHealth",
-            name = "Enable Health Display",
-            description = "Configures whether or not to display health information",
-            position = 1
-    )
-    default boolean enableHealth()
-    {
-        return true;
-    }
+	@ConfigItem(
+		keyName = "enableHealth",
+		name = "Enable Health Display",
+		description = "Configures whether or not to display health information",
+		position = 1
+	)
+	default boolean enableHealth()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "enablePrayer",
-            name = "Enable Prayer Display",
-            description = "Configures whether or not to display prayer information",
-            position = 2
-    )
-    default boolean enablePrayer()
-    {
-        return true;
-    }
+	@ConfigItem(
+		keyName = "enablePrayer",
+		name = "Enable Prayer Display",
+		description = "Configures whether or not to display prayer information",
+		position = 2
+	)
+	default boolean enablePrayer()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "enableEnergy",
-            name = "Enable Run Energy Display",
-            description = "Configures whether or not to display run energy information",
-            position = 3
-    )
-    default boolean enableEnergy()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "enableEnergy",
+			name = "Enable Run Energy Display",
+			description = "Configures whether or not to display run energy information",
+			position = 3
+	)
+	default boolean enableEnergy()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "enableSpec",
-            name = "Enable Special Attack Display",
-            description = "Configures whether or not to display special attack information",
-            position = 4
-    )
-    default boolean enableSpec()
-    {
-        return true;
-    }
+	@ConfigItem(
+		keyName = "enableSpec",
+		name = "Enable Special Attack Display",
+		description = "Configures whether or not to display special attack information",
+		position = 4
+	)
+	default boolean enableSpec()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "colorHigh",
-            name = "Color High",
-            description = "The color displayed for high values.",
-            position = 5
-    )
-    default Color colorHigh()
-    {
-        return Color.GREEN;
-    }
+	@ConfigItem(
+		keyName = "colorHigh",
+		name = "Color High",
+		description = "The color displayed for high values.",
+		position = 5
+	)
+	default Color colorHigh()
+	{
+		return Color.GREEN;
+	}
 
-    @ConfigItem(
-            keyName = "colorMed",
-            name = "Color Medium",
-            description = "The color displayed for medium values.",
-            position = 6
-    )
-    default Color colorMed()
-    {
-        return Color.YELLOW;
-    }
+	@ConfigItem(
+		keyName = "colorMed",
+		name = "Color Medium",
+		description = "The color displayed for medium values.",
+		position = 6
+	)
+	default Color colorMed()
+	{
+		return Color.YELLOW;
+	}
 
-    @ConfigItem(
-            keyName = "colorLow",
-            name = "Color Low",
-            description = "The color displayed for low values.",
-            position = 7
-    )
-    default Color colorLow()
-    {
-        return Color.RED;
-    }
+	@ConfigItem(
+		keyName = "colorLow",
+		name = "Color Low",
+		description = "The color displayed for low values.",
+		position = 7
+	)
+	default Color colorLow()
+	{
+		return Color.RED;
+	}
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerInfoPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/playerinfo/PlayerInfoPlugin.java
@@ -18,65 +18,65 @@ import java.awt.image.BufferedImage;
 import static net.runelite.client.plugins.playerinfo.PlayerInfoCustomIndicator.*;
 
 @PluginDescriptor(
-        name = "Player Information",
-        description = "An alternative to the Minimap Orbs",
-        tags = {"combat", "overlay"},
-        enabledByDefault = false
+	name = "Player Information",
+	description = "An alternative to the Minimap Orbs",
+	tags = {"combat", "overlay"},
+	enabledByDefault = false
 )
 @Singleton
 public class PlayerInfoPlugin extends Plugin
 {
-    @Inject
-    private Client client;
+	@Inject
+	private Client client;
 
-    @Inject
-    private ClientThread clientThread;
+	@Inject
+	private ClientThread clientThread;
 
-    @Inject
-    private InfoBoxManager infoBoxManager;
+	@Inject
+	private InfoBoxManager infoBoxManager;
 
-    @Inject
-    private PlayerInfoConfig config;
+	@Inject
+	private PlayerInfoConfig config;
 
-    @Inject
-    private SpriteManager spriteManager;
+	@Inject
+	private SpriteManager spriteManager;
 
-    @Inject
-    private SkillIconManager skillIconManager;
+	@Inject
+	private SkillIconManager skillIconManager;
 
-    @Provides
-    PlayerInfoConfig provideConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(PlayerInfoConfig.class);
-    }
+	@Provides
+	PlayerInfoConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PlayerInfoConfig.class);
+	}
 
-    @Override
-    protected void startUp() throws Exception
-    {
-        clientThread.invoke(() ->
-        {
-            if (client.getGameState().ordinal() < GameState.LOGIN_SCREEN.ordinal())
-            {
-                return false;
-            }
+	@Override
+	protected void startUp() throws Exception
+	{
+		clientThread.invoke(() ->
+		{
+			if (client.getGameState().ordinal() < GameState.LOGIN_SCREEN.ordinal())
+			{
+				return false;
+			}
 
-            BufferedImage healthImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_HITPOINTS_ICON, 0);
-            BufferedImage prayerImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_PRAYER_ICON, 0);
-            BufferedImage energyImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_RUN_ICON, 0);
-            BufferedImage combatImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_SPECIAL_ICON, 0);
+			BufferedImage healthImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_HITPOINTS_ICON, 0);
+			BufferedImage prayerImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_PRAYER_ICON, 0);
+			BufferedImage energyImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_RUN_ICON, 0);
+			BufferedImage combatImg = spriteManager.getSprite(SpriteID.MINIMAP_ORB_SPECIAL_ICON, 0);
 
-            infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(healthImg, this, config, client, IndicatorType.HEALTH));
-            infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(prayerImg, this, config, client, IndicatorType.PRAYER));
-            infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(energyImg, this, config, client, IndicatorType.ENERGY));
-            infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(combatImg, this, config, client, IndicatorType.SPECIAL));
+			infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(healthImg, this, config, client, IndicatorType.HEALTH));
+			infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(prayerImg, this, config, client, IndicatorType.PRAYER));
+			infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(energyImg, this, config, client, IndicatorType.ENERGY));
+			infoBoxManager.addInfoBox(new PlayerInfoCustomIndicator(combatImg, this, config, client, IndicatorType.SPECIAL));
 
-            return true;
-        });
-    }
+			return true;
+		});
+	}
 
-    @Override
-    protected void shutDown() throws Exception
-    {
-        infoBoxManager.removeIf(i -> i instanceof PlayerInfoCustomIndicator);
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
+		infoBoxManager.removeIf(i -> i instanceof PlayerInfoCustomIndicator);
+	}
 }
