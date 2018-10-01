b3d280349c07aeb4faadffa4082afd735dae8812
Unknown <tannermjelde@gmail.com>
2018-10-01 10:43:34 -0700

    Remove 'Take' option from items on the ignored list.
    Edit description of Remove Ignored option

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
@@ -76,6 +76,7 @@ import static net.runelite.client.plugins.grounditems.config.MenuHighlightMode.N
 import static net.runelite.client.plugins.grounditems.config.MenuHighlightMode.OPTION;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ColorUtil;
+import net.runelite.client.util.Text;
 
 @PluginDescriptor(
 	name = "Ground Items",
@@ -393,9 +394,38 @@ public class GroundItemsPlugin extends Plugin
 			{
 				lastEntry.setTarget(lastEntry.getTarget() + " (" + quantity + ")");
 			}
+			if(config.removeIgnored() && event.getOption().equals("Take") && hiddenItemList.contains(Text.removeTags(event.getTarget())))
+			{
+				menuEntries = removeOption(event.getOption(), event.getTarget());
+			}
 
 			client.setMenuEntries(menuEntries);
 		}
+	}
+
+	private MenuEntry[] removeOption(String option, String target)
+	{
+		MenuEntry[] entries = client.getMenuEntries();
+		int j = 0;
+		if(entries.length > 1)
+		{
+			MenuEntry[] newEntries = new MenuEntry[entries.length - 1];
+			for (int i = 0; i < entries.length; ++i)
+			{
+				if (!(entries[i].getOption().equals(option) && entries[i].getTarget().equals(target)))
+				{
+					newEntries[j++] = entries[i];
+				}
+			}
+
+			return newEntries;
+		}
+		else
+		{
+			return entries;
+		}
+
+
 	}
 
 	void updateList(String item, boolean hiddenList)
