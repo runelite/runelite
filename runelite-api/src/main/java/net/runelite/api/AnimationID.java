3d39a358fe2572b31d1efa14816810adea3f723c
Tomas Slusny <slusnucky@gmail.com>
2018-10-21 20:38:24 +0200

    Add POH incense burner timers

--- runelite-api/src/main/java/net/runelite/api/AnimationID.java
+++ runelite-api/src/main/java/net/runelite/api/AnimationID.java
@@ -182,4 +182,7 @@ public final class AnimationID
 	public static final int ROCKSLUG_DEATH = 1568;
 	public static final int ZYGOMITE_DEATH = 3327;
 	public static final int IMP_DEATH = 172;
+
+	// POH Animations
+	public static final int INCENSE_BURNER = 3687;
 }
