71de57652367eb99b98bf149a1dc3119e537137f
forsco <github@forsco.net>
2018-10-24 23:51:34 +1100

    Add base plugin and NPC Loot Log

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/discordwebhook/DiscordWebhookPlugin.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2018, Forsco <https://github.com/forsco>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.runelite.client.plugins.discordwebhook;
+
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Provides;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.ItemComposition;
+import net.runelite.api.NPC;
+import net.runelite.api.WorldType;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.vars.AccountType;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.events.NpcLootReceived;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.ItemStack;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.http.api.discord.DiscordClient;
+import net.runelite.http.api.discord.DiscordEmbed;
+import net.runelite.http.api.discord.DiscordMessage;
+import net.runelite.http.api.discord.embed.AuthorEmbed;
+import net.runelite.http.api.discord.embed.FieldEmbed;
+import net.runelite.http.api.discord.embed.FooterEmbed;
+import okhttp3.HttpUrl;
+
+@PluginDescriptor(
+	name = "Discord Logger",
+	description = "Configure events to be posted to a Discord channel via a webhook",
+	tags = {"discord", "webhook", "log", "logger"}
+)
+public class DiscordWebhookPlugin extends Plugin
+{
+	private static final DiscordClient CLIENT = new DiscordClient();
+	private static final String ICONBASEURL = "https://static.runelite.net/cache/item/icon/"; // Add item id + ".png"
+
+	private String iconUrl;
+	private boolean checkAccount;
+	private AccountType accountType;
+
+	private HttpUrl url;
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private DiscordWebhookConfig config;
+
+	@Inject
+	private ItemManager itemManager;
+
+	@Inject
+	private ScheduledExecutorService executorService;
+
+	@Provides
+	DiscordWebhookConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(DiscordWebhookConfig.class);
+	}
+
+	@Override
+	protected void startUp()
+	{
+		discordInit();
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		switch (event.getGameState())
+		{
+			case LOGIN_SCREEN:
+			case LOGGED_IN:
+				checkAccount = true;
+				break;
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick tick)
+	{
+		if (!checkAccount)
+		{
+			return;
+		}
+
+		checkAccount = false;
+		accountType = client.getAccountType();
+
+		if (!client.getWorldType().equals(WorldType.DEADMAN) || !client.getWorldType().equals(WorldType.SEASONAL_DEADMAN))
+		{
+			switch (accountType)
+			{
+				case NORMAL:
+					iconUrl = ICONBASEURL + 1038 + ".png";
+					break;
+				case IRONMAN:
+					iconUrl = "https://raw.githubusercontent.com/runelite/runelite/master/runelite-client/src/main/resources/net/runelite/client/plugins/hiscore/ironman.png";
+					break;
+				case HARDCORE_IRONMAN:
+					iconUrl = "https://raw.githubusercontent.com/runelite/runelite/master/runelite-client/src/main/resources/net/runelite/client/plugins/hiscore/hardcore_ironman.png";
+					break;
+				case ULTIMATE_IRONMAN:
+					iconUrl = "https://raw.githubusercontent.com/runelite/runelite/master/runelite-client/src/main/resources/net/runelite/client/plugins/hiscore/ultimate_ironman.png";
+					break;
+			}
+		}
+
+		if (client.getWorldType().equals(WorldType.DEADMAN) || client.getWorldType().equals(WorldType.SEASONAL_DEADMAN))
+		{
+			iconUrl = "https://raw.githubusercontent.com/runelite/runelite/master/runelite-client/src/main/resources/net/runelite/client/plugins/hiscore/seasonal_deadman.png";
+		}
+	}
+
+	@Subscribe
+	public void npcLootReceived(final NpcLootReceived npcLootReceived)
+	{
+		if (config.lootLogType().equals(LootLogType.NONE) || !url.isHttps())
+		{
+			return;
+		}
+
+		if (config.lootLogType().equals(LootLogType.ALL) && url.pathSize() > 1)
+		{
+			final NPC npc = npcLootReceived.getNpc();
+			final String npcName = npc.getName();
+			final Collection<ItemStack> items = npcLootReceived.getItems();
+
+			List lootList = new ArrayList();
+
+			for (ItemStack item : items)
+			{
+				final int itemId = item.getId();
+				final ItemComposition itemComposition = itemManager.getItemComposition(itemId);
+				final String itemName = itemComposition.getName();
+				final int itemQuantity = item.getQuantity();
+				final int gePrice = itemManager.getItemPrice(itemId);
+
+				if ((itemQuantity * gePrice) > config.getMinLootValue())
+				{
+					lootList.add(FieldEmbed.builder()
+					.name(itemName + " x " + itemQuantity)
+					.value((itemQuantity * gePrice) + " gp")
+					.build());
+				}
+			}
+
+			if (!lootList.isEmpty())
+			{
+				DiscordEmbed npcLootRecord = DiscordEmbed.builder()
+					.author(AuthorEmbed.builder()
+						.icon_url("")
+						.name(npcName)
+						.build())
+					.description("has been slain for:")
+					.fields(lootList)
+					.footer(FooterEmbed.builder()
+						.icon_url(iconUrl)
+						.text(client.getLocalPlayer().getName())
+						.build())
+					.build();
+
+				DiscordMessage discordMessage = new DiscordMessage("Runelite", "", "");
+				discordMessage.getEmbeds().add(npcLootRecord);
+				CLIENT.message(url, discordMessage);
+				lootList.clear();
+			}
+		}
+	}
+
+	private void discordInit()
+	{
+		url = HttpUrl.parse(config.getDiscordUrl());
+	}
+}
