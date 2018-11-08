7d9d898c6c743f79019ddd51930e1061aab1564a
mmb70 <mmb70@duke.edu>
2018-11-08 15:19:27 -0500

    Fixed maniacal monkey hunting trap bug

--- runelite-client/src/main/java/net/runelite/client/plugins/hunter/HunterPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/hunter/HunterPlugin.java
@@ -128,7 +128,7 @@ public class HunterPlugin extends Plugin
 
 			case ObjectID.MONKEY_TRAP: // Maniacal monkey trap placed
 				// If player is right next to "object" trap assume that player placed the trap
-				if (localPlayer.getWorldLocation().distanceTo(trapLocation) <= 1)
+				if (localPlayer.getWorldLocation().distanceTo(trapLocation) <= 2)
 				{
 					log.debug("Trap placed by \"{}\" on {}", localPlayer.getName(), trapLocation);
 					traps.put(trapLocation, new HunterTrap(gameObject));
