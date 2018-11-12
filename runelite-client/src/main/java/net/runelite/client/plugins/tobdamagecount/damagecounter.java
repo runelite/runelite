5cb9e9e9d3348e85e517de10d6e4d4a17d614d61
akarhi <bchau300@gmail.com>
2018-11-12 16:28:37 -0500

    Add files via upload

--- runelite-client/src/main/java/net/runelite/client/plugins/tobdamagecount/damagecounter.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/tobdamagecount/damagecounter.java
@@ -24,10 +24,9 @@
  */
 
 package net.runelite.client.plugins.tobdamagecount;
-
+ 
 import com.google.common.eventbus.Subscribe;
 import javax.inject.Inject;
-import java.text.DecimalFormat;
 import net.runelite.api.Actor;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
@@ -36,14 +35,9 @@ import net.runelite.api.Skill;
 import net.runelite.api.NPC;
 import net.runelite.api.NpcID;
 import net.runelite.api.Player;
-import net.runelite.api.EquipmentInventorySlot;
-import net.runelite.api.ItemID;
-import net.runelite.api.ItemComposition;
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.HitsplatApplied;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.NpcDespawned;
+import net.runelite.api.events.ActorDespawned;
 import net.runelite.api.events.LocalPlayerDeath;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
@@ -52,268 +46,205 @@ import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
-
 @PluginDescriptor(
-		name = "ToB Damage Counter",
-		description = "Gives you an estimation damage on a boss and taken after the fight is done" +
-				"the damage will be posted in the chat",
-		tags = {"combat", "npcs", "tob", "damage"},
-		enabledByDefault = false
+   name = "ToB Damage Counter",
+   description = "Gives you an estimation damage on a boss after the fight is done" +
+      "the damage will be posted in chat",
+   tags = {"combat", "npcs", "tob", "damage"},
+   enabledByDefault = false
 )
 
-public class damagecounter extends Plugin
+public class damagecounter extend Plugin
 {
-	private int currentWorld = -1;
-	private int DamageCount = 0;
-	private int currenthpxp = -1; // checking the current hp xp so be easier to find
-	private String BossName = null; //to ID the boss to calculate the damage
-	private int DamageTaken = 0;
-	private boolean status = true; //default boolean alive = true, dead = false
-	//formatting the number for damage taken and dealt with to look beeter
-	private static final DecimalFormat DAMAGEFORMAT = new DecimalFormat("#,###");
-	private static final double XP_RATIO = 1.3333;
-	private static final double BOSS_MODIFIER = 1.05;
-	private static final double SCYTHE_MODIFIER = .70;
-	private static final boolean ALIVE = true; //
-	private static final boolean DEAD = false; //if they're dead they cannot "recreate" the message of being alive
-	//locations at ToB
-	private static final int MAIDEN_REGION = 12613;
-	private static final int MAIDEN_REGION_1 = 12869;
-	private static final int BLOAT_REGION = 13125;
-	private static final int NYLOCAS_REGION = 13122;
-	private static final int SOTETSEG_REGION = 13123;
-	private static final int XARPUS_REGION = 12612;
-	private static final int VERZIK_REGION = 12611;
-	private static final int[] ToB_Region = {MAIDEN_REGION, MAIDEN_REGION_1, BLOAT_REGION, NYLOCAS_REGION,
-			SOTETSEG_REGION, XARPUS_REGION, VERZIK_REGION};
-
-	//setting up the array for a check list
-	private static int[] NPCARRAY = {NpcID.THE_MAIDEN_OF_SUGADINTI, NpcID.THE_MAIDEN_OF_SUGADINTI_8361,
-			NpcID.THE_MAIDEN_OF_SUGADINTI_8362, NpcID.THE_MAIDEN_OF_SUGADINTI_8363, NpcID.THE_MAIDEN_OF_SUGADINTI_8364,
-			NpcID.THE_MAIDEN_OF_SUGADINTI_8365, NpcID.PESTILENT_BLOAT, NpcID.NYLOCAS_VASILIAS,
-			NpcID.NYLOCAS_VASILIAS_8355, NpcID.NYLOCAS_VASILIAS_8356, NpcID.NYLOCAS_VASILIAS_8357, NpcID.SOTETSEG,
-			NpcID.SOTETSEG_8388, NpcID.XARPUS, NpcID.XARPUS_8339, NpcID.XARPUS_8340, NpcID.XARPUS_8341,
-			NpcID.VERZIK_VITUR, NpcID.VERZIK_VITUR_8369, NpcID.VERZIK_VITUR_8370, NpcID.VERZIK_VITUR_8371,
-			NpcID.VERZIK_VITUR_8372, NpcID.VERZIK_VITUR_8373, NpcID.VERZIK_VITUR_8374, NpcID.VERZIK_VITUR_8375};
-
-	@Inject
-	private Client client;
-	@Inject
-	private ChatMessageManager chatMessangerManager;
-	//every game tick it will go through methods
-	@Subscribe
-	private void onGameTick(GameTick tick)
-	{
-		if (client.getGameState() != GameState.LOGGED_IN)
-		{
-			ResetCounter();
-			return;
-		}
-		checkInterAction();
-		DamageCounting();
-		currenthpxp = client.getSkillExperience(Skill.HITPOINTS);
-	}
-	//checks for npcID and put the boss name into a string be easier to ID it
-	//once the boss is found it will never check it
-	private void checkInterAction()
-	{
-		Player localPlayer = client.getLocalPlayer();
-		Actor interacting = localPlayer.getInteracting();
-		if (client.getGameState() == GameState.LOGGED_IN)
-		{
-			if (BossName == null)
-			{
-				if (interacting instanceof NPC)
-				{
-					int interactingId = ((NPC) interacting).getId();
-					String interactingName = interacting.getName();
-					for (int aNPCARRAY : NPCARRAY)
-					{
-						if (aNPCARRAY == interactingId)
-						{
-							BossName = interactingName;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@Subscribe
-	//if you hop it will reset the counter
-	public void onGameStateChanged(GameStateChanged event)
-	{
-		if (event.getGameState() == GameState.LOGGED_IN)
-		{
-			if (currentWorld == -1)
-			{
-				currentWorld = client.getWorld();
-			}
-			else if (currentWorld != client.getWorld())
-			{
-				currentWorld = client.getWorld();
-				ResetCounter();
-			}
-		}
-	}
-
-	//grabbing the xp and calculating the damage
-	private int XPtoDamage()
-	{
-		int NewXp;
-		double damageOutput = 0;
-		int XPdrop;
-		if (currenthpxp != -1)
-		{
-			XPdrop = client.getSkillExperience(Skill.HITPOINTS);
-			NewXp = XPdrop - currenthpxp;
-			currenthpxp = -1;
-			damageOutput = NewXp / XP_RATIO;
-		}
-		//returns the damage you have done
-		return (int) Math.floor(damageOutput);
-	}
-
-	//calculating the scythe damage if the player has the weapon
-	private int ScytheDamageCalc()
-	{
-		int NewXp;
-		double damageOutput = 0;
-		int XPdrop;
-		if (currenthpxp != -1)
-		{
-			XPdrop = client.getSkillExperience(Skill.HITPOINTS);
-			NewXp = XPdrop - currenthpxp;
-			currenthpxp = -1;
-			damageOutput = NewXp * SCYTHE_MODIFIER;
-		}
-		//returns the damage you have done
-		return (int) Math.floor(damageOutput);
-	}
-
-	//adding up the damage for the print message checks every tick(aka attack tick)
-	private void DamageCounting()
-	{
-		Player localPlayer = client.getLocalPlayer();
-		Actor interacting = localPlayer.getInteracting();
-		if (client.getGameState() == GameState.LOGGED_IN)
-		{
-			if (interacting instanceof NPC)
-			{
-				String interactingName = interacting.getName();
-				ItemComposition item = client.getItemDefinition(ItemID.SCYTHE_OF_VITUR);
-				int WEAPONSLOT = EquipmentInventorySlot.WEAPON.getSlotIdx();
-				if (interactingName.equals(BossName))
-				{
-					//checks the weapon slot if they have a scythe or not
-					if (WEAPONSLOT == item.getId())
-					{
-						DamageCount += (ScytheDamageCalc() * BOSS_MODIFIER);
-					}
-					else
-					{
-						DamageCount += (XPtoDamage() * BOSS_MODIFIER);
-					}
-				}
-			}
-		}
-	}
-
-	@Subscribe
-	//will add the damage that you have taken from the current boss fight
-	private void DamageTakenCounter(HitsplatApplied Hit)
-	{
-		if (Hit.getActor().equals(client.getLocalPlayer()))
-		{
-			DamageTaken += Hit.getHitsplat().getAmount();
-		}
-	}
-
-	@Subscribe
-	//will check for the monster if it died works only on ToB Bosses
-	/*Verzik has three phases so the program will add up all the damage and prints it into one message
-	because every time she phases she "dies" so making sure the counter doesn't print out the damage for phase 1, 2,
-	and 3.
-	 */
-	public void onNpcDespawned(NpcDespawned npc)
-	{
-		NPC actor = (NPC) npc.getActor();
-		if (actor.isDead() && actor.getId() == NpcID.VERZIK_VITUR_8375 && status)
-		{
-			DamagePrint(actor);
-			ResetCounter();
-		}
-		else if (actor.isDead() && actor.getName().equals(BossName) && actor.getId() != NpcID.VERZIK_VITUR_8374 &&
-				actor.getId() != NpcID.VERZIK_VITUR_8372 &&  actor.getId() != NpcID.VERZIK_VITUR_8370 &&
-				status)
-		{
-			DamagePrint(actor);
-			ResetCounter();
-		}
-		//will reset the counter after the boss dies and if you died during the fight
-		else if (actor.isDead() && actor.getName().equals(BossName) && !status)
-		{
-			ResetCounter();
-		}
-	}
-
-	//just reset the counter for the next fight and status
-	private void ResetCounter()
-	{
-		DamageCount = 0;
-		DamageTaken = 0;
-		BossName = null;
-		status = ALIVE;
-	}
-
-	//print out the damage after the boss have died
-	//prevent people from spectating to get the damage message, it is impossible for them to get damage
-	private void DamagePrint(NPC actor)
-	{
-		if (DamageTaken != 0)
-		{
-			String MessageDamage = "Well done! You did " + DAMAGEFORMAT.format(DamageCount) + " damage to " +
-					actor.getName() + "!";
-			sendChatMessage(MessageDamage);
-			String MessageTaken = "You have taken " + DAMAGEFORMAT.format(DamageTaken) + " damage from this fight!";
-			sendChatMessage(MessageTaken);
-		}
-	}
-
-	@Subscribe
-	//whenever you have died in tob you will get a death message with damage
-	// made sure the message works at ToB area or else it will message every where
-	private void Death(LocalPlayerDeath death)
-	{
-		String DeathMessage = "You tried your best! You did " + DAMAGEFORMAT.format(DamageCount) + " damage to " +
-				BossName + "!";
-		String MessageTaken = "You have taken " + DAMAGEFORMAT.format(DamageTaken) + " damage from this fight!";
-		for (int i = 0; i < ToB_Region.length; i++)
-		{
-			if (WorldPoint.fromLocalInstance(client,
-					client.getLocalPlayer().getLocalLocation()).getRegionID() == ToB_Region[i])
-			{
-				sendChatMessage(DeathMessage);
-				sendChatMessage(MessageTaken);
-				ResetCounter();
-				//status will become "dead" after you died in the fight
-				status = DEAD;
-			}
-		}
-	}
-
-	//sends a message saying this "You have done XYZ damage to boss name! or the death message
-	// "Well done! you have done your best, you have done XYZ damage to boss name
-	private void sendChatMessage(String chatMessage)
-	{
-		final String message = new ChatMessageBuilder()
-				.append(ChatColorType.HIGHLIGHT)
-				.append(chatMessage)
-				.build();
-		chatMessangerManager.queue(
-				QueuedMessage.builder()
-						.type(ChatMessageType.GAME)
-						.runeLiteFormattedMessage(message)
-						.build());
-	}
+   private int currentWorld = -1;
+   private int Count;
+   private String Name;
+   private int currenthp = -1;
+   private boolean bossfound = false;
+   
+   //location at tob
+   private static final int MAIDEN_REGION = 12869;
+   private static final int BLOAT_REGION = 13125;
+   private static final int NYLOCAS_REGION = 13122;
+   private static final int SOTETSEG_REGION = 13123;
+   private static final int XARPUS_REGION = 12612;
+   private static final int VERZIK_REGION = 12611;
+   private static final int[] ToB_Region = MAIDEN_REGION, BLOAT_REGION, NYLOCAS_REGION, SOTETSEG_REGION,
+      XARPUS_REGION, VERZIK_REGION};
+   
+   //setting up the array for a check list
+   private static int[] NPCARRAY = {NpcID.THE_MAIDEN_OF_SUGADINTI, NpcID.THE_MAIDEN_OF_SUGADINTI_8361,
+   NpcID.THE_MAIDEN_OF_SUGADINTI_8362, NpcID.THE_MAIDEN_OF_SUGADINTI_8363, NpcID.THE_MAIDEN_OF_SUGADINTI_8364,
+   NpcID.THE_MAIDEN_OF_SUGADINTI_8365, NpcID.PESTILENT_BLOAT, NpcID.NYLOCAS_VASILIAS, NpcID.NYLOCAS_VASILIAS_8355,
+   NpcID.NYLOCAS_VASILIAS_8356, NpcID.NYLOCAS_VASILIAS_8357, NpcID.SOTETSEG, NpcID.SOTETSEG_8388, NpcID.XARPUS,
+   NpcID.XARPUS_8339, NpcID.XARPUS_8340, NpcID.XARPUS_8341, NpcID.VERZIK_VITUR, NpcID.VERZIK_VITUR_8369,
+   NpcID.VERZIK_VITUR_8370, NpcID.VERZIK_VITUR_8371, NpcID.VERZIK_VITUR_8372, NpcID.VERZIK_VITUR_8373,
+   NpcID.VERZIK_VITUR_8374, NpcID.VERZIK_VITUR_8375};
+   
+   @Inject
+   private Client client;
+   
+   @Inject 
+   private ChatMessageManager chatMessangerManager;
+   
+   @Subscribe
+   //every gametick it will check these methods
+   private void onGameTick(GameTick tick)
+   {
+      if (client.getGameState() != GameState.LOGGED_IN)
+      {
+         ResetCounter();
+         return;
+      }
+      checkInterAction();
+      DamageCounting();
+      currenthp = client.getSkillExperience(Skill.HITPOINTS);
+   }
+   
+   //checks for npcID and put the boss name into a string be easier to ID it
+   //once the boss is found it will never check it
+   private void checkInterAction()
+   {
+      Player localPlayer = client.getLocalPlayer();
+      Actor interacting = localPlayer.getInteracting();
+      if (client.getGameState() == GameState.LOGGED_IN)
+      {
+         if (!bossfound)
+         {
+            if (interacting instanceof NPC)
+            {
+               int interactingId = ((NPC) interacting).getId();
+               String interactingName = interacting.getName();
+               for (int aNPCARRAY : NPCARRAY)
+               {
+                  if (aNPCARRAY == interactingId)
+                  {
+                     this.Name = interactingName;
+                     this.bossfound = true;
+                  }
+               }
+            }
+         }
+      }
+   }
+   
+   @Subscribe
+   //if you hop it will reset the counter
+   public void onGameStateChanged(GameStateChanged event)
+   {
+      if (event.getGameState() == GameState.LOGGED_IN)
+      {
+         if (currentWorld == -1)
+         {
+            currentWorld = client.getWorld();
+         }
+         else if (currentWorld != client.getWorld())
+         {
+            currentWorld = client.getWorld();
+            ResetCounter();
+         }
+      }
+   }
+   
+   //grabbing the xp and calculating the damage
+   private int XPtoDamage()
+   {
+      int NewXp = 0;
+      double damageOutput = 0;
+      int XPdrop = 0;
+      if (currenthp != -1)
+      {
+         XPdrop = client.getSkillExperience(Skill.HITPOINTS);
+         NewXp = XPdrop - currenthp;
+         currenthp -1;
+         damageOutput = NewXp / 1.3333;
+      }
+      //returns the damage you have done
+      return (int) Math.floor(damageOutput);
+   }
+   
+   //adding up the damage for the print message checks every tick(aka attack tick)
+   private void DamageCounting()
+   {
+      Player localPlayer = client.getLocalPlayer();
+      Actor interacting = localPlayer.getInteracting();
+      if (client.getGameState() == GameState.LOGGED_IN)
+      {
+         if (interacting instanceof NPC)
+         {
+            String interactingName = ((NPC) interacting).getName();
+            if (interactingName == this.Name)
+            {
+               this.Count += XPtoDamage();
+            }
+         }
+      }
+   }
+   
+   @Subscribe
+   //will check for the monster if it died works only on ToB Bosses
+   //Verzik has three phases so the program will add up all the damage and prints it into one message
+   //making sure else if statement doesnt check with Verzik
+   public void onNpcDespawned(ActorDespawned npc)
+   {
+      NPC actor = (NPC) npc.getActor();
+      if (actor.isDead() && actor.getId() == NpcID.VERZIK_VITUR_8375)
+      {
+         DamagePrint(actor);
+         ResetCounter();
+      }
+      else if (actor.isDead() && actor.getName().equals(this.Name) && actor.getId() != NpcID.VERZIK_VITUR_8374 &&
+         actor.getId() != NpcID.VERZIK_VITUR_8372 &&  actor.getId() != NpcID.VERZIK_VITUR_8370)
+      {
+         DamagePrint(actor);
+         ResetCounter();
+      }
+   }
+   
+   //just reset the counter
+   private void ResetCounter()
+   {
+      this.Count = 0;
+      this.Name = null;
+      this.bossfound = false;
+   }
+   
+   //print out the damage after the boss have died
+   private void DamagePrint(NPC actor)
+   {
+      String MessageDamage = "Well done! You have done " + this.Count + " damage to " + actor.getName() + "!";
+      sendChatMessage(MessageDamage);
+   }
+   
+   @Subscribe
+   //whenever you have died in tob you will get a death message with damage
+   //made sure the message works at ToB area or else we will get it everwhere
+   private void Death(LocalPlayerDeath death)
+   {
+      String DeathMessage = "You have tried your best! You have done " + this.Count + " damage to this " +
+         this.Name +"!";
+      for (int i = 0; i < ToB_Region.length; i++)
+      {
+         if (client.getLocalPlayer().getWorldLocation().getRegionID() == ToB_Region[i])
+         {
+            sendChatMessage(DeathMessage);
+            ResetCounter();
+         }
+      }
+   }
+   
+   //sends a message saying this "You have done XYZ damage to boss name! or the death message
+   //"Well done! you have done your best, you have done XYZ damage to boss name
+   private void sendChatMessage(String chatMessage)
+   {
+      final String message = new ChatMessageBuilder()
+            .append(ChatColorType.HIGHLIGHT)
+            .append(chatMessage)
+            .build();
+      chatMessageManager.queue(
+            QueuedMessage.builder()
+                  .type(ChatMessageType.GAME)
+                  .runeLiteFormattedMessage(message)
+                  .build());
+   }
 }
