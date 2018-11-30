2ce8520a3449dd727e72b899737f79e99234304f
Bryan Haley <bryanhaley.professional@gmail.com>
2018-11-29 23:02:28 -0800

    Added Feature for Hiding Other Player's Loot

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItem.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItem.java
@@ -27,6 +27,7 @@ package net.runelite.client.plugins.grounditems;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Value;
+import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.coords.WorldPoint;
 
 @Data
@@ -38,11 +39,14 @@ class GroundItem
 	private String name;
 	private int quantity;
 	private WorldPoint location;
+	private LocalPoint localLoc;
 	private int height;
 	private int haPrice;
 	private int gePrice;
 	private int offset;
 	private boolean tradeable;
+
+	int getId() { return id; }
 
 	int getHaPrice()
 	{
@@ -53,6 +57,10 @@ class GroundItem
 	{
 		return gePrice * quantity;
 	}
+
+	WorldPoint getWorldLocation() { return location; }
+
+	LocalPoint getLocalLocation() { return localLoc; }
 
 	@Value
 	static class GroundItemKey
