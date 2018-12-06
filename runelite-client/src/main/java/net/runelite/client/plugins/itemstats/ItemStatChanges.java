ff71d7cd7cc5ea314ec22197be676eee023962bf
CJ <werts1255@gmail.com>
2018-12-06 02:00:50 -0700

    Change papaya fruit health restore
    Papaya fruit health restore change from 4 to 8 when hovered

--- runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
@@ -88,7 +88,7 @@ public class ItemStatChanges
 		add(food(maxHP -> (int) Math.ceil(maxHP * .05)), WATERMELON_SLICE);
 		add(food(perc(.1, 1)), COOKED_SWEETCORN, SWEETCORN_7088 /* Bowl of cooked sweetcorn */);
 		add(combo(food(1), boost(DEFENCE, perc(.02, 1))), CABBAGE_1967 /* Draynor Manor */);
-		add(combo(2, food(4), heal(RUN_ENERGY, 5)), PAPAYA_FRUIT);
+		add(combo(2, food(8), heal(RUN_ENERGY, 5)), PAPAYA_FRUIT);
 		add(range(food(5), food(7)), THIN_SNAIL_MEAT);
 		add(range(food(7), food(9)), FAT_SNAIL_MEAT);
 
