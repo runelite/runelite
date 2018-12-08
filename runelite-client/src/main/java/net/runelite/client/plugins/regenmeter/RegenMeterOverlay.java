c6cb4db1fa562c191ff306f959eccd0590d3bf0b
Kyle Goodale <KGoodale13@users.noreply.github.com>
2018-12-07 19:23:26 -0500

    Optimizations to regen overlay

--- runelite-client/src/main/java/net/runelite/client/plugins/regenmeter/RegenMeterOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/regenmeter/RegenMeterOverlay.java
@@ -35,8 +35,10 @@ import java.awt.geom.Arc2D;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.VarPlayer;
+import net.runelite.api.events.ConfigChanged;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -48,10 +50,14 @@ public class RegenMeterOverlay extends Overlay
 	private static final Color OVERLAY_COLOR = new Color(255, 255, 255, 60);
 	private static final double DIAMETER = 26D;
 	private static final int OFFSET = 27;
+	private static final Stroke STROKE = new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
 
 	private final Client client;
 	private RegenMeterPlugin plugin;
 	private RegenMeterConfig config;
+
+	private boolean showHitPoints;
+	private boolean showSpecial;
 
 	private static Color brighter(int color)
 	{
@@ -68,6 +74,28 @@ public class RegenMeterOverlay extends Overlay
 		this.client = client;
 		this.plugin = plugin;
 		this.config = config;
+		updateConfig();
+	}
+
+	private Rectangle getBounds(WidgetInfo widgetInfo){
+		Widget widget = client.getWidget(widgetInfo);
+		if (widget == null || widget.isHidden()) {
+			return null;
+		}
+		return widget.getBounds();
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (!event.getGroup().equals("regenmeter"))
+			updateConfig();
+	}
+
+	public void updateConfig()
+	{
+		this.showHitPoints = config.showHitpoints();
+		this.showSpecial = config.showSpecial();
 	}
 
 	@Override
@@ -75,20 +103,18 @@ public class RegenMeterOverlay extends Overlay
 	{
 		g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 
-		if (config.showHitpoints())
+		if (showHitPoints)
 		{
 			renderRegen(g, WidgetInfo.MINIMAP_HEALTH_ORB, plugin.getHitpointsPercentage(), HITPOINTS_COLOR);
 		}
 
-		if (config.showSpecial())
+		if (showSpecial)
 		{
 			if (client.getVar(VarPlayer.SPECIAL_ATTACK_ENABLED) == 1)
 			{
-				final Widget widget = client.getWidget(WidgetInfo.MINIMAP_SPEC_ORB);
-
-				if (widget != null && !widget.isHidden())
+				final Rectangle bounds = getBounds(WidgetInfo.MINIMAP_SPEC_ORB);
+				if (bounds != null)
 				{
-					final Rectangle bounds = widget.getBounds();
 					g.setColor(RegenMeterOverlay.OVERLAY_COLOR);
 					g.fillOval(
 						bounds.x + OFFSET,
@@ -105,17 +131,12 @@ public class RegenMeterOverlay extends Overlay
 
 	private void renderRegen(Graphics2D g, WidgetInfo widgetInfo, double percent, Color color)
 	{
-		Widget widget = client.getWidget(widgetInfo);
-		if (widget == null || widget.isHidden())
-		{
-			return;
+		final Rectangle bounds = getBounds(widgetInfo);
+		if (bounds != null) {
+			Arc2D.Double arc = new Arc2D.Double(bounds.x + OFFSET, bounds.y + (bounds.height / 2 - DIAMETER / 2), DIAMETER, DIAMETER, 90.d, -360.d * percent, Arc2D.OPEN);
+			g.setStroke(STROKE);
+			g.setColor(color);
+			g.draw(arc);
 		}
-		Rectangle bounds = widget.getBounds();
-
-		Arc2D.Double arc = new Arc2D.Double(bounds.x + OFFSET, bounds.y + (bounds.height / 2 - DIAMETER / 2), DIAMETER, DIAMETER, 90.d, -360.d * percent, Arc2D.OPEN);
-		final Stroke STROKE = new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
-		g.setStroke(STROKE);
-		g.setColor(color);
-		g.draw(arc);
 	}
 }
