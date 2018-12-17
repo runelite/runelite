c8105908c05452187b75f060e030fbdbed4d3368
Sam Beresford <seb1g13@soton.ac.uk>
2018-12-17 10:45:02 +0100

    Add item overlay to show when monsters are weak enough to finish off (#6710)
    Current behaviour shows the required item to finish an NPC off above it's head when it's health is below the required threshold.
    Works for Rockslugs, Gargoyles, Desert Lizards and Zygomites
    
    Fixes: #784

--- runelite-client/src/main/java/net/runelite/client/plugins/slayer/SlayerConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/slayer/SlayerConfig.java
@@ -99,6 +99,17 @@ public interface SlayerConfig extends Config
 		return Color.RED;
 	}
 
+	@ConfigItem(
+		position = 7,
+		keyName = "weaknessPrompt",
+		name = "Show Monster Weakness",
+		description = "Show an overlay on a monster when it is weak enough to finish off (Only Lizards, Gargoyles & Rockslugs)"
+	)
+	default boolean weaknessPrompt()
+	{
+		return true;
+	}
+
 	// Stored data
 	@ConfigItem(
 		keyName = "taskName",
