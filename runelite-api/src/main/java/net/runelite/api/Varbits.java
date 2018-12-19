3c5d48cc8799da282f374488b5031f0ff7575c4d
Hydrox6 <ikada@protonmail.ch>
2018-12-19 17:43:52 +0000

    Add Flax and Bonemeal options to Daily Task plugin

--- runelite-api/src/main/java/net/runelite/api/Varbits.java
+++ runelite-api/src/main/java/net/runelite/api/Varbits.java
@@ -359,6 +359,12 @@ public enum Varbits
 	DAILY_ESSENCE_COLLECTED(4547),
 	DAILY_RUNES_COLLECTED(4540),
 	DAILY_SAND_COLLECTED(4549),
+	DAILY_FLAX_STATE(4559),
+	/**
+	 * This varbit tracks how much bonemeal has been redeemed from Robin
+	 * The player gets 13 for each diary completed above and including Medium, for a maxiumum of 39
+ 	*/
+	DAILY_BONEMEAL_STATE(4543),
 
 	/**
 	 * Fairy Ring
