e5cd4a3352210cc5344cd5e5ad8bb8908ea65160
Carl Linley <carl.linley@outlook.com>
2018-12-27 19:54:11 +0000

    Format in accordance with the coding standard

--- runelite-client/src/main/java/net/runelite/client/plugins/collapsemenuentries/CollapseMenuEntriesPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/collapsemenuentries/CollapseMenuEntriesPlugin.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2018, Sypanite <https://github.com/Sypanite>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.collapsemenuentries;
 
 import net.runelite.client.eventbus.Subscribe;
@@ -10,7 +34,6 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.ColorUtil;
-
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,9 +45,11 @@ import java.util.Iterator;
 		description = "Collapse duplicate context menu entries on ground items, reducing clutter.",
 		tags = {"collapse", "item", "context", "ground", "menu", "entries"}
 )
-public class CollapseMenuEntriesPlugin extends Plugin {
+public class CollapseMenuEntriesPlugin extends Plugin
+{
 
-	static class ItemOption {
+	static class ItemOption
+	{
 		protected String option = "";
 		protected String target = "";
 		protected int numOccurrences = 1;
@@ -41,22 +66,26 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 
 	@Inject
 	private Client client;
+
 	@Inject
 	private CollapseMenuEntriesConfig config;
 
 	private HashMap<Integer, ArrayList<ItemOption>> optionMap;
 
 	@Override
-	protected void startUp() {
+	protected void startUp() throws Exception
+	{
 		optionMap = new HashMap<>();
 	}
 
 	@Subscribe
-	public void onMenuOpened(MenuOpened event) {
+	public void onMenuOpened(MenuOpened event)
+	{
 		MenuEntry[] menuEntries = client.getMenuEntries();
 		int numOptionsCulled = buildOptionMap(menuEntries);
 
-		if (numOptionsCulled > 0) {
+		if (numOptionsCulled > 0)
+		{
 			rebuildContextMenu(menuEntries, numOptionsCulled);
 		}
 	}
@@ -66,29 +95,35 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 	 * @param menuEntries - The client's array of menu entries.
 	 * @return The number of duplicate options that'll be culled from the final menu.
 	 **/
-	private int buildOptionMap(MenuEntry[] menuEntries) {
+	private int buildOptionMap(MenuEntry[] menuEntries)
+	{
 		optionMap.clear();
 
 		int numOptionsCulled = 0;
 
-		for (int i = 0; i != menuEntries.length; i++) {
+		for (int i = 0; i != menuEntries.length; i++)
+		{
 			MenuEntry entry = menuEntries[i];
 
-			if (isGroundItemOption(entry) == false) {
+			if (isGroundItemOption(entry) == false)
+			{
 				continue;
 			}
 
 			int itemID = entry.getIdentifier();
 
-			if (optionMap.containsKey(itemID)) {
+			if (optionMap.containsKey(itemID))
+			{
 				ArrayList<ItemOption> itemOptions = optionMap.get(itemID);
 				Iterator<ItemOption> optionIterator = itemOptions.iterator();
 				boolean bFoundOption = false;
 
-				while (optionIterator.hasNext()) {
+				while (optionIterator.hasNext())
+				{
 					ItemOption itemOption = optionIterator.next();
 
-					if (entry.getOption().equals(itemOption.option)) {
+					if (entry.getOption().equals(itemOption.option))
+					{
 						itemOption.numOccurrences++;
 						numOptionsCulled++;
 						bFoundOption = true;
@@ -96,14 +131,16 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 					}
 				}
 
-				if (bFoundOption == false) {
+				if (bFoundOption == false)
+				{
 					ItemOption newOption = new ItemOption();
 					newOption.option = entry.getOption();
 					newOption.target = entry.getTarget();
 					itemOptions.add(newOption);
 				}
 			}
-			else {
+			else
+			{
 				ArrayList<ItemOption> newOptionList = new ArrayList<>();
 
 				ItemOption newOption = new ItemOption();
@@ -122,24 +159,31 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 	 * @param menuEntries - The client's array of menu entries.
 	 * @param numOptionsCulled - The number of duplicate options that'll be culled from the final menu.
 	 **/
-	private void rebuildContextMenu(MenuEntry[] menuEntries, int numOptionsCulled) {
+	private void rebuildContextMenu(MenuEntry[] menuEntries, int numOptionsCulled)
+	{
 		MenuEntry[] newMenuEntries = new MenuEntry[menuEntries.length - numOptionsCulled];
 		int newEntryIndex = 0;
 
-		for (int i = 0; i != menuEntries.length; i++) {
+		for (int i = 0; i != menuEntries.length; i++)
+		{
 			MenuEntry entry = menuEntries[i];
 
-			if (isGroundItemOption(entry)) {
+			if (isGroundItemOption(entry))
+			{
 				int itemID = entry.getIdentifier();
 				ArrayList<ItemOption> itemOptions = optionMap.get(itemID);
 				Iterator<ItemOption> optionIterator = itemOptions.iterator();
 
-				while (optionIterator.hasNext()) {
+				while (optionIterator.hasNext())
+				{
 					ItemOption itemOption = optionIterator.next();
 
-					if (entry.getOption().equals(itemOption.option)) {
-						if (itemOption.bProcessedInRebuild == false) {
-							if (itemOption.numOccurrences > 1 && config.showNumber()) {
+					if (entry.getOption().equals(itemOption.option))
+					{
+						if (itemOption.bProcessedInRebuild == false)
+						{
+							if (itemOption.numOccurrences > 1 && config.showNumber())
+							{
 								entry.setTarget(itemOption.target + "</col>" + ColorUtil.wrapWithColorTag(COUNT_PREFIX + itemOption.numOccurrences + COUNT_SUFFIX, config.collapsedColour()));
 							}
 							itemOption.bProcessedInRebuild = true;
@@ -148,7 +192,8 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 					}
 				}
 			}
-			else {
+			else
+			{
 				newMenuEntries[newEntryIndex++] = entry;
 			}
 		}
@@ -161,14 +206,15 @@ public class CollapseMenuEntriesPlugin extends Plugin {
 	 * These are declared as named constants.
 	 * @param entry - The menu entry.
 	 **/
-	private boolean isGroundItemOption(MenuEntry entry) {
+	private boolean isGroundItemOption(MenuEntry entry)
+	{
 		int type = entry.getType();
-
 		return type == GROUND_ITEM_OPTION_1 || type == GROUND_ITEM_OPTION_2 || type <= GROUND_ITEM_OPTION_3 || type == GROUND_ITEM_EXAMINE;
 	}
 
 	@Provides
-	CollapseMenuEntriesConfig getConfig(ConfigManager configManager) {
+	CollapseMenuEntriesConfig getConfig(ConfigManager configManager)
+	{
 		return configManager.getConfig(CollapseMenuEntriesConfig.class);
 	}
 }
