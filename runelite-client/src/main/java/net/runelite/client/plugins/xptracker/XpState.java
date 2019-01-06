802478c6da5b94a0b252d18f3db2dfd98ba9d7cd
Party <party@runemate.com>
2019-01-06 21:08:53 +0000

    #7193 Address blank line review comments.

--- runelite-client/src/main/java/net/runelite/client/plugins/xptracker/XpState.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/xptracker/XpState.java
@@ -32,7 +32,6 @@ import net.runelite.api.Skill;
 
 /**
  * Internal state for the XpTrackerPlugin
- * <p>
  * Note: This class's operations are not currently synchronized.
  * It is intended to be called by the XpTrackerPlugin on the client thread.
  */
@@ -55,7 +54,7 @@ class XpState
 
 	/**
 	 * Resets a single skill
-	 * @param skill     Skill to reset
+	 * @param skill Skill to reset
 	 * @param currentXp Current XP to set to, if unknown set to -1
 	 */
 	void resetSkill(Skill skill, int currentXp)
@@ -84,10 +83,10 @@ class XpState
 	 * When the result of this operation is XpUpdateResult.UPDATED, the UI should be updated accordingly.
 	 * This is to distinguish events that reload all the skill's current values (such as world hopping)
 	 * and also first-login when the skills are not initialized (the start XP will be -1 in this case).
-	 * @param skill       Skill to update
-	 * @param currentXp   Current known XP for this skill
+	 * @param skill Skill to update
+	 * @param currentXp Current known XP for this skill
 	 * @param goalStartXp Possible XP start goal
-	 * @param goalEndXp   Possible XP end goal
+	 * @param goalEndXp Possible XP end goal
 	 * @return Whether or not the skill has been initialized, there was no change, or it has been updated
 	 */
 	XpUpdateResult updateSkill(Skill skill, int currentXp, int goalStartXp, int goalEndXp)
@@ -136,8 +135,8 @@ class XpState
 
 	/**
 	 * Updates skill with average actions based on currently interacted NPC.
-	 * @param skill     experience gained skill
-	 * @param npc       currently interacted NPC
+	 * @param skill experience gained skill
+	 * @param npc currently interacted NPC
 	 * @param npcHealth health of currently interacted NPC
 	 */
 	void updateNpcExperience(Skill skill, NPC npc, Integer npcHealth)
@@ -178,8 +177,8 @@ class XpState
 	/**
 	 * Update number of actions performed for skill (e.g amount of kills in this case) if last interacted
 	 * NPC died
-	 * @param skill     skill to update actions for
-	 * @param npc       npc that just died
+	 * @param skill skill to update actions for
+	 * @param npc npc that just died
 	 * @param npcHealth max health of npc that just died
 	 * @return UPDATED in case new kill was successfully added
 	 */
@@ -205,7 +204,7 @@ class XpState
 	/**
 	 * Forcefully initialize a skill with a known start XP from the current XP.
 	 * This is used in resetAndInitState by the plugin. It should not result in showing the XP in the UI.
-	 * @param skill     Skill to initialize
+	 * @param skill Skill to initialize
 	 * @param currentXp Current known XP for the skill
 	 */
 	void initializeSkill(Skill skill, int currentXp)
