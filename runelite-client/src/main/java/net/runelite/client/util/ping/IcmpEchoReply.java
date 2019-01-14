d3c4bc7461f449c76b3f3c660b904d65841cbcbe
stcktrce <40738917+stcktrce@users.noreply.github.com>
2019-01-14 18:02:00 +0100

    Move the Ping library out of the WorldHopper plugin and into the Util package

--- runelite-client/src/main/java/net/runelite/client/util/ping/IcmpEchoReply.java
+++ runelite-client/src/main/java/net/runelite/client/util/ping/IcmpEchoReply.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.worldhopper.ping;
+package net.runelite.client.util.ping;
 
 import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
