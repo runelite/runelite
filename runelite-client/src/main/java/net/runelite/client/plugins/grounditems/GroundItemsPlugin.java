4a7484725221558b7ea13c8c83f2f7268a170b6d
Ricky Loader <loaderricky123@gmail.com>
2019-01-14 13:45:22 +1300

    Conflict

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsPlugin.java
@@ -224,12 +224,12 @@ public class GroundItemsPlugin extends Plugin
 			existing.setQuantity(existing.getQuantity() + groundItem.getQuantity());
 		}
 
-		boolean isHighlighted = config.highlightedColor().equals(getHighlighted(
+		boolean shouldNotify = !config.onlyShowLoot() && config.highlightedColor().equals(getHighlighted(
 			groundItem.getName(),
 			groundItem.getGePrice(),
 			groundItem.getHaPrice()));
 
-		if (config.notifyHighlightedDrops() && isHighlighted)
+		if (config.notifyHighlightedDrops() && shouldNotify)
 		{
 			notifyHighlightedItem(groundItem);
 		}
@@ -299,6 +299,16 @@ public class GroundItemsPlugin extends Plugin
 			if (groundItem != null)
 			{
 				groundItem.setMine(true);
+
+				boolean shouldNotify = config.onlyShowLoot() && config.highlightedColor().equals(getHighlighted(
+					groundItem.getName(),
+					groundItem.getGePrice(),
+					groundItem.getHaPrice()));
+
+				if (config.notifyHighlightedDrops() && shouldNotify)
+				{
+					notifyHighlightedItem(groundItem);
+				}
 			}
 		}
 	}
