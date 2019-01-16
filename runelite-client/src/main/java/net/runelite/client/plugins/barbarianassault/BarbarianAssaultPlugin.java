02762962983608cb3e5fb0c81b2f11d36ebbfa08
FrostyFridge <>
2019-01-15 23:42:21 -0800

    BA one click call functionality

--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
@@ -28,15 +28,12 @@ package net.runelite.client.plugins.barbarianassault;
 import com.google.inject.Provides;
 import java.awt.Font;
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.ItemID;
-import net.runelite.api.Varbits;
-import net.runelite.api.events.ChatMessage;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.VarbitChanged;
-import net.runelite.api.events.WidgetLoaded;
+
+import net.runelite.api.*;
+import net.runelite.api.events.*;
 import net.runelite.api.kit.KitType;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetID;
@@ -60,6 +57,8 @@ import net.runelite.client.util.ImageUtil;
 )
 public class BarbarianAssaultPlugin extends Plugin
 {
+	private final List<MenuEntry> entries = new ArrayList<>();
+
 	private static final int BA_WAVE_NUM_INDEX = 2;
 	private static final String START_WAVE = "1";
 	private static final String ENDGAME_REWARD_NEEDLE_TEXT = "<br>5";
@@ -166,6 +165,39 @@ public class BarbarianAssaultPlugin extends Plugin
 			case ItemID.HEALER_ICON:
 				overlay.setCurrentRound(new Round(Role.HEALER));
 				break;
+		}
+	}
+
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		if (config.removeWrong() && overlay.getCurrentRound() != null && event.getTarget().endsWith("horn"))
+		{
+			MenuEntry[] menuEntries = client.getMenuEntries();
+			WidgetInfo callInfo = overlay.getCurrentRound().getRoundRole().getCall();
+			Widget callWidget = client.getWidget(callInfo);
+			String call = Calls.getOption(callWidget.getText());
+			MenuEntry correctCall = null;
+
+			entries.clear();
+			for (MenuEntry entry : menuEntries)
+			{
+				String option = entry.getOption();
+				if (option.equals(call))
+				{
+					correctCall = entry;
+				}
+				else if (!option.startsWith("Tell-"))
+				{
+					entries.add(entry);
+				}
+			}
+
+			if (correctCall != null)
+			{
+				entries.add(correctCall);
+				client.setMenuEntries(entries.toArray(new MenuEntry[entries.size()]));
+			}
 		}
 	}
 
--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Calls.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/Calls.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2018, Cameron <https://github.com/noremac201>
- * Copyright (c) 2018, Jacob M <https://github.com/jacoblairm>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,41 +24,61 @@
  */
 package net.runelite.client.plugins.barbarianassault;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
+import java.util.HashMap;
+import java.util.Map;
 
-@ConfigGroup("barbarianAssault")
-public interface BarbarianAssaultConfig extends Config
+public enum Calls
 {
-	@ConfigItem(
-			keyName = "removeUnused",
-			name = "Remove incorrect calls",
-			description = "Removes incorrect calls for Role horn"
-	)
+    //Attacker Calls
+    RED_EGG("Red egg", "Tell-red"),
+    GREEN_EGG("Green egg", "Tell-green"),
+    BLUE_EGG("Blue egg", "Tell-blue"),
+    //Collector Calls
+    CONTROLLED("Controlled/Bullet/Wind", "Tell-controlled"),
+    ACCURATE("Accurate/Field/Water", "Tell-accurate"),
+    AGGRESSIVE("Aggressive/Blunt/Earth", "Tell-aggressive"),
+    DEFENSIVE("Defensive/Barbed/Fire", "Tell-defensive"),
+    //Healer Calls
+    TOFU("Tofu", "Tell-tofu"),
+    CRACKERS("Crackers", "Tell-crackers"),
+    WORMS("Worms", "Tell-worms"),
+    //Defender Calls
+    POIS_WORMS("Pois. Worms", "Tell-worms"),
+    POIS_TOFU("Pois. Tofu", "Tell-tofu"),
+    POIS_MEAT("Pois. Meat", "Tell-meat");
 
-	default boolean removeWrong()
-	{
-		return true;
-	}
+    private final String call;
+    private final String option;
 
-	@ConfigItem(
-		keyName = "showTimer",
-		name = "Show call change timer",
-		description = "Show time to next call change"
-	)
-	default boolean showTimer()
-	{
-		return true;
-	}
+    private static final Map<String, String> CALL_MENU = new HashMap<>();
 
-	@ConfigItem(
-		keyName = "waveTimes",
-		name = "Show wave and game duration",
-		description = "Displays wave and game duration"
-	)
-	default boolean waveTimes()
-	{
-		return true;
-	}
+    static
+    {
+        for (Calls s : values())
+        {
+            CALL_MENU.put(s.getCall(), s.getOption());
+        }
+    }
+
+    Calls(String call, String option)
+    {
+        this.call = call;
+        this.option = option;
+    }
+
+    public String getCall()
+    {
+        return call;
+    }
+
+    public String getOption()
+    {
+        return option;
+    }
+
+    public static String getOption(String call)
+    {
+        return CALL_MENU.get(call);
+    }
+
 }
