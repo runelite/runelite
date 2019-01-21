8071393e247aa3c97cf47eed024b45d68377c4a4
4444N <gregoryrobertson97@gmail.com>
2019-01-21 03:07:11 -0500

    Removed widgets & updated formatting

--- runelite-client/src/main/java/net/runelite/client/plugins/kittennotifier/KittenNotifierConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/kittennotifier/KittenNotifierConfig.java
@@ -5,23 +5,26 @@ import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("kittennotifier")
 public interface KittenNotifierConfig extends Config{
-    @ConfigItem(
-            keyName = "absolutelyNeeded",
-            name = "Notify only on Absolute Need",
-            description = "Only notify when kitten absolutely needs food or attention."
-    )
-    default boolean absolutelyNeeded() { return false; }
-    @ConfigItem(
-            keyName = "catOwned",
-            name = "",
-            description = "",
-            hidden = true
-    )
-    default boolean catOwned() { return false; }
-    @ConfigItem(
-            keyName = "catOwned",
-            name = "",
-            description = ""
-    )
-    void catOwned(Boolean bool);
+	@ConfigItem
+	(
+		keyName = "absolutelyNeeded",
+		name = "Notify only on Absolute Need",
+		description = "Only notify when kitten absolutely needs food or attention."
+	)
+	default boolean absolutelyNeeded() { return false; }
+	@ConfigItem
+	(
+		keyName = "catOwned",
+		name = "",
+		description = "",
+		hidden = true
+	)
+	default boolean catOwned() { return false; }
+	@ConfigItem
+	(
+		keyName = "catOwned",
+		name = "",
+		description = ""
+	)
+	void catOwned(Boolean bool);
 }
