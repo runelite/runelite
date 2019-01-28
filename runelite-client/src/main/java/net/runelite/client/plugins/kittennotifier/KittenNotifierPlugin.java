545bde31205d4f26674020205985ae34462d704c
4444N <gregoryrobertson97@gmail.com>
2019-01-27 19:22:23 -0500

    Add copyright notice, and resolve suggestions

--- runelite-client/src/main/java/net/runelite/client/plugins/kittennotifier/KittenNotifierPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/kittennotifier/KittenNotifierPlugin.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2019 NNNN4
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.kittennotifier;
 import com.google.inject.Provides;
 import net.runelite.api.ChatMessageType;
@@ -36,57 +60,53 @@ public class KittenNotifierPlugin extends Plugin
 		return configManager.getConfig(KittenNotifierConfig.class);
 	}
 
-	private NPC kitCat;
-	private String kitCatWas;
+	private NPC catName;
+	private String catNameWas;
 
 	@Subscribe
 	public void onChatMessage(ChatMessage event)
 	{
 		if (config.kittenNeeds() && event.getType() == ChatMessageType.SERVER)
 		{
-			if (event.getMessage().contentEquals("<col=ef1020>Your kitten is hungry.</col>"))
+			if (event.getMessage().matches(".+Your kitten is.+hungry.+"))
 			{
 				notifier.notify("Your kitten is hungry.");
 			}
-			if (event.getMessage().contentEquals("<col=ef1020>Your kitten wants attention.</col>"))
+			if (event.getMessage().matches(".+Your kitten.+wants attention.+"))
 			{
 				notifier.notify("Your kitten wants attention.");
 			}
-			if (event.getMessage().contentEquals("<col=ef1020>Your kitten is very hungry.</col>"))
-			{
-				notifier.notify("Your kitten is very hungry.");
-			}
-			if (event.getMessage().contentEquals("<col=ef1020>Your kitten really wants attention.</col>"))
-			{
-				notifier.notify("Your kitten really wants attention.");
-			}
 		}
 	}
+
 	@Subscribe
 	public void onGameTick(GameTick event)
 	{
-		if (kitCat != null && kitCat.getName() != null)
+		if (catName == null || catName.getName() == null)
 		{
-			if (kitCatWas.contentEquals("Kitten") && kitCat.getName().contentEquals("Cat"))
-			{
-				notifier.notify("Your kitten has grown into a cat.");
-			}
-			kitCatWas = kitCat.getName();
+			catName = null;
 		}
 		else
 		{
-			kitCat = null;
+			System.out.println(catName.getName());
+			if (catNameWas.equals("Kitten") && catName.getName().equals("Cat"))
+			{
+				notifier.notify("Your kitten has grown into a cat.");
+			}
+			catNameWas = catName.getName();
+
 		}
 	}
+
 	@Subscribe
 	public void onNpcSpawned(NpcSpawned event)
 	{
-		if (event.getNpc().getInteracting() != null && event.getNpc().getInteracting().getName().contentEquals(client.getLocalPlayer().getName()))
+		if (event.getNpc().getInteracting() != null && event.getNpc().getInteracting().getName().equals(client.getLocalPlayer().getName()))
 		{
-			if (event.getNpc().getName().contentEquals("Kitten") || event.getNpc().getName().contentEquals("Cat"))
+			if (event.getNpc().getName().equals("Kitten") || event.getNpc().getName().equals("Cat"))
 			{
-				kitCat = event.getNpc();
-				kitCatWas = event.getNpc().getName();
+				catName = event.getNpc();
+				catNameWas = event.getNpc().getName();
 			}
 		}
 	}
