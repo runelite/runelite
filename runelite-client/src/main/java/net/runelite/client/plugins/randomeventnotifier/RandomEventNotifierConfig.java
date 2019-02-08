f9aba992ed25df3fc7c5d081961c0f51a2b196e3
Dylan Vollrath <dylanvollrath@gmail.com>
2019-02-07 23:29:29 -0800

    Replaced all spaces with tab characters

--- runelite-client/src/main/java/net/runelite/client/plugins/randomeventnotifier/RandomEventNotifierConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/randomeventnotifier/RandomEventNotifierConfig.java
@@ -7,10 +7,10 @@
  * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *	list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ *	this list of conditions and the following disclaimer in the documentation
+ *	and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -34,223 +34,223 @@ import net.runelite.client.config.ConfigItem;
 public interface RandomEventNotifierConfig extends Config
 {
 
-    @ConfigItem(
-            keyName = "drJekyllEnabled",
-            name = "Dr Jekyll",
-            description = "Configures if Dr Jekyll random event notifications are enabled",
-            position = 1
-    )
-    default boolean drJekyllEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "drJekyllEnabled",
+			name = "Dr Jekyll",
+			description = "Configures if Dr Jekyll random event notifications are enabled",
+			position = 1
+	)
+	default boolean drJekyllEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "beeKeeperEnabled",
-            name = "Bee Keeper",
-            description = "Configures if Bee Keeper random event notifications are enabled",
-            position = 2
-    )
-    default boolean beeKeeperEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "beeKeeperEnabled",
+			name = "Bee Keeper",
+			description = "Configures if Bee Keeper random event notifications are enabled",
+			position = 2
+	)
+	default boolean beeKeeperEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-    		keyName = "captArnavEnabled",
-            name = "Capt Arnav",
-            description = "Configures if Capt Arnav random event notifications are enabled",
-            position = 3
-    )
-    default boolean captArnavEnabled()
-    {
-    	return true;
-    }
+	@ConfigItem(
+			keyName = "captArnavEnabled",
+			name = "Capt Arnav",
+			description = "Configures if Capt Arnav random event notifications are enabled",
+			position = 3
+	)
+	default boolean captArnavEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "sergeantDamienEnabled",
-            name = "Sergeant Damien",
-            description = "Configures if Sergeant Damien random event notifications are enabled",
-            position = 4
-    )
-    default boolean sergeantDamienEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "sergeantDamienEnabled",
+			name = "Sergeant Damien",
+			description = "Configures if Sergeant Damien random event notifications are enabled",
+			position = 4
+	)
+	default boolean sergeantDamienEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "drunkenDwarfEnabled",
-            name = "Drunken Dwarf",
-            description = "Configures if Drunken Dwarf random event notifications are enabled",
-            position = 5
-    )
-    default boolean drunkenDwarfEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "drunkenDwarfEnabled",
+			name = "Drunken Dwarf",
+			description = "Configures if Drunken Dwarf random event notifications are enabled",
+			position = 5
+	)
+	default boolean drunkenDwarfEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "freakyForesterEnabled",
-            name = "Freaky Forester",
-            description = "Configures if Freaky Forester random event notifications are enabled",
-            position = 6
-    )
-    default boolean freakyForesterEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "freakyForesterEnabled",
+			name = "Freaky Forester",
+			description = "Configures if Freaky Forester random event notifications are enabled",
+			position = 6
+	)
+	default boolean freakyForesterEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "frogPrincessEnabled",
-            name = "Frog Princess",
-            description = "Configures if Frog Princess random event notifications are enabled",
-            position = 7
-    )
-    default boolean frogPrincessEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "frogPrincessEnabled",
+			name = "Frog Princess",
+			description = "Configures if Frog Princess random event notifications are enabled",
+			position = 7
+	)
+	default boolean frogPrincessEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "genieEnabled",
-            name = "Genie",
-            description = "Configures if Genie random event notifications are enabled",
-            position = 8
-    )
-    default boolean genieEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "genieEnabled",
+			name = "Genie",
+			description = "Configures if Genie random event notifications are enabled",
+			position = 8
+	)
+	default boolean genieEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "evilBobEnabled",
-            name = "Evil Bob",
-            description = "Configures if Evil Bob random event notifications are enabled",
-            position = 9
-    )
-    default boolean evilBobEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "evilBobEnabled",
+			name = "Evil Bob",
+			description = "Configures if Evil Bob random event notifications are enabled",
+			position = 9
+	)
+	default boolean evilBobEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "postiePeteEnabled",
-            name = "Postie Pete",
-            description = "Configures if Postie Pete random event notifications are enabled",
-            position = 10
-    )
-    default boolean postiePeteEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "postiePeteEnabled",
+			name = "Postie Pete",
+			description = "Configures if Postie Pete random event notifications are enabled",
+			position = 10
+	)
+	default boolean postiePeteEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "leoEnabled",
-            name = "Leo",
-            description = "Configures if Leo random event notifications are enabled",
-            position = 11
-    )
-    default boolean leoEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "leoEnabled",
+			name = "Leo",
+			description = "Configures if Leo random event notifications are enabled",
+			position = 11
+	)
+	default boolean leoEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "mysteriousOldManEnabled",
-            name = "Mysterious Old Man",
-            description = "Configures if Mysterious Old Man random event notifications are enabled",
-            position = 12
-    )
-    default boolean mysteriousOldManEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "mysteriousOldManEnabled",
+			name = "Mysterious Old Man",
+			description = "Configures if Mysterious Old Man random event notifications are enabled",
+			position = 12
+	)
+	default boolean mysteriousOldManEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "pilloryGuardEnabled",
-            name = "Pillory Guard",
-            description = "Configures if Pillory Guard random event notifications are enabled",
-            position = 13
-    )
-    default boolean pilloryGuardEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "pilloryGuardEnabled",
+			name = "Pillory Guard",
+			description = "Configures if Pillory Guard random event notifications are enabled",
+			position = 13
+	)
+	default boolean pilloryGuardEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "flippaEnabled",
-            name = "Flippa",
-            description = "Configures if Flippa random event notifications are enabled",
-            position = 14
-    )
-    default boolean flippaEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "flippaEnabled",
+			name = "Flippa",
+			description = "Configures if Flippa random event notifications are enabled",
+			position = 14
+	)
+	default boolean flippaEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "quizMasterEnabled",
-            name = "Quiz Master",
-            description = "Configures if Quiz Master random event notifications are enabled",
-            position = 15
-    )
-    default boolean quizMasterEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "quizMasterEnabled",
+			name = "Quiz Master",
+			description = "Configures if Quiz Master random event notifications are enabled",
+			position = 15
+	)
+	default boolean quizMasterEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "rickTurpentineEnabled",
-            name = "Rick Turpentine",
-            description = "Configures if Rick Turpentine random event notifications are enabled",
-            position = 16
-    )
-    default boolean rickTurpentineEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "rickTurpentineEnabled",
+			name = "Rick Turpentine",
+			description = "Configures if Rick Turpentine random event notifications are enabled",
+			position = 16
+	)
+	default boolean rickTurpentineEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "sandwichLadyEnabled",
-            name = "Sandwich Lady",
-            description = "Configures if Sandwich Lady random event notifications are enabled",
-            position = 17
-    )
-    default boolean sandwichLadyEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "sandwichLadyEnabled",
+			name = "Sandwich Lady",
+			description = "Configures if Sandwich Lady random event notifications are enabled",
+			position = 17
+	)
+	default boolean sandwichLadyEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "dunceEnabled",
-            name = "Dunce",
-            description = "Configures if Dunce random event notifications are enabled",
-            position = 18
-    )
-    default boolean dunceEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "dunceEnabled",
+			name = "Dunce",
+			description = "Configures if Dunce random event notifications are enabled",
+			position = 18
+	)
+	default boolean dunceEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "strangePlantEnabled",
-            name = "Strange Plant",
-            description = "Configures if Strange Plant random event notifications are enabled",
-            position = 19
-    )
-    default boolean strangePlantEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "strangePlantEnabled",
+			name = "Strange Plant",
+			description = "Configures if Strange Plant random event notifications are enabled",
+			position = 19
+	)
+	default boolean strangePlantEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            keyName = "nilesMilesGilesEnabled",
-            name = "Niles Miles Giles",
-            description = "Configures if Niles Miles Giles random event notifications are enabled",
-            position = 20
-    )
-    default boolean nilesMilesGilesEnabled()
-    {
-        return true;
-    }
+	@ConfigItem(
+			keyName = "nilesMilesGilesEnabled",
+			name = "Niles Miles Giles",
+			description = "Configures if Niles Miles Giles random event notifications are enabled",
+			position = 20
+	)
+	default boolean nilesMilesGilesEnabled()
+	{
+		return true;
+	}
 }
