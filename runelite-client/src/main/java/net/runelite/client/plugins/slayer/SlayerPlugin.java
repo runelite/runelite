ee2c0fcaa0f43db01928fca45b5a1d6b2e70f02c
Davis Cook <daviscook447@gmail.com>
2019-02-12 01:20:10 -0500

    fix checkstyle violation

--- runelite-client/src/main/java/net/runelite/client/plugins/slayer/SlayerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/slayer/SlayerPlugin.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
 import joptsimple.internal.Strings;
 import lombok.AccessLevel;
