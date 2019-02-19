bbe582723f887d4b8aa097d1db242d82f6feb978
JZIMMS_LAPTOP\Jordan <jjz14@pitt.edu>
2019-02-19 06:47:59 -0500

    Update WikiPlugin.java
    Added a proper target for diary guide and removed quick guide from diary menu.

--- runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
@@ -250,7 +250,6 @@ public class WikiPlugin extends Plugin
 					break;
 			}
 		}
-		//System.out.println(ev.getActionParam());
 	}
 
 	private void openSearchInput()
@@ -265,34 +264,58 @@ public class WikiPlugin extends Plugin
 		int widgetIndex = event.getActionParam0();
 		int widgetID = event.getActionParam1();
 
-		if ((!Ints.contains(QUESTLIST_WIDGET_IDS, widgetID) || !"Read Journal:".equals(event.getOption())) &&
-			(!(Ints.compare(DIARYLIST_WIDGET_ID, widgetID) == 0) && (!"Open Ardougne Journal".equals(event.getOption())
-			|| !"Open Desert Journal".equals(event.getOption()) || !"Open Falador Journal".equals(event.getOption())
-			|| !"Open Falador Journal".equals(event.getOption()) || !"Open Fremennik Journal".equals(event.getOption())
-			|| !"Open Kandarin Journal".equals(event.getOption()) || !"Open Karamja Journal".equals(event.getOption())
-			|| !"Open Kourend & Kebos Journal".equals(event.getOption()) || !"Open Lumbridge & Draynor Journal".equals(event.getOption())
-			|| !"Open Morytania Journal".equals(event.getOption()) || !"Open Varrock Journal".equals(event.getOption())
-			|| !"Open Western Provinces Journal".equals(event.getOption()) || !"Open Wilderness Journal".equals(event.getOption()))))
+		//check to see if mouse is pointing to a quest or a diary
+		boolean isQuest = Ints.contains(QUESTLIST_WIDGET_IDS, widgetID) || "Read Journal:".equals(event.getOption());
+		boolean isDiary = Ints.compare(DIARYLIST_WIDGET_ID, widgetID) == 0 && ("Open Ardougne Journal".equals(event.getOption())
+						|| "Open Desert Journal".equals(event.getOption()) || "Open Falador Journal".equals(event.getOption())
+						|| "Open Falador Journal".equals(event.getOption()) || "Open Fremennik Journal".equals(event.getOption())
+						|| "Open Kandarin Journal".equals(event.getOption()) || "Open Karamja Journal".equals(event.getOption())
+						|| "Open Kourend & Kebos Journal".equals(event.getOption()) || "Open Lumbridge & Draynor Journal".equals(event.getOption())
+						|| "Open Morytania Journal".equals(event.getOption()) || "Open Varrock Journal".equals(event.getOption())
+						|| "Open Western Provinces Journal".equals(event.getOption()) || "Open Wilderness Journal".equals(event.getOption()));
+
+		if (!isQuest && !isDiary)
 		{
 			return;
 		}
 
+		//number of menus to add
+		int addMenuNum = 2;
+
+		if (isDiary)
+		{
+			addMenuNum = 1;
+		}
+
 		MenuEntry[] menuEntries = client.getMenuEntries();
-		menuEntries = Arrays.copyOf(menuEntries, menuEntries.length + 2);
+		menuEntries = Arrays.copyOf(menuEntries, menuEntries.length + addMenuNum);
+
 
 		MenuEntry menuEntry = menuEntries[menuEntries.length - 1] = new MenuEntry();
-		menuEntry.setTarget(event.getTarget());
+		if (isQuest)
+		{
+			menuEntry.setTarget(event.getTarget());
+		}
+		else if (isDiary)
+		{
+			String diaryName = event.getOption().substring(5, (event.getOption().length() - 8));
+			menuEntry.setTarget(diaryName + " Diary");
+		}
 		menuEntry.setOption(MENUOP_GUIDE);
 		menuEntry.setParam0(widgetIndex);
 		menuEntry.setParam1(widgetID);
 		menuEntry.setType(MenuAction.RUNELITE.getId());
 
-		menuEntry = menuEntries[menuEntries.length - 2] = new MenuEntry();
-		menuEntry.setTarget(event.getTarget());
-		menuEntry.setOption(MENUOP_QUICKGUIDE);
-		menuEntry.setParam0(widgetIndex);
-		menuEntry.setParam1(widgetID);
-		menuEntry.setType(MenuAction.RUNELITE.getId());
+		//Diaries don't have a quickguide
+		if (isQuest)
+		{
+			menuEntry = menuEntries[menuEntries.length - 2] = new MenuEntry();
+			menuEntry.setTarget(event.getTarget());
+			menuEntry.setOption(MENUOP_QUICKGUIDE);
+			menuEntry.setParam0(widgetIndex);
+			menuEntry.setParam1(widgetID);
+			menuEntry.setType(MenuAction.RUNELITE.getId());
+		}
 
 		client.setMenuEntries(menuEntries);
 	}
