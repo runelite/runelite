43cc546fba9044b627eea01edd0e3cc129705e58
JZIMMS_LAPTOP\Jordan <jjz14@pitt.edu>
2019-02-19 23:39:33 -0500

    Update WikiPlugin.java
    Added a regex pattern to match diary names and added menu option "Wiki" .

--- runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
@@ -58,6 +58,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.LinkBrowser;
 import net.runelite.client.util.Text;
 import okhttp3.HttpUrl;
+import java.util.regex.Pattern;
 
 @Slf4j
 @PluginDescriptor(
@@ -85,8 +86,8 @@ public class WikiPlugin extends Plugin
 	private static final String MENUOP_QUICKGUIDE = "Quick Guide";
 	private static final String MENUOP_WIKI = "Wiki";
 
-	private static final Pattern SKILL_REGEX = Pattern.compile("([A-Za-z]+) guide");
-	private static final Pattern DIARY_REGEX = Pattern.compile("([A-Za-z &]+) Journal");
+	private static final String DIARY_REGEX = "Open [\\w &]+ Journal";
+
 
 	@Inject
 	private SpriteManager spriteManager;
@@ -282,6 +283,8 @@ public class WikiPlugin extends Plugin
 				case MENUOP_QUICKGUIDE:
 					quickguide = true;
 					//fallthrough;
+				case MENUOP_WIKI:
+					//fallthrough;
 				case MENUOP_GUIDE:
 					ev.consume();
 					String quest = Text.removeTags(ev.getMenuTarget());
@@ -334,13 +337,7 @@ public class WikiPlugin extends Plugin
 
 		//check to see if mouse is pointing to a quest or a diary
 		boolean isQuest = Ints.contains(QUESTLIST_WIDGET_IDS, widgetID) || "Read Journal:".equals(event.getOption());
-		boolean isDiary = Ints.compare(DIARYLIST_WIDGET_ID, widgetID) == 0 && ("Open Ardougne Journal".equals(event.getOption())
-						|| "Open Desert Journal".equals(event.getOption()) || "Open Falador Journal".equals(event.getOption())
-						|| "Open Falador Journal".equals(event.getOption()) || "Open Fremennik Journal".equals(event.getOption())
-						|| "Open Kandarin Journal".equals(event.getOption()) || "Open Karamja Journal".equals(event.getOption())
-						|| "Open Kourend & Kebos Journal".equals(event.getOption()) || "Open Lumbridge & Draynor Journal".equals(event.getOption())
-						|| "Open Morytania Journal".equals(event.getOption()) || "Open Varrock Journal".equals(event.getOption())
-						|| "Open Western Provinces Journal".equals(event.getOption()) || "Open Wilderness Journal".equals(event.getOption()));
+		boolean isDiary = Ints.compare(DIARYLIST_WIDGET_ID, widgetID) == 0 && Pattern.matches(DIARY_REGEX, event.getOption());
 
 		if (!isQuest && !isDiary)
 		{
@@ -379,13 +376,14 @@ public class WikiPlugin extends Plugin
 		if (isQuest)
 		{
 			menuEntry.setTarget(event.getTarget());
+			menuEntry.setOption(MENUOP_GUIDE);
 		}
 		else if (isDiary)
 		{
 			String diaryName = event.getOption().substring(5, (event.getOption().length() - 8));
 			menuEntry.setTarget(diaryName + " Diary");
+			menuEntry.setOption(MENUOP_WIKI);
 		}
-		menuEntry.setOption(MENUOP_GUIDE);
 		menuEntry.setParam0(widgetIndex);
 		menuEntry.setParam1(widgetID);
 		menuEntry.setType(MenuAction.RUNELITE.getId());
