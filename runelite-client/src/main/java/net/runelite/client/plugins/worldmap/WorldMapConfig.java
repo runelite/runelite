755577947b76315f64ae7373458267480dd311ad
Yani <yani@xenokore.com>
2019-02-22 16:14:55 +0100

    worldmap plugin: implement waypoint feature

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/WorldMapConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/WorldMapConfig.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2018, Morgan Lewis <https://github.com/MESLewis>
+ * Copyright (c) 2019, Yani <yani@xenokore.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -204,6 +204,17 @@ public interface WorldMapConfig extends Config
 		position = 16
 	)
 	default boolean rareTreeLevelIcon()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "isWaypointEnabled",
+			name = "Enable waypoint",
+			description = "Allows you to set a waypoint by double clicking on the worldmap",
+			position = 17
+	)
+	default boolean isWaypointEnabled()
 	{
 		return true;
 	}
