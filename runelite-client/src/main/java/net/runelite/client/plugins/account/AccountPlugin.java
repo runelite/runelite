8597d3b693db57ee47e62b0d945fe3c05571fc86
Hydrox6 <ikada@protonmail.ch>
2019-02-28 22:50:19 +0000

    Add mapping for username to display name

--- runelite-client/src/main/java/net/runelite/client/plugins/account/AccountPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/account/AccountPlugin.java
@@ -24,15 +24,27 @@
  */
 package net.runelite.client.plugins.account;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
 import java.awt.image.BufferedImage;
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.inject.Inject;
 import javax.swing.JOptionPane;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.Player;
+import net.runelite.api.WorldType;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.api.events.SessionClose;
 import net.runelite.api.events.SessionOpen;
+import net.runelite.api.vars.AccountType;
 import net.runelite.client.account.AccountSession;
 import net.runelite.client.account.SessionManager;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -49,6 +61,15 @@ import net.runelite.client.util.ImageUtil;
 @Slf4j
 public class AccountPlugin extends Plugin
 {
+	private static final String CONFIG_GROUP = "account";
+	private static final Set<WorldType> DIFFERENT_SAVE_WORLD_TYPES = ImmutableSet.of(WorldType.DEADMAN, WorldType.DEADMAN_TOURNAMENT, WorldType.SEASONAL_DEADMAN, WorldType.TOURNAMENT);
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private ConfigManager configManager;
+
 	@Inject
 	private SessionManager sessionManager;
 
@@ -60,6 +81,7 @@ public class AccountPlugin extends Plugin
 
 	private NavigationButton loginButton;
 	private NavigationButton logoutButton;
+	private boolean updateMapping;
 
 	private static final BufferedImage LOGIN_IMAGE, LOGOUT_IMAGE;
 
@@ -103,6 +125,7 @@ public class AccountPlugin extends Plugin
 	{
 		clientToolbar.removeNavigation(loginButton);
 		clientToolbar.removeNavigation(logoutButton);
+		updateMapping = false;
 	}
 
 	private void loginClick()
@@ -141,4 +164,45 @@ public class AccountPlugin extends Plugin
 		addAndRemoveButtons();
 	}
 
+	@Subscribe
+	public void onGameTick(GameTick event)
+	{
+		if (updateMapping)
+		{
+			updateMapping = false;
+
+			final Player player = client.getLocalPlayer();
+
+			if (player == null)
+			{
+				return;
+			}
+
+			final String name = player.getName();
+
+			if (Strings.isNullOrEmpty(name))
+			{
+				return;
+			}
+
+			configManager.setConfiguration(CONFIG_GROUP + "." + client.getUsername(), "name", name);
+
+			EnumSet<WorldType> worldTypes = client.getWorldType();
+			worldTypes.retainAll(DIFFERENT_SAVE_WORLD_TYPES);
+			if (worldTypes.size() == 0)
+			{
+				AccountType type = client.getAccountType();
+				configManager.setConfiguration(CONFIG_GROUP + "." + client.getUsername(), "type", type);
+			}
+		}
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState() == GameState.LOGGED_IN)
+		{
+			updateMapping = true;
+		}
+	}
 }
