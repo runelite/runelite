fdbe75a48680829ac074cfa7d25f8d2017a917e7
Adam <Adam@sigterm.info>
2019-03-02 18:59:21 -0500

    clanchat plugin: add join/leave messages
    Co-authored-by: trimbe <benno.tremback@gmail.com>

--- runelite-api/src/main/java/net/runelite/api/ScriptID.java
+++ runelite-api/src/main/java/net/runelite/api/ScriptID.java
@@ -46,6 +46,11 @@ public final class ScriptID
 	public static final int CHATBOX_INPUT = 96;
 
 	/**
+	 * Rebuilds the chatbox
+	 */
+	public static final int BUILD_CHATBOX = 216;
+
+	/**
 	 * Opens the Private Message chat interface
 	 *
 	 * Jagex refers to this script as {@code meslayer_mode6}
