0682ad737ca3cd601dded5bdf85ba73d83a3116e
Bart Vollebregt <bartvollebregt@hotmail.com>
2019-03-23 13:25:31 +0100

    Fixed prayer bonus

--- runelite-client/src/main/java/net/runelite/client/plugins/maxhit/config/PrayerBonusConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/maxhit/config/PrayerBonusConfig.java
@@ -33,8 +33,8 @@ public enum PrayerBonusConfig
 	SUPERHUMAN_STRENGTH(MaxHitCalculator.CombatMethod.MELEE, Varbits.PRAYER_SUPERHUMAN_STRENGTH, 0.1),
 	ULTIMATE_STRENGTH(MaxHitCalculator.CombatMethod.MELEE, Varbits.PRAYER_ULTIMATE_STRENGTH, 0.15),
 	CHIVALRY(MaxHitCalculator.CombatMethod.MELEE, Varbits.PRAYER_CHIVALRY, 0.18),
+	PIETY(MaxHitCalculator.CombatMethod.MELEE, Varbits.PRAYER_PIETY, 0.23),
 
-	PIETY(MaxHitCalculator.CombatMethod.RANGE, Varbits.PRAYER_PIETY, 0.23),
 	SHARP_EYE(MaxHitCalculator.CombatMethod.RANGE, Varbits.PRAYER_SHARP_EYE, 0.05),
 	HAWK_EYE(MaxHitCalculator.CombatMethod.RANGE, Varbits.PRAYER_HAWK_EYE, 0.1),
 	EAGLE_EYE(MaxHitCalculator.CombatMethod.RANGE, Varbits.PRAYER_EAGLE_EYE, 0.15),
