c28746a9022002a292e051338469cc6ea5c408a8
Bart Vollebregt <bartvollebregt@hotmail.com>
2019-03-23 13:26:40 +0100

    Fixed magix max hit tests

---
+++ runelite-client/src/test/java/net/runelite/client/plugins/maxhit/calculators/MaxHitCalculatorTest.java
@@ -0,0 +1,41 @@
+package net.runelite.client.plugins.maxhit.calculators;
+
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import net.runelite.api.Client;
+import net.runelite.client.plugins.maxhit.calculators.testconfig.MagicMaxHitConfig;
+import net.runelite.client.plugins.maxhit.calculators.testconfig.MaxHitConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MaxHitCalculatorTest
+{
+	@Mock
+	@Bind
+	protected Client client;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
+	}
+
+	@Test
+	public void calculate()
+	{
+		testMaxHitConfig(MagicMaxHitConfig.values());
+	}
+
+	private void testMaxHitConfig(MaxHitConfig[] maxHitConfigs)
+	{
+		for (MaxHitConfig maxHitConfig : maxHitConfigs)
+		{
+			maxHitConfig.test(client);
+		}
+	}
+}
---
+++ runelite-client/src/test/java/net/runelite/client/plugins/maxhit/calculators/testconfig/MaxHitConfig.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.maxhit.calculators.testconfig;
+
+import net.runelite.api.Client;
+
+public interface MaxHitConfig
+{
+	void test(Client client);
+}
