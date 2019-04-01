c415902b6d6b62592c3f4e5ebcbad8dce8be9a08
abelbriggs1 <abelbriggs1@hotmail.com>
2019-04-01 18:29:20 -0500

    Fixed mistake - forgot to stage changes.

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/config/ValueCalculationMode.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/config/ValueCalculationMode.java
@@ -1,5 +1,21 @@
 package net.runelite.client.plugins.grounditems.config;
 
-public class ValueCalculationMode
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ValueCalculationMode
 {
+    HA("High Alchemy"), // calc highlight by HA value
+    GE("Grand Exchange"), // calc by GE
+    BOTH("Both"); // use whatever is highest.
+
+    private final String name;
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
 }
