290a4166663c83e07560fe29f7cf220ba283c6cf
soyas <genetic-soybean@users.noreplay.github.com>
2019-04-06 00:06:10 +0200

    removes draw distance

--- runelite-client/src/main/java/net/runelite/client/plugins/groundmarkers/GroundMarkerOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/groundmarkers/GroundMarkerOverlay.java
@@ -43,8 +43,6 @@ import net.runelite.client.ui.overlay.OverlayUtil;
 
 public class GroundMarkerOverlay extends Overlay
 {
-	private static final int MAX_DRAW_DISTANCE = 32;
-
 	private final Client client;
 	private final GroundMarkerConfig config;
 	private final GroundMarkerPlugin plugin;
@@ -87,13 +85,6 @@ public class GroundMarkerOverlay extends Overlay
 
 	private void drawTile(Graphics2D graphics, WorldPoint point, Color color)
 	{
-		WorldPoint playerLocation = client.getLocalPlayer().getWorldLocation();
-
-		if (point.distanceTo(playerLocation) >= MAX_DRAW_DISTANCE)
-		{
-			return;
-		}
-
 		LocalPoint lp = LocalPoint.fromWorld(client, point);
 		if (lp == null)
 		{
