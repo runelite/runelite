2bfe1898543848e89bf6c54be3bfb35c04f5fab0
winterdaze <35933468+winterdaze-rs@users.noreply.github.com>
2019-04-09 16:20:20 -0400

    Add Fight Cave and Inferno timers

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/tzhaartimers/TzhaarTimers.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2019, winterdaze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.tzhaartimers;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.ui.overlay.infobox.InfoBox;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class TzhaarTimers extends InfoBox
+{
+	private final Instant startTime;
+	private LocalTime time;
+	private Instant lastTime;
+
+	public TzhaarTimers(BufferedImage image, TzhaarTimersPlugin plugin, Instant startTime, Instant lastTime)
+	{
+		super(image, plugin);
+		this.startTime = startTime;
+		this.lastTime = lastTime;
+	}
+
+	@Override
+	public String getText()
+	{
+		if (startTime == null)
+		{
+			return "";
+		}
+
+		if (lastTime == null)
+		{
+			Duration elapsed = Duration.between(startTime, Instant.now());
+			time = LocalTime.ofSecondOfDay(elapsed.getSeconds());
+		}
+		else
+		{
+			Duration elapsed = Duration.between(startTime, lastTime);
+			time = LocalTime.ofSecondOfDay(elapsed.getSeconds());
+		}
+
+		if (time.getHour() > 0)
+		{
+			return time.format(DateTimeFormatter.ofPattern("HH:mm"));
+		}
+		return time.format(DateTimeFormatter.ofPattern("mm:ss"));
+	}
+
+	@Override
+	public Color getTextColor()
+	{
+		return Color.WHITE;
+	}
+
+	@Override
+	public String getTooltip()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("Elapsed time: ");
+		builder.append(time.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+
+		return builder.toString();
+	}
+}
