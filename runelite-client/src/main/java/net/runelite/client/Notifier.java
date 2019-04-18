32b848b449f3816870fed1ba2f3f581aed88d0d4
AraxxingRS <araxxingrs@gmail.com>
2019-04-18 01:30:39 +0100

    fix unneeded space

--- runelite-client/src/main/java/net/runelite/client/Notifier.java
+++ runelite-client/src/main/java/net/runelite/client/Notifier.java
@@ -43,7 +43,6 @@ import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Singleton;
-
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
