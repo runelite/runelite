8bfc0f2b211191f9c0ca812369fd76df34fee424
Adam <Adam@sigterm.info>
2019-04-19 14:27:49 -0400

    widgetitem: associate Widget with WidgetItem

--- runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -1355,7 +1355,7 @@ public abstract class RSClientMixin implements RSClient
 			{
 				if (renderX >= minX && renderX <= maxX && renderY >= minY && renderY <= maxY)
 				{
-					WidgetItem widgetItem = new WidgetItem(widget.getItemId(), widget.getItemQuantity(), -1, widget.getBounds());
+					WidgetItem widgetItem = new WidgetItem(widget.getItemId(), widget.getItemQuantity(), -1, widget.getBounds(), widget);
 					callbacks.drawItem(widget.getItemId(), widgetItem);
 				}
 			}
