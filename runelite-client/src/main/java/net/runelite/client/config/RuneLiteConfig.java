bfe14827051d084c4b4e250906b5c28e6b7d321f
Ganom <theariall@gmail.com>
2019-04-18 23:22:30 -0400

    Adding external plugin support (#4)
    * Adding archetype
    
    * Update RuneLiteConfig.java
    
    * Update Plugin.java
    
    * Update PluginManager.java
    
    * Adding pluginwatcher & classloader
    
    * Update RuneLite.java
    
    * Update pom.xml
    
    * Update settings.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Removing old example plugin
    
    * Fixing the fix of the fix for plugin archetype.

--- runelite-client/src/main/java/net/runelite/client/config/RuneLiteConfig.java
+++ runelite-client/src/main/java/net/runelite/client/config/RuneLiteConfig.java
@@ -64,6 +64,17 @@ public interface RuneLiteConfig extends Config
 	}
 
 	@ConfigItem(
+		keyName = "enablePlugins",
+		name = "Enable loading of external plugins",
+		description = "Enable loading of external plugins",
+			position = 10
+	)
+	default boolean enablePlugins()
+	{
+		return false;
+	}
+
+	@ConfigItem(
 		keyName = "containInScreen",
 		name = "Contain in screen",
 		description = "Makes the client stay contained in the screen when attempted to move out of it.<br>Note: Only works if custom chrome is enabled.",
