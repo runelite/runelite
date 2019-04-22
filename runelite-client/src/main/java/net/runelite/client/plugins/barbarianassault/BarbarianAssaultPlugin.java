545251819d6f57a079b3ac550fe12201f1563f58
Jonathan <48493132+Raieverr@users.noreply.github.com>
2019-04-22 14:23:45 -0600

    Type Change

--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
@@ -64,7 +64,8 @@ import net.runelite.client.util.ImageUtil;
 @PluginDescriptor(
 	name = "Barbarian Assault",
 	description = "Show a timer to the next call change and game/wave duration in chat.",
-	tags = {"minigame", "overlay", "timer"}
+	tags = {"minigame", "overlay", "timer"},
+	type = "utility"
 )
 public class BarbarianAssaultPlugin extends Plugin {
 	private static final int BA_WAVE_NUM_INDEX = 2;
