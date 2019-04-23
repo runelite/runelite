b0cb024a1afef0f3c5b69d2ca881b7522c32494a
Scott Burns <sd.burns1998@gmail.com>
2019-04-23 04:00:39 +0200

    Add back drawQuantity to InventorySetupBankOverlay

--- runelite-client/src/main/java/net/runelite/client/plugins/inventorysetups/InventorySetupBankOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/inventorysetups/InventorySetupBankOverlay.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.widgets.WidgetItem;
 import net.runelite.client.game.ItemManager;
+import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
 
 @Slf4j
@@ -51,7 +52,25 @@ public class InventorySetupBankOverlay extends WidgetItemOverlay
 				Rectangle bounds = itemWidget.getCanvasBounds();
 				final BufferedImage outline = itemManager.getItemOutline(itemId, itemWidget.getQuantity(), color);
 				graphics.drawImage(outline, (int) bounds.getX() + 1, (int) bounds.getY() + 1, null);
+
+				if (itemWidget.getQuantity() > 1)
+				{
+					drawQuantity(graphics, itemWidget, Color.YELLOW);
+				}
+				else if (itemWidget.getQuantity() == 0)
+				{
+					drawQuantity(graphics, itemWidget, Color.YELLOW.darker());
+				}
 			}
 		}
+	}
+
+	private void drawQuantity(Graphics2D graphics, WidgetItem item, Color darker)
+	{
+		graphics.setColor(Color.BLACK);
+		graphics.drawString(String.valueOf(item.getQuantity()), item.getCanvasLocation().getX() + 2, item.getCanvasLocation().getY() + 11);
+		graphics.setColor(darker);
+		graphics.setFont(FontManager.getRunescapeSmallFont());
+		graphics.drawString(String.valueOf(item.getQuantity()), item.getCanvasLocation().getX() + 1, item.getCanvasLocation().getY() + 10);
 	}
 }
