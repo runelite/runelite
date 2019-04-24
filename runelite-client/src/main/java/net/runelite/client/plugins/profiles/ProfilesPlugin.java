65f4b33f6273b64230c78e55b7ea4bcdbccbb655
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-04-24 02:02:13 +0100

    Add encryption to account data - thx ThatGamerBlue

--- runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPlugin.java
@@ -1,10 +1,30 @@
 /*
- * Decompiled with CFR 0.139.
+ * Copyright (c) 2019, Spedwards <https://github.com/Spedwards>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package net.runelite.client.plugins.profiles;
 
 import com.google.inject.Provides;
-import java.awt.image.BufferedImage;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -14,9 +34,10 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import javax.inject.Inject;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.api.events.ConfigChanged;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -25,112 +46,85 @@ import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
+import javax.inject.Inject;
+import java.awt.image.BufferedImage;
 
 @PluginDescriptor(
-   
-    name="Account Switcher",
-    description="Allow for a allows you to easily switch between multiple OSRS Accounts", 
-    tags={"profile", "account", "login", "log in"},
-    type = PluginType.UTILITY
+	name = "Account Switcher",
+	description = "Allow for a allows you to easily switch between multiple OSRS Accounts",
+	tags = {"profile", "account", "login", "log in", "pklite"},
+	type = PluginType.UTILITY
 )
+public class ProfilesPlugin extends Plugin
+{
+	@Inject
+	private ClientToolbar clientToolbar;
 
-public class ProfilesPlugin
-extends Plugin {
-    @Inject
-    private ClientToolbar clientToolbar;
-    @Inject
-    private Client client;
-    @Inject
-    private ProfilesConfig config;
-    private ProfilesPanel panel;
-    private NavigationButton navButton;
-    String text = "Hello World";
-    private static String key = "Bar12345Bar12345";
-    private static Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+	@Inject
+	private Client client;
 
-    @Provides
-    ProfilesConfig getConfig(ConfigManager configManager) {
-        return configManager.getConfig(ProfilesConfig.class);
-    }
+	@Inject
+	private ProfilesConfig config;
 
-    @Override
-    protected void startUp() throws Exception {
-        this.panel = this.injector.getInstance(ProfilesPanel.class);
-        BufferedImage icon = ImageUtil.getResourceStreamFromClass(this.getClass(), "/net/runelite/client/plugins/profiles/profiles_icon.png");
-        this.navButton = NavigationButton.builder().tooltip("Profiles").icon(icon).priority(8).panel(this.panel).build();
-        this.clientToolbar.addNavigation(this.navButton);
-    }
 
-    @Override
-    protected void shutDown() {
-        this.clientToolbar.removeNavigation(this.navButton);
-    }
-    
-    @Subscribe
-    private void onConfigChanged(ConfigChanged event) throws Exception {
-        if (event.getGroup().equals("profiles") && event.getKey().equals("rememberPassword")) {
-            this.panel = this.injector.getInstance(ProfilesPanel.class);
-            this.shutDown();
-            this.startUp();
-        }
-    }
+	private ProfilesPanel panel;
+	private NavigationButton navButton;
 
-    public static String decryptText(String text) {
-        byte[] bb = new byte[text.length()];
-        for (int i = 0; i < text.length(); ++i) {
-            bb[i] = (byte)text.charAt(i);
-        }
-        Cipher cipher = null;
-        try {
-            cipher = Cipher.getInstance("AES");
-        }
-        catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
-        try {
-            cipher.init(2, aesKey);
-        }
-        catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
-        try {
-            Logger.getLogger("EncryptionLogger").info("Decrypted " + text + " to " + new String(cipher.doFinal(bb)));
-            return new String(cipher.doFinal(bb));
-        }
-        catch (BadPaddingException | IllegalBlockSizeException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
 
-    public static String encryptText(String text) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(1, aesKey);
-            byte[] encrypted = cipher.doFinal(text.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : encrypted) {
-                sb.append((char)b);
-            }
-            Logger.getLogger("EncryptionLogger").info("Encrypted " + text + " to " + sb.toString());
-            return sb.toString();
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
-        catch (BadPaddingException e) {
-            e.printStackTrace();
-        }
-        catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        }
-        catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+	@Provides
+	ProfilesConfig getConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(ProfilesConfig.class);
+	}
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		panel = injector.getInstance(ProfilesPanel.class);
+
+		final BufferedImage icon = ImageUtil.getResourceStreamFromClass(getClass(), "profiles_icon.png");
+
+		navButton = NavigationButton.builder()
+			.tooltip("Profiles")
+			.icon(icon)
+			.priority(8)
+			.panel(panel)
+			.build();
+
+		clientToolbar.addNavigation(navButton);
+
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		clientToolbar.removeNavigation(navButton);
+	}
+
+	@Subscribe
+	void onGameStateChanged(GameStateChanged event)
+	{
+		if (event.getGameState().equals(GameState.LOGIN_SCREEN) && config.switchPanel())
+		{
+			if (!navButton.isSelected())
+			{
+				navButton.getOnSelect().run();
+			}
+		}
+	}
+
+	@Subscribe
+	private void onConfigChanged(ConfigChanged event) throws Exception
+	{
+		if (event.getGroup().equals("profiles"))
+		{
+			if (event.getKey().equals("rememberPassword"))
+			{
+				panel = injector.getInstance(ProfilesPanel.class);
+				this.shutDown();
+				this.startUp();
+			}
+		}
+	}
+
 }
-
