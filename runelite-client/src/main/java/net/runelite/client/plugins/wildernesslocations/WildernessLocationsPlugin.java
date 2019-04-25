01ab3e1c222d1bdc71a2f1a494f5587aa99db713
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-04-25 21:56:37 +0100

    Update WildernessLocationsPlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/wildernesslocations/WildernessLocationsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wildernesslocations/WildernessLocationsPlugin.java
@@ -22,7 +22,7 @@ import net.runelite.client.util.WildernessLocation;
 @PluginDescriptor(
 		name = "Wild Locations",
 		description = "Indicates the players current location in the wild",
-		tags = {"Wildy", "Wilderness Location", "location", "loc", "pvp", "pklite"}
+		tags = {"Wildy", "Wilderness Location", "location", "loc", "pvp", "pklite"},
 		type = PluginType.PVP
 )
 public class WildernessLocationsPlugin extends Plugin
