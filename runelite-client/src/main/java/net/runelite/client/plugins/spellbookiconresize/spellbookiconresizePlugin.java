b297e3e48701161151b7a58d52f1be3de98b7120
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-04-26 00:08:37 +0100

    Update spellbookiconresizePlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/spellbookiconresize/spellbookiconresizePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/spellbookiconresize/spellbookiconresizePlugin.java
@@ -22,6 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package net.runelite.client.plugins.spellbookiconresize;
 
 import com.google.inject.Provides;
