5b8af0cb317478f44d2b4e6a2ce4cc5535f397bf
Ryan <rlw0014@gmail.com>
2019-04-25 23:45:14 -0400

    Restart total points each wave 1

--- runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
@@ -81,7 +81,7 @@ public class BarbarianAssaultPlugin extends Plugin
 
 	private boolean hasAnnounced;
 
-	private ArrayList<Integer> widgetsTextList = new ArrayList<Integer>();
+	private ArrayList<Integer> widgetsTextList = new ArrayList<>();
 
 	String[] descriptions = {"Runners: ",
 			"Hitpoints: ",
@@ -218,6 +218,7 @@ public class BarbarianAssaultPlugin extends Plugin
 				gameTime = new GameTimer();
 				totalHpHealed = 0;
 				totalCollectedEggCount = 0;
+				widgetsTextList = null;
 			}
 			else if (gameTime != null)
 			{
