657bb52b1a00624adeada02003d5e4b0d3534ad2
Ashy <anon>
2019-05-05 02:55:07 +0100

    Add Drakes Breath to Aoe/Projectile Warnings

--- runelite-client/src/main/java/net/runelite/client/plugins/aoewarnings/AoeWarningConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/aoewarnings/AoeWarningConfig.java
@@ -250,4 +250,13 @@ public interface AoeWarningConfig extends Config
 	{
 		return true;
 	}
+
+    @ConfigItem(
+            keyName = "drake",
+            name = "Drakes Breath",
+            description = "Configures if Drakes Breath tile markers are displayed"
+    )
+    default boolean isDrakeEnabled() {
+        return true;
+    }
 }
