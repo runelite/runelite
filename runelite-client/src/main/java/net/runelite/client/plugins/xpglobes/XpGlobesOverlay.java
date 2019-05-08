53ab28c667694a98de83127e4b6aba201fbfb02a
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-05-08 09:44:06 +0100

    Update XpGlobesOverlay.java

--- runelite-client/src/main/java/net/runelite/client/plugins/xpglobes/XpGlobesOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/xpglobes/XpGlobesOverlay.java
@@ -293,12 +293,15 @@ public class XpGlobesOverlay extends Overlay
 					.build());
 			}
 
-			String timeLeft = xpTrackerService.getTimeTillGoal(mouseOverSkill.getSkill());
-			xpTooltip.getChildren().add(LineComponent.builder()
-				.left("Time left:")
-				.leftColor(Color.ORANGE)
-				.right(timeLeft)
-				.build());
+			if (config.enableTimeToLevel())
+			{
+				String timeLeft = xpTrackerService.getTimeTillGoal(mouseOverSkill.getSkill());
+				xpTooltip.getChildren().add(LineComponent.builder()
+					.left("Time left:")
+					.leftColor(Color.ORANGE)
+					.right(timeLeft)
+					.build());
+			}
 		}
 
 		xpTooltip.render(graphics);
