649a90001e7939a83bace044393a651a481f6a28
redrumze <jzomerlei@gmail.com>
2019-05-09 02:10:56 -0400

    Fixed Style

--- runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
@@ -584,7 +584,7 @@ public class ChatCommandsPlugin extends Plugin
 	 */
 	private void doMath(ChatMessage chatMessage, String message)
 	{
-		if(!config.enableMath())
+		if (!config.enableMath())
 		{
 			return;
 		}
@@ -1183,24 +1183,29 @@ public class ChatCommandsPlugin extends Plugin
 	 * @param str Input equation as String
 	 * @return output double value
 	 */
-	public static double eval(final String str) {
+	public static double eval(final String str)
+	{
 		return new Object() {
 			int pos = -1, ch;
 
-			void nextChar() {
+			void nextChar()
+			{
 				ch = (++pos < str.length()) ? str.charAt(pos) : -1;
 			}
 
-			boolean eat(int charToEat) {
+			boolean eat(int charToEat)
+			{
 				while (ch == ' ') nextChar();
-				if (ch == charToEat) {
+				if (ch == charToEat)
+				{
 					nextChar();
 					return true;
 				}
 				return false;
 			}
 
-			double parse() {
+			double parse()
+			{
 				nextChar();
 				double x = parseExpression();
 				if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
@@ -1213,37 +1218,56 @@ public class ChatCommandsPlugin extends Plugin
 			// factor = `+` factor | `-` factor | `(` expression `)`
 			//        | number | functionName factor | factor `^` factor
 
-			double parseExpression() {
+			double parseExpression()
+			{
 				double x = parseTerm();
-				for (;;) {
+				for (;;)
+				{
 					if      (eat('+')) x += parseTerm(); // addition
 					else if (eat('-')) x -= parseTerm(); // subtraction
 					else return x;
 				}
 			}
 
-			double parseTerm() {
+			double parseTerm()
+			{
 				double x = parseFactor();
-				for (;;) {
-					if      (eat('*')) x *= parseFactor(); // multiplication
-					else if (eat('/')) x /= parseFactor(); // division
-					else return x;
+				for (;;)
+				{
+					if (eat('*'))
+					{
+						x *= parseFactor(); // multiplication
+					}
+					else if (eat('/'))
+					{
+						x /= parseFactor(); // division
+					}
+					else
+					{
+						return x;
+					}
 				}
 			}
 
-			double parseFactor() {
+			double parseFactor()
+			{
 				if (eat('+')) return parseFactor(); // unary plus
 				if (eat('-')) return -parseFactor(); // unary minus
 
 				double x;
 				int startPos = this.pos;
-				if (eat('(')) { // parentheses
+				if (eat('(')) // parentheses
+				{
 					x = parseExpression();
 					eat(')');
-				} else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+				}
+				else if ((ch >= '0' && ch <= '9') || ch == '.') // numbers
+				{
 					while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
 					x = Double.parseDouble(str.substring(startPos, this.pos));
-				} else if (ch >= 'a' && ch <= 'z') { // functions
+				}
+				else if (ch >= 'a' && ch <= 'z') // functions
+				{
 					while (ch >= 'a' && ch <= 'z') nextChar();
 					String func = str.substring(startPos, this.pos);
 					x = parseFactor();
@@ -1252,7 +1276,9 @@ public class ChatCommandsPlugin extends Plugin
 					else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
 					else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
 					else throw new RuntimeException("Unknown function: " + func);
-				} else {
+				}
+				else
+				{
 					throw new RuntimeException("Unexpected: " + (char)ch);
 				}
 
