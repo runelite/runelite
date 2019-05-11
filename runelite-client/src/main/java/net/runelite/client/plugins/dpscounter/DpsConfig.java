75f835f3221e9f365374fde404c01310198e1536
Adam <Adam@sigterm.info>
2019-05-11 15:43:47 -0400

    add config

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/dpscounter/DpsConfig.java
@@ -0,0 +1,19 @@
+package net.runelite.client.plugins.dpscounter;
+
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("dpscounter")
+public interface DpsConfig
+{
+	@ConfigItem(
+		position = 0,
+		name = "Show Damage",
+		keyName = "showDamage",
+		description = "Show total damage instead of DPS"
+	)
+	default boolean showDamage()
+	{
+		return false;
+	}
+}
