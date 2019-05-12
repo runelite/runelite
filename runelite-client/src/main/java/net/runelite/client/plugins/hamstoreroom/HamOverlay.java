28ee0e2bbc3c0a1cd14ebf7ce5dbb7f4cde2e85b
Barragek0 <barragek0lol@gmail.com>
2019-05-12 20:13:36 +0100

    Ham Store Room plugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/hamstoreroom/HamOverlay.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019, Alex <https://github.com/Barragek0>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.hamstoreroom;
+
+import java.awt.Color;
+import net.runelite.api.Client;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPosition;
+
+import javax.inject.Inject;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import net.runelite.client.ui.overlay.OverlayUtil;
+
+class HamOverlay extends Overlay
+{
+	private final Client client;
+	private final HamPlugin hamPlugin;
+
+	@Inject
+	private HamOverlay(Client client, HamPlugin hamPlugin)
+	{
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.ABOVE_SCENE);
+		this.client = client;
+		this.hamPlugin = hamPlugin;
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		if (client.getLocalPlayer().getWorldLocation().getRegionID() != 10321)
+		{
+			return null;
+		}
+
+		if (hamPlugin.steelObject.getCanvasTilePoly() != null)
+		{
+			OverlayUtil.renderPolygon(graphics, hamPlugin.steelObject.getCanvasTilePoly(), hamPlugin.drawSteel ? Color.GREEN.brighter() : Color.RED.brighter());
+		}
+		if (hamPlugin.ironObject.getCanvasTilePoly() != null)
+		{
+			OverlayUtil.renderPolygon(graphics, hamPlugin.ironObject.getCanvasTilePoly(), hamPlugin.drawIron ? Color.GREEN.brighter() : Color.RED.brighter());
+		}
+		if (hamPlugin.silverObject.getCanvasTilePoly() != null)
+		{
+			OverlayUtil.renderPolygon(graphics, hamPlugin.silverObject.getCanvasTilePoly(), hamPlugin.drawSilver ? Color.GREEN.brighter() : Color.RED.brighter());
+		}
+		if (hamPlugin.bronzeObject.getCanvasTilePoly() != null)
+		{
+			OverlayUtil.renderPolygon(graphics, hamPlugin.bronzeObject.getCanvasTilePoly(), hamPlugin.drawBronze ? Color.GREEN.brighter() : Color.RED.brighter());
+		}
+		return null;
+	}
+
+
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/hamstoreroom/HamPlugin.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2019, Alex <https://github.com/Barragek0>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.hamstoreroom;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import net.runelite.api.Client;
+import net.runelite.api.GameObject;
+import net.runelite.api.InventoryID;
+import net.runelite.api.Item;
+import net.runelite.api.ItemID;
+import net.runelite.api.ObjectID;
+import net.runelite.api.events.GameObjectSpawned;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+	name = "H.A.M Store Room",
+	description = "Show green / red overlays on chests in H.A.M store room, depending on the keys you have",
+	tags = {"overlay"}
+)
+public class HamPlugin extends Plugin
+{
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private HamOverlay hamOverlay;
+
+	@Inject
+	private Client client;
+
+	@Getter(AccessLevel.PACKAGE)
+	GameObject steelObject;
+	@Getter(AccessLevel.PACKAGE)
+	GameObject ironObject;
+	@Getter(AccessLevel.PACKAGE)
+	GameObject silverObject;
+	@Getter(AccessLevel.PACKAGE)
+	GameObject bronzeObject;
+
+	@Getter(AccessLevel.PACKAGE)
+	boolean drawSteel = false;
+	@Getter(AccessLevel.PACKAGE)
+	boolean drawIron = false;
+	@Getter(AccessLevel.PACKAGE)
+	boolean drawSilver = false;
+	@Getter(AccessLevel.PACKAGE)
+	boolean drawBronze = false;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		overlayManager.add(hamOverlay);
+	}
+
+	@Subscribe
+	public void onGameObjectSpawned(GameObjectSpawned event)
+	{
+		GameObject object = event.getGameObject();
+		if (object != null)
+		{
+			check(object);
+		}
+	}
+
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event)
+	{
+		if (event.getItemContainer() != client.getItemContainer(InventoryID.INVENTORY))
+		{
+			return;
+		}
+		drawSteel = false;
+		drawIron = false;
+		drawSilver = false;
+		drawBronze = false;
+		for (Item item : event.getItemContainer().getItems())
+		{
+			switch (item.getId())
+			{
+				case ItemID.STEEL_KEY:
+					drawSteel = true;
+					break;
+				case ItemID.IRON_KEY_8869:
+					drawIron = true;
+					break;
+				case ItemID.SILVER_KEY:
+					drawSilver = true;
+					break;
+				case ItemID.BRONZE_KEY_8867:
+					drawBronze = true;
+					break;
+			}
+		}
+	}
+
+	public void check(GameObject object)
+	{
+		switch (object.getId())
+		{
+			case ObjectID.SMALL_CHEST:
+				if (object.getY() < 8000) // There's another one of these chests in the room north
+				{
+					steelObject = object;
+				}
+				break;
+			case ObjectID.SMALL_CHEST_15726:
+				ironObject = object;
+				break;
+			case ObjectID.SMALL_CHEST_15724:
+				silverObject = object;
+				break;
+			case ObjectID.SMALL_CHEST_15723:
+				bronzeObject = object;
+				break;
+		}
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		overlayManager.remove(hamOverlay);
+	}
+}
