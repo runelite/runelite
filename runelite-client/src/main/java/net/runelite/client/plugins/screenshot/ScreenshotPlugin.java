19d83b366ec89a1c6c5fd3e441f4951e50bfa4c6
gregg1494 <gregg1494@gmail.com>
2019-05-13 23:42:13 -0500

    fixed kindom reward

--- runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
@@ -430,7 +430,7 @@ public class ScreenshotPlugin extends Plugin
 	public void onWidgetLoaded(WidgetLoaded event)
 	{
 		String fileName;
-		String screenshotSubDir;
+		String screenshotSubDir = "";
 		int groupId = event.getGroupId();
 
 		switch (groupId)
@@ -465,8 +465,7 @@ public class ScreenshotPlugin extends Plugin
 			case KINGDOM_GROUP_ID:
 			{
 				fileName = "Kingdom " + LocalDate.now();
-				screenshotSubDir = "Kingdom Rewards";
-				takeScreenshot(fileName, screenshotSubDir);
+				takeScreenshot(fileName, "Kingdom Rewards");
 				break;
 			}
 			case CHAMBERS_OF_XERIC_REWARD_GROUP_ID:
