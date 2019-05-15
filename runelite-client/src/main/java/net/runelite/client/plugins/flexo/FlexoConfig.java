2004c3d4bf3a8bea56b2aaef9dc4a120cdbf4288
Scott Burns <sd.burns1998@gmail.com>
2019-05-16 00:48:25 +0200

    Refactor flexo

--- runelite-client/src/main/java/net/runelite/client/plugins/flexo/FlexoConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/flexo/FlexoConfig.java
@@ -29,180 +29,198 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("flexo")
-public interface FlexoConfig extends Config {
+public interface FlexoConfig extends Config
+{
 
-    @ConfigItem(
-            position = 0,
-            keyName = "overlayEnabled",
-            name = "Overlay Enabled",
-            description = "Shows clicking area and points etc."
-    )
-    default boolean overlayEnabled() {
-        return true;
-    }
+	@ConfigItem(
+		position = 0,
+		keyName = "overlayEnabled",
+		name = "Overlay Enabled",
+		description = "Shows clicking area and points etc."
+	)
+	default boolean overlayEnabled()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            position = 1,
-            keyName = "minDelayAmount",
-            name = "Min Delay",
-            description = "Minimum delay that is applied to every action at the end (ms)"
-    )
-    default int minDelayAmt() {
-        return 45;
-    }
-
-
-    @ConfigItem(
-            position = 2,
-            keyName = "reactionTime",
-            name = "Reaction Time",
-            description = "The base time between actions (ms)"
-    )
-    default int getReactionTimeVariation() {
-        return 80;
-    }
-
-    @ConfigItem(
-            position = 3,
-            keyName = "mouseDragSpeed",
-            name = "Mouse drag speed",
-            description = "The speed at which steps are executed. Keep at 49? cuz jagex mouse recorder?"
-    )
-    default int getMouseDragSpeed() {
-        return 49;
-    }
+	@ConfigItem(
+		position = 1,
+		keyName = "minDelayAmount",
+		name = "Min Delay",
+		description = "Minimum delay that is applied to every action at the end (ms)"
+	)
+	default int minDelayAmt()
+	{
+		return 45;
+	}
 
 
-    @ConfigItem(
-            position = 4,
-            keyName = "overshoots",
-            name = "Overshoots",
-            description = "Higher number = more overshoots"
-    )
-    default int getOvershoots() {
-        return 4;
-    }
+	@ConfigItem(
+		position = 2,
+		keyName = "reactionTime",
+		name = "Reaction Time",
+		description = "The base time between actions (ms)"
+	)
+	default int getReactionTimeVariation()
+	{
+		return 80;
+	}
 
-    @ConfigItem(
-            position = 5,
-            keyName = "variatingFlow",
-            name = "Flow - Variating",
-            description = ""
-    )
-    default boolean getVariatingFlow() {
-        return true;
-    }
-
-    @ConfigItem(
-            position = 6,
-            keyName = "slowStartupFlow",
-            name = "Flow - Slow startup",
-            description = ""
-    )
-    default boolean getSlowStartupFlow() {
-        return true;
-    }
+	@ConfigItem(
+		position = 3,
+		keyName = "mouseDragSpeed",
+		name = "Mouse drag speed",
+		description = "The speed at which steps are executed. Keep at 49? cuz jagex mouse recorder?"
+	)
+	default int getMouseDragSpeed()
+	{
+		return 49;
+	}
 
 
-    @ConfigItem(
-            position = 7,
-            keyName = "slowStartup2Flow",
-            name = "Flow - Slow startup 2",
-            description = ""
-    )
-    default boolean getSlowStartup2Flow() {
-        return true;
-    }
+	@ConfigItem(
+		position = 4,
+		keyName = "overshoots",
+		name = "Overshoots",
+		description = "Higher number = more overshoots"
+	)
+	default int getOvershoots()
+	{
+		return 4;
+	}
 
-    @ConfigItem(
-            position = 8,
-            keyName = "jaggedFlow",
-            name = "Flow - Jagged",
-            description = ""
-    )
-    default boolean getJaggedFlow() {
-        return true;
-    }
+	@ConfigItem(
+		position = 5,
+		keyName = "variatingFlow",
+		name = "Flow - Variating",
+		description = ""
+	)
+	default boolean getVariatingFlow()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            position = 9,
-            keyName = "interruptedFlow",
-            name = "Flow - Interrupted",
-            description = ""
-    )
-    default boolean getInterruptedFlow() {
-        return false;
-    }
-
-
-    @ConfigItem(
-            position = 10,
-            keyName = "interruptedFlow2",
-            name = "Flow - Interrupted 2",
-            description = ""
-    )
-    default boolean getInterruptedFlow2() {
-        return false;
-    }
-
-    @ConfigItem(
-            position = 11,
-            keyName = "stoppingFlow",
-            name = "Flow - Stopping",
-            description = ""
-    )
-    default boolean getStoppingFlow() {
-        return false;
-    }
-
-    @ConfigItem(
-            position = 12,
-            keyName = "deviationSlopeDivider",
-            name = "Deviation slope divider",
-            description = ""
-    )
-    default int getDeviationSlope() {
-        return 10;
-    }
+	@ConfigItem(
+		position = 6,
+		keyName = "slowStartupFlow",
+		name = "Flow - Slow startup",
+		description = ""
+	)
+	default boolean getSlowStartupFlow()
+	{
+		return true;
+	}
 
 
-    @ConfigItem(
-            position = 13,
-            keyName = "noisinessDivider",
-            name = "Noisiness divider",
-            description = ""
-    )
-    default String getNoisinessDivider() {
-        return "2.0D";
-    }
+	@ConfigItem(
+		position = 7,
+		keyName = "slowStartup2Flow",
+		name = "Flow - Slow startup 2",
+		description = ""
+	)
+	default boolean getSlowStartup2Flow()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            position = 14,
-            keyName = "debugNPCs",
-            name = "Debug NPCs",
-            description = "Draws clickArea and clickPoints across all visible npcs"
-    )
-    default boolean getDebugNPCs() {
-        return false;
-    }
+	@ConfigItem(
+		position = 8,
+		keyName = "jaggedFlow",
+		name = "Flow - Jagged",
+		description = ""
+	)
+	default boolean getJaggedFlow()
+	{
+		return true;
+	}
 
-    @ConfigItem(
-            position = 15,
-            keyName = "debugPlayers",
-            name = "Debug Players",
-            description = "Draws clickArea and clickPoints across all visible players"
-    )
-    default boolean getDebugPlayers() {
-        return false;
-    }
+	@ConfigItem(
+		position = 9,
+		keyName = "interruptedFlow",
+		name = "Flow - Interrupted",
+		description = ""
+	)
+	default boolean getInterruptedFlow()
+	{
+		return false;
+	}
 
-    @ConfigItem(
-            position = 16,
-            keyName = "debugGroundItems",
-            name = "Debug Ground Items",
-            description = "Draws clickArea and clickPoints across all visible ground items"
-    )
-    default boolean getDebugGroundItems() {
-        return false;
-    }
+
+	@ConfigItem(
+		position = 10,
+		keyName = "interruptedFlow2",
+		name = "Flow - Interrupted 2",
+		description = ""
+	)
+	default boolean getInterruptedFlow2()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 11,
+		keyName = "stoppingFlow",
+		name = "Flow - Stopping",
+		description = ""
+	)
+	default boolean getStoppingFlow()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 12,
+		keyName = "deviationSlopeDivider",
+		name = "Deviation slope divider",
+		description = ""
+	)
+	default int getDeviationSlope()
+	{
+		return 10;
+	}
+
+
+	@ConfigItem(
+		position = 13,
+		keyName = "noisinessDivider",
+		name = "Noisiness divider",
+		description = ""
+	)
+	default String getNoisinessDivider()
+	{
+		return "2.0D";
+	}
+
+	@ConfigItem(
+		position = 14,
+		keyName = "debugNPCs",
+		name = "Debug NPCs",
+		description = "Draws clickArea and clickPoints across all visible npcs"
+	)
+	default boolean getDebugNPCs()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 15,
+		keyName = "debugPlayers",
+		name = "Debug Players",
+		description = "Draws clickArea and clickPoints across all visible players"
+	)
+	default boolean getDebugPlayers()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 16,
+		keyName = "debugGroundItems",
+		name = "Debug Ground Items",
+		description = "Draws clickArea and clickPoints across all visible ground items"
+	)
+	default boolean getDebugGroundItems()
+	{
+		return false;
+	}
 }
