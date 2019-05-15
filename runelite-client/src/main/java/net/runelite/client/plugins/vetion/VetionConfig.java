03f2a76b9ce34968d982a0adc29f954c9718b531
Scott Burns <sd.burns1998@gmail.com>
2019-05-16 01:19:01 +0200

    Refactor vetion

--- runelite-client/src/main/java/net/runelite/client/plugins/vetion/VetionConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/vetion/VetionConfig.java
@@ -31,14 +31,14 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("vetion")
 public interface VetionConfig extends Config
 {
-    @ConfigItem(
-            keyName = "earthquakeTimerActive",
-            name = "Vet'ion Earthquake Timer",
-            description = "Configures whether or not a timer is shown to track the cooldown of Vet'ion's earthquake attack",
-            position = 0
-    )
-    default boolean eartquakeTimerActive()
-    {
-        return true;
-    }
+	@ConfigItem(
+		keyName = "earthquakeTimerActive",
+		name = "Vet'ion Earthquake Timer",
+		description = "Configures whether or not a timer is shown to track the cooldown of Vet'ion's earthquake attack",
+		position = 0
+	)
+	default boolean eartquakeTimerActive()
+	{
+		return true;
+	}
 }
