2b61a17447f8e3bec0ea86dd8674f9bcc032a1f8
Scott Burns <sd.burns1998@gmail.com>
2019-05-16 01:01:29 +0200

    Refactor PluginSorter

--- runelite-client/src/main/java/net/runelite/client/plugins/pluginsorter/PluginSorterPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/pluginsorter/PluginSorterPlugin.java
@@ -25,10 +25,14 @@
 package net.runelite.client.plugins.pluginsorter;
 
 import com.google.inject.Provides;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.inject.Inject;
 import net.runelite.api.GameState;
 import net.runelite.api.events.ConfigChanged;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigItemDescriptor;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -37,70 +41,76 @@ import net.runelite.client.plugins.PluginType;
 import net.runelite.client.plugins.config.ConfigPanel;
 import net.runelite.client.plugins.config.PluginListItem;
 
-import javax.inject.Inject;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 @PluginDescriptor(
-        name = "Plugin Organizer",
-        description = "Hides and colors 3rd party plugins for better control",
-        tags = {"plugins","organizer"},
-        type = PluginType.PLUGIN_ORGANIZER
+	name = "Plugin Organizer",
+	description = "Hides and colors 3rd party plugins for better control",
+	tags = {"plugins", "organizer"},
+	type = PluginType.PLUGIN_ORGANIZER
 )
-public class PluginSorterPlugin extends Plugin {
+public class PluginSorterPlugin extends Plugin
+{
+	//Cache the hidden plugins
+	private static List<PluginListItem> removedPlugins = new ArrayList<>();
 
-    //Cache the hidden plugins
-    public static List<PluginListItem> removedPlugins = new ArrayList<>();
+	@Inject
+	private PluginSorterConfig config;
 
-    @Inject
-    private PluginSorterConfig config;
+	@Provides
+	PluginSorterConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PluginSorterConfig.class);
+	}
 
-    @Provides
-    PluginSorterConfig provideConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(PluginSorterConfig.class);
-    }
+	@Override
+	protected void startUp() throws Exception
+	{
+		updateColors();
+	}
 
-    @Override
-    protected void startUp() throws Exception
-    {
-        updateColors();
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
 
-    @Override
-    protected void shutDown() throws Exception
-    {
+	}
 
-    }
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGIN_SCREEN)
+		{
+			if (config.hidePlugins())
+			{
+				hidePlugins();
+			}
+			updateColors();
+		}
+	}
 
-    @Subscribe
-    public void onGameStateChanged (GameStateChanged gameStateChanged)
-    {
-        if (gameStateChanged.getGameState()== GameState.LOGIN_SCREEN) {
-            if (config.hidePlugins())
-                hidePlugins();
-            updateColors();
-        }
-    }
+	@Subscribe
+	public void onConfigChanged(ConfigChanged configChanged)
+	{
+		if (configChanged.getKey().equals("hidePlugins"))
+		{
+			if (config.hidePlugins())
+			{
+				hidePlugins();
+			}
+			else
+			{
+				showPlugins();
+			}
+		}
+		updateColors();
+	}
 
-    @Subscribe
-    public void onConfigChanged(ConfigChanged configChanged) {
-        if (configChanged.getKey().equals("hidePlugins")) {
-            if (config.hidePlugins()) {
-                hidePlugins();
-            } else {
-                showPlugins();
-            }
-        }
-        updateColors();
-    }
-
-    public void updateColors() {
-        for (PluginListItem pli : ConfigPanel.pluginList) {
-            if (pli.getPlugin()!=null) {
-				switch (pli.getPlugin().getClass().getAnnotation(PluginDescriptor.class).type()) {
+	private void updateColors()
+	{
+		for (PluginListItem pli : ConfigPanel.pluginList)
+		{
+			if (pli.getPlugin() != null)
+			{
+				switch (pli.getPlugin().getClass().getAnnotation(PluginDescriptor.class).type())
+				{
 					case EXTERNAL:
 						pli.nameLabel.setForeground(config.externalColor());
 						break;
@@ -117,15 +127,18 @@ public class PluginSorterPlugin extends Plugin {
 						pli.nameLabel.setForeground(Color.WHITE);
 						break;
 				}
-            }
-        }
-    }
+			}
+		}
+	}
 
-    public void hidePlugins() {
-        Iterator<PluginListItem> iter = ConfigPanel.pluginList.iterator();
-        while (iter.hasNext()) {
-            PluginListItem pli = iter.next();
-            if (pli.getPlugin() != null) {
+	private void hidePlugins()
+	{
+		Iterator<PluginListItem> iter = ConfigPanel.pluginList.iterator();
+		while (iter.hasNext())
+		{
+			PluginListItem pli = iter.next();
+			if (pli.getPlugin() != null)
+			{
 				switch (pli.getPlugin().getClass().getAnnotation(PluginDescriptor.class).type())
 				{
 					case PVM:
@@ -139,14 +152,15 @@ public class PluginSorterPlugin extends Plugin {
 					default:
 						break;
 				}
-            }
-        }
-    }
+			}
+		}
+	}
 
-    public void showPlugins() {
-        List<PluginListItem> tempList = new ArrayList<>();
+	private void showPlugins()
+	{
+		List<PluginListItem> tempList = new ArrayList<>();
 		tempList.addAll(removedPlugins);
 		tempList.addAll(ConfigPanel.pluginList);
-        ConfigPanel.pluginList = tempList;
-    }
+		ConfigPanel.pluginList = tempList;
+	}
 }
