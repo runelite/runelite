f30e496ee065ddd839056b61a9c6396247933c69
Scott Burns <sd.burns1998@gmail.com>
2019-05-16 01:06:40 +0200

    Refactor PrayerAlert

--- runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertConfig.java
@@ -23,24 +23,25 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("prayeralert")
 public interface PrayerAlertConfig extends Config
 {
-    @ConfigItem(
-            position = 1,
-            keyName = "alwaysShowAlert",
-            name = "Always show prayer alert",
-            description = "Show the alert, even without prayer restore in inventory"
-    )
-    default boolean alwaysShowAlert()
-    {
-        return false;
-    }
-    @ConfigItem(
-            position = 2,
-            keyName = "oldRenderMode",
-            name = "Render using old method",
-            description = "Render the prayer alert using the old method"
-    )
-    default boolean oldRenderMode()
-    {
-        return false;
-    }
+	@ConfigItem(
+		position = 1,
+		keyName = "alwaysShowAlert",
+		name = "Always show prayer alert",
+		description = "Show the alert, even without prayer restore in inventory"
+	)
+	default boolean alwaysShowAlert()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 2,
+		keyName = "oldRenderMode",
+		name = "Render using old method",
+		description = "Render the prayer alert using the old method"
+	)
+	default boolean oldRenderMode()
+	{
+		return false;
+	}
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayeralert/PrayerAlertPlugin.java
@@ -18,45 +18,42 @@ package net.runelite.client.plugins.prayeralert;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 @PluginDescriptor(
-        name = "Prayer Alerter",
-        description = "Alert the player when prayer is low",
-        tags = {"prayer", "overlay"},
-        type = PluginType.UTILITY,
-        enabledByDefault = false
+	name = "Prayer Alerter",
+	description = "Alert the player when prayer is low",
+	tags = {"prayer", "overlay"},
+	type = PluginType.UTILITY,
+	enabledByDefault = false
 )
 public class PrayerAlertPlugin extends Plugin
 {
-    @Inject
-    private OverlayManager overlayManager;
+	@Inject
+	private OverlayManager overlayManager;
 
-    @Inject
-    private PrayerAlertOverlay overlay;
+	@Inject
+	private PrayerAlertOverlay overlay;
 
-    @Inject
-    private PrayerAlertConfig config;
+	@Provides
+	PrayerAlertConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(PrayerAlertConfig.class);
+	}
 
-    @Provides
-    PrayerAlertConfig provideConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(PrayerAlertConfig.class);
-    }
+	@Override
+	protected void startUp() throws Exception
+	{
+		overlayManager.add(overlay);
+	}
 
-    @Override
-    protected void startUp() throws Exception
-    {
-        overlayManager.add(overlay);
-    }
-
-    @Override
-    protected void shutDown() throws Exception
-    {
-        overlayManager.remove(overlay);
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
+		overlayManager.remove(overlay);
+	}
 }
