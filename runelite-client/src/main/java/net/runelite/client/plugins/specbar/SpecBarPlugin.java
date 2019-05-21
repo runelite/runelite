51d3ec835aafb6f80e31e9268972945a6141fd29
Lucwousin <Lucwousin@users.noreply.github.com>
2019-05-21 17:46:29 +0200

    Rework specbar plugin (#347)

--- runelite-client/src/main/java/net/runelite/client/plugins/specbar/SpecBarPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/specbar/SpecBarPlugin.java
@@ -1,13 +1,32 @@
+/*
+ * Copyright (c) 2019, Lucas <https://github.com/Lucwousin>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.specbar;
 
 import javax.inject.Inject;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.WidgetHiddenChanged;
-import net.runelite.api.widgets.Widget;
-import net.runelite.api.widgets.WidgetID;
-import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -37,38 +56,15 @@ public class SpecBarPlugin extends Plugin
 	}
 
 	@Subscribe
-	private void onWidgetHiddenChanged(WidgetHiddenChanged event)
+	public void onScriptCallbackEvent(ScriptCallbackEvent event)
 	{
-		if (WidgetInfo.TO_GROUP(event.getWidget().getId()) == WidgetID.COMBAT_GROUP_ID)
+		if (!"drawSpecbarAnyway".equals(event.getEventName()))
 		{
-			hideSpecBar();
-		}
-	}
-
-	@Subscribe
-	private void onGameStateChanged(GameStateChanged event)
-	{
-		if (event.getGameState().equals(GameState.LOGGING_IN))
-		{
-			hideSpecBar();
+			return;
 		}
 
-	}
-
-	/**
-	 * Displays the special attack bar
-	 */
-	private void hideSpecBar()
-	{
-		try
-		{
-			Widget specBar = client.getWidget(WidgetInfo.COMBAT_SPECIAL_ATTACK);
-			specBar.setHidden(false);
-			specBar.revalidate();
-		}
-		catch (NullPointerException e)
-		{
-			// Stops the nulls that occur before the spec bar is loaded by player equipping a spec wep
-		}
+		int[] iStack = client.getIntStack();
+		int iStackSize = client.getIntStackSize();
+		iStack[iStackSize - 1] = 1;
 	}
 }
