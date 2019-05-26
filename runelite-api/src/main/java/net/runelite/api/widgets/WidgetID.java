2800a226a11641143a0a193519bea94c1c9a4b6a
Adam <Adam@sigterm.info>
2019-05-25 21:48:52 -0400

    client: fix spec bar not updating with hide auto retaliate on
    Move the widget listener from the auto retaliate text to the spec bar,
    so when auto retaliate is hidden the listeners still function.

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -533,6 +533,7 @@ public class WidgetID
 		static final int SPELL_ICON = 27;
 		static final int SPELL_TEXT = 28;
 		static final int AUTO_RETALIATE = 29;
+		static final int SPEC_BAR = 34;
 	}
 
 	static class VolcanicMine
