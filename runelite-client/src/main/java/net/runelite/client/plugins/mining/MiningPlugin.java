2e7afa25d4b920ce2997bc8093f7ca3efbac1a4a
vardy <jarred.vardy@gmail.com>
2019-06-12 00:15:06 +0800

    Added overlay to coal bag  to display how much coal it currently contains.

--- runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningPlugin.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019, Adam <Adam@sigterm.info>
+ * Copyright (c) 2019, Jarred Vardy <jarred.vardy@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,42 +25,66 @@
  */
 package net.runelite.client.plugins.mining;
 
+import com.google.inject.Provides;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.inject.Inject;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameObject;
 import net.runelite.api.GameState;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemContainer;
+import net.runelite.api.ItemID;
 import static net.runelite.api.ObjectID.DEPLETED_VEIN_26665;
 import static net.runelite.api.ObjectID.DEPLETED_VEIN_26666;
 import static net.runelite.api.ObjectID.DEPLETED_VEIN_26667;
 import static net.runelite.api.ObjectID.DEPLETED_VEIN_26668;
 import static net.runelite.api.ObjectID.EMPTY_WALL;
 import net.runelite.api.WallObject;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameObjectDespawned;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.WallObjectSpawned;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.util.Text;
 
 @Slf4j
 @PluginDescriptor(
 	name = "Mining",
-	description = "Show ore respawn timers",
-	tags = {"overlay", "skilling", "timers"},
+	description = "Show ore respawn timers and coal bag overlay",
+	tags = {"overlay", "skilling", "timers", "coal", "coalbag", "coal bag"},
 	enabledByDefault = false
 )
 public class MiningPlugin extends Plugin
 {
 	private static final int ROCK_DISTANCE = 14;
 	private static final int MINING_GUILD_REGION = 12183;
+
+	private static final Pattern COAL_BAG_EMPTY_MESSAGE = Pattern.compile("^The coal bag is (now )?empty\\.$");
+	private static final Pattern COAL_BAG_ONE_MESSAGE = Pattern.compile("^The coal bag contains one piece of coal\\.$");
+	private static final Pattern COAL_BAG_AMOUNT_MESSAGE = Pattern.compile("^The coal bag contains (\\d+) pieces of coal\\.$");
+
+	static final int MAX_INVY_SPACE = 28;
+	private static final int FULL_BAG_AMOUNT = 27;
+
+	static final String FILL_OPTION = "fill";
+	static final String EMPTY_OPTION = "empty";
+
 
 	@Inject
 	private Client client;
@@ -68,7 +93,14 @@ public class MiningPlugin extends Plugin
 	private OverlayManager overlayManager;
 
 	@Inject
-	private MiningOverlay overlay;
+	private MiningOverlay miningOverlay;
+
+	@Inject
+	private MiningCoalBagOverlay coalBagOverlay;
+
+	@Getter
+	@Setter
+	private int amountOfCoalInBag;
 
 	@Getter(AccessLevel.PACKAGE)
 	private final List<RockRespawn> respawns = new ArrayList<>();
@@ -77,14 +109,22 @@ public class MiningPlugin extends Plugin
 	@Override
 	protected void startUp()
 	{
-		overlayManager.add(overlay);
+		overlayManager.add(miningOverlay);
+		overlayManager.add(coalBagOverlay);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		overlayManager.remove(overlay);
+		overlayManager.remove(miningOverlay);
+		overlayManager.remove(coalBagOverlay);
 		respawns.clear();
+	}
+
+	@Provides
+	MiningConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(MiningConfig.class);
 	}
 
 	@Subscribe
@@ -159,6 +199,68 @@ public class MiningPlugin extends Plugin
 				break;
 			}
 		}
+	}
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		if (event.getWidgetId() != WidgetInfo.INVENTORY.getId())
+		{
+			return;
+		}
+
+		ItemContainer inventoryItemContainer = client.getItemContainer(InventoryID.INVENTORY);
+
+		switch (event.getMenuOption().toLowerCase())
+		{
+			case FILL_OPTION:
+				update((int) Arrays.stream(inventoryItemContainer.getItems()).filter(i -> i.getId() == ItemID.COAL).count());
+				break;
+			case EMPTY_OPTION:
+				int amt = MAX_INVY_SPACE - (int) Arrays.stream(inventoryItemContainer.getItems()).filter(i -> i.getId() != -1).count();
+				update(-amt);
+				break;
+		}
+	}
+
+	/**
+	 * Use onChatMessage when a player chooses the `Check` menu option on coal bags.
+	 *
+	 * @param event
+	 */
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		String chatMsg = Text.removeTags(event.getMessage());
+		if (COAL_BAG_EMPTY_MESSAGE.matcher(chatMsg).find())
+		{
+			update(-getAmountOfCoalInBag());
+		}
+		else if (COAL_BAG_ONE_MESSAGE.matcher(chatMsg).find())
+		{
+			update(-getAmountOfCoalInBag() + 1);
+		}
+		else
+		{
+			Matcher matcher = COAL_BAG_AMOUNT_MESSAGE.matcher(chatMsg);
+			if (matcher.find())
+			{
+				update(Integer.parseInt(matcher.group(1)) - getAmountOfCoalInBag());
+			}
+		}
+	}
+
+	/**
+	 * Update the player's count of coal in their Coal Bag
+	 *
+	 * @param delta How much to add/subtract from the amount.
+	 *              Supply a negative number to subtract, or positive number to add.
+	 */
+	protected void update(int delta)
+	{
+		// check for upper/lower bounds of amount of coal in a bag
+		// 0 <= X <= 27
+		setAmountOfCoalInBag(Math.max(0, Math.min(FULL_BAG_AMOUNT, getAmountOfCoalInBag() + delta)));
 	}
 
 	private boolean inMiningGuild()
