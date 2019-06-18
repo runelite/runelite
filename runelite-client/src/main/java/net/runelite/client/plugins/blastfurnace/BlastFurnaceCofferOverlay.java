runelite/runelite

commit 9df05ef772386d35d6cbcc69c6347d08385b7615

Fix's for overlays (#644)

runelite-client/src/main/java/net/runelite/client/plugins/blastfurnace/BlastFurnaceCofferOverlay.java
@@ -38,6 +38,7 @@ import net.runelite.client.ui.overlay.OverlayMenuEntry;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.PanelComponent;
 import net.runelite.client.ui.overlay.components.table.TableComponent;
+import net.runelite.client.ui.overlay.components.table.TableAlignment;
 import net.runelite.client.util.StackFormatter;
 
 class BlastFurnaceCofferOverlay extends Overlay
@@ -59,6 +60,9 @@ class BlastFurnaceCofferOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		TableComponent tableComponent = new TableComponent();
+		tableComponent.setColumnAlignments(TableAlignment.LEFT, TableAlignment.RIGHT);
+
 		if (plugin.getConveyorBelt() == null)
 		{
 			return null;
@@ -72,12 +76,10 @@ class BlastFurnaceCofferOverlay extends Overlay
 		{
 			sack.setHidden(true);
 
-			TableComponent tableComponent = new TableComponent();
 			tableComponent.addRow("Coffer:", StackFormatter.quantityToStackSize(client.getVar(BLAST_FURNACE_COFFER)) + " gp");
 
-			panelComponent.getChildren().add(tableComponent);
 		}
-
+		panelComponent.getChildren().add(tableComponent);
 		return panelComponent.render(graphics);
 	}
 }

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemRecoilOverlay.java
@@ -35,6 +35,8 @@ import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.ImageComponent;
 import net.runelite.client.ui.overlay.components.PanelComponent;
+import net.runelite.client.ui.overlay.components.table.TableAlignment;
+import net.runelite.client.ui.overlay.components.table.TableComponent;
 
 class ItemRecoilOverlay extends Overlay
 {
@@ -55,6 +57,9 @@ class ItemRecoilOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		TableComponent tableComponent = new TableComponent();
+		tableComponent.setColumnAlignments(TableAlignment.LEFT, TableAlignment.RIGHT);
+
 		this.imagePanelComponent.getChildren().clear();
 		if (config.showrecoil())
 		{
@@ -64,6 +69,8 @@ class ItemRecoilOverlay extends Overlay
 				imagePanelComponent.setBackgroundColor(plugin
 						.isRingOfRecoilEquipped() ? ACTIVATED_BACKGROUND_COLOR : NOT_ACTIVATED_BACKGROUND_COLOR);
 				imagePanelComponent.getChildren().add(new ImageComponent(recoilImage));
+
+				imagePanelComponent.getChildren().add(tableComponent);
 				return imagePanelComponent.render(graphics);
 			}
 		}

commit 4ce7cc714cb39cc0432b385b8975dc7b06edbfc6

Fix blast furnance table issue introduced in #644

runelite-client/src/main/java/net/runelite/client/plugins/blastfurnace/BlastFurnaceCofferOverlay.java
@@ -60,13 +60,13 @@ class BlastFurnaceCofferOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
-		TableComponent tableComponent = new TableComponent();
-		tableComponent.setColumnAlignments(TableAlignment.LEFT, TableAlignment.RIGHT);
-
 		if (plugin.getConveyorBelt() == null)
 		{
 			return null;
 		}
+
+		TableComponent tableComponent = new TableComponent();
+		tableComponent.setColumnAlignments(TableAlignment.LEFT, TableAlignment.RIGHT);
 
 		Widget sack = client.getWidget(WidgetInfo.BLAST_FURNACE_COFFER);
 
@@ -77,9 +77,13 @@ class BlastFurnaceCofferOverlay extends Overlay
 			sack.setHidden(true);
 
 			tableComponent.addRow("Coffer:", StackFormatter.quantityToStackSize(client.getVar(BLAST_FURNACE_COFFER)) + " gp");
-
 		}
-		panelComponent.getChildren().add(tableComponent);
+
+		if (!tableComponent.isEmpty())
+		{
+			panelComponent.getChildren().add(tableComponent);
+		}
+
 		return panelComponent.render(graphics);
 	}
 }