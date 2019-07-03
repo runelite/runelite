411dc435bed0c20f0afcae966582f9b0780a4d94
kristo.naeris <kristo.naeris@industry62.com>
2019-07-03 17:58:46 +0300

    add an option in World Hopper configuration to disable hop-to menu option

--- runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldHopperConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldHopperConfig.java
@@ -103,10 +103,21 @@ public interface WorldHopperConfig extends Config
 	}
 
 	@ConfigItem(
+			keyName = "menuOption",
+			name = " Show Hop-to menu option",
+			description = "Show Hop-to option in menus",
+			position = 6
+	)
+	default boolean menuOption()
+	{
+		return true;
+	}
+
+	@ConfigItem(
 		keyName = "subscriptionFilter",
 		name = "Show subscription types",
 		description = "Only show free worlds, member worlds, or both types of worlds in sidebar",
-		position = 6
+		position = 7
 	)
 	default SubscriptionFilterMode subscriptionFilter()
 	{
