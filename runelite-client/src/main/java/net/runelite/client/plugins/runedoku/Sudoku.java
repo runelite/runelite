aa3fc15e70a61b0ae57bcec528dedf39b3687814
sdburns1998 <sdburn1998@gmail.com>
2019-07-07 18:30:11 +0200

    runedoku: Remove debugging

--- runelite-client/src/main/java/net/runelite/client/plugins/runedoku/Sudoku.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/runedoku/Sudoku.java
@@ -102,17 +102,6 @@ class Sudoku
 		return true;
 	}
 
-	public void display()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			for (int j = 0; j < SIZE; j++)
-			{
-				System.out.print(" " + board[i][j]);
-			}
-		}
-	}
-
 	int[][] getBoard()
 	{
 		return board;
