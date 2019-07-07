0731d03e3cdda3c84dc3af4fe23efb9dbec17279
sdburns1998 <sdburn1998@gmail.com>
2019-07-07 04:47:01 +0200

    chattranslation: Stringbuffer -> Stringbuilder

--- runelite-client/src/main/java/net/runelite/client/plugins/chattranslation/Translator.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chattranslation/Translator.java
@@ -23,7 +23,7 @@ class Translator
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
-		StringBuffer response = new StringBuffer();
+		StringBuilder response = new StringBuilder();
 
 		while ((inputLine = in.readLine()) != null)
 		{
