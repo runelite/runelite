9ed6759c838a6e1c8b0380e03217a09ed0699452
sdburns1998 <sdburn1998@gmail.com>
2019-07-07 21:53:02 +0200

    whalewatchers: Final fields

--- runelite-client/src/main/java/net/runelite/client/plugins/whalewatchers/WhaleWatchersGloryOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/whalewatchers/WhaleWatchersGloryOverlay.java
@@ -28,8 +28,8 @@ import net.runelite.client.ui.overlay.components.TitleComponent;
 @Singleton
 public class WhaleWatchersGloryOverlay extends Overlay
 {
-	private WhaleWatchersPlugin plugin;
-	private PanelComponent panelComponent;
+	private final WhaleWatchersPlugin plugin;
+	private final PanelComponent panelComponent;
 
 	@Inject
 	private ItemManager itemManager;
