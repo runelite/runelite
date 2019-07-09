eb72223a5e8535025eea90c746afc0c58c19e449
Daniel Serpa <serpa88@hotmail.ca>
2019-07-09 16:09:03 -0400

    quest list plugin: add not completed filter

--- runelite-client/src/main/java/net/runelite/client/plugins/questlist/QuestListPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/questlist/QuestListPlugin.java
@@ -357,7 +357,14 @@ public class QuestListPlugin extends Plugin
 			else
 			{
 				// Otherwise hide if it doesn't match the filter state
-				hidden = currentFilterState != QuestState.ALL && questState != currentFilterState;
+				if (currentFilterState == QuestState.NOT_COMPLETED)
+				{
+					hidden = questState == QuestState.COMPLETE;
+				}
+				else
+				{
+					hidden = currentFilterState != QuestState.ALL && questState != currentFilterState;
+				}
 			}
 
 			quest.setHidden(hidden);
@@ -391,7 +398,8 @@ public class QuestListPlugin extends Plugin
 		NOT_STARTED(0xff0000, "Not started", SpriteID.MINIMAP_ORB_HITPOINTS),
 		IN_PROGRESS(0xffff00, "In progress", SpriteID.MINIMAP_ORB_HITPOINTS_DISEASE),
 		COMPLETE(0xdc10d, "Completed", SpriteID.MINIMAP_ORB_HITPOINTS_POISON),
-		ALL(0, "All", SpriteID.MINIMAP_ORB_PRAYER);
+		ALL(0, "All", SpriteID.MINIMAP_ORB_PRAYER),
+		NOT_COMPLETED(0, "Not Completed", SpriteID.MINIMAP_ORB_RUN);
 
 		private final int color;
 		private final String name;
