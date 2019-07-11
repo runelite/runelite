be6646e519aa14276ebd851a77abb591ba160a8c
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-07-11 07:50:43 +0200

    lootpile: Add a couple ticks for the death animation and don't show negative numbers

--- runelite-client/src/main/java/net/runelite/client/plugins/lootassist/LootPile.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/lootassist/LootPile.java
@@ -7,7 +7,7 @@ import net.runelite.api.coords.WorldPoint;
 
 class LootPile
 {
-	private static final long TIME_UNTIL_VISIBLE = 60000;
+	private static final long TIME_UNTIL_VISIBLE = 62400;
 
 	@Getter(AccessLevel.PACKAGE)
 	private final long timeAppearing;
