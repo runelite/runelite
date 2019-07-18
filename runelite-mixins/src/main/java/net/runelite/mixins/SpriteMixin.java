18880c15737f9b26eb834e8121ac626cfc43a990
Zeruth <TheRealNull@gmail.com>
2019-07-18 16:04:59 -0400

    mixins: fix checkstyle violations

--- runelite-mixins/src/main/java/net/runelite/mixins/SpriteMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/SpriteMixin.java
@@ -3,13 +3,9 @@ package net.runelite.mixins;
 import net.runelite.api.Sprite;
 import java.util.HashMap;
 import java.util.Map;
-import net.runelite.api.mixins.Copy;
 import net.runelite.api.mixins.Inject;
 import net.runelite.api.mixins.Mixin;
-import net.runelite.api.mixins.Replace;
-import net.runelite.rs.api.RSAbstractArchive;
 import net.runelite.rs.api.RSClient;
-import net.runelite.rs.api.RSSprite;
 
 @Mixin(RSClient.class)
 public abstract class SpriteMixin implements RSClient
