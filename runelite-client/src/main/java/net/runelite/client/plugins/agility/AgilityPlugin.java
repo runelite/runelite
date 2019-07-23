29533cd6c70845c9b78755cd284953786cd174bd
Adam <Adam@sigterm.info>
2019-07-23 10:59:14 -0400

    client: change item layer item uages to TileItem

--- runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityPlugin.java
@@ -34,13 +34,13 @@ import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.Item;
 import net.runelite.api.ItemID;
 import static net.runelite.api.ItemID.AGILITY_ARENA_TICKET;
 import net.runelite.api.Player;
 import net.runelite.api.Skill;
 import static net.runelite.api.Skill.AGILITY;
 import net.runelite.api.Tile;
+import net.runelite.api.TileItem;
 import net.runelite.api.TileObject;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.BoostedLevelChanged;
@@ -235,7 +235,7 @@ public class AgilityPlugin extends Plugin
 			return;
 		}
 
-		final Item item = itemSpawned.getItem();
+		final TileItem item = itemSpawned.getItem();
 		final Tile tile = itemSpawned.getTile();
 
 		if (item.getId() == ItemID.MARK_OF_GRACE)
