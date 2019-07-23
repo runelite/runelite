6e679ce98d1a418d940581f415bd90c7fe552d91
Adam <Adam@sigterm.info>
2019-07-23 17:22:19 -0400

    api: mark getItemDefinition as nonnull

--- runelite-api/src/main/java/net/runelite/api/Client.java
+++ runelite-api/src/main/java/net/runelite/api/Client.java
@@ -29,6 +29,7 @@ import java.awt.Dimension;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import net.runelite.api.annotations.VisibleForDevtools;
 import net.runelite.api.coords.LocalPoint;
@@ -346,6 +347,7 @@ public interface Client extends GameEngine
 	 * @return the corresponding item composition
 	 * @see ItemID
 	 */
+	@Nonnull
 	ItemComposition getItemDefinition(int id);
 
 	/**
