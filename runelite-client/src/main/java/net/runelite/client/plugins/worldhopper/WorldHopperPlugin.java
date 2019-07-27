705370c5a0d75730659685c19694a16b50e02b03
Adam <Adam@sigterm.info>
2019-07-27 12:36:31 -0400

    world hopper: add option to show current world ping
    If enabled, the current world is pinged every second and the ping is
    drawn in an overlay
    
    Co-authored-by: gregg1494 <gregg1494@gmail.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldHopperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldHopperPlugin.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2017, Adam <Adam@sigterm.info>
  * Copyright (c) 2018, Lotto <https://github.com/devLotto>
+ * Copyright (c) 2019, gregg1494 <https://github.com/gregg1494>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -45,6 +46,7 @@ import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.swing.SwingUtilities;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
@@ -78,6 +80,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.worldhopper.ping.Ping;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ExecutorServiceExceptionLogger;
 import net.runelite.client.util.HotkeyListener;
 import net.runelite.client.util.Text;
@@ -130,6 +133,12 @@ public class WorldHopperPlugin extends Plugin
 	@Inject
 	private WorldHopperConfig config;
 
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private WorldHopperPingOverlay worldHopperOverlay;
+
 	private ScheduledExecutorService hopperExecutorService;
 
 	private NavigationButton navButton;
@@ -143,11 +152,14 @@ public class WorldHopperPlugin extends Plugin
 
 	private int favoriteWorld1, favoriteWorld2;
 
-	private ScheduledFuture<?> worldResultFuture, pingFuture;
+	private ScheduledFuture<?> worldResultFuture, pingFuture, currPingFuture;
 	private WorldResult worldResult;
 	private int currentWorld;
 	private Instant lastFetch;
 	private boolean firstRun;
+
+	@Getter(AccessLevel.PACKAGE)
+	private int currentPing;
 
 	private final HotkeyListener previousKeyListener = new HotkeyListener(() -> config.previousKey())
 	{
@@ -176,6 +188,7 @@ public class WorldHopperPlugin extends Plugin
 	protected void startUp() throws Exception
 	{
 		firstRun = true;
+		currentPing = -1;
 
 		keyManager.registerKeyListener(previousKeyListener);
 		keyManager.registerKeyListener(nextKeyListener);
@@ -200,6 +213,8 @@ public class WorldHopperPlugin extends Plugin
 			clientToolbar.addNavigation(navButton);
 		}
 
+		overlayManager.add(worldHopperOverlay);
+
 		panel.setFilterMode(config.subscriptionFilter());
 
 		// The plugin has its own executor for pings, as it blocks for a long time
@@ -209,6 +224,7 @@ public class WorldHopperPlugin extends Plugin
 
 		// Give some initial delay - this won't run until after pingInitialWorlds finishes from tick() anyway
 		pingFuture = hopperExecutorService.scheduleWithFixedDelay(this::pingNextWorld, 15, 3, TimeUnit.SECONDS);
+		currPingFuture = hopperExecutorService.scheduleWithFixedDelay(this::pingCurrentWorld, 15, 1, TimeUnit.SECONDS);
 	}
 
 	@Override
@@ -216,6 +232,11 @@ public class WorldHopperPlugin extends Plugin
 	{
 		pingFuture.cancel(true);
 		pingFuture = null;
+
+		currPingFuture.cancel(true);
+		currPingFuture = null;
+
+		overlayManager.remove(worldHopperOverlay);
 
 		keyManager.unregisterKeyListener(previousKeyListener);
 		keyManager.unregisterKeyListener(nextKeyListener);
@@ -807,8 +828,39 @@ public class WorldHopperPlugin extends Plugin
 
 		World world = worlds.get(currentWorld++);
 
+		// If we are displaying the ping overlay, there is a separate scheduled task for the current world
+		boolean displayPing = config.displayPing() && client.getGameState() == GameState.LOGGED_IN;
+		if (displayPing && client.getWorld() == world.getId())
+		{
+			return;
+		}
+
 		int ping = Ping.ping(world);
 		log.trace("Ping for world {} is: {}", world.getId(), ping);
 		SwingUtilities.invokeLater(() -> panel.updatePing(world.getId(), ping));
+	}
+
+	/**
+	 * Ping the current world for the ping overlay
+	 */
+	private void pingCurrentWorld()
+	{
+		// There is no reason to ping the current world if not logged in, as the overlay doesn't draw
+		if (worldResult == null || !config.displayPing() || client.getGameState() != GameState.LOGGED_IN)
+		{
+			return;
+		}
+
+		final World currentWorld = worldResult.findWorld(client.getWorld());
+		if (currentWorld == null)
+		{
+			log.debug("unable to find current world: {}", client.getWorld());
+			return;
+		}
+
+		currentPing = Ping.ping(currentWorld);
+		log.trace("Ping for current world is: {}", currentPing);
+
+		SwingUtilities.invokeLater(() -> panel.updatePing(currentWorld.getId(), currentPing));
 	}
 }
