78e886e258966b50cfab828cf0e0ff0bee390565
Kyleeld <48519776+Kyleeld@users.noreply.github.com>
2019-07-31 15:58:43 +0100

    Update WidgetID.java (#1207)

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -975,7 +975,7 @@ public class WidgetID
 		static final int PVP_WIDGET_CONTAINER = 54;  // OUTDATED?
 		static final int SKULL = 56;  // OUTDATED?
 		static final int ATTACK_RANGE = 59;  // OUTDATED?
-		static final int BOUNTY_HUNTER_INFO = 19;
+		static final int BOUNTY_HUNTER_INFO = 18;
 		static final int KILLDEATH_RATIO = 15;
 		static final int SKULL_CONTAINER = 61;
 		static final int SAFE_ZONE = 63;
