8ce6bea4d215cf78454949bc1123222f463da4ba
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-09-04 12:51:38 +0200

    profiles: Remove unused imports

--- runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/profiles/ProfilesPanel.java
@@ -26,8 +26,6 @@ package net.runelite.client.plugins.profiles;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
