c94dd00a05d3d708379fa10c5eb0989632e12a36
Adam <Adam@sigterm.info>
2019-09-05 19:50:10 -0400

    clue plugin: support Prifddinas clues
    This adds hotcold solver support in Prifddinas, and allows coordinate
    clues to be located in the city.
    
    Modify coordinate clues to support "real" and "overworld" locations so
    that regardless of which region you are in, the overlays work for
    either.
    
    Closes #9459

--- runelite-client/src/test/java/net/runelite/client/plugins/cluescrolls/ClueScrollPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/cluescrolls/ClueScrollPluginTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2018, Tomas Slusny <slusnucky@gmail.com>
+ * Copyright (c) 2019 Hydrox6 <ikada@protonmail.ch>
+ * Copyright (c) 2019 Adam <Adam@sigterm.info>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,17 +23,42 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.cluescrolls.clues;
+package net.runelite.client.plugins.cluescrolls;
 
 import net.runelite.api.coords.WorldPoint;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
 
-public interface LocationClueScroll
+public class ClueScrollPluginTest
 {
-	WorldPoint getLocation();
-
-	default WorldPoint[] getLocations()
+	@Test
+	public void getGetMirrorPoint()
 	{
-		WorldPoint location = getLocation();
-		return location == null ? new WorldPoint[0] : new WorldPoint[]{location};
+		WorldPoint point, converted;
+
+		// Zalcano's entrance portal
+		point = new WorldPoint(3282, 6058, 0);
+		converted = ClueScrollPlugin.getMirrorPoint(point, true);
+		assertNotEquals(point, converted);
+
+		// Elven Crystal Chest, which is upstairs
+		point = new WorldPoint(3273, 6082, 2);
+		converted = ClueScrollPlugin.getMirrorPoint(point, true);
+		assertNotEquals(point, converted);
+
+		// Around the area of the Elite coordinate clue
+		point = new WorldPoint(2185, 3280, 0);
+		// To overworld
+		converted = ClueScrollPlugin.getMirrorPoint(point, true);
+		assertEquals(point, converted);
+		// To real
+		converted = ClueScrollPlugin.getMirrorPoint(point, false);
+		assertNotEquals(point, converted);
+
+		// Brugsen Bursen, Grand Exchange
+		point = new WorldPoint(3165, 3477, 0);
+		converted = ClueScrollPlugin.getMirrorPoint(point, false);
+		assertEquals(point, converted);
 	}
 }
