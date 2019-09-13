57998652c5ec112d406bf1e7ed0f8ee4aea9f8d6
JohnPolich <stjnp20@moravian.edu>
2019-09-13 12:38:21 -0400

    Formatting change

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -473,6 +473,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 			{
 				swap("enchant", option, target, true);
 			}
+			
 			if(config.swapStore())
 			{
 				swap("trade-builders-store", option, target, true);
