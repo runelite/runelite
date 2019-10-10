5acdcae75d22c139efcc9a77eb335f6ea4d30e51
Max Weber <mii7303@gmail.com>
2019-10-10 05:49:14 -0600

    runelite-api: Add some more documentation

--- runelite-api/src/main/java/net/runelite/api/VarPlayer.java
+++ runelite-api/src/main/java/net/runelite/api/VarPlayer.java
@@ -28,7 +28,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * An enumeration of local player variables.
+ * Server controlled "content-developer" integers.
+ *
+ * VarPlayers are stored per RuneScape player save, and synchronized
+ * from the server to the client. The client can change them preemptively
+ * if it thinks they will change the next tick as a lag-hiding measure.
+ * The client CANNOT directly make the server change a varbit.
  */
 @AllArgsConstructor
 @Getter
