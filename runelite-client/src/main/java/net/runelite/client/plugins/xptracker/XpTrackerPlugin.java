935e0150356090e12b153e90995488bda3a53e9b
dekvall <dkvldev@gmail.com>
2019-10-19 17:08:54 +0200

    xp tracker: enable xp progress tooltips for p2p skills on f2p worlds
    The information is already available if you look it up on the hiscores
    and the members only skills already have changed opacity to indicate that
    they are members only. Only tooltips for skills with >0 xp will be overridden
    so that free players who have never been members will get the intended F2P feeling.

--- runelite-client/src/main/java/net/runelite/client/plugins/xptracker/XpTrackerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/xptracker/XpTrackerPlugin.java
@@ -57,10 +57,13 @@ import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.NpcDespawned;
 import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.widgets.WidgetID;
 import static net.runelite.api.widgets.WidgetInfo.TO_GROUP;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.game.NPCManager;
 import net.runelite.client.game.SkillIconManager;
 import net.runelite.client.plugins.Plugin;
@@ -103,6 +106,9 @@ public class XpTrackerPlugin extends Plugin
 
 	@Inject
 	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
 
 	@Inject
 	private SkillIconManager skillIconManager;
@@ -158,6 +164,7 @@ public class XpTrackerPlugin extends Plugin
 			.build();
 
 		clientToolbar.addNavigation(navButton);
+		clientThread.invokeLater(this::refreshMembersSkills);
 
 		// Initialize the tracker & last xp if already logged in
 		fetchXp = true;
@@ -170,6 +177,16 @@ public class XpTrackerPlugin extends Plugin
 		overlayManager.removeIf(e -> e instanceof XpInfoBoxOverlay);
 		xpState.reset();
 		clientToolbar.removeNavigation(navButton);
+		clientThread.invokeLater(this::refreshMembersSkills);
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (event.getGroup().equals("xpTracker") && event.getKey().equals("enableSkillTabTooltipForF2P"))
+		{
+			clientThread.invokeLater(this::refreshMembersSkills);
+		}
 	}
 
 	@Subscribe
@@ -469,6 +486,32 @@ public class XpTrackerPlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onScriptCallbackEvent(ScriptCallbackEvent scriptCallbackEvent)
+	{
+		final String eventName = scriptCallbackEvent.getEventName();
+
+		if (!xpTrackerConfig.enableSkillTabTooltipForF2P() || !eventName.equals("skillTabP2PTooltip"))
+		{
+			return;
+		}
+
+		// Stack is: skillId, showXp
+		final int[] intStack = client.getIntStack();
+		final int intStackSize = client.getIntStackSize();
+
+		final int skillId = intStack[intStackSize - 1];
+		final Skill skill = Skill.values()[skillId];
+		final int exp = client.getSkillExperience(skill);
+
+		if (exp > 0)
+		{
+			// alter members only mapping for skill
+			intStack[intStackSize - 2] = 1;
+		}
+
+	}
+
+	@Subscribe
 	public void onMenuEntryAdded(final MenuEntryAdded event)
 	{
 		int widgetID = event.getActionParam1();
@@ -717,6 +760,17 @@ public class XpTrackerPlugin extends Plugin
 		for (Skill skill : Skill.values())
 		{
 			pauseSkill(skill, pause);
+		}
+	}
+
+	private void refreshMembersSkills()
+	{
+		for (Skill skill : Skill.values())
+		{
+			if (skill.isMembersOnly())
+			{
+				client.queueChangedSkill(skill);
+			}
 		}
 	}
 }
