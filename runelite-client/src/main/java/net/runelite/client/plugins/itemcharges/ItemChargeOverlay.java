100231d731a1fd79caceeb3ff0b06093e63c17dd
Crystalknoct <andrewj0013@gmail.com>
2019-10-19 23:51:17 -0600

    itemchargeoverlay: add basket/fix sack
    add basket config and fix sack config

--- runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
@@ -36,9 +36,11 @@ import static net.runelite.client.plugins.itemcharges.ItemChargeType.ABYSSAL_BRA
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.BELLOWS;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.FUNGICIDE_SPRAY;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.IMPBOX;
+import static net.runelite.client.plugins.itemcharges.ItemChargeType.SACK;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.TELEPORT;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.WATERCAN;
 import static net.runelite.client.plugins.itemcharges.ItemChargeType.WATERSKIN;
+import static net.runelite.client.plugins.itemcharges.ItemChargeType.FRUIT_BASKET;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
 import net.runelite.client.ui.overlay.components.TextComponent;
@@ -160,6 +162,8 @@ class ItemChargeOverlay extends WidgetItemOverlay
 				|| (type == WATERCAN && !plugin.isShowWateringCanCharges())
 				|| (type == WATERSKIN && !plugin.isShowWaterskinCharges())
 				|| (type == BELLOWS && !plugin.isShowBellowCharges())
+				|| (type == FRUIT_BASKET && !plugin.isShowBasketCharges())
+				|| (type == SACK && !plugin.isShowSackCharges())
 				|| (type == ABYSSAL_BRACELET && !plugin.isShowAbyssalBraceletCharges()))
 			{
 				return;
@@ -180,7 +184,7 @@ class ItemChargeOverlay extends WidgetItemOverlay
 	{
 		return plugin.isShowTeleportCharges() || plugin.isShowDodgyCount() || plugin.isShowFungicideCharges()
 			|| plugin.isShowImpCharges() || plugin.isShowWateringCanCharges() || plugin.isShowWaterskinCharges()
-			|| plugin.isShowBellowCharges() || plugin.isShowAbyssalBraceletCharges() || plugin.isShowExplorerRingCharges()
+			|| plugin.isShowBellowCharges() || plugin.isShowBasketCharges() || plugin.isShowSackCharges() || plugin.isShowAbyssalBraceletCharges() || plugin.isShowExplorerRingCharges()
 			|| plugin.isShowRingOfForgingCount();
 	}
 }
