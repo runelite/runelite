0c7f6fda9cbf02408c35a5353b8208e5aad7c2f8
Adam <Adam@sigterm.info>
2019-11-15 09:47:11 -0500

    world hopper: assign leagues world color
    Co-authored-by: Hydrox6 <ikada@protonmail.ch>

--- runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldTableRow.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldhopper/WorldTableRow.java
@@ -62,6 +62,7 @@ class WorldTableRow extends JPanel
 	private static final Color TOURNAMENT_WORLD = new Color(79, 145, 255);
 	private static final Color MEMBERS_WORLD = new Color(210, 193, 53);
 	private static final Color FREE_WORLD = new Color(200, 200, 200);
+	private static final Color LEAGUE_WORLD = new Color(157, 237, 1);
 
 	static
 	{
@@ -248,6 +249,10 @@ class WorldTableRow extends JPanel
 			|| world.getTypes().contains(WorldType.DEADMAN))
 		{
 			activityField.setForeground(DANGEROUS_WORLD);
+		}
+		else if (world.getTypes().contains(WorldType.LEAGUE))
+		{
+			activityField.setForeground(LEAGUE_WORLD);
 		}
 		else if (world.getTypes().contains(WorldType.TOURNAMENT))
 		{
