99fd6278e63516d0089a3a8e904737cc075fdab5
emiljensen2 <55587410+emiljensen2@users.noreply.github.com>
2019-11-16 04:16:45 +0100

    notifier: require client to be focused to cancel notifications

--- runelite-client/src/main/java/net/runelite/client/Notifier.java
+++ runelite-client/src/main/java/net/runelite/client/Notifier.java
@@ -195,9 +195,9 @@ public class Notifier
 			case SOLID_UNTIL_CANCELLED:
 			case FLASH_UNTIL_CANCELLED:
 				// Any interaction with the client since the notification started will cancel it after the minimum duration
-				if (client.getMouseIdleTicks() < MINIMUM_FLASH_DURATION_TICKS
+				if ((client.getMouseIdleTicks() < MINIMUM_FLASH_DURATION_TICKS
 					|| client.getKeyboardIdleTicks() < MINIMUM_FLASH_DURATION_TICKS
-					|| client.getMouseLastPressedMillis() > mouseLastPressedMillis)
+					|| client.getMouseLastPressedMillis() > mouseLastPressedMillis) && clientUI.isFocused())
 				{
 					flashStart = null;
 				}
