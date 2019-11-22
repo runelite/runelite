ef6ca92a5c2ccb80391618fdb5d410e8bdd1b941
Kyle <48519776+xKylee@users.noreply.github.com>
2019-11-22 13:55:32 +0000

    Update XpDropPlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/experiencedrop/XpDropPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/experiencedrop/XpDropPlugin.java
@@ -70,6 +70,8 @@ public class XpDropPlugin extends Plugin
 	private static final double HITPOINT_RATIO = 1.33; // Base rate of hp xp per point damage
 	private static final double DMM_MULTIPLIER_RATIO = 10;
 	private static final double TL_MULTIPLIER_RATIO = 5;
+	private static final int TWISTED_LEAGUE_WAY_OF_THE_WARRIOR = 3;
+	private static final int TWISTED_LEAGUE_XERICS_WISDOM = 3;
 	@Inject
 	private Client client;
 	@Inject
@@ -362,6 +364,15 @@ public class XpDropPlugin extends Plugin
 		if (client.getWorldType().contains(WorldType.LEAGUE))
 		{
 			damageDealt = damageDealt / TL_MULTIPLIER_RATIO;
+			
+			if (client.getVar(Varbits.TWISTED_LEAGUE_RELIC_3) == TWISTED_LEAGUE_WAY_OF_THE_WARRIOR)
+			{
+				damageDealt = damageDealt / 2;
+			}
+			if (client.getVar(Varbits.TWISTED_LEAGUE_RELIC_5) == TWISTED_LEAGUE_XERICS_WISDOM)
+			{
+				damageDealt = damageDealt / 2;
+			}
 		}
 		
 		// Some NPCs have an XP modifier, account for it here.
