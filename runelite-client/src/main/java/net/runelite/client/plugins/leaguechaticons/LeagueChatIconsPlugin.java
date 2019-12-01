500cef3a0bae88e17f2252c49492978a03f84428
hsamoht <6449361+hsamoht@users.noreply.github.com>
2019-12-01 15:48:17 +0100

    League Chat Icons: Rename plugin from 'Chat League Icons' to 'League Chat Icons'

--- runelite-client/src/main/java/net/runelite/client/plugins/leaguechaticons/LeagueChatIconsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/leaguechaticons/LeagueChatIconsPlugin.java
@@ -58,7 +58,7 @@ import net.runelite.http.api.worlds.WorldResult;
 import net.runelite.http.api.worlds.WorldType;
 
 @PluginDescriptor(
-	name = "Chat League Icons",
+	name = "League Chat Icons",
 	description = "Changes the chat icon for players on league worlds",
 	enabledByDefault = false
 )
