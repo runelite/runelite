8087d16aea051592bfa8b8b61b723116516175a8
hsamoht <6449361+hsamoht@users.noreply.github.com>
2019-12-02 15:32:00 +0100

    Bank: Add config to disable left click search

--- runelite-client/src/main/java/net/runelite/client/plugins/bank/BankPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/bank/BankPlugin.java
@@ -86,6 +86,7 @@ public class BankPlugin extends Plugin
 	private static final String DEPOSIT_INVENTORY = "Deposit inventory";
 	private static final String DEPOSIT_LOOT = "Deposit loot";
 	private static final String SEED_VAULT_TITLE = "Seed Vault";
+	private static final String SEARCH = "Search";
 
 	private static final String NUMBER_REGEX = "[0-9]+(\\.[0-9]+)?[kmb]?";
 	private static final Pattern VALUE_SEARCH_PATTERN = Pattern.compile("^(?<mode>ge|ha|alch)?" +
@@ -144,7 +145,8 @@ public class BankPlugin extends Plugin
 		{
 			if ((entry.getOption().equals(DEPOSIT_WORN) && config.rightClickBankEquip())
 				|| (entry.getOption().equals(DEPOSIT_INVENTORY) && config.rightClickBankInventory())
-				|| (entry.getOption().equals(DEPOSIT_LOOT) && config.rightClickBankLoot()))
+				|| (entry.getOption().equals(DEPOSIT_LOOT) && config.rightClickBankLoot())
+				|| (entry.getOption().equals(SEARCH) && config.rightClickSearch()))
 			{
 				event.setForceRightClick(true);
 				return;
@@ -157,7 +159,8 @@ public class BankPlugin extends Plugin
 	{
 		if ((event.getOption().equals(DEPOSIT_WORN) && config.rightClickBankEquip())
 			|| (event.getOption().equals(DEPOSIT_INVENTORY) && config.rightClickBankInventory())
-			|| (event.getOption().equals(DEPOSIT_LOOT) && config.rightClickBankLoot()))
+			|| (event.getOption().equals(DEPOSIT_LOOT) && config.rightClickBankLoot())
+			|| (event.getOption().equals(SEARCH) && config.rightClickSearch()))
 		{
 			forceRightClickFlag = true;
 		}
