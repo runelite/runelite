a58b3e72406752c622488942745e9ff4ca9dbe29
Alexsuperfly <alexcsumner@gmail.com>
2019-12-03 23:21:46 -0500

    mining: show respawn timers after GameState change

--- runelite-client/src/main/java/net/runelite/client/plugins/mining/RockRespawn.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mining/RockRespawn.java
@@ -38,4 +38,9 @@ class RockRespawn
 	private final Instant startTime;
 	private final int respawnTime;
 	private final int zOffset;
+
+	boolean isExpired()
+	{
+		return Instant.now().isAfter(startTime.plusMillis(respawnTime));
+	}
 }
