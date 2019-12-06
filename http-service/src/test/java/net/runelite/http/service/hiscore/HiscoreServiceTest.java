9ae4e772a4dfaf2e23747151a5f86059c71dfc05
Adam <Adam@sigterm.info>
2019-12-06 16:08:30 -0500

    http-api: update for boss hiscores
    This leaves Callisto and Cerberus commented out as they are currently
    not being returned by the hiscores

--- http-service/src/test/java/net/runelite/http/service/hiscore/HiscoreServiceTest.java
+++ http-service/src/test/java/net/runelite/http/service/hiscore/HiscoreServiceTest.java
@@ -70,7 +70,48 @@ public class HiscoreServiceTest
 			+ "42,14113\n"
 			+ "1,777\n"
 			+ "254,92\n"
-			+ "-1,-1\n"; // lms
+			+ "-1,-1\n" // lms
+			+ "24870,37\n"
+			+ "15020,388\n"
+			+ "50463,147\n"
+			+ "-1,-1\n"
+			+ "22744,107\n"
+			+ "-1,-1\n"
+			+ "20150,17\n"
+			+ "29400,18\n"
+			+ "13465,172\n"
+			+ "1889,581\n"
+			+ "42891,11\n"
+			+ "1624,1957\n"
+			+ "1243,2465\n"
+			+ "1548,2020\n"
+			+ "-1,-1\n"
+			+ "16781,327\n"
+			+ "19004,149\n"
+			+ "-1,-1\n"
+			+ "72046,5\n"
+			+ "5158,374\n"
+			+ "20902,279\n"
+			+ "702,6495\n"
+			+ "10170,184\n"
+			+ "8064,202\n"
+			+ "6936,2\n"
+			+ "-1,-1\n"
+			+ "-1,-1\n"
+			+ "19779,22\n"
+			+ "58283,10\n"
+			+ "-1,-1\n"
+			+ "-1,-1\n"
+			+ "-1,-1\n"
+			+ "29347,130\n"
+			+ "723,4\n"
+			+ "1264,38\n"
+			+ "44595,4\n"
+			+ "24820,4\n"
+			+ "12116,782\n"
+			+ "2299,724\n"
+			+ "19301,62\n"
+			+ "1498,5847\n";
 
 	private final MockWebServer server = new MockWebServer();
 
@@ -107,6 +148,8 @@ public class HiscoreServiceTest
 		Assert.assertEquals(254, result.getClueScrollMaster().getRank());
 		Assert.assertEquals(-1, result.getLastManStanding().getLevel());
 		Assert.assertEquals(2460, result.getLeaguePoints().getLevel());
+		Assert.assertEquals(37, result.getAbyssalSire().getLevel());
+		Assert.assertEquals(5847, result.getZulrah().getLevel());
 	}
 
 }
