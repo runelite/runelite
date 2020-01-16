2a8bd36bcc044186f6b7b4bdb82b6885fd87fca3
Kyle <48519776+xKylee@users.noreply.github.com>
2020-01-16 12:00:41 +0000

    statusorbs: remove twisted league stuff (#2250)
    statusorbs: remove twisted league stuff

--- runelite-client/src/main/java/net/runelite/client/plugins/statusorbs/StatusOrbsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/statusorbs/StatusOrbsPlugin.java
@@ -82,7 +82,6 @@ public class StatusOrbsPlugin extends Plugin
 
 	private static final int SPEC_REGEN_TICKS = 50;
 	private static final int NORMAL_HP_REGEN_TICKS = 100;
-	private static final int TWISTED_LEAGUE_ENDLESS_ENDURANCE_RELIC = 2;
 
 	@Inject
 	private Client client;
@@ -250,12 +249,6 @@ public class StatusOrbsPlugin extends Plugin
 		{
 			ticksPerHPRegen /= 2;
 			hpPerMs *= 2;
-		}
-
-		if (client.getVar(Varbits.TWISTED_LEAGUE_RELIC_1) == TWISTED_LEAGUE_ENDLESS_ENDURANCE_RELIC)
-		{
-			ticksPerHPRegen /= 4;
-			hpPerMs *= 4;
 		}
 
 		ticksSinceHPRegen = (ticksSinceHPRegen + 1) % ticksPerHPRegen;
