fea317c4dbdca03afb83046d1c513e54074e3158
Paveldin <40346808+Paveldin@users.noreply.github.com>
2020-01-25 00:33:27 -0500

    Update IdleNotifierConfig.java

--- runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
@@ -24,6 +24,9 @@
  */
 package net.runelite.client.plugins.idlenotifier;
 
+import java.awt.Color;
+import net.runelite.client.Notifier;
+import net.runelite.client.config.Alpha;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
@@ -111,8 +114,8 @@ public interface IdleNotifierConfig extends Config
 	@ConfigItem(
 		keyName = "oxygen",
 		name = "Oxygen Notification Threshold",
-		position = 8,
-		description = "The amount of remaining oxygen to send a notification at. A value of 0 will disable notification."
+		description = "The amount of remaining oxygen to send a notification at. A value of 0 will disable notification.",
+		position = 8
 	)
 	default int getOxygenThreshold()
 	{
@@ -122,11 +125,95 @@ public interface IdleNotifierConfig extends Config
 	@ConfigItem(
 		keyName = "spec",
 		name = "Special Attack Energy Notification Threshold",
-		position = 9,
-		description = "The amount of spec energy reached to send a notification at. A value of 0 will disable notification."
+		description = "The amount of spec energy reached to send a notification at. A value of 0 will disable notification.",
+		position = 9
 	)
 	default int getSpecEnergyThreshold()
 	{
 		return 0;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "animationidleflashcolor",
+			name = "Idle Animation Color",
+			description = "Color of flash notification after the player is idle",
+			position = 10
+	)
+	default Color getAnimationIdleFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "logoutidleflashcolor",
+			name = "Idle Logout Color",
+			description = "Color of flash notification when the player is about to be logged out",
+			position = 11
+	)
+	default Color getLogoutIdleFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "combatidleflashcolor",
+			name = "Combat Idle Color",
+			description = "Color of flash notification after the player is out of combat",
+			position = 12
+	)
+	default Color getCombatIdleFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "hitpointsflashcolor",
+			name = "Hitpoints Color",
+			description = "Color of flash notification after hitpoints reach the threshold",
+			position = 13
+	)
+	default Color getHitpointsFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "prayerflashcolor",
+			name = "Prayer Color",
+			description = "Color of flash notification after prayer points reach the threshold",
+			position = 14
+	)
+	default Color getPrayerFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "Oxygenflashcolor",
+			name = "Oxygen Color",
+			description = "Color of flash notification after oxygen level reach the threshold",
+			position = 15
+	)
+	default Color getOxygenFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
+	}
+
+	@Alpha
+	@ConfigItem(
+			keyName = "Energyflashcolor",
+			name = "Energy Color",
+			description = "Color of flash notification after energy percentage reach the threshold",
+			position = 16
+	)
+	default Color getEnergyFlashColor()
+	{
+		return Notifier.FLASH_COLOR;
 	}
 }
