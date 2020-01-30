3037f1947a424ec52a8a9fa7bdf574e468d7f45b
Paveldin <40346808+Paveldin@users.noreply.github.com>
2020-01-30 13:21:27 -0500

    Update IdleNotifierConfig.java

--- runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
@@ -31,6 +31,8 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
+import java.awt.*;
+
 @ConfigGroup("idlenotifier")
 public interface IdleNotifierConfig extends Config
 {
@@ -135,84 +137,12 @@ public interface IdleNotifierConfig extends Config
 
 	@Alpha
 	@ConfigItem(
-			keyName = "animationidleflashcolor",
-			name = "Idle Animation Color",
-			description = "Color of flash notification after the player is idle",
-			position = 10
+		keyName = "notificationFlashColor",
+		name = "Notification Flash Color",
+		description = "Changes the color of the notification flashes.",
+		position = 10
 	)
-	default Color getAnimationIdleFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "logoutidleflashcolor",
-			name = "Idle Logout Color",
-			description = "Color of flash notification when the player is about to be logged out",
-			position = 11
-	)
-	default Color getLogoutIdleFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "combatidleflashcolor",
-			name = "Combat Idle Color",
-			description = "Color of flash notification after the player is out of combat",
-			position = 12
-	)
-	default Color getCombatIdleFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "hitpointsflashcolor",
-			name = "Hitpoints Color",
-			description = "Color of flash notification after hitpoints reach the threshold",
-			position = 13
-	)
-	default Color getHitpointsFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "prayerflashcolor",
-			name = "Prayer Color",
-			description = "Color of flash notification after prayer points reach the threshold",
-			position = 14
-	)
-	default Color getPrayerFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "Oxygenflashcolor",
-			name = "Oxygen Color",
-			description = "Color of flash notification after oxygen level reach the threshold",
-			position = 15
-	)
-	default Color getOxygenFlashColor()
-	{
-		return Notifier.FLASH_COLOR;
-	}
-
-	@Alpha
-	@ConfigItem(
-			keyName = "Energyflashcolor",
-			name = "Energy Color",
-			description = "Color of flash notification after energy percentage reach the threshold",
-			position = 16
-	)
-	default Color getEnergyFlashColor()
+	default Color getNotificationFlashColor()
 	{
 		return Notifier.FLASH_COLOR;
 	}
