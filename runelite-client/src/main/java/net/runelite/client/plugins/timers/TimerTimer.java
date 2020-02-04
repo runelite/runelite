e0df1b0e80afefa790ec176566cd97f3569f27d9
Lucas <lws.ned1260@gmail.com>
2020-02-03 23:09:27 -0800

    Timers: Use poison VarPlayer value for antipoison/venom timers
    This commit allows the poison timer to be much more accurate as it
    receives periodic updates from the server, and covers edge cases where
    poison may be delayed or cured from sources besides potion clicks.
    (banked potion drinks, HP orb "Cure" menu option, etc.)
    
    This commit removes the `initialDelay` field from GameTimer as the field
    was used only to chain antivenom timers into their follow-up antipoison
    timers.
    
    Fixes runelite/runelite#1471
    Fixes runelite/runelite#10724

--- runelite-client/src/main/java/net/runelite/client/plugins/timers/TimerTimer.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timers/TimerTimer.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017, Adam <Adam@sigterm.info>
+ * Copyright (c) 2019, Lucas <https://github.com/Lucwousin>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,7 +26,6 @@
 package net.runelite.client.plugins.timers;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.ui.overlay.infobox.InfoBoxPriority;
@@ -35,25 +35,11 @@ class TimerTimer extends Timer
 {
 	private final GameTimer timer;
 
-	TimerTimer(GameTimer timer, Plugin plugin)
+	TimerTimer(GameTimer timer, Duration duration, Plugin plugin)
 	{
-		super(timer.getDuration().toMillis(), ChronoUnit.MILLIS, null, plugin);
+		super(duration.toMillis(), ChronoUnit.MILLIS, null, plugin);
 		this.timer = timer;
 		setPriority(InfoBoxPriority.MED);
-	}
-
-	@Override
-	public boolean render()
-	{
-		final boolean rendered = super.render();
-
-		if (rendered)
-		{
-			final Duration fromStart = Duration.between(getStartTime(), Instant.now());
-			return !fromStart.minus(timer.getInitialDelay()).isNegative();
-		}
-
-		return false;
 	}
 
 	public GameTimer getTimer()
