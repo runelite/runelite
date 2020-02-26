93d1e2494386022d6991289cdd5202d4ce271efd
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-02-26 11:24:44 +0100

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -27,7 +27,7 @@ object ProjectVersions {
     const val launcherVersion = "2.0.4"
     const val rlVersion = "1.6.7"
 
-    const val openosrsVersion = "3.0.4"
+    const val openosrsVersion = "3.0.5"
 
     const val rsversion = 188
     const val cacheversion = 165
