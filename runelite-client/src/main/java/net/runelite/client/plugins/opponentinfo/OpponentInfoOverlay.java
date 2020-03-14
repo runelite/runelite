82cef9329663833ee800575d32b52c97fda34e4a
Adam <Adam@sigterm.info>
2020-03-14 12:07:22 -0400

    opponentinfo: account for panel border in overlay size

--- runelite-client/src/main/java/net/runelite/client/plugins/opponentinfo/OpponentInfoOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/opponentinfo/OpponentInfoOverlay.java
@@ -151,8 +151,8 @@ class OpponentInfoOverlay extends Overlay
 		panelComponent.getChildren().clear();
 
 		// Opponent name
-		int textWidth = Math.max(ComponentConstants.STANDARD_WIDTH, fontMetrics.stringWidth(opponentName));
-		panelComponent.setPreferredSize(new Dimension(textWidth, 0));
+		int panelWidth = Math.max(ComponentConstants.STANDARD_WIDTH, fontMetrics.stringWidth(opponentName) + ComponentConstants.STANDARD_BORDER + ComponentConstants.STANDARD_BORDER);
+		panelComponent.setPreferredSize(new Dimension(panelWidth, 0));
 		panelComponent.getChildren().add(TitleComponent.builder()
 			.text(opponentName)
 			.build());
@@ -221,8 +221,8 @@ class OpponentInfoOverlay extends Overlay
 		// Opponents opponent
 		if (opponentsOpponentName != null && opponentInfoConfig.showOpponentsOpponent())
 		{
-			textWidth = Math.max(textWidth, fontMetrics.stringWidth(opponentsOpponentName));
-			panelComponent.setPreferredSize(new Dimension(textWidth, 0));
+			panelWidth = Math.max(panelWidth, fontMetrics.stringWidth(opponentsOpponentName) + ComponentConstants.STANDARD_BORDER + ComponentConstants.STANDARD_BORDER);
+			panelComponent.setPreferredSize(new Dimension(panelWidth, 0));
 			panelComponent.getChildren().add(TitleComponent.builder()
 				.text(opponentsOpponentName)
 				.build());
