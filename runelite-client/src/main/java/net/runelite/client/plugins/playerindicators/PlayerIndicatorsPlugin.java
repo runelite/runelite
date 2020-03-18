b85675f6240acee68877a0629fac9329a9937e95
Lotto <prozet@live.se>
2020-03-19 00:12:28 +0100

    api: remove Follow and Trade menu actions
    They are just the third and fourth player options but deprioritized by having 2000 added to them.

--- runelite-client/src/main/java/net/runelite/client/plugins/playerindicators/PlayerIndicatorsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/playerindicators/PlayerIndicatorsPlugin.java
@@ -103,8 +103,8 @@ public class PlayerIndicatorsPlugin extends Plugin
 		}
 
 		int identifier = menuEntryAdded.getIdentifier();
-		if (type == FOLLOW.getId() || type == TRADE.getId()
-			|| type == SPELL_CAST_ON_PLAYER.getId() || type == ITEM_USE_ON_PLAYER.getId()
+		if (type == SPELL_CAST_ON_PLAYER.getId()
+			|| type == ITEM_USE_ON_PLAYER.getId()
 			|| type == PLAYER_FIRST_OPTION.getId()
 			|| type == PLAYER_SECOND_OPTION.getId()
 			|| type == PLAYER_THIRD_OPTION.getId()
