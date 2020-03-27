53051325e1960a210d73324336978a1face5d256
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-03-27 21:21:45 +0100

    project: Bump OpenOSRS version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -27,7 +27,7 @@ object ProjectVersions {
     const val launcherVersion = "2.2.0"
     const val rlVersion = "1.6.10"
 
-    const val openosrsVersion = "3.2.0"
+    const val openosrsVersion = "3.2.1"
 
     const val rsversion = 189
     const val cacheversion = 165
