6351bffce47db9adef72dcd8f5aa1385500831f2
Robert Nönnig <mail@rnoennig.de>
2020-03-29 21:55:41 +0200

    fixes prayer flick showing when minimap is hidden #11133

--- runelite-client/src/main/java/net/runelite/client/plugins/prayer/PrayerFlickOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayer/PrayerFlickOverlay.java
@@ -63,14 +63,8 @@ class PrayerFlickOverlay extends Overlay
 			return null;
 		}
 
-		// Prayer orb widget will be hidden when minimap is hidden
-		Widget prayerOrb = client.getWidget(WidgetInfo.MINIMAP_PRAYER_ORB);
-		if (prayerOrb == null || prayerOrb.isHidden()) {
-			return null;
-		}
-
 		Widget xpOrb = client.getWidget(WidgetInfo.MINIMAP_QUICK_PRAYER_ORB);
-		if (xpOrb == null)
+		if (xpOrb == null || xpOrb.isHidden())
 		{
 			return null;
 		}
