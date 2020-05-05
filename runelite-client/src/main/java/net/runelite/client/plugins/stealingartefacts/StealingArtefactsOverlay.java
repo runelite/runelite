6c505ef30dd6b58592822bef4cb04cea4edde01f
ThePharros <18340303+ThePharros@users.noreply.github.com>
2020-05-04 21:20:07 -0400

    Create StealingArtefactsOverlay.java

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/stealingartefacts/StealingArtefactsOverlay.java
@@ -0,0 +1,53 @@
+package net.runelite.client.plugins.stealingartefacts;
+
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayPriority;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+
+
+public class StealingArtefactsOverlay extends Overlay
+{
+	private final StealingArtefactsPlugin plugin;
+	private final PanelComponent panelComponent = new PanelComponent();
+
+	@Inject
+	private StealingArtefactsOverlay(StealingArtefactsPlugin plugin)
+	{
+		super(plugin);
+		setPosition(OverlayPosition.TOP_LEFT);
+		setPriority(OverlayPriority.LOW);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		panelComponent.getChildren().clear();
+
+		StealingArtefactState stealingArtefactState = plugin.getStealingArtefactState();
+
+		if (!plugin.isInPortPiscariliusRegion() || stealingArtefactState == null)
+		{
+			return null;
+		}
+
+		panelComponent.getChildren().add(TitleComponent.builder()
+				.text("Stealing Artefacts")
+				.color(Color.WHITE)
+				.build());
+
+		panelComponent.getChildren().add(TitleComponent.builder()
+				.text(stealingArtefactState.getDescription())
+				.color(stealingArtefactState.getColor())
+				.build());
+
+		return panelComponent.render(graphics);
+	}
+}
