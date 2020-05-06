4e96b905cf6d7357088a2d51754004ad827d1154
Corey Forsyth <corey.j.forsyth@gmail.com>
2020-05-06 02:41:34 -0400

    duplicate chat plugin style fix

--- runelite-client/src/main/java/net/runelite/client/plugins/duplicatechat/DuplicateChatPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/duplicatechat/DuplicateChatPlugin.java
@@ -46,153 +46,169 @@ import java.util.HashMap;
 import java.util.Map;
 
 @PluginDescriptor(
-        name = "Duplicate Chat Filter",
-        description = "Collapse duplicate game chats into a single message",
-        enabledByDefault = false
+		name = "Duplicate Chat Filter",
+		description = "Collapse duplicate game chats into a single message",
+		enabledByDefault = false
 )
 @Slf4j
 public class DuplicateChatPlugin extends Plugin
 {
 
-    private static final String COUNT_FORMAT = " (%d)";
-    private Multiset<String> chatCounts;
-    private Map<String, Integer> chatIndex;
+	private static final String COUNT_FORMAT = " (%d)";
+	private Multiset<String> chatCounts;
+	private Map<String, Integer> chatIndex;
 
-    @Inject
-    private Client client;
+	@Inject
+	private Client client;
 
-    @Inject
-    private DuplicateChatConfig config;
+	@Inject
+	private DuplicateChatConfig config;
 
-    @Provides
-    DuplicateChatConfig provideConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(DuplicateChatConfig.class);
-    }
+	@Provides
+	DuplicateChatConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(DuplicateChatConfig.class);
+	}
 
-    @Override
-    protected void startUp() throws Exception
-    {
-        chatIndex = new HashMap<>();
-        chatCounts = HashMultiset.create();
-        client.refreshChat();
-    }
+	@Override
+	protected void startUp() throws Exception
+	{
+		chatIndex = new HashMap<>();
+		chatCounts = HashMultiset.create();
+		client.refreshChat();
+	}
 
-    @Override
-    protected void shutDown() throws Exception
-    {
-        client.refreshChat();
-    }
+	@Override
+	protected void shutDown() throws Exception
+	{
+		client.refreshChat();
+	}
 
-    @Subscribe
-    public void onChatMessage(ChatMessage chatMessage) {
-        if (chatCounts.elementSet().size() > config.getMaxTrackedChats()) {
-            cleanupChatTrackers();
-        }
-        if (shouldProcessMessageType(chatMessage.getType())){
-            chatCounts.add(chatMessage.getName() + chatMessage.getMessage());
-        }
-        refreshChatIndex();
-    }
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage)
+	{
+		if (chatCounts.elementSet().size() > config.getMaxTrackedChats())
+		{
+			cleanupChatTrackers();
+		}
+		if (shouldProcessMessageType(chatMessage.getType()))
+		{
+			chatCounts.add(chatMessage.getName() + chatMessage.getMessage());
+		}
+		refreshChatIndex();
+	}
 
-    private void refreshChatIndex() {
-        client.getMessages().forEach(messageNode ->{
-            String fullMessage = messageNode.getName() + messageNode.getValue();
-            Integer currentSetIndex = chatIndex.get(fullMessage);
-            if (currentSetIndex == null ||
-                    currentSetIndex < messageNode.getId()) {
-                chatIndex.put(fullMessage, messageNode.getId());
-            }
-        });
-    }
+	private void refreshChatIndex()
+	{
+		client.getMessages().forEach(messageNode ->
+		{
+			String fullMessage = messageNode.getName() + messageNode.getValue();
+			Integer currentSetIndex = chatIndex.get(fullMessage);
+			if (currentSetIndex == null ||
+					currentSetIndex < messageNode.getId())
+			{
+				chatIndex.put(fullMessage, messageNode.getId());
+			}
+		});
+	}
 
-    @Subscribe
-    public void onScriptCallbackEvent(ScriptCallbackEvent event)
-    {
-        if (!"chatFilterCheck".equals(event.getEventName()))
-        {
-            return;
-        }
+	@Subscribe
+	public void onScriptCallbackEvent(ScriptCallbackEvent event)
+	{
+		if (!"chatFilterCheck".equals(event.getEventName()))
+		{
+			return;
+		}
 
-        int[] intStack = client.getIntStack();
-        int intStackSize = client.getIntStackSize();
-        int messageType = intStack[intStackSize - 2];
-        int messageId = intStack[intStackSize - 1];
+		int[] intStack = client.getIntStack();
+		int intStackSize = client.getIntStackSize();
+		int messageType = intStack[intStackSize - 2];
+		int messageId = intStack[intStackSize - 1];
 
-        ChatMessageType chatMessageType = ChatMessageType.of(messageType);
+		ChatMessageType chatMessageType = ChatMessageType.of(messageType);
 
-        // Only process game messages for duplicates
-        if (!shouldProcessMessageType(chatMessageType)) {
-            return;
-        }
-        String[] stringStack = client.getStringStack();
-        int stringStackSize = client.getStringStackSize();
-        String message = stringStack[stringStackSize - 1];
-        MessageNode messageNode = client.getMessages().get(messageId);
-        String fullMessage = messageNode.getName() + message;
+		// Only process game messages for duplicates
+		if (!shouldProcessMessageType(chatMessageType))
+		{
+			return;
+		}
+		String[] stringStack = client.getStringStack();
+		int stringStackSize = client.getStringStackSize();
+		String message = stringStack[stringStackSize - 1];
+		MessageNode messageNode = client.getMessages().get(messageId);
+		String fullMessage = messageNode.getName() + message;
 
-        if (chatIndex.containsKey(fullMessage) &&
-                chatIndex.get(fullMessage) != messageId ||
-                shouldBlockPlayerChat(fullMessage, chatMessageType))
-        {
-            // Block the message
-            intStack[intStackSize - 3] = 0;
-        }
-        else
-        {
-            // Replace the message
-            String messageWithCount = addCountToGameMessage(message, chatCounts.count(fullMessage));
-            stringStack[stringStackSize - 1] = messageWithCount;
-        }
-    }
+		if (chatIndex.containsKey(fullMessage) &&
+				chatIndex.get(fullMessage) != messageId ||
+				shouldBlockPlayerChat(fullMessage, chatMessageType))
+		{
+			// Block the message
+			intStack[intStackSize - 3] = 0;
+		}
+		else
+		{
+			// Replace the message
+			String messageWithCount = addCountToGameMessage(message, chatCounts.count(fullMessage));
+			stringStack[stringStackSize - 1] = messageWithCount;
+		}
+	}
 
-    @Subscribe
-    public void onConfigChanged(ConfigChanged event)
-    {
-        if (!"duplicatechat".equals(event.getGroup()))
-        {
-            return;
-        }
-        client.refreshChat();
-    }
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (!"duplicatechat".equals(event.getGroup()))
+		{
+			return;
+		}
+		client.refreshChat();
+	}
 
-    private String addCountToGameMessage(String gameMessage, int count) {
-        if (count > 1){
-            return gameMessage +
-                    ColorUtil.wrapWithColorTag(String.format(COUNT_FORMAT, count), config.getChatCountColor());
-        } else {
-            return gameMessage;
-        }
-    }
+	private String addCountToGameMessage(String gameMessage, int count)
+	{
+		if (count > 1)
+		{
+			return gameMessage +
+					ColorUtil.wrapWithColorTag(String.format(COUNT_FORMAT, count), config.getChatCountColor());
+		}
+		else
+		{
+			return gameMessage;
+		}
+	}
 
-    private void cleanupChatTrackers() {
-        log.info("Cleaning up chat counts. Current size: {}", chatCounts.elementSet().size());
-        chatCounts.removeIf(message -> chatCounts.count(message) <= 1);
-        log.info("After clearing messages with count == 1: {}", chatCounts.elementSet().size());
-        if (chatCounts.elementSet().size() > config.getMaxTrackedChats()) {
-            chatCounts.clear();
-        }
-        chatIndex.clear();
-    }
+	private void cleanupChatTrackers()
+	{
+		log.info("Cleaning up chat counts. Current size: {}", chatCounts.elementSet().size());
+		chatCounts.removeIf(message -> chatCounts.count(message) <= 1);
+		log.info("After clearing messages with count == 1: {}", chatCounts.elementSet().size());
+		if (chatCounts.elementSet().size() > config.getMaxTrackedChats())
+		{
+			chatCounts.clear();
+		}
+		chatIndex.clear();
+	}
 
-    private boolean shouldBlockPlayerChat(String fullMessage, ChatMessageType chatMessageType) {
-        return chatMessageType == ChatMessageType.PUBLICCHAT &&
-                config.getMaxRepeatedPublicChats() > 1 &&
-                chatCounts.count(fullMessage) > config.getMaxRepeatedPublicChats();
-    }
+	private boolean shouldBlockPlayerChat(String fullMessage, ChatMessageType chatMessageType)
+	{
+		return chatMessageType == ChatMessageType.PUBLICCHAT &&
+				config.getMaxRepeatedPublicChats() > 1 &&
+				chatCounts.count(fullMessage) > config.getMaxRepeatedPublicChats();
+	}
 
-    private boolean shouldProcessMessageType(ChatMessageType chatMessageType) {
-        switch (chatMessageType){
-            case ENGINE:
-            case GAMEMESSAGE:
-            case NPC_EXAMINE:
-            case OBJECT_EXAMINE:
-            case SPAM:
-                return true;
-            case PUBLICCHAT:
-                return config.consolidatePlayerChat();
-            default:
-                return false;
-        }
-    }
+	private boolean shouldProcessMessageType(ChatMessageType chatMessageType)
+	{
+		switch (chatMessageType)
+		{
+			case ENGINE:
+			case GAMEMESSAGE:
+			case NPC_EXAMINE:
+			case OBJECT_EXAMINE:
+			case SPAM:
+				return true;
+			case PUBLICCHAT:
+				return config.consolidatePlayerChat();
+			default:
+				return false;
+		}
+	}
 }
