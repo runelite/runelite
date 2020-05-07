1a2d482587a68cdb69e8f24cafbbc62292d15c9e
Corey Forsyth <corey.j.forsyth@gmail.com>
2020-05-07 02:39:04 -0400

    No wildcard imports

--- runelite-client/src/main/java/net/runelite/client/plugins/chatfilter/ChatFilterConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatfilter/ChatFilterConfig.java
@@ -29,7 +29,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-import java.awt.*;
+import java.awt.Color;
 
 @ConfigGroup("chatfilter")
 public interface ChatFilterConfig extends Config
