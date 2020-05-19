94482f5aed49604fa7c45631c98cddc2b0ebb208
loldudester <loldudester@users.noreply.github.com>
2020-05-19 20:51:29 +0100

    keyremapping: Don't remap f-keys when the bank pin interface is open

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -154,6 +154,7 @@ public class WidgetID
 	public static final int ADVENTURE_LOG_ID = 187;
 	public static final int COUNTERS_LOG_GROUP_ID = 625;
 	public static final int GAUNTLET_TIMER_GROUP_ID = 637;
+	public static final int BANK_PIN_GROUP_ID = 213;
 
 	static class WorldMap
 	{
@@ -917,5 +918,10 @@ public class WidgetID
 	static class GauntletTimer
 	{
 		static final int CONTAINER = 2;
+	}
+
+	static class BankPin
+	{
+		static final int CONTAINER = 0;
 	}
 }
