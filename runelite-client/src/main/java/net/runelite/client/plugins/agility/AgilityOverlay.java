e5adab24ddb088b35d7e09ac5a90bbb0909129a7
Adam <Adam@sigterm.info>
2020-06-12 21:33:05 -0400

    agility overlay: correctly use sepulchre overlay color

--- runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityOverlay.java
@@ -145,7 +145,7 @@ class AgilityOverlay extends Overlay
 		Set<NPC> npcs = plugin.getNpcs();
 		if (!npcs.isEmpty() && config.highlightSepulchreNpcs())
 		{
-			Color color = config.getOverlayColor();
+			Color color = config.sepulchreHighlightColor();
 			for (NPC npc : npcs)
 			{
 				NPCComposition npcComposition = npc.getComposition();
