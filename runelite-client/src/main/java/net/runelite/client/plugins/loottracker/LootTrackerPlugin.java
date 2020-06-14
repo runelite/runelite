127da65d22d97d749db41b722564e9baf6cc1299
Patrick Makarewicz <pmakarewicz@terryberry.com>
2020-06-14 09:30:57 -0400

    master: dont need another regex for coins

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
@@ -167,7 +167,6 @@ public class LootTrackerPlugin extends Plugin
 	//Regex for chatbox matching
 	private static final Pattern LOOT_DROP_PATTERN =
 			Pattern.compile("^(?:Valuable|Untradeable|(?<name>.*?) received a) drop: (?:(?<quantity>[\\d,]+) x )?(?<item>[A-Za-z' \\(\\d\\)]+)(?: \\([\\d,]+ coins\\))?$");
-	private static final Pattern COIN_DROP_PATTERN = Pattern.compile("([\\d|,]+ x Coins)");
 
 	// Last man standing map regions
 	private static final Set<Integer> LAST_MAN_STANDING_REGIONS = ImmutableSet.of(13658, 13659, 13914, 13915, 13916);
@@ -807,12 +806,11 @@ public class LootTrackerPlugin extends Plugin
 		if (lootMatcher.find())
 		{
 			final String itemName = lootMatcher.group("item");
-			final Matcher coinDropMatch = COIN_DROP_PATTERN.matcher(itemName);
 			quantity = (lootMatcher.group("quantity") != null) ? Integer.parseInt(lootMatcher.group("quantity").replace(",", "")) : 1;
 			List<ItemPrice> itemLookup = itemManager.strictSearch(itemName);
 
 			int idFromLookup = (itemLookup.size() != 0) ? itemLookup.get(0).getId() : DetermineWorthlessLoot(itemName);
-			int lootId = (coinDropMatch.matches()) ? ItemID.COINS : idFromLookup;
+			int lootId = (itemName.equals("Coins")) ? ItemID.COINS : idFromLookup;
 
 			if (lootId == -1)
 			{
