c5f8b88146ac5ef4c191a87c2d57f5e65a41d9d9
Trevor <trevorguidry6@gmail.com>
2020-07-01 18:24:35 -0400

    cache: add default values to HealthBarDefinition

--- cache/src/main/java/net/runelite/cache/definitions/HealthBarDefinition.java
+++ cache/src/main/java/net/runelite/cache/definitions/HealthBarDefinition.java
@@ -31,13 +31,13 @@ public class HealthBarDefinition
 {
 	public int id;
 	public int field3276;
-	public int field3277;
-	public int field3278;
-	public int field3283;
-	public int field3272;
-	public int field3275;
-	public int healthBarFrontSpriteId;
-	public int healthBarBackSpriteId;
-	public int healthScale;
-	public int healthBarPadding;
+	public int field3277 = 255;
+	public int field3278 = 255;
+	public int field3283 = -1;
+	public int field3272 = 1;
+	public int field3275 = 70;
+	public int healthBarFrontSpriteId = -1;
+	public int healthBarBackSpriteId = -1;
+	public int healthScale = 30;
+	public int healthBarPadding = 0;
 }
