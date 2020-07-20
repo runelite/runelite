05c4a5c1d5e1cc64672fead0c223f47cfcae1ff1
Snowflak3 <Snowflak3@users.noreply.github.com>
2020-07-20 18:00:01 -0500

    notifier: make flash color configurable
    Co-authored-by: Snowflak3 <mitchellgn@yahoo.com>

--- runelite-client/src/main/java/net/runelite/client/Notifier.java
+++ runelite-client/src/main/java/net/runelite/client/Notifier.java
@@ -98,7 +98,6 @@ public class Notifier
 		.build();
 
 	// Notifier properties
-	private static final Color FLASH_COLOR = new Color(255, 0, 0, 70);
 	private static final int MINIMUM_FLASH_DURATION_MILLIS = 2000;
 	private static final int MINIMUM_FLASH_DURATION_TICKS = MINIMUM_FLASH_DURATION_MILLIS / Constants.CLIENT_TICK_LENGTH;
 
@@ -248,7 +247,7 @@ public class Notifier
 		}
 
 		final Color color = graphics.getColor();
-		graphics.setColor(FLASH_COLOR);
+		graphics.setColor(runeLiteConfig.notificationFlashColor());
 		graphics.fill(new Rectangle(client.getCanvas().getSize()));
 		graphics.setColor(color);
 	}
