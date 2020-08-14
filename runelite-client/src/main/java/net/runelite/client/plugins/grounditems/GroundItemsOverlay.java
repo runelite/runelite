89468a23574318de0dfaa323235c270e7cbe2af3
Arman Rafian <arafian@calpoly.edu>
2020-08-13 23:23:49 -0700

    grounditems plugin: fix despawn timer for private KBD instances

--- runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grounditems/GroundItemsOverlay.java
@@ -78,6 +78,7 @@ public class GroundItemsOverlay extends Overlay
 	private static final Duration DESPAWN_TIME_LOOT = Duration.ofMinutes(2);
 	private static final Duration DESPAWN_TIME_DROP = Duration.ofMinutes(3);
 	private static final int KRAKEN_REGION = 9116;
+	private static final int KBD_REGION = 9033;
 
 	private final Client client;
 	private final GroundItemsPlugin plugin;
@@ -391,8 +392,16 @@ public class GroundItemsOverlay extends Overlay
 			{
 				return;
 			}
+			// Items in the KBD instance use the standard despawn timer
+			else if (isInKBD())
+			{
+				despawnTime = spawnTime.plus(DESPAWN_TIME_LOOT);
+			}
+			else
+			{
+				despawnTime = spawnTime.plus(DESPAWN_TIME_INSTANCE);
+			}
 
-			despawnTime = spawnTime.plus(DESPAWN_TIME_INSTANCE);
 			fillColor = PRIVATE_TIMER_COLOR;
 		}
 		else
@@ -476,6 +485,11 @@ public class GroundItemsOverlay extends Overlay
 	private boolean isInKraken()
 	{
 		return ArrayUtils.contains(client.getMapRegions(), KRAKEN_REGION);
+	}
+
+	private boolean isInKBD()
+	{
+		return ArrayUtils.contains(client.getMapRegions(), KBD_REGION);
 	}
 
 }
