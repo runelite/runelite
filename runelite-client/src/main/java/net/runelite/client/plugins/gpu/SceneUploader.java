3595465b7abf0e7134970303e51e5abc4ecd6bc3
Jonathon Reesor <jonathon.reesor@gmail.com>
2020-08-20 00:38:32 -0400

    Revert switching from put(int) to put(int[]).
    Microbenchmarks in JMH indicate that sequential put(int) calls are faster.

--- runelite-client/src/main/java/net/runelite/client/plugins/gpu/SceneUploader.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gpu/SceneUploader.java
@@ -45,9 +45,6 @@ import net.runelite.api.WallObject;
 class SceneUploader
 {
 
-	private static final int[] TWELVE_ZERO_INTS = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	private static final float[] TWELVE_ZERO_FLOATS = { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
-
 	@Inject
 	private Client client;
 
@@ -323,7 +320,9 @@ class SceneUploader
 				}
 				else
 				{
-					uvBuffer.put(TWELVE_ZERO_FLOATS);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
 				}
 			}
 		}
@@ -381,11 +380,15 @@ class SceneUploader
 			}
 			else if (color3 == -2)
 			{
-				vertexBuffer.put(TWELVE_ZERO_INTS);
+				vertexBuffer.put(0, 0, 0, 0);
+				vertexBuffer.put(0, 0, 0, 0);
+				vertexBuffer.put(0, 0, 0, 0);
 
 				if (faceTextures != null)
 				{
-					uvBuffer.put(TWELVE_ZERO_FLOATS);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+					uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
 				}
 				len += 3;
 				continue;
@@ -458,11 +461,15 @@ class SceneUploader
 		}
 		else if (color3 == -2)
 		{
-			vertexBuffer.put(TWELVE_ZERO_INTS);
+			vertexBuffer.put(0, 0, 0, 0);
+			vertexBuffer.put(0, 0, 0, 0);
+			vertexBuffer.put(0, 0, 0, 0);
 
 			if (padUvs || faceTextures != null)
 			{
-				uvBuffer.put(TWELVE_ZERO_FLOATS);
+				uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+				uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+				uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
 			}
 			return 3;
 		}
@@ -563,7 +570,9 @@ class SceneUploader
 		}
 		else
 		{
-			uvBuffer.put(TWELVE_ZERO_FLOATS);
+			uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+			uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
+			uvBuffer.put(0.0f, 0.0f, 0.0f, 0.0f);
 		}
 	}
 }
