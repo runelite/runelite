e388f5a0f7242711d44ef3726e38d7181fb9dea6
Michael Archer <mikearche@gmail.com>
2020-09-04 00:28:59 -0700

    timers: Update stunned Abyssal Sire ID (#12410)
    A recent game update changed the NPC IDs used for sire's stunned state,
    causing the stun timer to become applied and removed at the wrong time.

--- runelite-client/src/main/java/net/runelite/client/plugins/timers/TimersPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timers/TimersPlugin.java
@@ -823,7 +823,7 @@ public class TimersPlugin extends Plugin
 			switch (npcId)
 			{
 				// Show the countdown when the Sire enters the stunned state.
-				case NpcID.ABYSSAL_SIRE_5887:
+				case NpcID.ABYSSAL_SIRE_5888:
 					createGameTimer(ABYSSAL_SIRE_STUN);
 					break;
 
@@ -831,7 +831,7 @@ public class TimersPlugin extends Plugin
 				// This is necessary because the Sire leaves the stunned
 				// state early once all all four respiratory systems are killed.
 				case NpcID.ABYSSAL_SIRE:
-				case NpcID.ABYSSAL_SIRE_5888:
+				case NpcID.ABYSSAL_SIRE_5887:
 				case NpcID.ABYSSAL_SIRE_5889:
 				case NpcID.ABYSSAL_SIRE_5890:
 				case NpcID.ABYSSAL_SIRE_5891:
