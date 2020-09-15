197c667edfb9ffc1500bb6e721eb0d0e4194dc3e
Adam <Adam@sigterm.info>
2020-09-15 18:58:11 -0400

    item manager: fix item outlines to more accurately fit model
    Not sure where these original arguments come from, but it was
    incorrectly noting the items and then attempting to adjust zoom to
    account for that, which makes most item outlines slightly off

--- runelite-client/src/main/java/net/runelite/client/game/ItemManager.java
+++ runelite-client/src/main/java/net/runelite/client/game/ItemManager.java
@@ -482,7 +482,7 @@ public class ItemManager
 	 */
 	private BufferedImage loadItemOutline(final int itemId, final int itemQuantity, final Color outlineColor)
 	{
-		final SpritePixels itemSprite = client.createItemSprite(itemId, itemQuantity, 1, 0, 0, true, 710);
+		final SpritePixels itemSprite = client.createItemSprite(itemId, itemQuantity, 1, 0, 0, false, Constants.CLIENT_DEFAULT_ZOOM);
 		return itemSprite.toBufferedOutline(outlineColor);
 	}
 
