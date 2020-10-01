f52eeaf4d3a1838b8de91f45709228d60048958a
Max Weber <mii7303@gmail.com>
2020-10-01 00:54:51 -0600

    wiki: remove remainder of quest/diary lookup code
    2b077f4364b2f45a774a12778bbdd5fc26aab006

--- runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/wiki/WikiPlugin.java
@@ -69,20 +69,11 @@ import okhttp3.HttpUrl;
 )
 public class WikiPlugin extends Plugin
 {
-	private static final int[] QUESTLIST_WIDGET_IDS = new int[]
-		{
-			WidgetInfo.QUESTLIST_FREE_CONTAINER.getId(),
-			WidgetInfo.QUESTLIST_MEMBERS_CONTAINER.getId(),
-			WidgetInfo.QUESTLIST_MINIQUEST_CONTAINER.getId(),
-		};
-
 	static final HttpUrl WIKI_BASE = HttpUrl.parse("https://oldschool.runescape.wiki");
 	static final HttpUrl WIKI_API = WIKI_BASE.newBuilder().addPathSegments("api.php").build();
 	static final String UTM_SORUCE_KEY = "utm_source";
 	static final String UTM_SORUCE_VALUE = "runelite";
 
-	private static final String MENUOP_GUIDE = "Guide";
-	private static final String MENUOP_QUICKGUIDE = "Quick Guide";
 	private static final String MENUOP_WIKI = "Wiki";
 
 	@Inject
@@ -340,25 +331,8 @@ public class WikiPlugin extends Plugin
 
 		if (ev.getMenuAction() == MenuAction.RUNELITE)
 		{
-			boolean quickguide = false;
 			switch (ev.getMenuOption())
 			{
-				case MENUOP_QUICKGUIDE:
-					quickguide = true;
-					//fallthrough;
-				case MENUOP_GUIDE:
-					ev.consume();
-					String quest = Text.removeTags(ev.getMenuTarget());
-					HttpUrl.Builder ub = WIKI_BASE.newBuilder()
-						.addPathSegment("w")
-						.addPathSegment(quest)
-						.addQueryParameter(UTM_SORUCE_KEY, UTM_SORUCE_VALUE);
-					if (quickguide)
-					{
-						ub.addPathSegment("Quick_guide");
-					}
-					LinkBrowser.browse(ub.build().toString());
-					break;
 				case MENUOP_WIKI:
 					LinkBrowser.browse(WIKI_BASE.newBuilder()
 						.addPathSegment("w")
