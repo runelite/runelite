438329a96ba3b6815aefec24831d5a4c5044f717
JasonM97 <dezinated@gmail.com>
2020-10-17 02:39:55 -0400

    Added DefaultLoader and DefaultDefinition for cases that don't exist yet. It will just return the raw bytes

---
+++ cache/src/main/java/net/runelite/cache/definitions/DefaultDefinition.java
@@ -0,0 +1,9 @@
+package net.runelite.cache.definitions;
+
+import lombok.Data;
+
+@Data
+public class DefaultDefinition extends Definition
+{
+	public byte[] b;
+}
---
+++ cache/src/main/java/net/runelite/cache/definitions/loaders/DefaultLoader.java
@@ -0,0 +1,15 @@
+package net.runelite.cache.definitions.loaders;
+
+import net.runelite.cache.definitions.DefaultDefinition;
+
+public class DefaultLoader extends Loader
+{
+
+	@Override
+	public DefaultDefinition load(int id, byte[] b)
+	{
+		DefaultDefinition def = new DefaultDefinition();
+		def.b = b;
+		return def;
+	}
+}
