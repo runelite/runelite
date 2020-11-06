79286202e6350a7f1558223f65cba54e4c894727
Adam <Adam@sigterm.info>
2020-11-06 15:48:48 -0500

    ci: update glslang to 8.13.3743

--- ci/build.sh
+++ ci/build.sh
@@ -6,8 +6,8 @@ CACHEDIR="$HOME/.cache/runelite"
 mkdir -p "${CACHEDIR}"
 GLSLANG_ARCHIVE="${CACHEDIR}/glslang.zip"
 GLSLANG_DIR="${CACHEDIR}/glslang"
-GLSLANG_RELEASE='https://github.com/KhronosGroup/glslang/releases/download/8.13.3559/glslang-master-linux-Release.zip'
-GLSLANG_CHECKSUM='9adcfdef5b52275e61068aafbb62747936c6c18ab6dc32a6ef707cfc7b0df423'
+GLSLANG_RELEASE='https://github.com/KhronosGroup/glslang/releases/download/8.13.3743/glslang-master-linux-Release.zip'
+GLSLANG_CHECKSUM='d02b22d35ba7bc786a115fbc90ff2caef7f1cd99d87ab053e3dba8efda5b405a'
 
 if [ ! -f "${GLSLANG_ARCHIVE}" ] || [ ! -d "${GLSLANG_DIR}" ] || ! echo "${GLSLANG_CHECKSUM} ${GLSLANG_ARCHIVE}" | sha256sum -c -; then
   wget -O "${GLSLANG_ARCHIVE}" "${GLSLANG_RELEASE}"
