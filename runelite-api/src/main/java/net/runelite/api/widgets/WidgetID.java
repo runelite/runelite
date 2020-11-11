36ed9307c8856237d183aac8f9600dd396ff821b
RuneLite Cache-Code Autoupdater <mii7303+rlccau@gmail.com>
2020-11-11 11:31:34 +0000

    Update Widget IDs to 2020-11-11-rev192
    Lost widget 399.3

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -831,7 +831,7 @@ public class WidgetID
 	static class QuestList
 	{
 		static final int BOX = 0;
-		static final int SCROLLBAR = 3;
+		static final int SCROLLBAR = 4;
 		static final int CONTAINER = 5;
 		static final int FREE_CONTAINER = 6;
 		static final int MEMBERS_CONTAINER = 7;
