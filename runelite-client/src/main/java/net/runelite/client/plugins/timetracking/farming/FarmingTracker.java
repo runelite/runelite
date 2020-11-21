36792cb3ad64475b2d27b72b72c2f92ad91c238d
MadCatzAno <creeperfan44@gmail.com>
2020-11-21 03:11:01 +0100

    Added gamestate check

--- runelite-client/src/main/java/net/runelite/client/plugins/timetracking/farming/FarmingTracker.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timetracking/farming/FarmingTracker.java
@@ -217,15 +217,19 @@ public class FarmingTracker
 			doneEstimate = ((stages - 1 - stage) + tickTime) * tickrate + (5 * 60);
 			// If Botanist relic active, sets new rates.
 			// TODO:: Remove after Leagues II.
-			if (client.getVarbitValue(Varbits.LEAGUE_RELIC_5.getId()) == 1)
+			if (client.getGameState() == GameState.LOGGED_IN)
 			{
-				tickrate = (state.getTickRate() / 5) * 60;
-				tickNow = (unixNow) / tickrate;
-				tickTime = (unixTime) / tickrate;
-				delta = (int) (tickNow - tickTime);
+				if (client.getVarbitValue(Varbits.LEAGUE_RELIC_5.getId()) == 1)
+				{
+					tickrate = (state.getTickRate() / 5) * 60;
+					tickNow = (unixNow) / tickrate;
+					tickTime = (unixTime) / tickrate;
+					delta = (int) (tickNow - tickTime);
 
-				doneEstimate = ((stages - 1 - stage) + tickTime) * tickrate;
+					doneEstimate = ((stages - 1 - stage) + tickTime) * tickrate;
+				}	
 			}
+			
 			
 			stage += delta;
 			if (stage >= stages)
