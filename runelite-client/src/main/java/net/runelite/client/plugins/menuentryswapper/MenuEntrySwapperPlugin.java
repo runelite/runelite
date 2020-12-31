0affb52af6f2e5ba2657e1b636b389e537b9d33b
Usman Akhtar <60450353+akhtar-u@users.noreply.github.com>
2020-12-30 23:56:04 -0500

    menu entry swapper: fix swap for sedridor

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -125,7 +125,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 
 	private static final Set<String> ESSENCE_MINE_NPCS = ImmutableSet.of(
 		"aubury",
-		"wizard sedridor",
+		"sedridor",
 		"wizard distentor",
 		"wizard cromperty",
 		"brimstail"
