47f26b2ff6cf7e6d60553b13134b0777cb787142
dekvall <dkvldev@gmail.com>
2021-01-05 12:53:20 +0100

    regenmeter: remove league modifier

--- runelite-client/src/main/java/net/runelite/client/plugins/regenmeter/RegenMeterPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/regenmeter/RegenMeterPlugin.java
@@ -36,7 +36,6 @@ import net.runelite.api.GameState;
 import net.runelite.api.Prayer;
 import net.runelite.api.Skill;
 import net.runelite.api.VarPlayer;
-import net.runelite.api.Varbits;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.VarbitChanged;
@@ -56,8 +55,6 @@ public class RegenMeterPlugin extends Plugin
 {
 	private static final int SPEC_REGEN_TICKS = 50;
 	private static final int NORMAL_HP_REGEN_TICKS = 100;
-
-	private static final int TRAILBLAZER_LEAGUE_FLUID_STRIKES_RELIC = 2;
 
 	@Inject
 	private Client client;
@@ -142,11 +139,6 @@ public class RegenMeterPlugin extends Plugin
 		if (client.isPrayerActive(Prayer.RAPID_HEAL))
 		{
 			ticksPerHPRegen /= 2;
-		}
-
-		if (client.getVar(Varbits.LEAGUE_RELIC_3) == TRAILBLAZER_LEAGUE_FLUID_STRIKES_RELIC)
-		{
-			ticksPerHPRegen /= 4;
 		}
 
 		ticksSinceHPRegen = (ticksSinceHPRegen + 1) % ticksPerHPRegen;
