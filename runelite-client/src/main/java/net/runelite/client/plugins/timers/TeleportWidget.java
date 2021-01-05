99d8a43327607d15b0f64f8db969cc359f45169e
dekvall <dkvldev@gmail.com>
2021-01-05 13:15:08 +0100

    Revert "timers: Fix leagues area teleport bug"
    This reverts commit cb31e1ddd4e6430611e012e5c5c99557442c94cf.

--- runelite-client/src/main/java/net/runelite/client/plugins/timers/TeleportWidget.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timers/TeleportWidget.java
@@ -32,9 +32,7 @@ import net.runelite.api.widgets.WidgetInfo;
 enum TeleportWidget
 {
 	HOME_TELEPORT,
-	MINIGAME_TELEPORT,
-	TRAILBLAZER_AREA_TELEPORT,
-	;
+	MINIGAME_TELEPORT;
 
 	private static final Collection HOME_TELEPORT_IDS = ImmutableList.of(
 		WidgetInfo.SPELL_LUMBRIDGE_HOME_TELEPORT.getId(),
@@ -57,10 +55,6 @@ enum TeleportWidget
 		else if (MINIGAME_TELEPORT_IDS.contains(widgetId))
 		{
 			return MINIGAME_TELEPORT;
-		}
-		else if (widgetId == WidgetInfo.TRAILBLAZER_AREA_TELEPORT.getId())
-		{
-			return TRAILBLAZER_AREA_TELEPORT;
 		}
 		return null;
 	}
