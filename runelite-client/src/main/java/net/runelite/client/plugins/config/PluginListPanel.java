2653c7fadaceeb3e624d9303ac543fd08c31426d
Adam <Adam@sigterm.info>
2021-01-21 20:34:39 -0500

    plugin list panel: remove unused executor

--- runelite-client/src/main/java/net/runelite/client/plugins/config/PluginListPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/PluginListPanel.java
@@ -32,7 +32,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.inject.Inject;
@@ -107,7 +106,6 @@ class PluginListPanel extends PluginPanel
 		ConfigManager configManager,
 		PluginManager pluginManager,
 		ExternalPluginManager externalPluginManager,
-		ScheduledExecutorService executorService,
 		EventBus eventBus,
 		Provider<ConfigPanel> configPanelProvider,
 		Provider<PluginHubPanel> pluginHubPanelProvider)
