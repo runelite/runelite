5545b9d0ad020912569f5add959838c145965c21
buttonpoker <juanvargas1994@gmail.com>
2021-02-12 17:16:44 -0800

    cannon: Remove Mourner Headquarters elf cannon spot (#13196)
    Following the release of the Song of the Elves quest, the mourners found
    in the Mourner Headquarters can no longer be hit by a cannon before,
    during, or after completion of the quest. [This is evidently a known
    issue with multi-NPCs.][1]
    
    [1]: https://twitter.com/JagexAsh/status/1359968976760094721

--- runelite-client/src/main/java/net/runelite/client/plugins/cannon/CannonSpots.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cannon/CannonSpots.java
@@ -49,7 +49,7 @@ enum CannonSpots
 	DUST_DEVIL(new WorldPoint(3218, 9366, 0)),
 	EARTH_WARRIOR(new WorldPoint(3120, 9987, 0)),
 	ELDER_CHAOS_DRUID(new WorldPoint(3237, 3622, 0)),
-	ELVES(new WorldPoint(2044, 4635, 0), new WorldPoint(3278, 6098, 0)),
+	ELVES(new WorldPoint(3278, 6098, 0)),
 	FIRE_GIANTS(new WorldPoint(2393, 9782, 0), new WorldPoint(2412, 9776, 0), new WorldPoint(2401, 9780, 0), new WorldPoint(3047, 10340, 0)),
 	GREATER_DEMONS(new WorldPoint(1435, 10086, 2), new WorldPoint(3224, 10132, 0)),
 	GREEN_DRAGON(new WorldPoint(3225, 10068, 0)),
