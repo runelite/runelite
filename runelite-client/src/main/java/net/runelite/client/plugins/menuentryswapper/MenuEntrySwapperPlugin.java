60f4bb4421fb7c70a775b3d1420bb315670adc89
Jordan <Nightfirecat@users.noreply.github.com>
2021-02-15 01:56:03 +0000

    Specify portal nexus as target of swap

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -310,7 +310,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 			"grand exchange", "great kourend", "harmony island", "kharyrll", "lumbridge", "lumbridge graveyard",
 			"lunar isle", "marim", "mind altar", "salve graveyard", "seers' village", "senntisten", "troll stronghold",
 			"varrock", "watchtower", "waterbirth island", "weiss", "west ardougne", "yanille"
-		).forEach(location -> swap(location, "teleport menu", config::swapPortalNexus));
+		).forEach(location -> swap(location, "portal nexus", "teleport menu", config::swapPortalNexus));
 
 		swap("shared", "private", config::swapPrivate);
 
