c0eeb523c7fa25d1a5e438edfa517b4b041b4d64
Hexagon <hexagon@fking.work>
2021-04-10 15:16:03 -0300

    Implement events

--- runelite-client/src/main/java/net/runelite/client/plugins/shootingstars/event/StarDepletionEvent.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/shootingstars/event/StarDepletionEvent.java
@@ -1,8 +1,20 @@
 package net.runelite.client.plugins.shootingstars.event;
 
+import lombok.Value;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.plugins.shootingstars.CrashedStar;
+
 /**
  * An event that is fired when a shooting star fully depletes. This event is meant to be consumed by hub plugins.
  */
+@Value(staticConstructor = "of")
 public class StarDepletionEvent
 {
+	int world;
+	WorldPoint worldPoint;
+
+	public static StarDepletionEvent from(CrashedStar star)
+	{
+		return new StarDepletionEvent(star.getWorld(), star.getWorldPoint());
+	}
 }
