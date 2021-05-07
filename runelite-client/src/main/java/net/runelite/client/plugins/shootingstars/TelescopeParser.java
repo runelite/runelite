8911676d65780b185794d7b7ae2e2eda2823022e
Hexagon <hexagon@fking.work>
2021-05-07 17:03:40 -0300

    Start of telescopeparser

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/shootingstars/TelescopeParser.java
@@ -0,0 +1,28 @@
+package net.runelite.client.plugins.shootingstars;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+
+public class TelescopeParser
+{
+	private static final Pattern TIME_PATTERN = Pattern.compile("\\d+ ?(hours?|minutes?)?");
+
+	public static StarRegion extractStarRegion(String text)
+	{
+		text = text.replace("<br>", " ");
+		for (StarRegion region : StarRegion.values())
+		{
+			if (text.contains(region.getName()))
+			{
+				return region;
+			}
+		}
+		return null;
+	}
+
+	public static Duration extractDuration(String text)
+	{
+		text = text.replace("<br>", " ");
+		return null;
+	}
+}
