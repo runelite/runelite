b0f2591b2ecca7a223c5399921810ee882dfe2f4
Max Weber <mii7303@gmail.com>
2021-05-19 08:00:09 -0600

    screenshot: use new report button offset

--- runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotOverlay.java
@@ -50,7 +50,7 @@ import net.runelite.client.ui.overlay.OverlayPriority;
 class ScreenshotOverlay extends Overlay
 {
 	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MMM. dd, yyyy");
-	private static final int REPORT_BUTTON_X_OFFSET = 404;
+	private static final int REPORT_BUTTON_X_OFFSET = 437;
 
 	private final Client client;
 	private final DrawManager drawManager;
