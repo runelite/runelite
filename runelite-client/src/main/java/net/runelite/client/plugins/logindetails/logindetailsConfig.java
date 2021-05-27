cf1c55c89cd2668fe219bc0b27da69412511a51b
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2021-05-27 01:30:37 +0100

    Save login details

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/logindetails/logindetailsConfig.java
@@ -0,0 +1,36 @@
+package net.runelite.client.plugins.logindetails;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+    @ConfigGroup("logindetails")
+    public interface logindetailsConfig extends Config
+    {
+        @ConfigItem(
+                position = 1,
+                keyName = "storeUsername",
+                name = "Username",
+                description = "Store your username"
+        )
+        default String storeUsername() { return ""; }
+
+        @ConfigItem(
+                position = 2,
+                keyName = "storePassword",
+                name = "Password",
+                description = "Store your password"
+        )
+        default String storePassword() { return ""; }
+
+        @ConfigItem
+                (
+                        position = 3,
+                        keyName = "saveDetails",
+                        name = "Store details",
+                        description = "Toggle saving login info"
+                )
+        default boolean toggleDetails() { return true; }
+    }
+
+
