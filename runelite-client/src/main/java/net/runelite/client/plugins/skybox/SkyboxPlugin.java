7bd27b82932a428a345931069027684a3f543cf9
Jordan Atwood <jordan.atwood423@gmail.com>
2021-06-22 10:21:15 -0700

    skybox: Show surface color while in Prifddinas

--- runelite-client/src/main/java/net/runelite/client/plugins/skybox/SkyboxPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/skybox/SkyboxPlugin.java
@@ -33,6 +33,7 @@ import net.runelite.api.Constants;
 import net.runelite.api.GameState;
 import net.runelite.api.Player;
 import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.BeforeRender;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
@@ -104,7 +105,7 @@ public class SkyboxPlugin extends Plugin
 			return;
 		}
 
-		Color overrideColor = player.getWorldLocation().getY() < Constants.OVERWORLD_MAX_Y
+		Color overrideColor = WorldPoint.getMirrorPoint(player.getWorldLocation(), true).getY() < Constants.OVERWORLD_MAX_Y
 			? config.customOverworldColor() : config.customOtherColor();
 		if (overrideColor != null)
 		{
