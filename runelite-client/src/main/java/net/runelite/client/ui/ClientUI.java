407f7e6d11d22c4dc13634fae99a09b153f921a5
Adam <Adam@sigterm.info>
2021-08-16 12:37:47 -0400

    clientui: fix restoring sidepanel when expanding sidebar
    Closing and opening the sidebar when the side panel is collapsed
    currently expands the side panel to the most recently opened panel,
    even if it was collapsed at the time the sidebar was closed, and also
    incorrectly doesn't set the nav button as selected.
    
    This changes the behavior of expanding the sidebar to not open the side
    panel if it was not opened at the time the sidebar was closed.

--- runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
+++ runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
@@ -211,6 +211,7 @@ public class ClientUI
 					currentButton.setSelected(false);
 					currentNavButton.setSelected(false);
 					currentButton = null;
+					currentNavButton = null;
 				}
 				else
 				{
