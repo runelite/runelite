df231d1ff98ef53412db55efc016c29fc096b549
Adam <Adam@sigterm.info>
2021-09-16 12:55:04 -0400

    xtea: skip unknown map regions
    The client will include keys 0,0,0,0 for unknown map regions, which should just be skipped over instead of failing the entire request

--- http-service/src/main/java/net/runelite/http/service/xtea/XteaEntry.java
+++ http-service/src/main/java/net/runelite/http/service/xtea/XteaEntry.java
@@ -25,8 +25,10 @@
 package net.runelite.http.service.xtea;
 
 import java.time.Instant;
+import lombok.Data;
 
-public class XteaEntry
+@Data
+class XteaEntry
 {
 	private int region;
 	private Instant time;
@@ -35,75 +37,4 @@ public class XteaEntry
 	private int key2;
 	private int key3;
 	private int key4;
-
-	public int getRegion()
-	{
-		return region;
-	}
-
-	public void setRegion(int region)
-	{
-		this.region = region;
-	}
-
-	public Instant getTime()
-	{
-		return time;
-	}
-
-	public void setTime(Instant time)
-	{
-		this.time = time;
-	}
-
-	public int getRev()
-	{
-		return rev;
-	}
-
-	public void setRev(int rev)
-	{
-		this.rev = rev;
-	}
-
-	public int getKey1()
-	{
-		return key1;
-	}
-
-	public void setKey1(int key1)
-	{
-		this.key1 = key1;
-	}
-
-	public int getKey2()
-	{
-		return key2;
-	}
-
-	public void setKey2(int key2)
-	{
-		this.key2 = key2;
-	}
-
-	public int getKey3()
-	{
-		return key3;
-	}
-
-	public void setKey3(int key3)
-	{
-		this.key3 = key3;
-	}
-
-	public int getKey4()
-	{
-		return key4;
-	}
-
-	public void setKey4(int key4)
-	{
-		this.key4 = key4;
-	}
-
 }
