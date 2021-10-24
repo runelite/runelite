d48cf29ade26e4cf1b5ae12972772aee320d4073
Hexagon <hexagon@fking.work>
2021-10-24 20:56:27 -0300

    Update telescope parse unit test

--- runelite-client/src/test/java/net/runelite/client/plugins/shootingstars/TelescopeParserTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/shootingstars/TelescopeParserTest.java
@@ -49,72 +49,86 @@ public class TelescopeParserTest
 				{
 					"You see a shooting star! The star looks like it will land in Asgarnia in<br>the next 49 to 51 minutes.",
 					StarRegion.ASGARNIA,
-					Duration.parse("PT50M")
+					Duration.parse("PT49M"),
+					Duration.parse("PT51M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land on Crandor<br>or Karamja in the next 9 to 11 minutes.",
 					StarRegion.CRANDOR_KARAMJA,
-					Duration.parse("PT10M")
+					Duration.parse("PT9M"),
+					Duration.parse("PT11M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in the Feldip<br>Hills or on the Isle of Souls in the next 1 hour 50 minutes to 1 hour<br>52 minutes.",
 					StarRegion.FELDIP_HILLS,
-					Duration.parse("PT1H51M")
+					Duration.parse("PT1H50M"),
+					Duration.parse("PT1H52M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land on Fossil<br>Island or on Mos Le'Harmless in the next 24 to 26 minutes.",
 					StarRegion.FOSSIL_ISLAND,
-					Duration.parse("PT25M")
+					Duration.parse("PT24M"),
+					Duration.parse("PT26M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in the<br>Fremennik Lands or on Lunar Isle in the next 18 to 20 minutes.",
 					StarRegion.FREMENNIK_LANDS,
-					Duration.parse("PT19M")
+					Duration.parse("PT18M"),
+					Duration.parse("PT20M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Great<br>Kourend in the next 30 to 32 minutes.",
 					StarRegion.GREAT_KOUREND,
-					Duration.parse("PT31M")
+					Duration.parse("PT30M"),
+					Duration.parse("PT32M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Kandarin<br>in the next 1 to 3 minutes.",
 					StarRegion.KANDARIN,
-					Duration.parse("PT2M")
+					Duration.parse("PT1M"),
+					Duration.parse("PT3M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in the Kebos<br>Lowlands in the next 1 hour 30 minutes to 1 hour 32 minutes.",
 					StarRegion.KEBOS_LOWLANDS,
-					Duration.parse("PT1H31M")
+					Duration.parse("PT1H30M"),
+					Duration.parse("PT1H32M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in the<br>Kharidian Desert in the next 57 to 59 minutes.",
 					StarRegion.KHARIDIAN_DESERT,
-					Duration.parse("PT58M")
+					Duration.parse("PT57M"),
+					Duration.parse("PT59M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Misthalin in<br>the next 53 to 55 minutes.",
 					StarRegion.MISTHALIN,
-					Duration.parse("PT54M")
+					Duration.parse("PT53M"),
+					Duration.parse("PT55M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Morytania<br>in the next 1 hour 27 minutes to 1 hour 29 minutes.",
 					StarRegion.MORYTANIA,
-					Duration.parse("PT1H28M")
+					Duration.parse("PT1H27M"),
+					Duration.parse("PT1H29M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Piscatoris<br>or the Gnome Stronghold in the next 1 hour 37 minutes to 1 hour<br>39 minutes.",
 					StarRegion.PISCATORIS_GNOME_STRONGHOLD,
-					Duration.parse("PT1H38M")
+					Duration.parse("PT1H37M"),
+					Duration.parse("PT1H39M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in Tirannwn<br>in the next 1 hour 54 minutes to 1 hour 56 minutes.",
 					StarRegion.TIRANNWN,
-					Duration.parse("PT1H55M")
+					Duration.parse("PT1H54M"),
+					Duration.parse("PT1H56M")
 				},
 				{
 					"You see a shooting star! The star looks like it will land in the<br>Wilderness in the next 1 hour 48 minutes to 1 hour 50 minutes.",
 					StarRegion.WILDERNESS,
-					Duration.parse("PT1H49M")
+					Duration.parse("PT1H48M"),
+					Duration.parse("PT1H50M")
 				}
 			}
 		);
@@ -122,7 +136,8 @@ public class TelescopeParserTest
 
 	private final String message;
 	private final StarRegion expectedRegion;
-	private final Duration expectedDuration;
+	private final Duration expectedEarliest;
+	private final Duration expectedLatest;
 
 	@Test
 	public void testExtractRegion()
@@ -132,9 +147,11 @@ public class TelescopeParserTest
 	}
 
 	@Test
-	public void testExtractDuration()
+	public void testExtractDurations()
 	{
-		Duration duration = TelescopeParser.extractEarliestDuration(message);
-		assertEquals(expectedDuration, duration);
+		Duration earliestDuration = TelescopeParser.extractEarliestDuration(message);
+		Duration latestDuration = TelescopeParser.extractLatestDuration(message);
+		assertEquals(expectedEarliest, earliestDuration);
+		assertEquals(expectedLatest, latestDuration);
 	}
 }
