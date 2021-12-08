1809ed5b18eba96845c244a59bedd4dbdd092379
Adam <Adam@sigterm.info>
2021-12-08 11:13:14 -0500

    tile indicators: add config sections

--- runelite-client/src/main/java/net/runelite/client/plugins/tileindicators/TileIndicatorsConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/tileindicators/TileIndicatorsConfig.java
@@ -29,27 +29,38 @@ import net.runelite.client.config.Alpha;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("tileindicators")
 public interface TileIndicatorsConfig extends Config
 {
-	@Alpha
-	@ConfigItem(
-		keyName = "highlightDestinationColor",
-		name = "Destination tile",
-		description = "Configures the highlight color of current destination",
+	@ConfigSection(
+		name = "Destination Tile",
+		description = "Destination tile configuration",
+		position = 0
+	)
+	String destinationTile = "destinationTile";
+
+	@ConfigSection(
+		name = "Hovered Tile",
+		description = "Hovered tile configuration",
 		position = 1
 	)
-	default Color highlightDestinationColor()
-	{
-		return Color.GRAY;
-	}
+	String hoveredTile = "hoveredTile";
+
+	@ConfigSection(
+		name = "Current Tile",
+		description = "Current tile configuration",
+		position = 2
+	)
+	String currentTile = "currentTile";
 
 	@ConfigItem(
 		keyName = "highlightDestinationTile",
 		name = "Highlight destination tile",
 		description = "Highlights tile player is walking to",
-		position = 2
+		position = 1,
+		section = destinationTile
 	)
 	default boolean highlightDestinationTile()
 	{
@@ -58,10 +69,24 @@ public interface TileIndicatorsConfig extends Config
 
 	@Alpha
 	@ConfigItem(
+		keyName = "highlightDestinationColor",
+		name = "Highlight color",
+		description = "Configures the highlight color of current destination",
+		position = 2,
+		section = destinationTile
+	)
+	default Color highlightDestinationColor()
+	{
+		return Color.GRAY;
+	}
+
+	@Alpha
+	@ConfigItem(
 		keyName = "destinationTileFillColor",
-		name = "Destination tile fill color",
+		name = "Fill color",
 		description = "Configures the fill color of destination tile",
-		position = 3
+		position = 3,
+		section = destinationTile
 	)
 	default Color destinationTileFillColor()
 	{
@@ -70,32 +95,22 @@ public interface TileIndicatorsConfig extends Config
 
 	@ConfigItem(
 		keyName = "destinationTileBorderWidth",
-		name = "Destination border width",
+		name = "Border width",
 		description = "Width of the destination tile marker border",
-		position = 4
+		position = 4,
+		section = destinationTile
 	)
 	default double destinationTileBorderWidth()
 	{
 		return 2;
 	}
 
-	@Alpha
-	@ConfigItem(
-		keyName = "highlightHoveredColor",
-		name = "Hovered tile",
-		description = "Configures the highlight color of hovered tile",
-		position = 5
-	)
-	default Color highlightHoveredColor()
-	{
-		return new Color(0, 0, 0, 0);
-	}
-
 	@ConfigItem(
 		keyName = "highlightHoveredTile",
 		name = "Highlight hovered tile",
 		description = "Highlights tile player is hovering with mouse",
-		position = 6
+		position = 1,
+		section = hoveredTile
 	)
 	default boolean highlightHoveredTile()
 	{
@@ -104,10 +119,24 @@ public interface TileIndicatorsConfig extends Config
 
 	@Alpha
 	@ConfigItem(
+		keyName = "highlightHoveredColor",
+		name = "Highlight color",
+		description = "Configures the highlight color of hovered tile",
+		position = 2,
+		section = hoveredTile
+	)
+	default Color highlightHoveredColor()
+	{
+		return new Color(0, 0, 0, 0);
+	}
+
+	@Alpha
+	@ConfigItem(
 		keyName = "hoveredTileFillColor",
-		name = "Hovered tile fill color",
+		name = "Fill color",
 		description = "Configures the fill color of hovered tile",
-		position = 7
+		position = 3,
+		section = hoveredTile
 	)
 	default Color hoveredTileFillColor()
 	{
@@ -116,32 +145,22 @@ public interface TileIndicatorsConfig extends Config
 
 	@ConfigItem(
 		keyName = "hoveredTileBorderWidth",
-		name = "Hovered tile border width",
+		name = "Border width",
 		description = "Width of the hovered tile marker border",
-		position = 8
+		position = 4,
+		section = hoveredTile
 	)
 	default double hoveredTileBorderWidth()
 	{
 		return 2;
 	}
 
-	@Alpha
-	@ConfigItem(
-		keyName = "highlightCurrentColor",
-		name = "True tile",
-		description = "Configures the highlight color of current true tile",
-		position = 9
-	)
-	default Color highlightCurrentColor()
-	{
-		return Color.CYAN;
-	}
-
 	@ConfigItem(
 		keyName = "highlightCurrentTile",
 		name = "Highlight true tile",
 		description = "Highlights true tile player is on as seen by server",
-		position = 10
+		position = 1,
+		section = currentTile
 	)
 	default boolean highlightCurrentTile()
 	{
@@ -150,10 +169,24 @@ public interface TileIndicatorsConfig extends Config
 
 	@Alpha
 	@ConfigItem(
+		keyName = "highlightCurrentColor",
+		name = "Highlight color",
+		description = "Configures the highlight color of current true tile",
+		position = 2,
+		section = currentTile
+	)
+	default Color highlightCurrentColor()
+	{
+		return Color.CYAN;
+	}
+
+	@Alpha
+	@ConfigItem(
 		keyName = "currentTileFillColor",
-		name = "True tile fill color",
+		name = "Fill color",
 		description = "Configures the fill color of current true tile",
-		position = 11
+		position = 3,
+		section = currentTile
 	)
 	default Color currentTileFillColor()
 	{
@@ -162,9 +195,10 @@ public interface TileIndicatorsConfig extends Config
 
 	@ConfigItem(
 		keyName = "currentTileBorderWidth",
-		name = "True tile border width",
+		name = "Border width",
 		description = "Width of the true tile marker border",
-		position = 12
+		position = 4,
+		section = currentTile
 	)
 	default double currentTileBorderWidth()
 	{
