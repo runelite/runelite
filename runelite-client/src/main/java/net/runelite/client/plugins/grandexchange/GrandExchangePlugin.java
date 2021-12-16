b1c3b39fda48fb34a596f9bf0366eb089c8c8065
Adam <Adam@sigterm.info>
2021-12-16 11:44:43 -0500

    ge: fix showing active trade price on buy offers
    The previously added script event was only for sell offers. Also remove buy limit from sell offers since it is not applicable.

--- runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangePlugin.java
@@ -710,13 +710,15 @@ public class GrandExchangePlugin extends Plugin
 			case "setGETitle":
 				setGeTitle();
 				break;
-			case "geExamineText":
+			case "geBuyExamineText":
+			case "geSellExamineText":
 			{
+				boolean buy = "geBuyExamineText".equals(event.getEventName());
 				String[] stack = client.getStringStack();
 				int sz = client.getStringStackSize();
 				String fee = stack[sz - 2];
 				String examine = stack[sz - 3];
-				String text = setExamineText(examine, fee);
+				String text = setExamineText(examine, fee, buy);
 				if (text != null)
 				{
 					stack[sz - 1] = text;
@@ -807,12 +809,12 @@ public class GrandExchangePlugin extends Plugin
 		}
 	}
 
-	private String setExamineText(String examine, String fee)
+	private String setExamineText(String examine, String fee, boolean buy)
 	{
 		final int itemId = client.getVar(VarPlayer.CURRENT_GE_ITEM);
 		StringBuilder sb = new StringBuilder();
 
-		if (config.enableGELimits())
+		if (buy && config.enableGELimits())
 		{
 			final ItemStats itemStats = itemManager.getItemStats(itemId, false);
 
@@ -823,7 +825,7 @@ public class GrandExchangePlugin extends Plugin
 			}
 		}
 
-		if (config.enableGELimitReset())
+		if (buy && config.enableGELimitReset())
 		{
 			Instant resetTime = getLimitResetTime(itemId);
 			if (resetTime != null)
@@ -851,7 +853,13 @@ public class GrandExchangePlugin extends Plugin
 			return null;
 		}
 
-		return shortenExamine(examine) + "<br>" + sb + "<br>" + fee;
+		if (!fee.isEmpty())
+		{
+			sb.append("<br>").append(fee);
+		}
+
+		// Sell offers include an additional fee text which doesn't fit, so we truncate the examine text
+		return (!buy ? shortenExamine(examine) : examine) + "<br>" + sb;
 	}
 
 	private static String shortenExamine(String examine)
