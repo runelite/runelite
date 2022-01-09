68838cc0ba03406e6d7e234865e9c2df813df7e3
Corey Forsyth <corey.j.forsyth@gmail.com>
2022-01-09 14:00:52 -0500

    Plugin Config: Update plugin config shortcut behavior
     - Pop plugin panel to top-level to prevent multiple configs on top of eachother
     - If a plugin doesn't have configurables, set searchbar to the plugin searchable name

--- runelite-client/src/main/java/net/runelite/client/plugins/config/PluginListPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/PluginListPanel.java
@@ -276,7 +276,7 @@ class PluginListPanel extends PluginPanel
 		{
 			if (pluginListItem.getPluginConfig().getName().equals(configGroup))
 			{
-				openConfigurationPanel(pluginListItem.getPluginConfig());
+				openConfigurationPanel(pluginListItem);
 				break;
 			}
 		}
@@ -288,17 +288,28 @@ class PluginListPanel extends PluginPanel
 		{
 			if (pluginListItem.getPluginConfig().getPlugin() == plugin)
 			{
-				openConfigurationPanel(pluginListItem.getPluginConfig());
+				openConfigurationPanel(pluginListItem);
 				break;
 			}
 		}
 	}
 
-	void openConfigurationPanel(PluginConfigurationDescriptor plugin)
+	void openConfigurationPanel(PluginListItem pluginListItem)
 	{
-		ConfigPanel panel = configPanelProvider.get();
-		panel.init(plugin);
-		muxer.pushState(panel);
+		while (muxer.getComponentCount() > 1)
+		{
+			muxer.popState();
+		}
+		if (pluginListItem.getPluginConfig().hasConfigurables())
+		{
+			ConfigPanel panel = configPanelProvider.get();
+			panel.init(pluginListItem.getPluginConfig());
+			muxer.pushState(panel);
+		}
+		else
+		{
+			searchBar.setText(pluginListItem.getSearchableName());
+		}
 	}
 
 	void startPlugin(Plugin plugin)
