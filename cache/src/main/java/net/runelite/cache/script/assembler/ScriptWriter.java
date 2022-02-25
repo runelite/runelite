ffc5380f28b8be9d35dd235187c51df49b6695a3
Adam <Adam@sigterm.info>
2022-02-25 14:30:06 -0500

    cache: script: remove unused import

--- cache/src/main/java/net/runelite/cache/script/assembler/ScriptWriter.java
+++ cache/src/main/java/net/runelite/cache/script/assembler/ScriptWriter.java
@@ -25,7 +25,6 @@
 package net.runelite.cache.script.assembler;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
