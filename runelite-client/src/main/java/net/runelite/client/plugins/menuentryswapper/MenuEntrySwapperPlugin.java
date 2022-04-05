d2308c7ede7b80b449947c983cdfe42ddc1a7cef
Adam <Adam@sigterm.info>
2022-04-05 12:55:12 -0400

    Revert "menu swapper: Add uncharged cell swap for gotr (#14795)"
    This reverts commit 0609c246a5766d871dc0614b601a7e1556cb402b.
    
    This is superseded by the new object swap

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -78,7 +78,6 @@ import static net.runelite.client.plugins.menuentryswapper.MenuEntrySwapperConfi
 import static net.runelite.client.plugins.menuentryswapper.MenuEntrySwapperConfig.KaramjaGlovesMode;
 import static net.runelite.client.plugins.menuentryswapper.MenuEntrySwapperConfig.MorytaniaLegsMode;
 import static net.runelite.client.plugins.menuentryswapper.MenuEntrySwapperConfig.RadasBlessingMode;
-import static net.runelite.client.plugins.menuentryswapper.MenuEntrySwapperConfig.UnchargedCellsMode;
 import net.runelite.client.util.Text;
 
 @PluginDescriptor(
@@ -442,10 +441,6 @@ public class MenuEntrySwapperPlugin extends Plugin
 		swap("climb", "climb-down", () -> (shiftModifier() ? config.swapStairsShiftClick() : config.swapStairsLeftClick()) == MenuEntrySwapperConfig.StairsMode.CLIMB_DOWN);
 
 		swap("deposit", "deposit-runes", config::swapDepositPool);
-
-		swap("take", "uncharged cells", "take-1", () -> config.swapUnchargedCells() == UnchargedCellsMode.TAKE_1);
-		swap("take", "uncharged cells", "take-5", () -> config.swapUnchargedCells() == UnchargedCellsMode.TAKE_5);
-		swap("take", "uncharged cells", "take-10", () -> config.swapUnchargedCells() == UnchargedCellsMode.TAKE_10);
 	}
 
 	private void swap(String option, String swappedOption, Supplier<Boolean> enabled)
