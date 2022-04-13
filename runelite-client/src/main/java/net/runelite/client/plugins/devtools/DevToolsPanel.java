8b3f129c1edcaf66354d392a3523edd93720fdd9
Adam <Adam@sigterm.info>
2022-04-12 22:05:17 -0400

    devtools: remove inventory tool

--- runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPanel.java
@@ -107,7 +107,6 @@ class DevToolsPanel extends PluginPanel
 		container.add(plugin.getWalls());
 		container.add(plugin.getDecorations());
 
-		container.add(plugin.getInventory());
 		container.add(plugin.getProjectiles());
 
 		container.add(plugin.getLocation());
