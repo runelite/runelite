96fbdda03d60430a35b7dc80f89f378dede7a3cb
Adam <Adam@sigterm.info>
2022-04-20 17:43:51 -0400

    runelite: remove otl requester
    This is being replaced with a different api signature later

--- runelite-client/src/main/java/net/runelite/client/RuneLite.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLite.java
@@ -32,7 +32,6 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.jagex.oldscape.pub.OAuthApi;
-import com.jagex.oldscape.pub.OtlTokenResponse;
 import java.applet.Applet;
 import java.io.File;
 import java.io.IOException;
@@ -48,7 +47,6 @@ import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
@@ -85,8 +83,6 @@ import net.runelite.client.ui.overlay.tooltip.TooltipOverlay;
 import net.runelite.client.ui.overlay.worldmap.WorldMapOverlay;
 import net.runelite.http.api.RuneLiteAPI;
 import okhttp3.Cache;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -110,9 +106,6 @@ public class RuneLite
 
 	@Getter
 	private static Injector injector;
-
-	@Inject
-	private OkHttpClient okHttpClient;
 
 	@Inject
 	private PluginManager pluginManager;
@@ -563,68 +556,6 @@ public class RuneLite
 
 		try
 		{
-			log.info("Initializing OTL token requester with access token");
-			oAuthApi.setOtlTokenRequester(url ->
-			{
-				CompletableFuture<OtlTokenResponse> f = new CompletableFuture<>();
-				okHttpClient.newCall(new Request.Builder()
-					.url(url)
-					.header("Authorization", "Bearer " + accessToken)
-					.get()
-					.build())
-					.enqueue(new Callback()
-					{
-						private void complete(boolean success, String token)
-						{
-							f.complete(new OtlTokenResponse()
-							{
-								@Override
-								public boolean isSuccess()
-								{
-									return success;
-								}
-
-								@Override
-								public String getToken()
-								{
-									return token;
-								}
-							});
-						}
-
-						@Override
-						public void onFailure(Call call, IOException e)
-						{
-							log.error("HTTP error while performing OTL request", e);
-							complete(false, null);
-						}
-
-						@Override
-						public void onResponse(Call call, Response response) throws IOException
-						{
-							if (response.code() != 200)
-							{
-								log.error("Non-OK response performing OTL request: {}", response.code());
-								complete(false, null);
-								response.close();
-								return;
-							}
-
-							if (response.body() == null)
-							{
-								log.error("OK response with empty body from OTL request");
-								complete(false, null);
-								response.close();
-								return;
-							}
-
-							log.debug("Successful OTL response");
-							complete(true, response.body().string());
-							response.close();
-						}
-					});
-				return f;
-			});
 		}
 		catch (LinkageError ex)
 		{
