b730f322a0126b84aba05d118bde4f242f6bc056
Spencer Kane <96400383+SpencerSKane@users.noreply.github.com>
2022-06-28 15:22:44 -0500

    Sacred Bone Burner included
    Sacred Bone Burner grants a 300% prayer experience bonus. This inclusion adds it to the PrayerBonus skill-calculator.

--- runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/PrayerBonus.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/PrayerBonus.java
@@ -37,7 +37,8 @@ public enum PrayerBonus implements SkillBonus
 	MORYTANIA_DIARY_3_SHADES("Morytania Diary 3 Shades(150%)", 0.5f),
 	BONECRUSHER("Bonecrusher (50%)", -0.5f),
 	SINISTER_OFFERING("Sinister Offering (300%)", 2),
-	DEMONIC_OFFERING("Demonic Offering (300%)", 2);
+	DEMONIC_OFFERING("Demonic Offering (300%)", 2),
+	SACRED_BONE_BURNER("Sacred Bone Burner (300%)", 2);
 
 	private final String name;
 	private final float value;
