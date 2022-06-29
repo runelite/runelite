b6fc61a7abaccc58760def25f75305dd4f1f1e6e
Max Weber <mii7303@gmail.com>
2022-06-28 23:36:03 -0600

    questlist: update for dbtabled quests

--- runelite-api/src/main/java/net/runelite/api/DBTableID.java
+++ runelite-api/src/main/java/net/runelite/api/DBTableID.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Abex
+ * Copyright (c) 2022 Abex
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,38 +24,10 @@
  */
 package net.runelite.api;
 
-/**
- * @see ParamHolder
- */
-public final class ParamID
+public class DBTableID
 {
-	/**
-	 * Long name for NPCs used in the HP hud
-	 */
-	public static final int NPC_HP_NAME = 510;
-	/**
-	 * @see SettingID
-	 */
-	public static final int SETTING_ID = 1077;
-	// defaults to 5
-	// 1 is continuous
-	public static final int SETTING_SLIDER_STEPS = 1101;
-	public static final int SETTING_CUSTOM_TRANSMIT = 1085;
-	// defaults to true
-	// track is foreground
-	public static final int SETTING_FOREGROUND_CLICKZONE = 1105;
-	public static final int SETTING_SLIDER_CUSTOM_ONOP = 1106;
-	public static final int SETTING_SLIDER_CUSTOM_SETPOS = 1107;
-	public static final int SETTING_SLIDER_IS_DRAGGABLE = 1108;
-	public static final int SETTING_SLIDER_DEADZONE = 1109;
-	public static final int SETTING_SLIDER_DEADTIME = 1110;
-
-	public static final int OC_ITEM_OP1 = 451;
-	public static final int OC_ITEM_OP2 = 452;
-	public static final int OC_ITEM_OP3 = 453;
-	public static final int OC_ITEM_OP4 = 454;
-	public static final int OC_ITEM_OP5 = 455;
-	public static final int OC_ITEM_OP6 = 456;
-	public static final int OC_ITEM_OP7 = 457;
-	public static final int OC_ITEM_OP8 = 458;
+	public static interface Quest
+	{
+		int NAME = 2;
+	}
 }
