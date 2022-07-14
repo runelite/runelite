a7f9aaa45a0911307cd095a61f86b0af90481efa
Jordan Atwood <nightfirecat@nightfirec.at>
2022-07-13 19:53:43 -0700

    NpcUtil: Handle Kalphite Queen regenerating after death
    Kalphite Queen has a quirk where one tick after reaching 0hp, before she
    begins her transformation animation (from first phase) or death
    animation (from second phase), she can regenerate 1 hitpoint via natural
    regeneration, thus apparently having a fully-red hitpoints bar and
    having `isDead()` set to true yet not actually be dead. This is
    problematic as it can cause her second phase form to become invisible
    via the entity hider plugin, or un-interactable via the menu entry
    swapper plugin.
    
    To correct this, `isDead()` can be reset upon actor death in her second
    phase, then set back to `true` once her death animation begins to ensure
    she is not pre-emptively set to be dead.

--- runelite-client/src/main/java/net/runelite/client/game/NpcUtil.java
+++ runelite-client/src/main/java/net/runelite/client/game/NpcUtil.java
@@ -35,6 +35,7 @@ import net.runelite.api.NPC;
 import net.runelite.api.NPCComposition;
 import net.runelite.api.NpcID;
 import net.runelite.api.ParamID;
+import net.runelite.api.events.ActorDeath;
 import net.runelite.api.events.AnimationChanged;
 import net.runelite.api.events.NpcChanged;
 import net.runelite.client.RuntimeConfig;
@@ -266,6 +267,7 @@ public class NpcUtil
 			case AnimationID.VERZIK_P2_WHITE_NYLO_EXPLOSION:
 			case AnimationID.VERZIK_P2_PURPLE_NYLO_EXPLOSION:
 			case AnimationID.VERZIK_P2_RED_NYLO_EXPLOSION:
+			case AnimationID.KALPHITE_QUEEN_P2_DEATH:
 				actor.setDead(true);
 				break;
 			default:
@@ -278,6 +280,23 @@ public class NpcUtil
 					}
 				}
 				break;
+		}
+	}
+
+	@Subscribe
+	public void onActorDeath(ActorDeath e)
+	{
+		final Actor actor = e.getActor();
+
+		if (!(actor instanceof NPC))
+		{
+			return;
+		}
+
+		// Kalphite Queen phase 2
+		if (((NPC) actor).getId() == NpcID.KALPHITE_QUEEN_965)
+		{
+			actor.setDead(false);
 		}
 	}
 }
