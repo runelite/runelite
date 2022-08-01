7da901dde2f9ef1bba87c955b1605e46a6307c92
ToMeXaS <tomex495@gmail.com>
2022-08-01 19:14:54 +0300

    Evka lopas

--- runelite-client/src/main/java/net/runelite/client/RuneLite.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLite.java
@@ -22,6 +22,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+//Evaldas lopas
 package net.runelite.client;
 
 import ch.qos.logback.classic.Level;
