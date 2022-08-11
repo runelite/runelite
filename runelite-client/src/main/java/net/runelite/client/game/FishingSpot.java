4471e447b80209bab7410ebcc0a309b3b3c7fc18
Grom-met <20033077+Grom-met@users.noreply.github.com>
2022-08-11 00:47:39 -0500

    Define LinkedHashMap classes

--- runelite-client/src/main/java/net/runelite/client/game/FishingSpot.java
+++ runelite-client/src/main/java/net/runelite/client/game/FishingSpot.java
@@ -38,7 +38,7 @@ import net.runelite.client.plugins.fishing.FishingTools;
 @Getter
 public enum FishingSpot
 {
-	SHRIMP("Net/Bait", "Anchovies", new LinkedHashMap<>()
+	SHRIMP("Net/Bait", "Anchovies", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Shrimp, Anchovies", ItemID.RAW_ANCHOVIES));
 		put(FishingTools.FISHING_ROD, new Catches("Sardines, Herring", ItemID.RAW_HERRING));
@@ -50,7 +50,7 @@ public enum FishingSpot
 		FISHING_SPOT_7459, FISHING_SPOT_7462, FISHING_SPOT_7467,
 		FISHING_SPOT_7469, FISHING_SPOT_7947, FISHING_SPOT_10513
 	),
-	LOBSTER("Cage/Harpoon", "Lobster", new LinkedHashMap<>()
+	LOBSTER("Cage/Harpoon", "Lobster", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.LOBSTER_POT, new Catches("Lobster", ItemID.RAW_LOBSTER));
 		put(FishingTools.HARPOON, new Catches("Tuna, Swordfish", ItemID.RAW_SWORDFISH));
@@ -61,7 +61,7 @@ public enum FishingSpot
 		FISHING_SPOT_7946, FISHING_SPOT_9173, FISHING_SPOT_9174,
 		FISHING_SPOT_10515, FISHING_SPOT_10635
 	),
-	SHARK("Big Net/Harpoon", "Shark", new LinkedHashMap<>()
+	SHARK("Big Net/Harpoon", "Shark", new LinkedHashMap<FishingTools, Catches>()
 	{
 		{
 			put(FishingTools.HARPOON, new Catches("Shark", ItemID.RAW_SHARK));
@@ -75,14 +75,14 @@ public enum FishingSpot
 		FISHING_SPOT_8525, FISHING_SPOT_8526, FISHING_SPOT_8527,
 		FISHING_SPOT_9171, FISHING_SPOT_9172, FISHING_SPOT_10514
 	),
-	MONKFISH("Net/Harpoon", "Monkfish", new LinkedHashMap<>()
+	MONKFISH("Net/Harpoon", "Monkfish", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Monkfish", ItemID.RAW_MONKFISH));
 		put(FishingTools.HARPOON, new Catches("Tuna, Swordfish", ItemID.RAW_SWORDFISH));
 	}},
 		FISHING_SPOT_4316
 	),
-	SALMON("Salmon, Trout", "Salmon", new LinkedHashMap<>()
+	SALMON("Salmon, Trout", "Salmon", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FLY_FISHING_ROD, new Catches("Salmon, Trout, Rainbow Fish", ItemID.RAW_SALMON));
 		put(FishingTools.FISHING_ROD, new Catches("Pike", ItemID.RAW_PIKE));
@@ -94,112 +94,112 @@ public enum FishingSpot
 		ROD_FISHING_SPOT_7463, ROD_FISHING_SPOT_7464, ROD_FISHING_SPOT_7468,
 		ROD_FISHING_SPOT_8524
 	),
-	LAVA_EEL("Bait (Lava)", "Lava Eel", new LinkedHashMap<>()
+	LAVA_EEL("Bait (Lava)", "Lava Eel", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.OILY_FISHING_ROD, new Catches("Lava Eel", ItemID.RAW_LAVA_EEL));
 	}},
 		FISHING_SPOT_4928, FISHING_SPOT_6784
 	),
-	BARB_FISH("Use-rod (Barbarian)", "Sturgeon, Salmon, Trout", new LinkedHashMap<>()
+	BARB_FISH("Use-rod (Barbarian)", "Sturgeon, Salmon, Trout", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.BARBARIAN_ROD, new Catches("Leaping Trout, Salmon, Sturgeon", ItemID.LEAPING_STURGEON));
 	}},
 		FISHING_SPOT_1542, FISHING_SPOT_7323
 	),
-	ANGLERFISH("Bait (Anglerfish)", "Anglerfish", new LinkedHashMap<>()
+	ANGLERFISH("Bait (Anglerfish)", "Anglerfish", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FISHING_ROD, new Catches("Anglerfish", ItemID.RAW_ANGLERFISH));
 	}},
 		ROD_FISHING_SPOT_6825
 	),
-	MINNOW("Net (Minnow)", "Minnow", new LinkedHashMap<>()
+	MINNOW("Net (Minnow)", "Minnow", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Minnow", ItemID.MINNOW));
 	}},
 		FISHING_SPOT_7730, FISHING_SPOT_7731, FISHING_SPOT_7732, FISHING_SPOT_7733
 	),
-	HARPOONFISH("Tempoross Cove", "Harpoonfish", new LinkedHashMap<>()
+	HARPOONFISH("Tempoross Cove", "Harpoonfish", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.HARPOON, new Catches("Harpoonfish", ItemID.HARPOONFISH));
 	}},
 		FISHING_SPOT_10565, FISHING_SPOT_10568, FISHING_SPOT_10569
 	),
-	INFERNAL_EEL("Bait (Infernal)", "Infernal Eel", new LinkedHashMap<>()
+	INFERNAL_EEL("Bait (Infernal)", "Infernal Eel", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.OILY_FISHING_ROD, new Catches("Infernal Eel", ItemID.INFERNAL_EEL));
 	}},
 		ROD_FISHING_SPOT_7676
 	),
-	KARAMBWAN("Fish (Karambwan)", "Karambwan", new LinkedHashMap<>()
+	KARAMBWAN("Fish (Karambwan)", "Karambwan", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.KARAMBWAN_VESSEL, new Catches("Karambwan", ItemID.RAW_KARAMBWAN));
 	}},
 		FISHING_SPOT_4712, FISHING_SPOT_4713
 	),
-	KARAMBWANJI("Net (Karambwanji)", "Karambwanji", new LinkedHashMap<>()
+	KARAMBWANJI("Net (Karambwanji)", "Karambwanji", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Karambwanji, Shrimp", ItemID.KARAMBWANJI));
 	}},
 		FISHING_SPOT_4710
 	),
-	SACRED_EEL("Bait (Sacred)", "Sacred eel", new LinkedHashMap<>()
+	SACRED_EEL("Bait (Sacred)", "Sacred eel", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FISHING_ROD, new Catches("Sacred Eel", ItemID.SACRED_EEL));
 	}},
 		FISHING_SPOT_6488
 	),
-	CAVE_EEL("Net/Bait (Frogspawn)", "Frog Spawn, Cave Eel", new LinkedHashMap<>()
+	CAVE_EEL("Net/Bait (Frogspawn)", "Frog Spawn, Cave Eel", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Frog Spawn", ItemID.FROG_SPAWN));
 		put(FishingTools.FISHING_ROD, new Catches("Slimy Eel, Cave Eel", ItemID.RAW_CAVE_EEL));
 	}},
 		FISHING_SPOT_1497, FISHING_SPOT_1498, FISHING_SPOT_1499, FISHING_SPOT_1500
 	),
-	SLIMY_EEL("Bait (Swamp)", "Slimy Eel", new LinkedHashMap<>()
+	SLIMY_EEL("Bait (Swamp)", "Slimy Eel", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FISHING_ROD, new Catches("Slimy Eel", ItemID.RAW_SLIMY_EEL));
 	}},
 		FISHING_SPOT_2653, FISHING_SPOT_2654, FISHING_SPOT_2655
 	),
-	DARK_CRAB("Cage/Harpoon (Wilderness)", "Dark Crab", new LinkedHashMap<>()
+	DARK_CRAB("Cage/Harpoon (Wilderness)", "Dark Crab", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.LOBSTER_POT, new Catches("Dark Crab, Lobster", ItemID.RAW_DARK_CRAB));
 		put(FishingTools.HARPOON, new Catches("Tuna, Swordfish", ItemID.RAW_SWORDFISH));
 	}},
 		FISHING_SPOT_1535, FISHING_SPOT_1536
 	),
-	COMMON_TENCH("Catch (Aerial Fishing)", "Aerial Fishing", new LinkedHashMap<>()
+	COMMON_TENCH("Catch (Aerial Fishing)", "Aerial Fishing", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.CORMORANTS_GLOVE, new Catches("Bluegill, Common Tench, Mottled Eel, Greater Siren", ItemID.COMMON_TENCH));
 	}},
 		FISHING_SPOT_8523
 	),
-	TUTORIAL_SHRIMP("Fish (Tutorial Island)", "Shrimp", new LinkedHashMap<>()
+	TUTORIAL_SHRIMP("Fish (Tutorial Island)", "Shrimp", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.SMALL_FISHING_NET, new Catches("Your first catch", ItemID.RAW_SHRIMPS));
 	}},
 		FISHING_SPOT_3317
 	),
-	ETCETERIA_LOBSTER("Cage/Harpoon (Approval only)", "Lobster (Approval only)", new LinkedHashMap<>()
+	ETCETERIA_LOBSTER("Cage/Harpoon (Approval only)", "Lobster (Approval only)", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.LOBSTER_POT, new Catches("Lobster (Approval only)", ItemID.RAW_LOBSTER));
 		put(FishingTools.HARPOON, new Catches("Tuna, Swordfish (Approval only)", ItemID.RAW_SWORDFISH));
 	}},
 		FISHING_SPOT_3657
 	),
-	QUEST_RUM_DEAL("Fish (Braindeath Island)", "Rum Deal (Quest)", new LinkedHashMap<>()
+	QUEST_RUM_DEAL("Fish (Braindeath Island)", "Rum Deal (Quest)", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FISHBOWL_NET, new Catches("Sluglings, Karamthulhu", ItemID.SLUGLINGS));
 	}},
 		FISHING_SPOT
 	),
-	QUEST_TAI_BWO_WANNAI_TRIO("Fish (Tai Bwo Wannai Trio)", "Tai Bwo Wannai Trio (Quest)", new LinkedHashMap<>()
+	QUEST_TAI_BWO_WANNAI_TRIO("Fish (Tai Bwo Wannai Trio)", "Tai Bwo Wannai Trio (Quest)", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.KARAMBWAN_VESSEL, new Catches("Karambwan", ItemID.RAW_KARAMBWAN));
 	}},
 		FISHING_SPOT_4714
 	),
-	QUEST_FISHING_CONTEST("Fish (Hemenster)", "Fishing Contest (Quest)", new LinkedHashMap<>()
+	QUEST_FISHING_CONTEST("Fish (Hemenster)", "Fishing Contest (Quest)", new LinkedHashMap<FishingTools, Catches>()
 	{{
 		put(FishingTools.FISHING_ROD, new Catches("Giant Carp", ItemID.RAW_GIANT_CARP));
 	}},
