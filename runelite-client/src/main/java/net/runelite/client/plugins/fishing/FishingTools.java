5fe2f98fc39319bd14911866f2559a101cfba02c
Sam <20033077+Grom-met@users.noreply.github.com>
2022-08-11 01:06:01 -0500

    Remove SpellCheckInspection Warning Suppression
    Co-authored-by: Jordan <Nightfirecat@users.noreply.github.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingTools.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingTools.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableSet;
 import lombok.Getter;
 import net.runelite.api.ItemID;
 
-@SuppressWarnings("SpellCheckingInspection")
 @Getter
 public enum FishingTools
 {
