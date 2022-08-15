4d65dd1db0f1e7100e22115c3e23f1f95764d978
Ron Young <admin@ryoung.io>
2022-08-15 16:08:17 -0500

    colorpicker: clamp window position to screen bounds
    Signed-off-by: Ron Young <admin@ryoung.io>

--- runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
@@ -543,7 +543,7 @@ class ConfigPanel extends PluginPanel
 					colorPickerBtn.getColor(),
 					cid.getItem().name(),
 					alphaHidden);
-				colorPicker.setLocation(getLocationOnScreen());
+				colorPicker.setLocationRelativeTo(colorPickerBtn);
 				colorPicker.setOnColorChange(c ->
 				{
 					colorPickerBtn.setColor(c);
