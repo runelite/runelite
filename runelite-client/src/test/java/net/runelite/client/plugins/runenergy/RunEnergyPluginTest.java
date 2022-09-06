f6cc3e21836706a905aab4768333ae1e8c8f2f9b
hugocowan <hugocowan@hotmail.co.uk>
2022-09-06 21:06:05 +0100

    Checkstyle fixes

--- runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
@@ -22,65 +22,66 @@ import static org.mockito.Mockito.verify;
 
 @Slf4j
 @RunWith(MockitoJUnitRunner.class)
-public class RunEnergyPluginTest {
+public class RunEnergyPluginTest
+{
 
-    @Inject
-    RunEnergyPlugin runEnergyPlugin;
+	@Inject
+	RunEnergyPlugin runEnergyPlugin;
 
-    @Mock
-    @Bind
-    ConfigManager configManager;
+	@Mock
+	@Bind
+	ConfigManager configManager;
 
-    @Mock
-    @Bind
-    RunEnergyConfig runEnergyConfig;
+	@Mock
+	@Bind
+	RunEnergyConfig runEnergyConfig;
 
-    @Mock
-    @Bind
-    ChatColorConfig chatColorConfig;
+	@Mock
+	@Bind
+	ChatColorConfig chatColorConfig;
 
-    @Mock
-    @Bind
-    RuneLiteConfig runeLiteConfig;
+	@Mock
+	@Bind
+	RuneLiteConfig runeLiteConfig;
 
-    @Mock
-    @Bind
-    Client client;
+	@Mock
+	@Bind
+	Client client;
 
-    @Before
-    public void before()
-    {
-        Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
-    }
+	@Before
+	public void before()
+	{
+		Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
+	}
 
-    @Test
-    public void testChargeMessage()
-    {
-        String chargeMessage = "You load your Ring of endurance with 1 stamina dose.<br>It now has 669 charges.";
-        ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", chargeMessage, "", 0);
+	@Test
+	public void testChargeMessage()
+	{
+		String chargeMessage = "You load your Ring of endurance with 1 stamina dose.<br>It now has 669 charges.";
+		ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", chargeMessage, "", 0);
 
-        runEnergyPlugin.onChatMessage(chatMessage);
-        verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", 669);
-    }
+		runEnergyPlugin.onChatMessage(chatMessage);
+		verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", 669);
+	}
 
-    @Test
-    public void testCheckMessage()
-    {
-        String checkMessage = "Your Ring of endurance is charged with 52 stamina doses.";
-        ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", checkMessage, "", 0);
+	@Test
+	public void testCheckMessage()
+	{
+		String checkMessage = "Your Ring of endurance is charged with 52 stamina doses.";
+		ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", checkMessage, "", 0);
 
-        runEnergyPlugin.onChatMessage(chatMessage);
-        verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", 52);
-    }
+		runEnergyPlugin.onChatMessage(chatMessage);
+		verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", 52);
+	}
 
-    @Test
-    public void testPotionMessage()
-    {
-        String potionMessage = "Your Ring of endurance doubles the duration of your stamina potion's effect.";
-        ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", potionMessage, "", 0);
+	@Test
+	public void testPotionMessage()
+	{
+		String potionMessage = "Your Ring of endurance doubles the duration of your stamina potion's effect.";
+		ChatMessage chatMessage = new ChatMessage(null, ChatMessageType.SPAM, "", potionMessage, "", 0);
 
-        runEnergyPlugin.onChatMessage(chatMessage);
-        verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", -1);
-    }
+		runEnergyPlugin.onChatMessage(chatMessage);
+		verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", -1);
+	}
 }
 
