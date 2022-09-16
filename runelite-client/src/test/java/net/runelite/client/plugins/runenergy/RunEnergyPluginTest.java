e7fab416879530d96621837f2e93e950862e0b2f
Adam <Adam@sigterm.info>
2022-09-15 21:47:41 -0400

    Fix test

--- runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
@@ -3,28 +3,29 @@ package net.runelite.client.plugins.runenergy;
 import com.google.inject.Guice;
 import com.google.inject.testing.fieldbinder.Bind;
 import com.google.inject.testing.fieldbinder.BoundFieldModule;
-import net.runelite.api.*;
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.BeforeRender;
+import javax.inject.Inject;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemContainer;
+import static net.runelite.api.ItemID.RING_OF_ENDURANCE;
+import net.runelite.api.Varbits;
 import net.runelite.api.events.ChatMessage;
-import net.runelite.api.events.GameTick;
 import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ChatColorConfig;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.config.RuneLiteConfig;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import javax.inject.Inject;
-
-import static net.runelite.api.ItemID.RING_OF_ENDURANCE;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RunEnergyPluginTest
@@ -53,16 +54,11 @@ public class RunEnergyPluginTest
 	@Bind
 	Client client;
 
-	private final ScriptCallbackEvent EVENT = new ScriptCallbackEvent();
-
-	// All these below tests are for Ring of endurance related code.
-
 	@Before
 	public void before()
 	{
 		Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
 		when(configManager.getRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", Integer.class)).thenReturn(1);
-		EVENT.setEventName("destroyOnOpKey");
 	}
 
 	@Test
@@ -104,7 +100,10 @@ public class RunEnergyPluginTest
 		Widget enduranceWidget = mock(Widget.class);
 		when(client.getWidget(WidgetInfo.DESTROY_ITEM_NAME)).thenReturn(enduranceWidget);
 		when(enduranceWidget.getText()).thenReturn("Ring of endurance");
-		runEnergyPlugin.onScriptCallbackEvent(EVENT);
+
+		ScriptCallbackEvent scriptCallbackEvent = new ScriptCallbackEvent();
+		scriptCallbackEvent.setEventName("destroyOnOpKey");
+		runEnergyPlugin.onScriptCallbackEvent(scriptCallbackEvent);
 
 		verify(configManager).setRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", 0);
 	}
@@ -121,26 +120,6 @@ public class RunEnergyPluginTest
 		assertEquals("312s", runEnergyPlugin.getEstimatedRunTimeRemaining(true));
 		when(configManager.getRSProfileConfiguration(RunEnergyConfig.GROUP_NAME, "ringOfEnduranceCharges", Integer.class)).thenReturn(512);
 		assertEquals("6:07", runEnergyPlugin.getEstimatedRunTimeRemaining(false));
-	}
-
-	@Test
-	public void testQuestionMarksInOrbText()
-	{
-		when(runEnergyPlugin.getRingOfEnduranceCharges()).thenReturn(0);
-		when(client.getWidget(WidgetInfo.MINIMAP_RUN_ORB_TEXT)).thenReturn(mock(Widget.class));
-
-		ItemContainer equipment = mock(ItemContainer.class);
-		when(client.getItemContainer(InventoryID.EQUIPMENT)).thenReturn(equipment);
-		when(equipment.count(RING_OF_ENDURANCE)).thenReturn(1);
-
-		final Player localPlayer = mock(Player.class);
-		when(client.getLocalPlayer()).thenReturn(localPlayer);
-		when(localPlayer.getWorldLocation()).thenReturn(new WorldPoint(3208, 3254, 0));
-
-		runEnergyPlugin.onGameTick(new GameTick());
-		runEnergyPlugin.onBeforeRender(new BeforeRender());
-
-		verify(client.getWidget(WidgetInfo.MINIMAP_RUN_ORB_TEXT)).setText("???");
 	}
 }
 
