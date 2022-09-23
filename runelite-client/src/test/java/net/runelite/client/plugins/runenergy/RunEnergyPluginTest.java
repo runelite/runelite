836aa078bc083695b11b711c7e94a000f5a6dc98
Adam <Adam@sigterm.info>
2022-09-23 16:19:28 -0400

    fix test mocks

--- runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/runenergy/RunEnergyPluginTest.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2022, Adam <Adam@sigterm.info>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.runelite.client.plugins.runenergy;
 
 import com.google.inject.Guice;
@@ -14,9 +38,9 @@ import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
-import net.runelite.client.config.ChatColorConfig;
+import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.config.RuneLiteConfig;
+import net.runelite.client.ui.overlay.OverlayManager;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,7 +54,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class RunEnergyPluginTest
 {
-
 	@Inject
 	RunEnergyPlugin runEnergyPlugin;
 
@@ -44,11 +67,11 @@ public class RunEnergyPluginTest
 
 	@Mock
 	@Bind
-	ChatColorConfig chatColorConfig;
+	ChatMessageManager chatMessageManager;
 
 	@Mock
 	@Bind
-	RuneLiteConfig runeLiteConfig;
+	OverlayManager overlayManager;
 
 	@Mock
 	@Bind
@@ -123,4 +146,3 @@ public class RunEnergyPluginTest
 		assertEquals("1:47", runEnergyPlugin.getEstimatedRunTimeRemaining(false));
 	}
 }
-
