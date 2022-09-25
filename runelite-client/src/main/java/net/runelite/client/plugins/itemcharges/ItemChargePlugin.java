runelite/runelite

commit 668d99dfef72c900b1c6c7a5ab121863ddb06252

api: add varbit id and value to event

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargePlugin.java
@@ -154,8 +154,6 @@ public class ItemChargePlugin extends Plugin
 	private static final int MAX_BLOOD_ESSENCE_CHARGES = 1000;
 	private static final int MAX_BRACELET_OF_CLAY_CHARGES = 28;
 
-	private int lastExplorerRingCharge = -1;
-
 	@Inject
 	private Client client;
 
@@ -502,11 +500,9 @@ public class ItemChargePlugin extends Plugin
 	@Subscribe
 	private void onVarbitChanged(VarbitChanged event)
 	{
-		int explorerRingCharge = client.getVarbitValue(Varbits.EXPLORER_RING_ALCHS);
-		if (lastExplorerRingCharge != explorerRingCharge)
+		if (event.getVarbitId() == Varbits.EXPLORER_RING_ALCHS)
 		{
-			lastExplorerRingCharge = explorerRingCharge;
-			updateExplorerRingCharges(explorerRingCharge);
+			updateExplorerRingCharges(event.getValue());
 		}
 	}
 

runelite-api/src/main/java/net/runelite/api/events/VarbitChanged.java
@@ -27,6 +27,7 @@
 package net.runelite.api.events;
 
 import lombok.Data;
+import net.runelite.api.annotations.Varbit;
 
 /**
  * An event when a varbit or varplayer has changed.
@@ -40,8 +41,26 @@ import lombok.Data;
 public class VarbitChanged
 {
 	/**
-	 * Index in the varp array that was changed.
-	 * For varplayer, this is the varplayer id.
+	 * The id of the varp that was changed.
+	 * For a varplayer, this is the varplayer id.
 	 */
-	private int index = -1;
+	private int varpId = -1;
+
+	/**
+	 * The id of the varbit that was changed.
+	 * For a varplayer, this is -1.
+	 */
+	@Varbit
+	private int varbitId = -1;
+
+	/**
+	 * The new value of the varp or varbit
+	 */
+	private int value;
+
+	@Deprecated
+	public int getIndex()
+	{
+		return varpId;
+	}
 }

runelite-client/src/main/java/net/runelite/client/plugins/attackstyles/AttackStylesPlugin.java
@@ -61,9 +61,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 )
 public class AttackStylesPlugin extends Plugin
 {
-	private int attackStyleVarbit = -1;
 	private int equippedWeaponTypeVarbit = -1;
-	private int castingModeVarbit = -1;
 	private AttackStyle attackStyle;
 	private final Set<Skill> warnedSkills = new HashSet<>();
 	private boolean warnedSkillSelected = false;
@@ -104,9 +102,9 @@ public class AttackStylesPlugin extends Plugin
 	private void start()
 	{
 		resetWarnings();
-		attackStyleVarbit = client.getVar(VarPlayer.ATTACK_STYLE);
+		int attackStyleVarbit = client.getVar(VarPlayer.ATTACK_STYLE);
 		equippedWeaponTypeVarbit = client.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE);
-		castingModeVarbit = client.getVarbitValue(Varbits.DEFENSIVE_CASTING_MODE);
+		int castingModeVarbit = client.getVarbitValue(Varbits.DEFENSIVE_CASTING_MODE);
 		updateAttackStyle(
 			equippedWeaponTypeVarbit,
 			attackStyleVarbit,
@@ -173,20 +171,20 @@ public class AttackStylesPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged event)
 	{
-		int currentAttackStyleVarbit = client.getVar(VarPlayer.ATTACK_STYLE);
-		int currentEquippedWeaponTypeVarbit = client.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE);
-		int currentCastingModeVarbit = client.getVarbitValue(Varbits.DEFENSIVE_CASTING_MODE);
-
-		if (attackStyleVarbit != currentAttackStyleVarbit || equippedWeaponTypeVarbit != currentEquippedWeaponTypeVarbit || castingModeVarbit != currentCastingModeVarbit)
+		if (event.getVarpId() == VarPlayer.ATTACK_STYLE.getId()
+			|| event.getVarbitId() == Varbits.EQUIPPED_WEAPON_TYPE
+			|| event.getVarbitId() == Varbits.DEFENSIVE_CASTING_MODE)
 		{
+			final int currentAttackStyleVarbit = client.getVar(VarPlayer.ATTACK_STYLE);
+			final int currentEquippedWeaponTypeVarbit = client.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE);
+			final int currentCastingModeVarbit = client.getVarbitValue(Varbits.DEFENSIVE_CASTING_MODE);
+
 			boolean weaponSwitch = currentEquippedWeaponTypeVarbit != equippedWeaponTypeVarbit;
 
-			attackStyleVarbit = currentAttackStyleVarbit;
 			equippedWeaponTypeVarbit = currentEquippedWeaponTypeVarbit;
-			castingModeVarbit = currentCastingModeVarbit;
 
-			updateAttackStyle(equippedWeaponTypeVarbit, attackStyleVarbit,
-				castingModeVarbit);
+			updateAttackStyle(equippedWeaponTypeVarbit, currentAttackStyleVarbit,
+				currentCastingModeVarbit);
 			updateWarning(weaponSwitch);
 
 			// this isn't required, but will hide styles 1 tick earlier than the script event, which fires

runelite-client/src/main/java/net/runelite/client/plugins/barbarianassault/BarbarianAssaultPlugin.java
@@ -63,7 +63,6 @@ public class BarbarianAssaultPlugin extends Plugin
 
 	@Getter(AccessLevel.PACKAGE)
 	private Image clockImage;
-	private int inGameBit = 0;
 	private String currentWave = START_WAVE;
 	private GameTimer gameTime;
 
@@ -110,7 +109,6 @@ public class BarbarianAssaultPlugin extends Plugin
 		overlayManager.remove(healerOverlay);
 		gameTime = null;
 		currentWave = START_WAVE;
-		inGameBit = 0;
 		clockImage = null;
 	}
 
@@ -155,6 +153,23 @@ public class BarbarianAssaultPlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onVarbitChanged(VarbitChanged event)
+	{
+		if (event.getVarbitId() == Varbits.IN_GAME_BA && event.getValue() == 1)
+		{
+			currentRound = null;
+
+			// Use an instance check to determine if this is exiting a game or a tutorial
+			// After exiting tutorials there is a small delay before changing IN_GAME_BA back to
+			// 0 whereas when in a real wave it changes while still in the instance.
+			if (config.waveTimes() && gameTime != null && client.isInInstancedRegion())
+			{
+				announceTime("Wave " + currentWave + " duration: ", gameTime.getTime(true));
+			}
+		}
+	}
+
+	@Subscribe
 	public void onChatMessage(ChatMessage event)
 	{
 		if (event.getType() == ChatMessageType.GAMEMESSAGE
@@ -171,30 +186,6 @@ public class BarbarianAssaultPlugin extends Plugin
 			{
 				gameTime.setWaveStartTime();
 			}
-		}
-	}
-
-	@Subscribe
-	public void onVarbitChanged(VarbitChanged event)
-	{
-		int inGame = client.getVarbitValue(Varbits.IN_GAME_BA);
-
-		if (inGameBit != inGame)
-		{
-			if (inGameBit == 1)
-			{
-				currentRound = null;
-
-				// Use an instance check to determine if this is exiting a game or a tutorial
-				// After exiting tutorials there is a small delay before changing IN_GAME_BA back to
-				// 0 whereas when in a real wave it changes while still in the instance.
-				if (config.waveTimes() && gameTime != null && client.isInInstancedRegion())
-				{
-					announceTime("Wave " + currentWave + " duration: ", gameTime.getTime(true));
-				}
-			}
-
-			inGameBit = inGame;
 		}
 	}
 

runelite-client/src/main/java/net/runelite/client/plugins/cannon/CannonPlugin.java
@@ -297,9 +297,9 @@ public class CannonPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged varbitChanged)
 	{
-		if (varbitChanged.getIndex() == VarPlayer.CANNON_AMMO.getId())
+		if (varbitChanged.getVarpId() == VarPlayer.CANNON_AMMO.getId())
 		{
-			cballsLeft = client.getVar(VarPlayer.CANNON_AMMO);
+			cballsLeft = varbitChanged.getValue();
 
 			if (config.showCannonNotifications() && !cannonBallNotificationSent && cballsLeft > 0 && config.lowWarningThreshold() >= cballsLeft)
 			{

runelite-client/src/main/java/net/runelite/client/plugins/corp/CorpPlugin.java
@@ -35,7 +35,6 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Actor;
 import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.MenuAction;
 import net.runelite.api.MenuEntry;
@@ -85,9 +84,6 @@ public class CorpPlugin extends Plugin
 
 	@Getter(AccessLevel.PACKAGE)
 	private final Set<Actor> players = new HashSet<>();
-
-	@Inject
-	private Client client;
 
 	@Inject
 	private ChatMessageManager chatMessageManager;
@@ -239,9 +235,9 @@ public class CorpPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged varbitChanged)
 	{
-		if (corp != null)
+		if (corp != null && varbitChanged.getVarbitId() == Varbits.CORP_DAMAGE)
 		{
-			int myDamage = client.getVarbitValue(Varbits.CORP_DAMAGE);
+			int myDamage = varbitChanged.getValue();
 			// avoid resetting our counter when the client's is reset
 			if (myDamage > 0)
 			{

runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPlugin.java
@@ -274,7 +274,7 @@ public class DevToolsPlugin extends Plugin
 				client.queueChangedVarp(varp);
 				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Set VarPlayer " + varp + " to " + value, null);
 				VarbitChanged varbitChanged = new VarbitChanged();
-				varbitChanged.setIndex(varp);
+				varbitChanged.setVarpId(varp);
 				eventBus.post(varbitChanged); // fake event
 				break;
 			}

runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatPlugin.java
@@ -145,7 +145,7 @@ public class ItemStatPlugin extends Plugin
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged event)
 	{
-		if (client.getVar(VarPlayer.CURRENT_GE_ITEM) == -1 && config.geStats())
+		if (event.getVarpId() == VarPlayer.CURRENT_GE_ITEM.getId() && config.geStats())
 		{
 			resetGEInventory();
 		}

commit 257fe6a883fa8da2066efd966d75e04e0529299e

Fix for explorer ring not updating its charge count on login in some cases (those being that it is currently full and the varbit was last at its full state or it is a fresh login). Likely caused by a regression in 668d99dfef72c900b1c6c7a5ab121863ddb06252.

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargePlugin.java
@@ -40,12 +40,14 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.EquipmentInventorySlot;
+import net.runelite.api.GameState;
 import net.runelite.api.InventoryID;
 import net.runelite.api.Item;
 import net.runelite.api.ItemContainer;
 import net.runelite.api.ItemID;
 import net.runelite.api.Varbits;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.events.VarbitChanged;
@@ -195,6 +197,8 @@ public class ItemChargePlugin extends Plugin
 	protected void startUp()
 	{
 		overlayManager.add(overlay);
+
+		clientThread.invoke(() -> updateExplorerRingCharges());
 	}
 
 	@Override
@@ -507,6 +511,17 @@ public class ItemChargePlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onGameStateChanged (GameStateChanged event)
+	{
+		// If the explorer's ring is full, and the last value of the varbit was 0 (full) or it is a fresh login, the
+		// VarbitChanged event does not fire on login. So, check the charge varbit.
+		if (event.getGameState() == GameState.LOGGED_IN)
+		{
+			updateExplorerRingCharges();
+		}
+	}
+
+	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
 	{
 		if (widgetLoaded.getGroupId() == WidgetID.DIALOG_SPRITE_GROUP_ID)
@@ -566,6 +581,11 @@ public class ItemChargePlugin extends Plugin
 	{
 		setItemCharges(ItemChargeConfig.KEY_BINDING_NECKLACE, value);
 		updateInfoboxes();
+	}
+
+	private void updateExplorerRingCharges()
+	{
+		updateExplorerRingCharges(client.getVarbitValue(Varbits.EXPLORER_RING_ALCHS));
 	}
 
 	private void updateExplorerRingCharges(final int value)