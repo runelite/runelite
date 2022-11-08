626b3524feb5402a2a30a973957006adf70ead48
BearSprayGit <Masonparker5@live.com>
2022-11-08 11:15:12 -0600

    removed lapsContainer

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -980,10 +980,4 @@ public final class WidgetID
 		static final int HEADER = 31;
 	}
 
-	static class LapsContainer
-	{
-		static final int CONTAINER = 0;
-		static final int HEADER = 4;
-		static final int LAPCOUNT = 7;
-	}
 }
