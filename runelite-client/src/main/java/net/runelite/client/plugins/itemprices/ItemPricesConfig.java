4a10d58b708c02cfca19006f416ec9d55905c405
Grom-met <20033077+Grom-met@users.noreply.github.com>
2022-11-09 21:30:59 -0600

    revert accidental movement of itemstats itemprices

--- runelite-client/src/main/java/net/runelite/client/plugins/itemprices/ItemPricesConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemprices/ItemPricesConfig.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.itemstats.itemprices;
+package net.runelite.client.plugins.itemprices;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
