e8a65a32f1bf6c4f85dbd05fc9af54f7de897d61
Adam <Adam@sigterm.info>
2022-11-21 15:36:29 -0500

    menu swapper: fix npe accessing widget actions
    Widget actions can be null if clearops is called

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -1065,7 +1065,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 			if (entry.getType() == MenuAction.CC_OP || entry.getType() == MenuAction.CC_OP_LOW_PRIORITY)
 			{
 				final Widget w = entry.getWidget();
-				if (w == null)
+				if (w == null || w.getActions() == null)
 				{
 					continue;
 				}
@@ -1448,11 +1448,11 @@ public class MenuEntrySwapperPlugin extends Plugin
 		// UI swaps
 		if ((menuAction == MenuAction.CC_OP || menuAction == MenuAction.CC_OP_LOW_PRIORITY)
 			&& w != null && (w.getIndex() == -1 || w.getItemId() != -1)
+			&& w.getActions() != null
 			&& !itemOp && WidgetInfo.TO_GROUP(w.getId()) != WidgetID.EQUIPMENT_GROUP_ID)
 		{
-			final String[] actions = w.getActions();
 			int numActions = 0;
-			for (String action : actions)
+			for (String action : w.getActions())
 			{
 				if (!Strings.isNullOrEmpty(action))
 				{
