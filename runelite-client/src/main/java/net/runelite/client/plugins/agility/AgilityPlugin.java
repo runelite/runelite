941c219715dea71a04d90894745bc3560f21cb58
John Kryspin <johnkryspin@gmail.com>
2022-12-11 11:47:05 -0500

    refactor imports

--- runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityPlugin.java
@@ -38,13 +38,35 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
-
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.ItemID;
 import static net.runelite.api.ItemID.AGILITY_ARENA_TICKET;
+import net.runelite.api.MenuAction;
+import net.runelite.api.NPC;
+import net.runelite.api.NullNpcID;
+import net.runelite.api.Player;
 import static net.runelite.api.Skill.AGILITY;
-
+import net.runelite.api.Tile;
+import net.runelite.api.TileItem;
+import net.runelite.api.TileObject;
 import net.runelite.api.coords.WorldPoint;
-import net.runelite.api.events.*;
+import net.runelite.api.events.DecorativeObjectDespawned;
+import net.runelite.api.events.DecorativeObjectSpawned;
+import net.runelite.api.events.GameObjectDespawned;
+import net.runelite.api.events.GameObjectSpawned;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.GroundObjectDespawned;
+import net.runelite.api.events.GroundObjectSpawned;
+import net.runelite.api.events.ItemDespawned;
+import net.runelite.api.events.ItemSpawned;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.api.events.NpcDespawned;
+import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.WallObjectDespawned;
+import net.runelite.api.events.WallObjectSpawned;
 import net.runelite.client.Notifier;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
@@ -100,6 +122,9 @@ public class AgilityPlugin extends Plugin
 	private Notifier notifier;
 
 	@Inject
+	private ChatMessageManager chatMessageManager;
+
+	@Inject
 	private Client client;
 
 	@Inject
@@ -113,9 +138,6 @@ public class AgilityPlugin extends Plugin
 
 	@Inject
 	private XpTrackerService xpTrackerService;
-
-	@Inject
-	private ChatMessageManager chatMessageManager;
 
 	@Getter
 	@Setter(AccessLevel.PACKAGE)
@@ -455,18 +477,18 @@ public class AgilityPlugin extends Plugin
 		if (event.getMenuAction().equals(MenuAction.EXAMINE_OBJECT))
 		{
 			Obstacles.SHORTCUT_OBSTACLE_IDS.get(event.getId()).stream().findFirst()
-				.ifPresent(element ->
-				{
-					final ChatMessageBuilder message = new ChatMessageBuilder()
-						.append(ChatColorType.HIGHLIGHT)
-						.append(element.getTooltip())
-						.append(ChatColorType.NORMAL);
+					.ifPresent(element ->
+					{
+						final ChatMessageBuilder message = new ChatMessageBuilder()
+								.append(ChatColorType.HIGHLIGHT)
+								.append(element.getTooltip())
+								.append(ChatColorType.NORMAL);
 
-					chatMessageManager.queue(QueuedMessage.builder()
-						.type(ChatMessageType.ITEM_EXAMINE)
-						.runeLiteFormattedMessage(message.build())
-						.build());
-				});
+						chatMessageManager.queue(QueuedMessage.builder()
+								.type(ChatMessageType.ITEM_EXAMINE)
+								.runeLiteFormattedMessage(message.build())
+								.build());
+					});
 		}
 	}
 }
