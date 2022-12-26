bbd51305e807110e0af6513bf7410e32b0be1980
Adam <Adam@sigterm.info>
2022-12-26 13:14:21 -0500

    hooks: release stretched mode image when disabled

--- runelite-client/src/main/java/net/runelite/client/callback/Hooks.java
+++ runelite-client/src/main/java/net/runelite/client/callback/Hooks.java
@@ -424,6 +424,18 @@ public class Hooks implements Callbacks
 		}
 		else
 		{
+			if (stretchedImage != null)
+			{
+				log.debug("Releasing stretched volatile image");
+
+				stretchedGraphics.dispose();
+				stretchedImage.flush();
+
+				stretchedGraphics = null;
+				stretchedImage = null;
+				lastStretchedDimensions = null;
+			}
+
 			finalImage = image;
 		}
 
