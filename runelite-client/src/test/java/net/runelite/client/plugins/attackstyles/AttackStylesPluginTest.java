fd21920734830a3047aee657d1d09e124f44edf3
Jordan Atwood <nightfirecat@nightfirec.at>
2023-02-02 23:17:09 -0800

    attack styles: Assign weapon type enum names

--- runelite-client/src/test/java/net/runelite/client/plugins/attackstyles/AttackStylesPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/attackstyles/AttackStylesPluginTest.java
@@ -135,7 +135,7 @@ public class AttackStylesPluginTest
 		when(strWidget.isHidden()).thenAnswer(x -> isStrHidden());
 
 		// equip type_4 weapon type on player
-		when(client.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE)).thenReturn(WeaponType.TYPE_4.ordinal());
+		when(client.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE)).thenReturn(WeaponType.CLAW.ordinal());
 		VarbitChanged varbitChanged = new VarbitChanged();
 		varbitChanged.setVarbitId(Varbits.EQUIPPED_WEAPON_TYPE);
 		attackPlugin.onVarbitChanged(varbitChanged);
@@ -172,19 +172,19 @@ public class AttackStylesPluginTest
 		attackPlugin.onConfigChanged(hideWidgetEvent);
 
 		// verify that the aggressive and accurate attack style widgets are no longer hidden
-		assertFalse(attackPlugin.getHiddenWidgets().get(WeaponType.TYPE_4, ComponentID.COMBAT_STYLE_ONE));
-		assertFalse(attackPlugin.getHiddenWidgets().get(WeaponType.TYPE_4, ComponentID.COMBAT_STYLE_THREE));
+		assertFalse(attackPlugin.getHiddenWidgets().get(WeaponType.CLAW, ComponentID.COMBAT_STYLE_ONE));
+		assertFalse(attackPlugin.getHiddenWidgets().get(WeaponType.CLAW, ComponentID.COMBAT_STYLE_THREE));
 	}
 
 	private boolean isAtkHidden()
 	{
 		return !attackPlugin.getHiddenWidgets().isEmpty()
-			&& attackPlugin.getHiddenWidgets().get(WeaponType.TYPE_4, ComponentID.COMBAT_STYLE_ONE);
+			&& attackPlugin.getHiddenWidgets().get(WeaponType.CLAW, ComponentID.COMBAT_STYLE_ONE);
 	}
 
 	private boolean isStrHidden()
 	{
 		return !attackPlugin.getHiddenWidgets().isEmpty()
-			&& attackPlugin.getHiddenWidgets().get(WeaponType.TYPE_4, ComponentID.COMBAT_STYLE_TWO);
+			&& attackPlugin.getHiddenWidgets().get(WeaponType.CLAW, ComponentID.COMBAT_STYLE_TWO);
 	}
 }
