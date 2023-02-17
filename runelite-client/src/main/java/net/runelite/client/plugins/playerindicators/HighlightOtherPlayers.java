f50cfc1761f3e37f592c6d69c747baa9b5da3ba4
Mike <mikebrasser2@hotmail.nl>
2023-02-17 15:57:53 +0100

    fix: curlybraces + indentation

--- runelite-client/src/main/java/net/runelite/client/plugins/playerindicators/HighlightOtherPlayers.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/playerindicators/HighlightOtherPlayers.java
@@ -3,14 +3,16 @@ package net.runelite.client.plugins.playerindicators;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public enum HighlightOtherPlayers {
+public enum HighlightOtherPlayers
+{
 	DISABLED("Disabled"),
 	EVERYWHERE("Everywhere"),
 	ONLYPVP("Only in PvP");
 	private final String option;
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return option;
 	}
 }
