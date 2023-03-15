ddfabe3d4601485ef18abddb2ab42473b9183f19
Adam <Adam@sigterm.info>
2023-03-15 19:21:19 -0400

    config manager: log profiles at startup
    (cherry picked from commit 3d088477bb917be1f0fff71c6f782c8594c8b22a)

--- runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
+++ runelite-client/src/main/java/net/runelite/client/config/ConfigManager.java
@@ -459,6 +459,8 @@ public class ConfigManager
 					continue;
 				}
 
+				log.info("Profile '{}' (sync: {}, active: {})", p.getName(), p.isSync(), p.isActive());
+
 				// --profile
 				if (configProfileName != null)
 				{
