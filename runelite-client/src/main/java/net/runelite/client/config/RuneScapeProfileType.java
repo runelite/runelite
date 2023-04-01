32f837a2fdb495af0446c271b407954f1ab326c4
Max Weber <mii7303@gmail.com>
2023-04-01 12:47:27 -0600

    config: document RSProfileType ordinal stability

--- runelite-client/src/main/java/net/runelite/client/config/RuneScapeProfileType.java
+++ runelite-client/src/main/java/net/runelite/client/config/RuneScapeProfileType.java
@@ -34,6 +34,8 @@ import net.runelite.api.WorldType;
 @RequiredArgsConstructor
 public enum RuneScapeProfileType
 {
+	// This enum should be ordinal-stable; new entries should only be added to the
+	// end and entries should never be removed
 	STANDARD(client -> true),
 	BETA(client -> client.getWorldType().contains(WorldType.NOSAVE_MODE)),
 	QUEST_SPEEDRUNNING(client -> client.getWorldType().contains(WorldType.QUEST_SPEEDRUNNING)),
