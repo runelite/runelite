5ac23487b092e721293740ae51f3126b6a9ed9d3
SirGirion <seallproducts@gmail.com>
2023-04-18 19:01:47 -0400

    Update runelite-client/src/main/java/net/runelite/client/plugins/woodcutting/WoodcuttingTreesOverlay.java
    Co-authored-by: Jordan <Nightfirecat@users.noreply.github.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/woodcutting/WoodcuttingTreesOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/woodcutting/WoodcuttingTreesOverlay.java
@@ -88,13 +88,10 @@ class WoodcuttingTreesOverlay extends Overlay
 			if (treeObject.getWorldLocation().distanceTo(client.getLocalPlayer().getWorldLocation()) <= 12)
 			{
 				// Check if this is a redwood tree at the farming patch
-				if (client.getObjectDefinition(treeObject.getId()).getImpostorIds() != null)
+				final ObjectComposition treeComp = client.getObjectDefinition(treeObject.getId());
+				if (treeComp.getImpostorIds() != null && Tree.findTree(treeComp.getImpostor().getId()) == null)
 				{
-					final Tree tree = Tree.findTree(client.getObjectDefinition(treeObject.getId()).getImpostor().getId());
-					if (tree != Tree.REDWOOD)
-					{
-						continue;
-					}
+					continue;
 				}
 				OverlayUtil.renderImageLocation(client, graphics, treeObject.getLocalLocation(), itemManager.getImage(axe.getItemId()), 120);
 			}
