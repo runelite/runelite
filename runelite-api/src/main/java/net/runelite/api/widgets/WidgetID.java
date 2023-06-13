3fee4ff9e82c785c480d6343d4be5ca331891fd7
RuneLite Cache-Code Autoupdater <mii7303+rlccau@gmail.com>
2023-06-13 11:40:31 -0600

    Update Widget IDs to 2023-6-14

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -699,7 +699,7 @@ public final class WidgetID
 
 	static class WorldSwitcher
 	{
-		static final int WORLD_LIST = 16;
+		static final int WORLD_LIST = 18;
 	}
 
 	static class Minigames
@@ -768,9 +768,9 @@ public final class WidgetID
 	static class Music
 	{
 		static final int CONTAINER = 0;
-		static final int SCROLL_CONTAINER = 3;
-		static final int LIST = 5;
-		static final int SCROLLBAR = 6;
+		static final int SCROLL_CONTAINER = 4;
+		static final int LIST = 6;
+		static final int SCROLLBAR = 7;
 	}
 
 	static class Barrows_Puzzle
