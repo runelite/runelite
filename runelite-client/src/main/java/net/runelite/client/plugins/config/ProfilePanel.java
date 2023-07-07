384a9fe36add8309f92ccfe0c42ffef50df3549f
Jordan Atwood <nightfirecat@nightfirec.at>
2023-07-06 21:50:34 -0700

    config: Add default profile linking to config profiles
    This commit adds the ability to mark config profiles as being the
    default for a set of rsprofiles, meaning that profiles will change
    automatically on rsprofile change. This will likely resolve most cases
    for players wanting to use the `--profile` launcher argument, and make
    it easier to use game mode-specific, or account-specific settings, as it
    can be made automatic.

--- runelite-client/src/main/java/net/runelite/client/plugins/config/ProfilePanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/ProfilePanel.java
@@ -26,7 +26,9 @@
 package net.runelite.client.plugins.config;
 
 import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
@@ -38,6 +40,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -69,8 +72,10 @@ import net.runelite.client.account.SessionManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.config.ConfigProfile;
 import net.runelite.client.config.ProfileManager;
+import net.runelite.client.config.RuneScapeProfileType;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ProfileChanged;
+import net.runelite.client.events.RuneScapeProfileChanged;
 import net.runelite.client.events.SessionClose;
 import net.runelite.client.events.SessionOpen;
 import net.runelite.client.plugins.screenmarkers.ScreenMarkerPlugin;
@@ -80,6 +85,7 @@ import net.runelite.client.ui.components.DragAndDropReorderPane;
 import net.runelite.client.ui.components.MouseDragEventForwarder;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.client.util.SwingUtil;
+import net.runelite.client.util.Text;
 
 @Slf4j
 class ProfilePanel extends PluginPanel
@@ -92,6 +98,8 @@ class ProfilePanel extends PluginPanel
 	private static final ImageIcon RENAME_ICON;
 	private static final ImageIcon RENAME_ACTIVE_ICON;
 	private static final ImageIcon CLONE_ICON = new ImageIcon(ImageUtil.loadImageResource(ProfilePanel.class, "mdi_content-duplicate.png"));
+	private static final ImageIcon LINK_ICON;
+	private static final ImageIcon LINK_ACTIVE_ICON;
 	private static final ImageIcon ARROW_RIGHT_ICON = new ImageIcon(ImageUtil.loadImageResource(ProfilePanel.class, "/util/arrow_right.png"));
 	private static final ImageIcon SYNC_ICON;
 	private static final ImageIcon SYNC_ACTIVE_ICON;
@@ -116,6 +124,10 @@ class ProfilePanel extends PluginPanel
 		BufferedImage rename = ImageUtil.loadImageResource(ProfilePanel.class, "mdi_rename.png");
 		RENAME_ICON = new ImageIcon(rename);
 		RENAME_ACTIVE_ICON = new ImageIcon(ImageUtil.recolorImage(rename, ColorScheme.BRAND_ORANGE));
+
+		BufferedImage link = ImageUtil.loadImageResource(ProfilePanel.class, "/util/link.png");
+		LINK_ICON = new ImageIcon(link);
+		LINK_ACTIVE_ICON = new ImageIcon(ImageUtil.recolorImage(link, ColorScheme.BRAND_ORANGE));
 
 		BufferedImage sync = ImageUtil.loadImageResource(ProfilePanel.class, "cloud_sync.png");
 		SYNC_ICON = new ImageIcon(sync);
@@ -239,6 +251,17 @@ class ProfilePanel extends PluginPanel
 	}
 
 	@Subscribe
+	private void onRuneScapeProfileChanged(RuneScapeProfileChanged ev)
+	{
+		if (!active)
+		{
+			return;
+		}
+
+		reload();
+	}
+
+	@Subscribe
 	public void onSessionOpen(SessionOpen sessionOpen)
 	{
 		if (!active)
@@ -281,6 +304,7 @@ class ProfilePanel extends PluginPanel
 			cards = new HashMap<>();
 
 			long activePanel = configManager.getProfile().getId();
+			final String rsProfileKey = configManager.getRSProfileKey();
 			boolean limited = profiles.stream().filter(v -> !v.isInternal()).count() >= MAX_PROFILES;
 
 			for (ConfigProfile profile : profiles)
@@ -291,7 +315,14 @@ class ProfilePanel extends PluginPanel
 				}
 
 				ProfileCard prev = prevCards.get(profile.getId());
-				ProfileCard pc = new ProfileCard(profile, activePanel == profile.getId(), limited, prev);
+				final long id = profile.getId();
+				final List<String> defaultForRsProfiles = profile.getDefaultForRsProfiles();
+				ProfileCard pc = new ProfileCard(
+					profile,
+					activePanel == id,
+					defaultForRsProfiles != null && defaultForRsProfiles.contains(rsProfileKey),
+					limited,
+					prev);
 				cards.put(profile.getId(), pc);
 				profilesList.add(pc);
 			}
@@ -305,6 +336,10 @@ class ProfilePanel extends PluginPanel
 
 	private class ProfileCard extends JPanel
 	{
+		private static final int CARD_WIDTH = 227;
+		private static final int LEFT_BORDER_WIDTH = 4;
+		private static final int LEFT_GAP = 4;
+
 		private final ConfigProfile profile;
 		private final JButton delete;
 		private final JTextField name;
@@ -315,7 +350,7 @@ class ProfilePanel extends PluginPanel
 		private boolean expanded;
 		private boolean active;
 
-		private ProfileCard(ConfigProfile profile, boolean isActive, boolean limited, ProfileCard prev)
+		private ProfileCard(ConfigProfile profile, boolean isActive, boolean rsProfileDefault, boolean limited, ProfileCard prev)
 		{
 			this.profile = profile;
 
@@ -421,6 +456,60 @@ class ProfilePanel extends PluginPanel
 				});
 				buttons.add(export);
 
+				if (configManager.getRSProfileKey() != null)
+				{
+					JToggleButton defaultForRsProfile = new JToggleButton(LINK_ICON);
+					SwingUtil.removeButtonDecorations(defaultForRsProfile);
+					defaultForRsProfile.setSelectedIcon(LINK_ACTIVE_ICON);
+					defaultForRsProfile.setSelected(rsProfileDefault);
+
+					final List<String> defaultForRsProfiles = profile.getDefaultForRsProfiles();
+					final StringBuilder tooltip = new StringBuilder("<html>");
+					if (defaultForRsProfiles == null || defaultForRsProfiles.isEmpty())
+					{
+						tooltip.append("Set profile as default for the current RuneScape account");
+					}
+					else
+					{
+						tooltip.append("This profile is the default for the following RuneScape accounts:");
+						for (final String rsProfileKey : profile.getDefaultForRsProfiles())
+						{
+							final String ign = configManager.getConfiguration(ConfigManager.RSPROFILE_GROUP, rsProfileKey, ConfigManager.RSPROFILE_DISPLAY_NAME);
+							if (Strings.isNullOrEmpty(ign))
+							{
+								continue;
+							}
+
+							final RuneScapeProfileType worldType = configManager.getConfiguration(ConfigManager.RSPROFILE_GROUP, rsProfileKey, ConfigManager.RSPROFILE_TYPE, RuneScapeProfileType.class);
+
+							tooltip.append("<br>");
+							tooltip.append(ign);
+							if (worldType != RuneScapeProfileType.STANDARD)
+							{
+								tooltip
+									.append(" (")
+									.append(Text.titleCase(worldType))
+									.append(')');
+							}
+						}
+					}
+					tooltip.append("</html>");
+					defaultForRsProfile.setToolTipText(tooltip.toString());
+
+					defaultForRsProfile.addActionListener(ev ->
+					{
+						if (rsProfileDefault)
+						{
+							unsetRsProfileDefaultProfile();
+						}
+						else
+						{
+							setRsProfileDefaultProfile(profile.getId());
+						}
+					});
+					buttons.add(defaultForRsProfile);
+				}
+
 				if (sessionManager.getAccountSession() != null)
 				{
 					JToggleButton sync = new JToggleButton(SYNC_ICON);
@@ -446,6 +535,15 @@ class ProfilePanel extends PluginPanel
 					}
 				});
 				buttons.add(delete);
+
+				// Ensure buttons do not expand beyond the intended card width; this would cause the activate button to
+				// disappear off the right edge of the card.
+				final int maxButtonsWidth = CARD_WIDTH - LEFT_BORDER_WIDTH - LEFT_GAP - activate.getPreferredSize().width;
+				if (buttons.getPreferredSize().width > maxButtonsWidth)
+				{
+					buttons.setMinimumSize(new Dimension(maxButtonsWidth, buttons.getMinimumSize().height));
+					buttons.setPreferredSize(new Dimension(maxButtonsWidth, buttons.getPreferredSize().height));
+				}
 			}
 
 			{
@@ -459,7 +557,7 @@ class ProfilePanel extends PluginPanel
 					.addComponent(activate, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
 
 				layout.setHorizontalGroup(layout.createSequentialGroup()
-					.addGap(4)
+					.addGap(LEFT_GAP)
 					.addGroup(layout.createParallelGroup()
 						.addComponent(name)
 						.addComponent(buttons))
@@ -533,7 +631,7 @@ class ProfilePanel extends PluginPanel
 		void setActive(boolean active)
 		{
 			this.active = active;
-			setBorder(new MatteBorder(0, 4, 0, 0, active
+			setBorder(new MatteBorder(0, LEFT_BORDER_WIDTH, 0, 0, active
 				? ColorScheme.BRAND_ORANGE
 				: ColorScheme.DARKER_GRAY_COLOR));
 			delete.setEnabled(!active);
@@ -658,6 +756,73 @@ class ProfilePanel extends PluginPanel
 		}
 
 		executor.submit(() -> configManager.switchProfile(profile));
+	}
+
+	private void unsetRsProfileDefaultProfile()
+	{
+		setRsProfileDefaultProfile(-1);
+	}
+
+	private void setRsProfileDefaultProfile(long id)
+	{
+		executor.submit(() ->
+		{
+			boolean switchProfile = false;
+			try (ProfileManager.Lock lock = profileManager.lock())
+			{
+				final String rsProfileKey = configManager.getRSProfileKey();
+				if (rsProfileKey == null)
+				{
+					return;
+				}
+
+				for (final ConfigProfile profile : lock.getProfiles())
+				{
+					final List<String> defaultForRsProfiles = profile.getDefaultForRsProfiles();
+					if (defaultForRsProfiles == null)
+					{
+						continue;
+					}
+					if (profile.getDefaultForRsProfiles().remove(rsProfileKey))
+					{
+						lock.dirty();
+					}
+				}
+
+				if (id == -1)
+				{
+					log.debug("Unsetting default profile for rsProfile {}", rsProfileKey);
+				}
+				else
+				{
+					final ConfigProfile profile = lock.findProfile(id);
+					if (profile == null)
+					{
+						log.warn("setting nonexistent profile {} as default for rsprofile", id);
+						// maybe profile was removed by another client, reload the panel
+						reload(lock.getProfiles());
+						return;
+					}
+
+					log.debug("Setting profile {} as default for rsProfile {}", profile.getName(), rsProfileKey);
+
+					if (profile.getDefaultForRsProfiles() == null)
+					{
+						profile.setDefaultForRsProfiles(new ArrayList<>());
+					}
+					profile.getDefaultForRsProfiles().add(rsProfileKey);
+					switchProfile = !profile.isActive();
+					lock.dirty();
+				}
+
+				reload(lock.getProfiles());
+			}
+
+			if (switchProfile)
+			{
+				switchToProfile(id);
+			}
+		});
 	}
 
 	private void exportProfile(ConfigProfile profile, File file)
