00fac827eb55bcbf91f7333e227156c3dadefb80
Felanbird <41973452+Felanbird@users.noreply.github.com>
2023-07-17 02:21:11 -0400

    clues: Move more arrows for NPCs found on upper floors
    As noted in fff31775852a6eef35922ff58763ee0ff34569fa - various clue scroll steps require interacting with NPCs found upstairs in buildings. This commit moves more of these locations to better indicate the need to go to an upper plane to find the target NPC.

--- runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/AnagramClue.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/AnagramClue.java
@@ -158,7 +158,7 @@ public class AnagramClue extends ClueScroll implements NpcClueScroll, ObjectClue
 		AnagramClue.builder()
 			.text("BY LOOK")
 			.npc("Bolkoy")
-			.location(new WorldPoint(2526, 3162, 0))
+			.location(new WorldPoint(2525, 3162, 0))
 			.area("Tree Gnome Village general store")
 			.question("How many flowers are there in the clearing below this platform?")
 			.answer("13")
