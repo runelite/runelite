06b712e484a75f1ce58687135eedd81616b2173a
Max Weber <mii7303@gmail.com>
2023-07-20 02:37:08 -0600

    music: fix playback not starting on unmute

--- runelite-client/src/main/java/net/runelite/client/plugins/music/MusicPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/music/MusicPlugin.java
@@ -73,6 +73,7 @@ import net.runelite.api.events.VolumeChanged;
 import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.widgets.JavaScriptCallback;
 import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetConfig;
 import net.runelite.api.widgets.WidgetID;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.api.widgets.WidgetPositionMode;
@@ -613,6 +614,7 @@ public class MusicPlugin extends Plugin
 			icon.setName(channel.getName());
 			icon.setOnMouseRepeatListener((Object[]) null);
 			icon.setOnOpListener((JavaScriptCallback) ev -> channel.toggleMute());
+			icon.setClickMask(0); // do not transmit op, it can get desynced from our state
 		}
 
 		@Override
@@ -628,6 +630,7 @@ public class MusicPlugin extends Plugin
 			if (this.icon != null)
 			{
 				this.icon.setOnOpListener((Object[]) null);
+				this.icon.setClickMask(WidgetConfig.transmitAction(0));
 			}
 
 			Widget root = client.getWidget(this.root);
