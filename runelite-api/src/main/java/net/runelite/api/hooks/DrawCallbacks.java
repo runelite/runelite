bbd2f1645496260f9e0c20bf769637c96948faae
Adam <Adam@sigterm.info>
2023-09-25 20:05:30 -0400

    draw callbacks: use floating point camera

--- runelite-api/src/main/java/net/runelite/api/hooks/DrawCallbacks.java
+++ runelite-api/src/main/java/net/runelite/api/hooks/DrawCallbacks.java
@@ -68,14 +68,17 @@ public interface DrawCallbacks
 
 	/**
 	 * Called before the scene is drawn
-	 * @param cameraX
-	 * @param cameraY
-	 * @param cameraZ
-	 * @param cameraPitch
-	 * @param cameraYaw
-	 * @param plane
 	 */
-	void drawScene(int cameraX, int cameraY, int cameraZ, int cameraPitch, int cameraYaw, int plane);
+	default void drawScene(double cameraX, double cameraY, double cameraZ, double cameraPitch, double cameraYaw, int plane)
+	{
+	}
+
+	/**
+	 * Called before the scene is drawn
+	 */
+	default void drawScene(int cameraX, int cameraY, int cameraZ, int cameraPitch, int cameraYaw, int plane)
+	{
+	}
 
 	/**
 	 * Called after the scene has been drawn
