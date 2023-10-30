e5ed7850f95fa888f96f0d424a6ee073a4b3f187
skillingdev <skilling.dev@gmail.com>
2023-10-30 15:58:54 -0400

    Fix pattern

--- runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
@@ -105,7 +105,7 @@ public class ScreenshotPlugin extends Plugin
 	private static final Pattern NUMBER_PATTERN = Pattern.compile("([0-9]+)");
 	private static final Pattern LEVEL_UP_PATTERN = Pattern.compile(".*Your ([a-zA-Z]+) (?:level is|are)? now (\\d+)\\.");
 	private static final Pattern LEVEL_UP_MESSAGE_PATTERN = Pattern.compile("Congratulations, you've just advanced your ([a-zA-Z]+) level. You are now level (\\d+)\\.");
-	private static final Pattern LEVEL_UP_MESSAGE_PATTERN_99 = Pattern.compile("Congratulations, you've reach the highest possible ([a-zA-Z]+) level of 99.");
+	private static final Pattern LEVEL_UP_MESSAGE_PATTERN_99 = Pattern.compile("Congratulations, you've reached the highest possible ([a-zA-Z]+) level of 99.");
 	private static final Pattern BOSSKILL_MESSAGE_PATTERN = Pattern.compile("Your (.+) kill count is: <col=ff0000>(\\d+)</col>.");
 	private static final Pattern VALUABLE_DROP_PATTERN = Pattern.compile(".*Valuable drop: ([^<>]+?\\(((?:\\d+,?)+) coins\\))(?:</col>)?");
 	private static final Pattern UNTRADEABLE_DROP_PATTERN = Pattern.compile(".*Untradeable drop: ([^<>]+)(?:</col>)?");
