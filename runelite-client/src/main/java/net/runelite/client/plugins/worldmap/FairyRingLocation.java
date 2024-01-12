f87eb710c0b88ad24d4b5d7af0455e46627855af
testing-ongithub <96100526+testing-ongithub@users.noreply.github.com>
2024-01-11 22:40:41 -0600

    worldmap: alphabetize fairy rings

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/FairyRingLocation.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/FairyRingLocation.java
@@ -31,13 +31,13 @@ import net.runelite.api.coords.WorldPoint;
 @Getter
 enum FairyRingLocation
 {
-	AIR("AIR", new WorldPoint(2699, 3249, 0)),
 	AIQ("AIQ", new WorldPoint(2995, 3112, 0)),
+	AIR("AIR", new WorldPoint(2699, 3249, 0)),
 	AJR("AJR", new WorldPoint(2779, 3615, 0)),
 	AJS("AJS", new WorldPoint(2499, 3898, 0)),
-	AKR("AKR", new WorldPoint(1823, 3539, 0)),
 	AKP("AKP", new WorldPoint(3283, 2704, 0)),
 	AKQ("AKQ", new WorldPoint(2318, 3617, 0)),
+	AKR("AKR", new WorldPoint(1823, 3539, 0)),
 	AKS("AKS", new WorldPoint(2570, 2958, 0)),
 	ALP("ALP", new WorldPoint(2502, 3638, 0)),
 	ALQ("ALQ", new WorldPoint(3598, 3496, 0)),
@@ -54,12 +54,14 @@ enum FairyRingLocation
 	BLR("BLR", new WorldPoint(2739, 3353, 0)),
 	BLS("BLS", new WorldPoint(1293, 3495, 0)),
 	CIP("CIP", new WorldPoint(2512, 3886, 0)),
-	CIR("CIR", new WorldPoint(1303, 3762, 0)),
 	CIQ("CIQ", new WorldPoint(2527, 3129, 0)),
+	CIR("CIR", new WorldPoint(1303, 3762, 0)),
+	CIS("CIS", new WorldPoint(1636, 3869, 0)),
 	CJR("CJR", new WorldPoint(2704, 3578, 0)),
 	CKR("CKR", new WorldPoint(2800, 3005, 0)),
 	CKS("CKS", new WorldPoint(3446, 3472, 0)),
 	CLP("CLP", new WorldPoint(3081, 3208, 0)),
+	CLR("CLR", new WorldPoint(2737, 2739, 0)),
 	CLS("CLS", new WorldPoint(2681, 3083, 0)),
 	DIP("DIP", new WorldPoint(3036, 4761, 0)),
 	DIS("DIS", new WorldPoint(3109, 3149, 0)),
@@ -70,8 +72,6 @@ enum FairyRingLocation
 	DKS("DKS", new WorldPoint(2743, 3721, 0)),
 	DLQ("DLQ", new WorldPoint(3422, 3018, 0)),
 	DLR("DLR", new WorldPoint(2212, 3101, 0)),
-	CIS("CIS", new WorldPoint(1636, 3869, 0)),
-	CLR("CLR", new WorldPoint(2737, 2739, 0)),
 	ZANARIS("Zanaris", new WorldPoint(2411, 4436, 0));
 
 	private final String code;
