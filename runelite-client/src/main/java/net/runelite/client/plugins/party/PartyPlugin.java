f61b1058128f845b068b6eac9de527063fde0849
Niels Evenblij <9275166+Niels-Evenblij@users.noreply.github.com>
2024-03-09 15:55:37 +0100

    small fix

--- runelite-client/src/main/java/net/runelite/client/plugins/party/PartyPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/party/PartyPlugin.java
@@ -205,7 +205,7 @@ public class PartyPlugin extends Plugin
 		// Delay sync so the eventbus can register prior to the sync response
 		SwingUtilities.invokeLater(this::requestSync);
 
-		if (config.joinPreviousPartyOnStartup() &&  && !config.previousPartyId().isEmpty())
+		if (config.joinPreviousPartyOnStartup() && !config.previousPartyId().isEmpty())
 		{
 			party.changeParty(config.previousPartyId());
 		}		
