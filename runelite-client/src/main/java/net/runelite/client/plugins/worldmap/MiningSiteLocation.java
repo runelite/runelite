e9adca57409fa2ac5de78650a03df5ce50b277d8
Macweese <50101641+Macweese@users.noreply.github.com>
2024-03-21 07:18:22 +0100

    worldmap: fix uzer mining spot

--- runelite-client/src/main/java/net/runelite/client/plugins/worldmap/MiningSiteLocation.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/worldmap/MiningSiteLocation.java
@@ -209,7 +209,7 @@ enum MiningSiteLocation
 		new Rock(26, Ore.IRON), new Rock(8, Ore.SILVER), new Rock(19, Ore.COAL), new Rock(14, Ore.GOLD),
 		new Rock(7, Ore.MITHRIL), new Rock(10, Ore.SOFT_CLAY), new Rock(7, Ore.ADAMANTITE), new Rock(4, Ore.RUNITE)),
 	// TUTORIAL ISLAND -- NOT AVAILABLE ON WORLD MAP
-	UZER(new WorldPoint(3415, 3160, 0), new Rock(10, Ore.CLAY)),
+	UZER(new WorldPoint(3424, 3164, 0), new Rock(10, Ore.CLAY)),
 	VARROCK_SOUTH_EAST(new WorldPoint(3286, 3365, 0),
 		new Rock(9, Ore.COPPER), new Rock(6, Ore.TIN), new Rock(4, Ore.IRON)),
 	VARROCK_SOUTH_WEST(new WorldPoint(3176, 3370, 0),
