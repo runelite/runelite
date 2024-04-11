ff0ba0e02999842f6fb6f7101784cc0799d0d021
Craig Hammond <craig@craighammond.me>
2024-04-11 00:39:41 -0400

    Fix PMD failure for PreserveStackTrace

--- runelite-client/src/main/java/net/runelite/client/rs/ClientLoader.java
+++ runelite-client/src/main/java/net/runelite/client/rs/ClientLoader.java
@@ -241,7 +241,7 @@ public class ClientLoader implements Supplier<Applet>
 		catch (IOException ex)
 		{
 			log.debug("error downloading backup config", ex);
-			throw err; // use error from Jagex's servers
+			throw err; // NOPMD: PreserveStackTrace - use error from Jagex's servers
 		}
 	}
 
