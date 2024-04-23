f17cfa01d876286c90d258e482b4bca934981fc8
NateOnLinux <nathan@carrell-family.com>
2024-04-23 02:18:49 -0400

    Updated copyright notice

--- runelite-client/src/main/java/net/runelite/client/config/RangeDouble.java
+++ runelite-client/src/main/java/net/runelite/client/config/RangeDouble.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * Copyright (c) 2024, NateOnLinux
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
