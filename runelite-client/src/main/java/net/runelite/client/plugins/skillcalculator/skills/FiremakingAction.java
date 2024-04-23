2053154862bcf0d92918da024ba308b3da6861dd
EGxo <evilgenious448@gmail.com>
2024-04-24 00:39:13 +1000

    skill calculator: Add creation and burning of pyre logs. Co-authored-by: DapperMickie <rustenhovenmick@gmail.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/FiremakingAction.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/FiremakingAction.java
@@ -27,26 +27,72 @@ package net.runelite.client.plugins.skillcalculator.skills;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.runelite.api.ItemID;
+import net.runelite.client.game.ItemManager;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Getter
-public enum FiremakingAction implements ItemSkillAction
+public enum FiremakingAction implements NamedSkillAction
 {
-	ACHEY_TREE_LOGS(ItemID.ACHEY_TREE_LOGS, 1, 40),
-	LOGS(ItemID.LOGS, 1, 40),
-	OAK_LOGS(ItemID.OAK_LOGS, 15, 60),
-	WILLOW_LOGS(ItemID.WILLOW_LOGS, 30, 90),
-	TEAK_LOGS(ItemID.TEAK_LOGS, 35, 105),
-	ARCTIC_PINE_LOGS(ItemID.ARCTIC_PINE_LOGS, 42, 125),
-	MAPLE_LOGS(ItemID.MAPLE_LOGS, 45, 135),
-	MAHOGANY_LOGS(ItemID.MAHOGANY_LOGS, 50, 157.5f),
-	YEW_LOGS(ItemID.YEW_LOGS, 60, 202.5f),
-	BLISTERWOOD_LOGS(ItemID.BLISTERWOOD_LOGS, 62, 96),
-	MAGIC_LOGS(ItemID.MAGIC_LOGS, 75, 303.8f),
-	REDWOOD_LOGS(ItemID.REDWOOD_LOGS, 90, 350),
+	MAKING_PYRE_LOGS_2_DOSE("Making pyre logs (2 dose)", 1, 10, ItemID.SACRED_OIL2, FiremakingMethod.SACRED_OIL),
+	MAKING_PYRE_LOGS_3_DOSE("Making pyre logs (3 dose)", 1, 12, ItemID.SACRED_OIL3, FiremakingMethod.SACRED_OIL),
+	MAKING_PYRE_LOGS_4_DOSE("Making pyre logs (3 dose)", 1, 16,ItemID.SACRED_OIL4, FiremakingMethod.SACRED_OIL),
+	ACHEY_TREE_LOGS("Achey tree logs", 1, 40, ItemID.ACHEY_TREE_LOGS, FiremakingMethod.NORMAL_LOGS),
+	LOGS("Logs", 1, 40, ItemID.LOGS, FiremakingMethod.NORMAL_LOGS),
+	PYRE_LOGS("Pyre logs", 5, 50, ItemID.PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	OAK_LOGS("Oak logs", 15, 60, ItemID.OAK_LOGS, FiremakingMethod.NORMAL_LOGS),
+	OAK_PYRE_LOGS("Oak pyre logs", 20, 70, ItemID.OAK_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	WILLOW_LOGS("Willow logs", 30, 90, ItemID.WILLOW_LOGS, FiremakingMethod.NORMAL_LOGS),
+	WILLOW_PYRE_LOGS("Willow pyre logs", 35, 100, ItemID.WILLOW_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	TEAK_LOGS("Teak logs", 35, 105, ItemID.TEAK_LOGS, FiremakingMethod.NORMAL_LOGS),
+	TEAK_PYRE_LOGS("Teak pyre logs", 40, 120, ItemID.TEAK_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	ARCTIC_PINE_LOGS("Arctic pine logs", 42, 125, ItemID.ARCTIC_PINE_LOGS, FiremakingMethod.NORMAL_LOGS),
+	MAPLE_LOGS("Maple logs", 45, 135, ItemID.MAPLE_LOGS, FiremakingMethod.NORMAL_LOGS),
+	ARCTIC_PYRE_LOGS("Arctic pyre logs", 47, 158, ItemID.ARCTIC_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	MAHOGANY_LOGS("Mahogany logs", 50, 157.5f, ItemID.MAHOGANY_LOGS, FiremakingMethod.NORMAL_LOGS),
+	MAPLE_PYRE_LOGS("Maple pyre logs", 50, 175, ItemID.MAPLE_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	MAHOGANY_PYRE_LOGS("Mahogany pyre logs", 55, 210, ItemID.MAHOGANY_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	YEW_LOGS("Yew logs", 60, 202.5f, ItemID.YEW_LOGS, FiremakingMethod.NORMAL_LOGS),
+	BLISTERWOOD_LOGS("Blisterwood logs", 62, 96, ItemID.BLISTERWOOD_LOGS, FiremakingMethod.NORMAL_LOGS),
+	YEW_PYRE_LOGS("Yew pyre logs", 65, 255, ItemID.YEW_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	MAGIC_LOGS("Magic logs", 75, 303.8f, ItemID.MAGIC_LOGS, FiremakingMethod.NORMAL_LOGS),
+	MAGIC_PYRE_LOGS("Magic pyre logs", 80, 404.5f, ItemID.MAGIC_PYRE_LOGS, FiremakingMethod.PYRE_LOGS),
+	REDWOOD_LOGS("Redwood logs", 90, 350, ItemID.REDWOOD_LOGS, FiremakingMethod.NORMAL_LOGS),
+	REDWOOD_PYRE_LOGS("Redwood pyre logs", 95, 500, ItemID.REDWOOD_LOGS, FiremakingMethod.PYRE_LOGS),
 	;
 
-	private final int itemId;
+	private final String name;
 	private final int level;
 	private final float xp;
+	private final int icon;
+	private final FiremakingMethod firemakingMethod;
+
+	@Override
+	public boolean isMembers(final ItemManager itemManager)
+	{
+		return true;
+	}
+
+	private enum FiremakingMethod
+	{
+		NORMAL_LOGS,
+		PYRE_LOGS,
+		SACRED_OIL
+	}
+
+	@Override
+	public Set<FiremakingBonus> getExcludedSkillBonuses()
+	{
+		switch (getFiremakingMethod())
+		{
+			case NORMAL_LOGS:
+				return EnumSet.of(FiremakingBonus.MORYTANIA_ELITE_DIARY);
+			case PYRE_LOGS:
+				return EnumSet.of(FiremakingBonus.PYROMANCER_OUTFIT, FiremakingBonus.FORESTERS_CAMPFIRE);
+			default:
+				return EnumSet.allOf(FiremakingBonus.class);
+		}
+	}
 }
