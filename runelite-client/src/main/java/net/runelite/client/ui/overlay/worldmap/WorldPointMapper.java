35c984ae7a64d221d73b5064612990789ccb7eae
Zoinkwiz <zoinkwiz@hotmail.co.uk>
2024-05-20 23:12:10 +0100

    Shift region checks for mappings into WorldPointMapper

--- runelite-client/src/main/java/net/runelite/client/ui/overlay/worldmap/WorldPointMapper.java
+++ runelite-client/src/main/java/net/runelite/client/ui/overlay/worldmap/WorldPointMapper.java
@@ -65,7 +65,7 @@ public class WorldPointMapper
 
 	public static int getWorldRegionIDFromMapRegionID(int worldRegionID, WorldMapPointMapping mapping)
 	{
-		if (!mapping.mapContainsRegion(worldRegionID)) return worldRegionID;
+		if (!mapContainsRegion(worldRegionID, mapping)) return worldRegionID;
 		// Work out relative shift of X and Y based on shift
 		int x = worldRegionID >> 8;
 		int y = worldRegionID & 0xFF;
@@ -85,7 +85,7 @@ public class WorldPointMapper
 	public static WorldPointWithWorldMapArea getMapWorldPointFromRealWorldPoint(WorldPoint realWorldPoint)
 	{
 		return Arrays.stream(WorldMapPointMapping.values())
-			.filter(mapping -> mapping.worldContainsWorldPoint(realWorldPoint))
+			.filter(mapping -> worldContainsWorldPoint(mapping, realWorldPoint))
 			.findFirst()
 			.map(mapping -> convertToMapWorldPointWithWorldMapArea(mapping, realWorldPoint))
 			.orElse(new WorldPointWithWorldMapArea(realWorldPoint, WorldMapArea.ANY));
@@ -102,7 +102,7 @@ public class WorldPointMapper
 	{
 		return PROCESSED_MAPPINGS.get(mapWorldPoint.getRegionID()).stream()
 			.filter(mapping -> mapping.getArea() == worldMapArea || worldMapArea == WorldMapArea.ANY)
-			.filter(mapping -> mapping.mapContainsWorldPoint(mapWorldPoint))
+			.filter(mapping -> mapContainsWorldPoint(mapping, mapWorldPoint))
 			.findFirst()
 			.map(mapping -> convertToRealWorldPointWithWorldMapArea(mapping, mapWorldPoint))
 			.orElse(new WorldPointWithWorldMapArea(mapWorldPoint, WorldMapArea.ANY));
@@ -152,5 +152,35 @@ public class WorldPointMapper
 			shiftedY,
 			mapping.getMinPlane());
 		return new WorldPointWithWorldMapArea(mappedPoint, mapping.getArea());
+	}
+
+	public static boolean worldContainsRegion(WorldMapPointMapping mapping, int regionID)
+	{
+		int x = regionID >> 8;
+		int y = regionID & 0xFF;
+		return mapping.getMinRegionXWorld() <= x && mapping.getMaxRegionXWorld() >= x && mapping.getMinRegionYWorld() <= y && mapping.getMaxRegionYWorld() >= y;
+	}
+
+	public static boolean worldContainsWorldPoint(WorldMapPointMapping mapping, WorldPoint wp)
+	{
+		boolean inRegion = worldContainsRegion(mapping, wp.getRegionID()) && wp.getPlane() >= mapping.getMinPlane() && wp.getPlane() <= mapping.getMaxPlane();
+		boolean inChunkRange = mapping.getMinChunkXWorld() <= wp.getRegionX() && mapping.getMaxChunkXWorld() >= wp.getRegionX()
+			&& mapping.getMinChunkYWorld() <= wp.getRegionY() && mapping.getMaxChunkYWorld() >= wp.getRegionY();
+		return inRegion && inChunkRange;
+	}
+
+	public static boolean mapContainsRegion(WorldMapPointMapping mapping, int regionID)
+	{
+		int x = regionID >> 8;
+		int y = regionID & 0xFF;
+		return mapping.getMinRegionXMap() <= x && mapping.getMaxRegionXMap() >= x && mapping.getMinRegionYMap() <= y && mapping.getMaxRegionYMap() >= y;
+	}
+
+	public static boolean mapContainsWorldPoint(WorldMapPointMapping mapping, WorldPoint wp)
+	{
+		boolean inRegion = mapContainsRegion(mapping, wp.getRegionID());
+		boolean inChunkRange = mapping.getMinChunkXMap() <= wp.getRegionX() && mapping.getMaxChunkXMap() >= wp.getRegionX()
+			&& mapping.getMinChunkYMap() <= wp.getRegionY() && mapping.getMaxChunkYMap() >= wp.getRegionY();
+		return inRegion && inChunkRange;
 	}
 }
