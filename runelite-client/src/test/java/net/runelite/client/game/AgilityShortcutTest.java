0de7534bcee093d8478842698f1e0464981e4228
Macweese <50101641+Macweese@users.noreply.github.com>
2024-06-19 11:18:26 +0200

    agility: use assertArrayEquals for test feedback
    This provides more helpful information on test failures

--- runelite-client/src/test/java/net/runelite/client/game/AgilityShortcutTest.java
+++ runelite-client/src/test/java/net/runelite/client/game/AgilityShortcutTest.java
@@ -1,8 +1,10 @@
 package net.runelite.client.game;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import junit.framework.TestCase;
-import static org.apache.commons.lang3.ArrayUtils.isSorted;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AgilityShortcutTest extends TestCase
@@ -10,7 +12,11 @@ public class AgilityShortcutTest extends TestCase
 	@Test
 	public void testAgilityShortcutsSorted()
 	{
-		assertTrue("Entries in agility shortcuts must be sorted by level required first, then alphabetically.",
-		isSorted(AgilityShortcut.values(), Comparator.comparingInt(AgilityShortcut::getLevel).thenComparing(Enum::name)));
+		final AgilityShortcut[] sorted = Arrays.stream(AgilityShortcut.values())
+			.sorted(Comparator.comparingInt(AgilityShortcut::getLevel).thenComparing(Enum::name))
+			.collect(Collectors.toList())
+			.toArray(AgilityShortcut[]::new);
+
+		Assert.assertArrayEquals("Entries in AgilityShortcut must be sorted by level required first, then alphabetically", sorted, AgilityShortcut.values());
 	}
 }
