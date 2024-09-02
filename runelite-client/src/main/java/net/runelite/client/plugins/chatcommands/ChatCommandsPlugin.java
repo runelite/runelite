7ed002a6510d206dbc57bf2133c12889ddef4ef1
Hugo Hörnquist <hugo@hornquist.se>
2024-09-02 02:17:24 +0200

    Add aditional aliases for General Graardor.
    Many people miss the second 'r' in Graardor. These aliases should simplify for those people.

--- runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
@@ -2025,6 +2025,8 @@ public class ChatCommandsPlugin extends Plugin
 			case "bando":
 			case "bandos":
 			case "graardor":
+			case "graador":
+			case "general graador":
 				return "General Graardor";
 
 			// dks
