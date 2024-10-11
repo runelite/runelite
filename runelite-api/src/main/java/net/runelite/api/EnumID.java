6c7d2d3aa279b136072dba0ad0b2fe799f74f88c
Adam <Adam@sigterm.info>
2024-10-11 19:26:04 -0400

    bank: compute potion store value

--- runelite-api/src/main/java/net/runelite/api/EnumID.java
+++ runelite-api/src/main/java/net/runelite/api/EnumID.java
@@ -99,4 +99,10 @@ public final class EnumID
 	 * val: enum potion
 	 */
 	public static final int POTIONSTORE_POTIONS = 4826;
+
+	/**
+	 * key: int id
+	 * val: enum potion
+	 */
+	public static final int POTIONSTORE_UNFINISHED_POTIONS = 4829;
 }
