22b0f4ab7b4f8514ebeb8135313478cc9c61232e
chrihb <chrihb@stud.ntnu.no>
2024-10-17 17:19:58 +0200

    Init NexDropRate

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/nexdroprate/NexDropRateConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.nexdroprate;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("nexdroprate")
+public interface NexDropRateConfig extends Config
+{
+  // Define configuration options here
+}
