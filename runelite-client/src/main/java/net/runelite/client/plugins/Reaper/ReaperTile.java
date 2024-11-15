a3ede1c268cc2bbfb59a824c1b38db41256cc7a6
Forks <rdyment@hotmail.ca>
2024-11-15 11:17:08 -0800

    So far it functions to detect when an NPC is within 10 unit distance from player, and when an NPC within that range is despawned (died).

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/Reaper/ReaperTile.java
@@ -0,0 +1,12 @@
+//package net.runelite.client.plugins.Reaper;
+//
+//import lombok.Value;
+//
+//@Value
+//class ReaperTile
+//{
+//    private int regionId;
+//    private int regionX;
+//    private int regionY;
+//    private int z;
+//}
