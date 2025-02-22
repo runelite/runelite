arbahb/ulu-p

item identification: add barbarian potions

runelite-client/src/main/java/net/runelite/client/plugins/itemidentification/ItemIdentification.java
@@ -360,6 +360,44 @@ enum ItemIdentification
 	DWARF_WEED_POTION(Type.POTION, "Dwarf", "D", ItemID.DWARF_WEED_POTION_UNF),
 	TORSTOL_POTION(Type.POTION, "Torstol", "TOR", ItemID.TORSTOL_POTION_UNF),
 
+	// Barbarian Potions
+	ATTACK_MIX(Type.POTION, "Att", "A", ItemID.ATTACK_MIX1, ItemID.ATTACK_MIX2),
+	STRENGTH_MIX(Type.POTION, "Str", "S", ItemID.STRENGTH_MIX1, ItemID.STRENGTH_MIX2),
+	DEFENCE_MIX(Type.POTION, "Def", "D", ItemID.DEFENCE_MIX1, ItemID.DEFENCE_MIX2),
+	COMBAT_MIX(Type.POTION, "Comb", "C", ItemID.COMBAT_MIX1, ItemID.COMBAT_MIX2),
+	MAGIC_MIX(Type.POTION, "Magic", "M", ItemID.MAGIC_MIX1, ItemID.MAGIC_MIX2),
+	RANGING_MIX(Type.POTION, "Range", "R", ItemID.RANGING_MIX1, ItemID.RANGING_MIX2),
+
+	SUPER_ATTACK_MIX(Type.POTION, "S.Att", "S.A", ItemID.SUPERATTACK_MIX1, ItemID.SUPERATTACK_MIX2),
+	SUPER_STRENGTH_MIX(Type.POTION, "S.Str", "S.S", ItemID.SUPER_STR_MIX1, ItemID.SUPER_STR_MIX2),
+	SUPER_DEFENCE_MIX(Type.POTION, "S.Def", "S.D", ItemID.SUPER_DEF_MIX1, ItemID.SUPER_DEF_MIX2),
+
+	RELICYMS_MIX(Type.POTION, "Relicym", "R.B", ItemID.RELICYMS_MIX1, ItemID.RELICYMS_MIX2),
+	ANTIFIRE_MIX(Type.POTION, "Antif", "Af", ItemID.ANTIFIRE_MIX1, ItemID.ANTIFIRE_MIX2),
+	EXTENDED_ANTIFIRE_MIX(Type.POTION, "E.Antif", "E.Af", ItemID.EXTENDED_ANTIFIRE_MIX1, ItemID.EXTENDED_ANTIFIRE_MIX2),
+	SUPER_ANTIFIRE_MIX(Type.POTION, "S.Antif", "S.Af", ItemID.SUPER_ANTIFIRE_MIX1, ItemID.SUPER_ANTIFIRE_MIX2),
+	EXTENDED_SUPER_ANTIFIRE_MIX(Type.POTION, "ES.Antif", "ES.Af", ItemID.EXTENDED_SUPER_ANTIFIRE_MIX1, ItemID.EXTENDED_SUPER_ANTIFIRE_MIX2),
+
+	ZAMORAK_MIX(Type.POTION, "Zamorak", "Z", ItemID.ZAMORAK_MIX1, ItemID.ZAMORAK_MIX2),
+	ANCIENT_MIX(Type.POTION, "Ancient", "Anc", ItemID.ANCIENT_MIX1, ItemID.ANCIENT_MIX2),
+
+	RESTORE_MIX(Type.POTION, "Restore", "Re", ItemID.RESTORE_MIX1, ItemID.RESTORE_MIX2),
+	SUPER_RESTORE_MIX(Type.POTION, "S.Rest", "S.Re", ItemID.SUPER_RESTORE_MIX1, ItemID.SUPER_RESTORE_MIX2),
+	PRAYER_MIX(Type.POTION, "Prayer", "P", ItemID.PRAYER_MIX1, ItemID.PRAYER_MIX2),
+	ENERGY_MIX(Type.POTION, "Energy", "En", ItemID.ENERGY_MIX1, ItemID.ENERGY_MIX2),
+	SUPER_ENERGY_MIX(Type.POTION, "S.Energ", "S.En", ItemID.SUPER_ENERGY_MIX1, ItemID.SUPER_ENERGY_MIX2),
+	STAMINA_MIX(Type.POTION, "Stamina", "Sta", ItemID.STAMINA_MIX1, ItemID.STAMINA_MIX2),
+
+	ANTIDOTE_P_MIX(Type.POTION, "AntiD+", "A+", ItemID.ANTIDOTE_MIX1, ItemID.ANTIDOTE_MIX2),
+	ANTIPOISON_MIX(Type.POTION, "AntiP", "AP", ItemID.ANTIPOISON_MIX1, ItemID.ANTIPOISON_MIX2),
+	ANTIPOISON_SUPERMIX(Type.POTION, "S.AntiP", "S.AP", ItemID.ANTIPOISON_SUPERMIX1, ItemID.ANTIPOISON_SUPERMIX2),
+
+	AGILITY_MIX(Type.POTION, "Agility", "Ag", ItemID.AGILITY_MIX1, ItemID.AGILITY_MIX2),
+	FISHING_MIX(Type.POTION, "Fishing", "Fi", ItemID.FISHING_MIX1, ItemID.FISHING_MIX2),
+	HUNTING_MIX(Type.POTION, "Hunter", "Hu", ItemID.HUNTING_MIX1, ItemID.HUNTING_MIX2),
+
+	MAGIC_ESSENCE_MIX(Type.POTION, "MagEss", "M.E", ItemID.MAGIC_ESSENCE_MIX1, ItemID.MAGIC_ESSENCE_MIX2),
+
 	// Moth & butterfly jars
 	BLACK_WARLOCK(Type.BUTTERFLY_MOTH_JAR, "B.War", "BW", ItemID.BLACK_WARLOCK),
 	MOONLIGHT_MOTH(Type.BUTTERFLY_MOTH_JAR, "Moon", "M", ItemID.MOONLIGHT_MOTH_28893),

runelite/runelite

runelite-client/src/main/java/net/runelite/client/plugins/itemidentification/ItemIdentification.java
@@ -261,17 +261,26 @@ enum ItemIdentification
 
 	// Potions
 	ATTACK(Type.POTION, "Att", "A", ItemID.ATTACK_POTION4, ItemID.ATTACK_POTION3, ItemID.ATTACK_POTION2, ItemID.ATTACK_POTION1),
+	ATTACK_MIX(Type.POTION, "Att", "A", ItemID.ATTACK_MIX1, ItemID.ATTACK_MIX2),
 	STRENGTH(Type.POTION, "Str", "S", ItemID.STRENGTH_POTION4, ItemID.STRENGTH_POTION3, ItemID.STRENGTH_POTION2, ItemID.STRENGTH_POTION1),
+	STRENGTH_MIX(Type.POTION, "Str", "S", ItemID.STRENGTH_MIX1, ItemID.STRENGTH_MIX2),
 	DEFENCE(Type.POTION, "Def", "D", ItemID.DEFENCE_POTION4, ItemID.DEFENCE_POTION3, ItemID.DEFENCE_POTION2, ItemID.DEFENCE_POTION1),
+	DEFENCE_MIX(Type.POTION, "Def", "D", ItemID.DEFENCE_MIX1, ItemID.DEFENCE_MIX2),
 	COMBAT(Type.POTION, "Com", "C", ItemID.COMBAT_POTION4, ItemID.COMBAT_POTION3, ItemID.COMBAT_POTION2, ItemID.COMBAT_POTION1),
+	COMBAT_MIX(Type.POTION, "Com", "C", ItemID.COMBAT_MIX1, ItemID.COMBAT_MIX2),
 	MAGIC(Type.POTION, "Magic", "M", ItemID.MAGIC_POTION4, ItemID.MAGIC_POTION3, ItemID.MAGIC_POTION2, ItemID.MAGIC_POTION1),
+	MAGIC_MIX(Type.POTION, "Magic", "M", ItemID.MAGIC_MIX1, ItemID.MAGIC_MIX2),
 	RANGING(Type.POTION, "Range", "R", ItemID.RANGING_POTION4, ItemID.RANGING_POTION3, ItemID.RANGING_POTION2, ItemID.RANGING_POTION1),
+	RANGING_MIX(Type.POTION, "Range", "R", ItemID.RANGING_MIX1, ItemID.RANGING_MIX2),
 	BASTION(Type.POTION, "Bastion", "B", ItemID.BASTION_POTION4, ItemID.BASTION_POTION3, ItemID.BASTION_POTION2, ItemID.BASTION_POTION1),
 	BATTLEMAGE(Type.POTION, "BatMage", "B.M", ItemID.BATTLEMAGE_POTION4, ItemID.BATTLEMAGE_POTION3, ItemID.BATTLEMAGE_POTION2, ItemID.BATTLEMAGE_POTION1),
 
 	SUPER_ATTACK(Type.POTION, "S.Att", "S.A", ItemID.SUPER_ATTACK4, ItemID.SUPER_ATTACK3, ItemID.SUPER_ATTACK2, ItemID.SUPER_ATTACK1),
+	SUPER_ATTACK_MIX(Type.POTION, "S.Att", "S.A", ItemID.SUPERATTACK_MIX1, ItemID.SUPERATTACK_MIX2),
 	SUPER_STRENGTH(Type.POTION, "S.Str", "S.S", ItemID.SUPER_STRENGTH4, ItemID.SUPER_STRENGTH3, ItemID.SUPER_STRENGTH2, ItemID.SUPER_STRENGTH1),
+	SUPER_STRENGTH_MIX(Type.POTION, "S.Str", "S.S", ItemID.SUPER_STR_MIX1, ItemID.SUPER_STR_MIX2),
 	SUPER_DEFENCE(Type.POTION, "S.Def", "S.D", ItemID.SUPER_DEFENCE4, ItemID.SUPER_DEFENCE3, ItemID.SUPER_DEFENCE2, ItemID.SUPER_DEFENCE1),
+	SUPER_DEFENCE_MIX(Type.POTION, "S.Def", "S.D", ItemID.SUPER_DEF_MIX1, ItemID.SUPER_DEF_MIX2),
 	SUPER_COMBAT(Type.POTION, "S.Com", "S.C", ItemID.SUPER_COMBAT_POTION4, ItemID.SUPER_COMBAT_POTION3, ItemID.SUPER_COMBAT_POTION2, ItemID.SUPER_COMBAT_POTION1),
 	SUPER_RANGING(Type.POTION, "S.Range", "S.Ra", ItemID.SUPER_RANGING_4, ItemID.SUPER_RANGING_3, ItemID.SUPER_RANGING_2, ItemID.SUPER_RANGING_1),
 	SUPER_MAGIC(Type.POTION, "S.Magic", "S.M", ItemID.SUPER_MAGIC_POTION_4, ItemID.SUPER_MAGIC_POTION_3, ItemID.SUPER_MAGIC_POTION_2, ItemID.SUPER_MAGIC_POTION_1),
@@ -286,34 +295,50 @@ enum ItemIdentification
 	DIVINE_BATTLEMAGE(Type.POTION, "BatMage", "B.M", ItemID.DIVINE_BATTLEMAGE_POTION4, ItemID.DIVINE_BATTLEMAGE_POTION3, ItemID.DIVINE_BATTLEMAGE_POTION2, ItemID.DIVINE_BATTLEMAGE_POTION1),
 
 	RESTORE(Type.POTION, "Restore", "Re", ItemID.RESTORE_POTION4, ItemID.RESTORE_POTION3, ItemID.RESTORE_POTION2, ItemID.RESTORE_POTION1),
+	RESTORE_MIX(Type.POTION, "Restore", "Re", ItemID.RESTORE_MIX1, ItemID.RESTORE_MIX2),
 	GUTHIX_BALANCE(Type.POTION, "GuthBal", "G.B.", ItemID.GUTHIX_BALANCE4, ItemID.GUTHIX_BALANCE3, ItemID.GUTHIX_BALANCE2, ItemID.GUTHIX_BALANCE1),
 	SUPER_RESTORE(Type.POTION, "S.Rest", "S.Re", ItemID.SUPER_RESTORE4, ItemID.SUPER_RESTORE3, ItemID.SUPER_RESTORE2, ItemID.SUPER_RESTORE1),
+	SUPER_RESTORE_MIX(Type.POTION, "S.Rest", "S.Re", ItemID.SUPER_RESTORE_MIX1, ItemID.SUPER_RESTORE_MIX2),
 	PRAYER(Type.POTION, "Prayer", "P", ItemID.PRAYER_POTION4, ItemID.PRAYER_POTION3, ItemID.PRAYER_POTION2, ItemID.PRAYER_POTION1),
+	PRAYER_MIX(Type.POTION, "Prayer", "P", ItemID.PRAYER_MIX1, ItemID.PRAYER_MIX2),
 	ENERGY(Type.POTION, "Energy", "En", ItemID.ENERGY_POTION4, ItemID.ENERGY_POTION3, ItemID.ENERGY_POTION2, ItemID.ENERGY_POTION1),
+	ENERGY_MIX(Type.POTION, "Energy", "En", ItemID.ENERGY_MIX1, ItemID.ENERGY_MIX2),
 	SUPER_ENERGY(Type.POTION, "S.Energ", "S.En", ItemID.SUPER_ENERGY4, ItemID.SUPER_ENERGY3, ItemID.SUPER_ENERGY2, ItemID.SUPER_ENERGY1),
+	SUPER_ENERGY_MIX(Type.POTION, "S.Energ", "S.En", ItemID.SUPER_ENERGY_MIX1, ItemID.SUPER_ENERGY_MIX2),
 	STAMINA(Type.POTION, "Stamina", "St", ItemID.STAMINA_POTION4, ItemID.STAMINA_POTION3, ItemID.STAMINA_POTION2, ItemID.STAMINA_POTION1),
+	STAMINA_MIX(Type.POTION, "Stamina", "Sta", ItemID.STAMINA_MIX1, ItemID.STAMINA_MIX2),
 	OVERLOAD(Type.POTION, "Overloa", "OL", ItemID.OVERLOAD_4, ItemID.OVERLOAD_3, ItemID.OVERLOAD_2, ItemID.OVERLOAD_1),
 	ABSORPTION(Type.POTION, "Absorb", "Ab", ItemID.ABSORPTION_4, ItemID.ABSORPTION_3, ItemID.ABSORPTION_2, ItemID.ABSORPTION_1),
 	BLIGHTED_SUPER_RESTORE(Type.POTION, "BS.Rest", "BS.Re", ItemID.BLIGHTED_SUPER_RESTORE4, ItemID.BLIGHTED_SUPER_RESTORE3, ItemID.BLIGHTED_SUPER_RESTORE2, ItemID.BLIGHTED_SUPER_RESTORE1),
 
 	ZAMORAK_BREW(Type.POTION, "ZammyBr", "Za", ItemID.ZAMORAK_BREW4, ItemID.ZAMORAK_BREW3, ItemID.ZAMORAK_BREW2, ItemID.ZAMORAK_BREW1),
+	ZAMORAK_MIX(Type.POTION, "ZammyBr", "Z", ItemID.ZAMORAK_MIX1, ItemID.ZAMORAK_MIX2),
 	SARADOMIN_BREW(Type.POTION, "SaraBr", "Sa", ItemID.SARADOMIN_BREW4, ItemID.SARADOMIN_BREW3, ItemID.SARADOMIN_BREW2, ItemID.SARADOMIN_BREW1),
 	ANCIENT_BREW(Type.POTION, "AncBr", "A.Br", ItemID.ANCIENT_BREW4, ItemID.ANCIENT_BREW3, ItemID.ANCIENT_BREW2, ItemID.ANCIENT_BREW1),
+	ANCIENT_MIX(Type.POTION, "AncBr", "Anc", ItemID.ANCIENT_MIX1, ItemID.ANCIENT_MIX2),
 	FORGOTTEN_BREW(Type.POTION, "ForgBr", "F.Br", ItemID.FORGOTTEN_BREW4, ItemID.FORGOTTEN_BREW3, ItemID.FORGOTTEN_BREW2, ItemID.FORGOTTEN_BREW1),
 
 	ANTIPOISON(Type.POTION, "AntiP", "AP", ItemID.ANTIPOISON4, ItemID.ANTIPOISON3, ItemID.ANTIPOISON2, ItemID.ANTIPOISON1),
+	ANTIPOISON_MIX(Type.POTION, "AntiP", "AP", ItemID.ANTIPOISON_MIX1, ItemID.ANTIPOISON_MIX2),
 	SUPERANTIPOISON(Type.POTION, "S.AntiP", "S.AP", ItemID.SUPERANTIPOISON4, ItemID.SUPERANTIPOISON3, ItemID.SUPERANTIPOISON2, ItemID.SUPERANTIPOISON1),
+	ANTIPOISON_SUPERMIX(Type.POTION, "S.AntiP", "S.AP", ItemID.ANTIPOISON_SUPERMIX1, ItemID.ANTIPOISON_SUPERMIX2),
 	ANTIDOTE_P(Type.POTION, "Antid+", "A+", ItemID.ANTIDOTE4, ItemID.ANTIDOTE3, ItemID.ANTIDOTE2, ItemID.ANTIDOTE1),
+	ANTIDOTE_P_MIX(Type.POTION, "Antid+", "A+", ItemID.ANTIDOTE_MIX1, ItemID.ANTIDOTE_MIX2),
 	ANTIDOTE_PP(Type.POTION, "Antid++", "A++", ItemID.ANTIDOTE4_5952, ItemID.ANTIDOTE3_5954, ItemID.ANTIDOTE2_5956, ItemID.ANTIDOTE1_5958),
 	ANTIVENOM(Type.POTION, "Anti-V", "AV", ItemID.ANTIVENOM4, ItemID.ANTIVENOM3, ItemID.ANTIVENOM2, ItemID.ANTIVENOM1),
 	ANTIVENOM_P(Type.POTION, "Anti-V+", "AV+", ItemID.ANTIVENOM4_12913, ItemID.ANTIVENOM3_12915, ItemID.ANTIVENOM2_12917, ItemID.ANTIVENOM1_12919),
 
 	RELICYMS_BALM(Type.POTION, "Relicym", "R.B", ItemID.RELICYMS_BALM4, ItemID.RELICYMS_BALM3, ItemID.RELICYMS_BALM2, ItemID.RELICYMS_BALM1),
+	RELICYMS_MIX(Type.POTION, "Relicym", "R.B", ItemID.RELICYMS_MIX1, ItemID.RELICYMS_MIX2),
 	SANFEW_SERUM(Type.POTION, "Sanfew", "Sf", ItemID.SANFEW_SERUM4, ItemID.SANFEW_SERUM3, ItemID.SANFEW_SERUM2, ItemID.SANFEW_SERUM1),
 	ANTIFIRE(Type.POTION, "Antif", "Af", ItemID.ANTIFIRE_POTION4, ItemID.ANTIFIRE_POTION3, ItemID.ANTIFIRE_POTION2, ItemID.ANTIFIRE_POTION1),
+	ANTIFIRE_MIX(Type.POTION, "Antif", "Af", ItemID.ANTIFIRE_MIX1, ItemID.ANTIFIRE_MIX2),
 	EXTENDED_ANTIFIRE(Type.POTION, "E.Antif", "E.Af", ItemID.EXTENDED_ANTIFIRE4, ItemID.EXTENDED_ANTIFIRE3, ItemID.EXTENDED_ANTIFIRE2, ItemID.EXTENDED_ANTIFIRE1),
+	EXTENDED_ANTIFIRE_MIX(Type.POTION, "E.Antif", "E.Af", ItemID.EXTENDED_ANTIFIRE_MIX1, ItemID.EXTENDED_ANTIFIRE_MIX2),
 	SUPER_ANTIFIRE(Type.POTION, "S.Antif", "S.Af", ItemID.SUPER_ANTIFIRE_POTION4, ItemID.SUPER_ANTIFIRE_POTION3, ItemID.SUPER_ANTIFIRE_POTION2, ItemID.SUPER_ANTIFIRE_POTION1),
+	SUPER_ANTIFIRE_MIX(Type.POTION, "S.Antif", "S.Af", ItemID.SUPER_ANTIFIRE_MIX1, ItemID.SUPER_ANTIFIRE_MIX2),
 	EXTENDED_SUPER_ANTIFIRE(Type.POTION, "ES.Antif", "ES.Af", ItemID.EXTENDED_SUPER_ANTIFIRE4, ItemID.EXTENDED_SUPER_ANTIFIRE3, ItemID.EXTENDED_SUPER_ANTIFIRE2, ItemID.EXTENDED_SUPER_ANTIFIRE1),
+	EXTENDED_SUPER_ANTIFIRE_MIX(Type.POTION, "ES.Antif", "ES.Af", ItemID.EXTENDED_SUPER_ANTIFIRE_MIX1, ItemID.EXTENDED_SUPER_ANTIFIRE_MIX2),
 
 	SERUM_207(Type.POTION, "Ser207", "S7", ItemID.SERUM_207_4, ItemID.SERUM_207_3, ItemID.SERUM_207_2, ItemID.SERUM_207_1),
 	SERUM_208(Type.POTION, "Ser208", "S8", ItemID.SERUM_208_4, ItemID.SERUM_208_3, ItemID.SERUM_208_2, ItemID.SERUM_208_1),
@@ -322,11 +347,15 @@ enum ItemIdentification
 	COMPOST_POTION(Type.POTION, "Compost", "COM", ItemID.COMPOST_POTION4, ItemID.COMPOST_POTION3, ItemID.COMPOST_POTION2, ItemID.COMPOST_POTION1),
 
 	AGILITY(Type.POTION, "Agility", "Ag", ItemID.AGILITY_POTION4, ItemID.AGILITY_POTION3, ItemID.AGILITY_POTION2, ItemID.AGILITY_POTION1),
+	AGILITY_MIX(Type.POTION, "Agility", "Ag", ItemID.AGILITY_MIX1, ItemID.AGILITY_MIX2),
 	FISHING(Type.POTION, "Fishing", "Fi", ItemID.FISHING_POTION4, ItemID.FISHING_POTION3, ItemID.FISHING_POTION2, ItemID.FISHING_POTION1),
+	FISHING_MIX(Type.POTION, "Fishing", "Fi", ItemID.FISHING_MIX1, ItemID.FISHING_MIX2),
 	HUNTER(Type.POTION, "Hunter", "Hu", ItemID.HUNTER_POTION4, ItemID.HUNTER_POTION3, ItemID.HUNTER_POTION2, ItemID.HUNTER_POTION1),
+	HUNTING_MIX(Type.POTION, "Hunter", "Hu", ItemID.HUNTING_MIX1, ItemID.HUNTING_MIX2),
 
 	GOBLIN(Type.POTION, "Goblin", "G", ItemID.GOBLIN_POTION4, ItemID.GOBLIN_POTION3, ItemID.GOBLIN_POTION2, ItemID.GOBLIN_POTION1),
 	MAGIC_ESS(Type.POTION, "MagEss", "M.E", ItemID.MAGIC_ESSENCE4, ItemID.MAGIC_ESSENCE3, ItemID.MAGIC_ESSENCE2, ItemID.MAGIC_ESSENCE1),
+	MAGIC_ESSENCE_MIX(Type.POTION, "MagEss", "M.E", ItemID.MAGIC_ESSENCE_MIX1, ItemID.MAGIC_ESSENCE_MIX2),
 	REJUVENATION(Type.POTION, "Rejuv", "Rj", ItemID.REJUVENATION_POTION_4, ItemID.REJUVENATION_POTION_3, ItemID.REJUVENATION_POTION_2, ItemID.REJUVENATION_POTION_1),
 
 	ELDER(Type.POTION, "Elder", "El", ItemID.ELDER_4, ItemID.ELDER_3, ItemID.ELDER_2, ItemID.ELDER_1, ItemID.ELDER_POTION_4, ItemID.ELDER_POTION_3, ItemID.ELDER_POTION_2, ItemID.ELDER_POTION_1, ItemID.ELDER_4_20924, ItemID.ELDER_3_20923, ItemID.ELDER_2_20922, ItemID.ELDER_1_20921),