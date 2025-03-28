ff8a02f32ab5397f57a9919f4f5a696b6035f5e7
170iqalbi <chrismadejczyk7@gmail.com>
2025-03-28 17:45:46 -0400

    Added offsets to properly display Daeyalt rock wall cooldown overlay

--- runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningRocksOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningRocksOverlay.java
@@ -33,6 +33,8 @@ import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.Perspective;
 import net.runelite.api.Point;
+import net.runelite.api.coords.Angle;
+import net.runelite.api.coords.Direction;
 import net.runelite.api.coords.LocalPoint;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
@@ -63,6 +65,8 @@ class MiningRocksOverlay extends Overlay
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		Direction playerOrientation = new Angle(client.getLocalPlayer().getOrientation()).getNearestDirection();
+
 		List<RockRespawn> respawns = plugin.getRespawns();
 		if (respawns.isEmpty())
 		{
@@ -77,15 +81,36 @@ class MiningRocksOverlay extends Overlay
 			{
 				continue;
 			}
+			Rock rock = rockRespawn.getRock();
+			Point point = null;
+			final int tileHeight = Perspective.getTileHeight(client, loc, client.getPlane());
+			final int offset = 100;
 
 			float percent = (now.toEpochMilli() - rockRespawn.getStartTime().toEpochMilli()) / (float) rockRespawn.getRespawnTime();
-			Point point = Perspective.localToCanvas(client, loc, client.getPlane(), rockRespawn.getZOffset());
+			if (rock == Rock.DAEYALT_WALL) {
+				switch (rockRespawn.getDirection()) {
+					case EAST:
+						point = Perspective.localToCanvas(client,  loc.getX() + offset, loc.getY(), tileHeight - rockRespawn.getZOffset());
+						break;
+					case NORTH:
+						point = Perspective.localToCanvas(client,  loc.getX(), loc.getY() + offset, tileHeight - rockRespawn.getZOffset());
+						break;
+					case SOUTH:
+						point = Perspective.localToCanvas(client,  loc.getX(), loc.getY() - offset, tileHeight - rockRespawn.getZOffset());
+						break;
+					case WEST:
+						point = Perspective.localToCanvas(client,  loc.getX() - offset, loc.getY(), tileHeight - rockRespawn.getZOffset());
+						break;
+				}
+			}
+			else {
+				point = Perspective.localToCanvas(client, loc, client.getPlane(), rockRespawn.getZOffset());
+			}
 			if (point == null || percent > 1.0f)
 			{
 				continue;
 			}
 
-			Rock rock = rockRespawn.getRock();
 
 			// Only draw timer for veins on the same level in motherlode mine
 			LocalPoint localLocation = client.getLocalPlayer().getLocalLocation();
