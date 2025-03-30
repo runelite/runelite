72fa8b7f7c130a932cc24dfd291ae9518efd512f
Max Weber <mii7303@gmail.com>
2025-03-30 13:30:50 -0600

    cache: add gamevals

--- cache/src/main/java/net/runelite/cache/definitions/GameValDefinition.java
+++ cache/src/main/java/net/runelite/cache/definitions/GameValDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, Adam <Adam@sigterm.info>
+ * Copyright (c) 2025 Abex
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,43 +22,17 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package net.runelite.cache.definitions;
 
-package net.runelite.cache;
+import java.util.Map;
+import lombok.Data;
 
-public enum IndexType
+@Data
+public class GameValDefinition
 {
-	ANIMATIONS(0),
-	SKELETONS(1),
-	CONFIGS(2),
-	INTERFACES(3),
-	SOUNDEFFECTS(4),
-	MAPS(5),
-	MUSIC_TRACKS(6),
-	MODELS(7),
-	SPRITES(8),
-	TEXTURES(9),
-	BINARY(10),
-	MUSIC_JINGLES(11),
-	CLIENTSCRIPT(12),
-	FONTS(13),
-	MUSIC_SAMPLES(14),
-	MUSIC_PATCHES(15),
-	WORLDMAP_GEOGRAPHY(18),
-	WORLDMAP(19),
-	WORLDMAP_GROUND(20),
-	DBTABLEINDEX(21),
-	ANIMAYAS(22),
-	GAMEVALS(24);
-
+	private int gameValId;
 	private int id;
 
-	IndexType(int id)
-	{
-		this.id = id;
-	}
-
-	public int getNumber()
-	{
-		return id;
-	}
+	private String name;
+	private Map<Integer, String> files = Map.of();
 }
