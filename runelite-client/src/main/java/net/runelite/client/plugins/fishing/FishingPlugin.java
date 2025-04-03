f37c3ba205a3ce2b868584097dc302d1c08e685d
cdfisher <cdfisher96@gmail.com>
2025-04-03 14:07:50 -0400

    fishing: add trailblazer and trailblazer reloaded harpoons
    Co-authored-by: Macweese <50101641+Macweese@users.noreply.github.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingPlugin.java
@@ -251,8 +251,11 @@ public class FishingPlugin extends Plugin
 				case ItemID.DRAGON_HARPOON:
 				case ItemID.DRAGON_HARPOON_OR:
 				case ItemID.INFERNAL_HARPOON:
+				case ItemID.INFERNAL_HARPOON_OR:
+				case ItemID.INFERNAL_HARPOON_OR_30342:
 				case ItemID.INFERNAL_HARPOON_UNCHARGED:
 				case ItemID.INFERNAL_HARPOON_UNCHARGED_25367:
+				case ItemID.INFERNAL_HARPOON_UNCHARGED_30343:
 				case ItemID.HARPOON:
 				case ItemID.BARBTAIL_HARPOON:
 				case ItemID.BIG_FISHING_NET:
@@ -270,7 +273,6 @@ public class FishingPlugin extends Plugin
 				case ItemID.KARAMBWAN_VESSEL_3159:
 				case ItemID.CORMORANTS_GLOVE:
 				case ItemID.CORMORANTS_GLOVE_22817:
-				case ItemID.INFERNAL_HARPOON_OR:
 				case ItemID.ECHO_HARPOON:
 				case ItemID.CRYSTAL_HARPOON:
 				case ItemID.CRYSTAL_HARPOON_23864:
