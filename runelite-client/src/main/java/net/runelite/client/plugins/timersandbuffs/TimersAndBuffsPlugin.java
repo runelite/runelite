arbahb/ulu-p

timers buffs: add smouldering heart
The interval of 25 is based on struct 965, param 1541 (script 5923)

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -630,6 +630,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarCounter(BURN_DAMAGE_NEXT_HIT, event.getValue());
 		}
 
+		if (event.getVarbitId() == Varbits.SMOULDERING_HEART && config.showOverload())
+		{
+			updateVarTimer(SMOULDERING_HEART, event.getValue(), i -> i * 25);
+		}
+
 		if (event.getVarbitId() == Varbits.BUFF_GOADING_POTION && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -677,6 +682,7 @@ public class TimersAndBuffsPlugin extends Plugin
 			removeGameTimer(OVERLOAD_RAID);
 			removeGameTimer(SMELLING_SALTS);
 			removeVarCounter(STONE_OF_JAS_EMPOWERMENT);
+			removeGameTimer(SMOULDERING_HEART);
 		}
 
 		if (!config.showPrayerEnhance())

runelite/runelite

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -628,6 +628,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarCounter(BURN_DAMAGE_NEXT_HIT, event.getValue());
 		}
 
+		if (event.getVarbitId() == VarbitID.WGS_SMOULDERING_HEART_TIMER && config.showTormentedDemonBuffs())
+		{
+			updateVarTimer(SMOULDERING_HEART, event.getValue(), i -> i * 25);
+		}
+
 		if (event.getVarbitId() == VarbitID.GOADING_POTION_TIMER && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -684,6 +689,7 @@ public class TimersAndBuffsPlugin extends Plugin
 		if (!config.showTormentedDemonBuffs())
 		{
 			removeVarCounter(STONE_OF_JAS_EMPOWERMENT);
+			removeGameTimer(SMOULDERING_HEART);
 		}
 
 		if (!config.showPrayerEnhance())