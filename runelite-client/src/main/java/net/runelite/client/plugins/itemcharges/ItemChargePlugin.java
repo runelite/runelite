arbahb/ulu-p

item charges: add ring of the elements

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargePlugin.java
@@ -526,6 +526,10 @@ public class ItemChargePlugin extends Plugin
 		{
 			updateExplorerRingCharges(event.getValue());
 		}
+		else if (event.getVarbitId() == Varbits.RING_OF_ELEMENTS_CHARGES)
+		{
+			updateRingOfElementsCharges(event.getValue());
+		}
 	}
 
 	@Subscribe
@@ -594,6 +598,12 @@ public class ItemChargePlugin extends Plugin
 	{
 		// Note: Varbit counts upwards. We count down from the maximum charges.
 		setItemCharges(ItemChargeConfig.KEY_EXPLORERS_RING, MAX_EXPLORER_RING_CHARGES - value);
+		updateInfoboxes();
+	}
+
+	private void updateRingOfElementsCharges(final int value)
+	{
+		setItemCharges(ItemChargeConfig.KEY_RING_OF_THE_ELEMENTS, value);
 		updateInfoboxes();
 	}
 

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemWithConfig.java
@@ -48,7 +48,8 @@ enum ItemWithConfig
 	EXPEDITIOUS_BRACELET(ItemID.EXPEDITIOUS_BRACELET, ItemChargeConfig.KEY_EXPEDITIOUS_BRACELET, ItemChargeType.EXPEDITIOUS_BRACELET),
 	CHRONICLE(ItemID.CHRONICLE, ItemChargeConfig.KEY_CHRONICLE, ItemChargeType.TELEPORT),
 	BLOOD_ESSENCE(ItemID.BLOOD_ESSENCE_ACTIVE, ItemChargeConfig.KEY_BLOOD_ESSENCE, ItemChargeType.BLOOD_ESSENCE),
-	BRACELET_OF_CLAY(ItemID.BRACELET_OF_CLAY, ItemChargeConfig.KEY_BRACELET_OF_CLAY, ItemChargeType.BRACELET_OF_CLAY);
+	BRACELET_OF_CLAY(ItemID.BRACELET_OF_CLAY, ItemChargeConfig.KEY_BRACELET_OF_CLAY, ItemChargeType.BRACELET_OF_CLAY),
+	RING_OF_THE_ELEMENTS(ItemID.RING_OF_THE_ELEMENTS_26818, ItemChargeConfig.KEY_RING_OF_THE_ELEMENTS, ItemChargeType.RING_OF_THE_ELEMENTS);
 
 	private final int itemId;
 	private final String configKey;

runelite/runelite

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargePlugin.java
@@ -487,6 +487,10 @@ public class ItemChargePlugin extends Plugin
 		{
 			updateBindingNecklaceCharges(event.getValue());
 		}
+		else if (event.getVarbitId() == VarbitID.RING_OF_THE_ELEMENTS_CHARGES)
+		{
+			updateRingOfElementsCharges(event.getValue());
+		}
 	}
 
 	@Subscribe
@@ -555,6 +559,12 @@ public class ItemChargePlugin extends Plugin
 	{
 		// Note: Varbit counts upwards. We count down from the maximum charges.
 		setItemCharges(ItemChargeConfig.KEY_EXPLORERS_RING, MAX_EXPLORER_RING_CHARGES - value);
+		updateInfoboxes();
+	}
+
+	private void updateRingOfElementsCharges(final int value)
+	{
+		setItemCharges(ItemChargeConfig.KEY_RING_OF_THE_ELEMENTS, value);
 		updateInfoboxes();
 	}
 

runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemWithConfig.java
@@ -48,7 +48,8 @@ enum ItemWithConfig
 	EXPEDITIOUS_BRACELET(ItemID.EXPEDITIOUS_BRACELET, ItemChargeConfig.KEY_EXPEDITIOUS_BRACELET, ItemChargeType.EXPEDITIOUS_BRACELET),
 	CHRONICLE(ItemID.CHRONICLE, ItemChargeConfig.KEY_CHRONICLE, ItemChargeType.TELEPORT),
 	BLOOD_ESSENCE(ItemID.BLOOD_ESSENCE_ACTIVE, ItemChargeConfig.KEY_BLOOD_ESSENCE, ItemChargeType.BLOOD_ESSENCE),
-	BRACELET_OF_CLAY(ItemID.JEWL_BRACELET_OF_CLAY, ItemChargeConfig.KEY_BRACELET_OF_CLAY, ItemChargeType.BRACELET_OF_CLAY);
+	BRACELET_OF_CLAY(ItemID.JEWL_BRACELET_OF_CLAY, ItemChargeConfig.KEY_BRACELET_OF_CLAY, ItemChargeType.BRACELET_OF_CLAY),
+	RING_OF_THE_ELEMENTS(ItemID.RING_OF_ELEMENTS_CHARGED, ItemChargeConfig.KEY_RING_OF_THE_ELEMENTS, ItemChargeType.RING_OF_THE_ELEMENTS);
 
 	private final int itemId;
 	private final String configKey;