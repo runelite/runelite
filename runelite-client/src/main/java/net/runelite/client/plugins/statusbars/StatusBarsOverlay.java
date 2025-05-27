f0b85bda609c111cfb35162fa341efbf15e63864
Lake <ldavid432@gmail.com>
2025-05-27 17:30:07 -0500

    status bars: auto-swap HP to warmth at wintertodt

--- runelite-client/src/main/java/net/runelite/client/plugins/statusbars/StatusBarsOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/statusbars/StatusBarsOverlay.java
@@ -292,8 +292,21 @@ class StatusBarsOverlay extends Overlay
 			offsetRightBarY = (location.getY() - offsetRight.getY());
 		}
 
-		BarRenderer left = barRenderers.get(config.leftBarMode());
-		BarRenderer right = barRenderers.get(config.rightBarMode());
+		StatusBarsConfig.BarMode leftBarMode = config.leftBarMode();
+		StatusBarsConfig.BarMode rightBarMode = config.rightBarMode();
+
+		if (leftBarMode == StatusBarsConfig.BarMode.HITPOINTS && plugin.isInWintertodtRegion() && config.replaceHpWithWarmth())
+		{
+			leftBarMode = StatusBarsConfig.BarMode.WARMTH;
+		}
+
+		if (rightBarMode == StatusBarsConfig.BarMode.HITPOINTS && plugin.isInWintertodtRegion() && config.replaceHpWithWarmth())
+		{
+			rightBarMode = StatusBarsConfig.BarMode.WARMTH;
+		}
+
+		BarRenderer left = barRenderers.get(leftBarMode);
+		BarRenderer right = barRenderers.get(rightBarMode);
 
 		if (left != null)
 		{
