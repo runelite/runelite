3801189adfdafb5455ca649536b05825aea78b11
Stan <stantonmoore42@gmail.com>
2025-08-07 03:58:32 -0400

    forgot : and ;

--- runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
@@ -2774,10 +2774,10 @@ public class ChatCommandsPlugin extends Plugin
 			case "brimstone chest":
 				return "Brimstone chest";
 
-			case "royal titans"
-			case "royal"
-			case "titans"
-				return "The Royal Titans"
+			case "royal titans":
+			case "royal":
+			case "titans":
+				return "The Royal Titans";
 
 			case "dom":
 			case "doom":
