bfb3738613c02bb7fd621a4ff94e9947278ef382
robmonte <17119716+robmonte@users.noreply.github.com>
2025-08-07 11:02:12 -0500

    Match new case style on config names

--- runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/agility/AgilityConfig.java
@@ -56,7 +56,7 @@ public interface AgilityConfig extends Config
 
 	@ConfigItem(
 		keyName = "extendDrawDistance",
-		name = "Extend Clickbox Draw Distances",
+		name = "Extend clickbox draw distances",
 		description = "Show agility course and obstacle clickboxes from further away. May reduce performance on weaker machines.",
 		position = 1
 	)
