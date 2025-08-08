4bb4a7735df0de3d33aeb28428211e93dca95bee
bytwyg <bytwyg@gmail.com>
2025-08-08 11:16:13 -0600

    Create FancyFlipPlugin.java

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/fancyflip/FancyFlipPlugin.java
@@ -0,0 +1,51 @@
+package net.runelite.client.plugins.fancyflip;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.ui.PluginPanel;
+
+@Slf4j
+@PluginDescriptor(
+        name = "FancyFlip",
+        description = "Flip assistant with directives and KPIs",
+        tags = {"ge","flip","market","profit"}
+)
+public class FancyFlipPlugin extends Plugin
+{
+    @Inject private Client client;
+    @Inject private ClientToolbar clientToolbar;
+    @Inject private FancyFlipConfig config;
+
+    private NavigationButton navButton;
+    private PluginPanel panel;
+
+    @Override
+    protected void startUp()
+    {
+        panel = new ui.FancyFlipPanel(); // simple placeholder panel
+        navButton = NavigationButton.builder()
+                .tooltip("FancyFlip")
+                .panel(panel)
+                .build();
+        clientToolbar.addNavigation(navButton);
+        log.info("FancyFlip started");
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        if (navButton != null) clientToolbar.removeNavigation(navButton);
+        navButton = null; panel = null;
+        log.info("FancyFlip stopped");
+    }
+
+    @Provides
+    FancyFlipConfig provideConfig(ConfigManager cm) { return cm.getConfig(FancyFlipConfig.class); }
+}
