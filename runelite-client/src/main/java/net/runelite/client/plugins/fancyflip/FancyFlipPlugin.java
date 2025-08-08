8b2720f2899a240a3ff558bc509b4645783bbe90
bytwyg <bytwyg@gmail.com>
2025-08-08 15:03:13 -0600

    Update FancyFlipPlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/fancyflip/FancyFlipPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/fancyflip/FancyFlipPlugin.java
@@ -1,34 +1,16 @@
-/*
- * Copyright (c) 2025, Codex <codex@openai.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package net.runelite.client.plugins.fancyflip;
 
 import com.google.inject.Provides;
+import java.awt.image.BufferedImage;
 import javax.inject.Inject;
 import javax.swing.Timer;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GrandExchangeOfferChanged;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -38,9 +20,7 @@ import net.runelite.client.plugins.fancyflip.ui.FancyFlipPanel;
 import net.runelite.client.plugins.fancyflip.wealth.WealthService;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
-import java.awt.image.BufferedImage;
 import net.runelite.client.util.ImageUtil;
-
 
 @Slf4j
 @PluginDescriptor(
@@ -61,6 +41,8 @@ public class FancyFlipPlugin extends Plugin
     private WealthService wealth;
     private Timer uiTick, wealthTick;
 
+    private boolean sessionStarted = false;
+
     @Override
     protected void startUp()
     {
@@ -72,10 +54,12 @@ public class FancyFlipPlugin extends Plugin
             wealth = new WealthService(client);
             wealth.refreshCommittedGp();
 
+            // init panel from config
             panel.setSellOnly(config.sellOnly());
             panel.setF2pOnly(config.f2pOnly());
             panel.setBlocklistCsv(config.blocklistCsv());
 
+            // persist panel changes back to config
             panel.addSellOnlyListener(e ->
                 configManager.setConfiguration("fancyflip", "sellOnly", panel.isSellOnly()));
             panel.addF2pOnlyListener(e ->
@@ -84,17 +68,16 @@ public class FancyFlipPlugin extends Plugin
                 configManager.setConfiguration("fancyflip", "blocklistCsv", panel.getBlocklistCsv()));
             panel.addResetListener(() -> {
                 ledger.reset();
-                sessionStarted = true;
+                sessionStarted = true;      // user explicitly starts a session
                 wealth.refreshCommittedGp();
                 sampleWealthNow();
             });
 
+            // load icon (fallback if missing)
             BufferedImage icon;
             try
             {
-                icon = ImageUtil.loadImageResource(
-                    FancyFlipPlugin.class, "pixel-diamond.png"
-                );
+                icon = ImageUtil.loadImageResource(FancyFlipPlugin.class, "pixel-diamond.png");
                 if (icon == null)
                 {
                     icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
@@ -108,13 +91,14 @@ public class FancyFlipPlugin extends Plugin
 
             navButton = NavigationButton.builder()
                 .tooltip("FancyFlip")
-                .icon(icon)          // guaranteed non-null
-                .panel(panel)        // non-null
+                .icon(icon)
+                .panel(panel)
                 .priority(5)
                 .build();
 
             clientToolbar.addNavigation(navButton);
 
+            // 1s UI tick
             uiTick = new Timer(1000, e -> {
                 long totalWealth = wealth.getTotalWealth(config.includeBankCoins(), 0);
                 panel.setProfit(ledger.getProfitGp());
@@ -128,6 +112,7 @@ public class FancyFlipPlugin extends Plugin
             });
             uiTick.start();
 
+            // 60s wealth sample
             wealthTick = new Timer(60_000, e -> {
                 if (sessionStarted)
                 {
@@ -144,42 +129,85 @@ public class FancyFlipPlugin extends Plugin
         }
     }
 
-
-@Override
-protected void shutDown()
-{
-    try
+    @Override
+    protected void shutDown()
     {
-        if (uiTick != null) uiTick.stop();
-        if (wealthTick != null) wealthTick.stop();
-        if (navButton != null)
+        try
         {
-            clientToolbar.removeNavigation(navButton);
+            if (uiTick != null) uiTick.stop();
+            if (wealthTick != null) wealthTick.stop();
+            if (navButton != null) clientToolbar.removeNavigation(navButton);
+        }
+        finally
+        {
+            navButton = null;
+            panel = null;
+            uiTick = null;
+            wealthTick = null;
+            ledger = null;
+            wealth = null;
+            sessionStarted = false;
+            log.info("FancyFlip stopped");
         }
     }
-    finally
+
+    // Start session on first successful login
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged e)
     {
-        navButton = null;
-        panel = null;
-        uiTick = null;
-        wealthTick = null;
-        ledger = null;
-        wealth = null;
-        log.info("FancyFlip stopped");
+        if (e.getGameState() == GameState.LOGGED_IN && !sessionStarted)
+        {
+            ledger.reset();          // sets sessionStart = now
+            sessionStarted = true;
+            sampleWealthNow();       // seed avg wealth immediately
+        }
     }
-}
 
     @Subscribe
     public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged e)
     {
+        if (wealth != null)
+        {
+            wealth.onGrandExchangeOfferChanged(e);
+            if (sessionStarted) sampleWealthNow();
+        }
     }
 
     @Subscribe
     public void onWidgetLoaded(WidgetLoaded e)
     {
+        if (wealth == null) return;
 
+        // Let WealthService handle snapshots (bank)
+        wealth.onWidgetLoaded(e);
+
+        // Sample immediately on bank or inventory open/reload
+        int gid = e.getGroupId();
+        if (gid == WidgetID.BANK_GROUP_ID || gid == WidgetID.INVENTORY_GROUP_ID)
+        {
+            if (!sessionStarted)
+            {
+                // if player opens inv/bank before login event fired, start session here
+                ledger.reset();
+                sessionStarted = true;
+            }
+            sampleWealthNow();
+        }
+    }
+
+    // Helper: take an immediate sample and push key UI fields
+    private void sampleWealthNow()
+    {
+        long total = wealth.getTotalWealth(config.includeBankCoins(), 0);
+        ledger.sampleWealth(total);
+        panel.setCurrentWealth(total);
+        panel.setAvgWealth(ledger.getAvgWealthGp());
+        panel.setSessionTime(ledger.getSessionTimeHms());
     }
 
     @Provides
-    FancyFlipConfig provideConfig(ConfigManager cm) { return cm.getConfig(FancyFlipConfig.class); }
+    FancyFlipConfig provideConfig(ConfigManager cm)
+    {
+        return cm.getConfig(FancyFlipConfig.class);
+    }
 }
