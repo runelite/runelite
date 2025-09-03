94bf79601da4bbe02ce76491607330826e54c378
Casey Zwicker <caseypzwicker@gmail.com>
2025-09-03 04:32:26 -0700

    To please the overload resolver

--- runelite-client/src/main/resources/net/runelite/client/plugins/gpu/scale/pixel.glsl
+++ runelite-client/src/main/resources/net/runelite/client/plugins/gpu/scale/pixel.glsl
@@ -5,7 +5,7 @@ vec4 texturePixel(sampler2D sampler, vec2 texUV) {
   vec2 textureCoords = texUV * vec2(sourceDimensions);
   vec2 locationWithinTexel = fract(textureCoords);
   vec2 pixelsPerTexel = vec2(targetDimensions) / vec2(sourceDimensions);
-  vec2 interpolationAmount = clamp(locationWithinTexel * pixelsPerTexel, 0, 0.5) + clamp((locationWithinTexel - vec2(1.0,1.0)) * pixelsPerTexel + vec2(0.5,0.5), 0, 0.5);
+  vec2 interpolationAmount = clamp(locationWithinTexel * pixelsPerTexel, 0.0, 0.5) + clamp((locationWithinTexel - vec2(1.0,1.0)) * pixelsPerTexel + vec2(0.5,0.5), 0.0, 0.5);
   vec2 finalTextureCoords = (floor(textureCoords) + interpolationAmount) / vec2(sourceDimensions);
   return texture(sampler, finalTextureCoords);
 }
