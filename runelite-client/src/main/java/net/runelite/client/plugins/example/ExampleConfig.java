f1aa26943dad7daa83d1f452fc76fedf9c1d0cc5
Baron Hines <baron.hines@gmail.com>
2025-09-13 15:17:05 -0500

    fake example plugin

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/example/ExampleConfig.java
@@ -0,0 +1,19 @@
+package net.runelite.client.plugins.example;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("example")
+public interface ExampleConfig extends Config
+{
+	@ConfigItem(
+		keyName = "greeting",
+		name = "Welcome Greeting",
+		description = "The message to show to the user when they login"
+	)
+	default String greeting()
+	{
+		return "Hello";
+	}
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/example/ExamplePlugin.java
@@ -0,0 +1,54 @@
+package net.runelite.client.plugins.example;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Example"
+)
+public class ExamplePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private ExampleConfig config;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.info("Example started!");
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		log.info("Example stopped!");
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+		}
+	}
+
+	@Provides
+	ExampleConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(ExampleConfig.class);
+	}
+}
