e2ab18590e90186cb63ce64deee09e43c8d09518
Patrick Watts <patrick.watts@hotmail.co.uk>
2025-09-25 02:09:46 +0400

    puzzlesolver: add ability to increase no. of dots shown
    The number of puzzle box moves can now be configured between four and eight when displaying the solution as dots. Leaving it at four there is no risk of overlapping moves while increasing the number of moves allows for faster puzzle solving.
    
    Dots now interpolate between two colors instead of being a single yellow color. While the further away dots become smaller they now also gradually become a different color, providing another visual queue.
    
    Puzzle box steps now default to being displayed as dots due to the arrows being confusing to many users.

--- runelite-client/src/main/java/net/runelite/client/plugins/puzzlesolver/PuzzleSolverConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/puzzlesolver/PuzzleSolverConfig.java
@@ -25,9 +25,11 @@
  */
 package net.runelite.client.plugins.puzzlesolver;
 
+import java.awt.Color;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup("puzzlesolver")
 public interface PuzzleSolverConfig extends Config
@@ -35,7 +37,8 @@ public interface PuzzleSolverConfig extends Config
 	@ConfigItem(
 		keyName = "displaySolution",
 		name = "Display solution",
-		description = "Display a solution to the puzzle."
+		description = "Display a solution to the puzzle.",
+		position = 0
 	)
 	default boolean displaySolution()
 	{
@@ -45,7 +48,8 @@ public interface PuzzleSolverConfig extends Config
 	@ConfigItem(
 		keyName = "displayRemainingMoves",
 		name = "Display remaining moves",
-		description = "Add a text line above puzzle boxes displaying the amount of remaining moves."
+		description = "Add a text line above puzzle boxes displaying the amount of remaining moves.",
+		position = 1
 	)
 	default boolean displayRemainingMoves()
 	{
@@ -55,10 +59,48 @@ public interface PuzzleSolverConfig extends Config
 	@ConfigItem(
 		keyName = "drawDots",
 		name = "Draw dots instead of arrows",
-		description = "Draw dots increasing in size instead of arrows for the solution."
+		description = "Draw dots increasing in size instead of arrows for the solution.",
+		position = 2
 	)
 	default boolean drawDots()
 	{
-		return false;
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "movesToShow",
+		name = "Number of dots",
+		description = "The number of moves as dots to show for the puzzle.",
+		position = 3
+	)
+	@Range(
+		min = 4,
+		max = 8
+	)
+	default int movesToShow()
+	{
+		return 4;
+	}
+
+	@ConfigItem(
+		keyName = "dotColor",
+		name = "Dot start color",
+		description = "Dot color for the first solution dot.",
+		position = 4
+	)
+	default Color dotColor()
+	{
+		return Color.YELLOW;
+	}
+
+	@ConfigItem(
+		keyName = "dotEndColor",
+		name = "Dot end color",
+		description = "Dot color of the last solution dot that dots blend towards.",
+		position = 5
+	)
+	default Color dotEndColor()
+	{
+		return Color.RED;
 	}
 }
