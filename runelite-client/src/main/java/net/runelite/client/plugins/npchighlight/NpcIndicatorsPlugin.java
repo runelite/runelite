4c594afa06fb6b653dc62e426e171d5a77afefd4
Alex <alex.bristow3@gmail.com>
2025-10-26 08:36:55 -0400

    revert npc

--- runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcIndicatorsPlugin.java
@@ -711,7 +711,7 @@ public class NpcIndicatorsPlugin extends Plugin
 			return Collections.emptyList();
 		}
 
-        return Text.fromCSV(configNpcs);
+    return Text.fromCSV(configNpcs);
 	}
 
 	void rebuild()
