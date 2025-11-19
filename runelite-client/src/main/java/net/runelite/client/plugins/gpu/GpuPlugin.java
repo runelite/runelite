421a014b59c203396fbdb7d7a412725bc891dd6b
Adam <Adam@sigterm.info>
2025-11-19 07:49:08 -0500

    Revert "gpu: readd smooth banding option"
    This reverts commit 738e93aa9c31e43843941525ed77296df90b4cea.

--- runelite-client/src/main/java/net/runelite/client/plugins/gpu/GpuPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gpu/GpuPlugin.java
@@ -253,7 +253,6 @@ public class GpuPlugin extends Plugin implements DrawCallbacks
 	private int uniFogDepth;
 	private int uniDrawDistance;
 	private int uniExpandedMapLoadingChunks;
-	private int uniSmoothBanding;
 	private int uniWorldProj;
 	private static int uniEntityProj;
 	static int uniEntityTint;
@@ -587,7 +586,6 @@ public class GpuPlugin extends Plugin implements DrawCallbacks
 		uniWorldProj = glGetUniformLocation(glProgram, "worldProj");
 		uniEntityProj = glGetUniformLocation(glProgram, "entityProj");
 		uniEntityTint = glGetUniformLocation(glProgram, "entityTint");
-		uniSmoothBanding = glGetUniformLocation(glProgram, "smoothBanding");
 		uniBrightness = glGetUniformLocation(glProgram, "brightness");
 		uniUseFog = glGetUniformLocation(glProgram, "useFog");
 		uniFogColor = glGetUniformLocation(glProgram, "fogColor");
@@ -949,7 +947,6 @@ public class GpuPlugin extends Plugin implements DrawCallbacks
 		// Brightness happens to also be stored in the texture provider, so we use that
 		TextureProvider textureProvider = client.getTextureProvider();
 		glUniform1f(uniBrightness, (float) textureProvider.getBrightness());
-		glUniform1f(uniSmoothBanding, config.smoothBanding() ? 0f : 1f);
 		glUniform1f(uniTextureLightMode, config.brightTextures() ? 1f : 0f);
 		if (client.getGameState() == GameState.LOGGED_IN)
 		{
