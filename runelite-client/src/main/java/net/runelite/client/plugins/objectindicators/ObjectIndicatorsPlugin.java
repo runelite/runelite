d2ad44eaae2929b5fb8d94ce033fbfb947efd907
Yook <yookiop@hotmail.com>
2026-04-22 21:36:02 +0200

    Dynamic shift key

--- runelite-client/src/main/java/net/runelite/client/plugins/objectindicators/ObjectIndicatorsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/objectindicators/ObjectIndicatorsPlugin.java
@@ -52,7 +52,6 @@ import net.runelite.api.Client;
 import net.runelite.api.DecorativeObject;
 import net.runelite.api.GameObject;
 import net.runelite.api.GroundObject;
-import net.runelite.api.KeyCode;
 import net.runelite.api.Menu;
 import net.runelite.api.MenuAction;
 import net.runelite.api.MenuEntry;
@@ -89,6 +88,7 @@ import net.runelite.client.ui.components.colorpicker.ColorPickerManager;
 import net.runelite.client.ui.components.colorpicker.RuneliteColorPicker;
 import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.ColorUtil;
+import net.runelite.client.ShiftHotkeyManager;
 
 @PluginDescriptor(
 	name = "Object Markers",
@@ -130,6 +130,9 @@ public class ObjectIndicatorsPlugin extends Plugin
 
 	@Inject
 	private ClientThread clientThread;
+
+	@Inject
+	private ShiftHotkeyManager shiftHotkeyManager;
 
 	@Provides
 	ObjectIndicatorsConfig provideConfig(ConfigManager configManager)
@@ -264,7 +267,7 @@ public class ObjectIndicatorsPlugin extends Plugin
 	@Subscribe
 	public void onMenuEntryAdded(MenuEntryAdded event)
 	{
-		if (event.getType() != MenuAction.EXAMINE_OBJECT.getId() || !client.isKeyPressed(KeyCode.KC_SHIFT))
+		if (event.getType() != MenuAction.EXAMINE_OBJECT.getId() || !shiftHotkeyManager.isPressed())
 		{
 			return;
 		}
