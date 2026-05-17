3f01f3b3795b69cf681f7f212cd0224064c67df9
Daniel-Tudose <tudosedaniel24@stud.ase.ro>
2026-05-17 20:04:13 +0300

    clues: center beginner clues

--- runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/BeginnerMapClue.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cluescrolls/clues/BeginnerMapClue.java
@@ -33,11 +33,11 @@ import net.runelite.api.gameval.InterfaceID;
 public class BeginnerMapClue extends MapClue implements LocationClueScroll
 {
 	private static final ImmutableList<BeginnerMapClue> CLUES = ImmutableList.of(
-		new BeginnerMapClue(InterfaceID.TRAIL_MAP01, new WorldPoint(3166, 3361, 0), MapClue.CHAMPIONS_GUILD),
-		new BeginnerMapClue(InterfaceID.TRAIL_MAP02, new WorldPoint(3290, 3374, 0), MapClue.VARROCK_EAST_MINE),
-		new BeginnerMapClue(InterfaceID.TRAIL_MAP03, new WorldPoint(3093, 3226, 0), MapClue.SOUTH_OF_DRAYNOR_BANK),
-		new BeginnerMapClue(InterfaceID.TRAIL_MAP06, new WorldPoint(3043, 3398, 0), MapClue.STANDING_STONES),
-		new BeginnerMapClue(InterfaceID.TRAIL_MAP11, new WorldPoint(3110, 3152, 0), MapClue.WIZARDS_TOWER_DIS)
+		new BeginnerMapClue(InterfaceID.TRAIL_MAP01, new WorldPoint(3167, 3360, 0), MapClue.CHAMPIONS_GUILD),
+		new BeginnerMapClue(InterfaceID.TRAIL_MAP02, new WorldPoint(3290, 3373, 0), MapClue.VARROCK_EAST_MINE),
+		new BeginnerMapClue(InterfaceID.TRAIL_MAP03, new WorldPoint(3092, 3226, 0), MapClue.SOUTH_OF_DRAYNOR_BANK),
+		new BeginnerMapClue(InterfaceID.TRAIL_MAP06, new WorldPoint(3043, 3399, 0), MapClue.STANDING_STONES),
+		new BeginnerMapClue(InterfaceID.TRAIL_MAP11, new WorldPoint(3109, 3153, 0), MapClue.WIZARDS_TOWER_DIS)
 	);
 
 	private final int widgetGroupID;
