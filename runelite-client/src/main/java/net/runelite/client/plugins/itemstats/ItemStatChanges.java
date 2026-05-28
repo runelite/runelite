322c5f16d3e448a5979780c9fc9732a27188391e
Jamie Luckett <jamieluckett@gmail.com>
2026-05-28 21:40:49 +0100

    itemstats: Add T-Bone steak

--- runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemstats/ItemStatChanges.java
@@ -131,6 +131,7 @@ public class ItemStatChanges
 		add(range(food(7), food(10)), ItemID.TBW_SPIDER_ON_STICK_COOKED, ItemID.TBW_SPIDER_ON_SHAFT_COOKED);
 		add(combo(food(8), food(6)), ItemID.GRAAHK_COOKED);
 		add(combo(food(9), food(8)), ItemID.KYATT_COOKED);
+		add(combo(food(9), boost(STRENGTH, 2)), ItemID.TBONE_STEAK);
 		add(combo(food(11), food(8)), ItemID.FENNECFOX_COOKED);
 		add(combo(food(13), food(10), heal(RUN_ENERGY, 10)), ItemID.DASHINGKEBBIT_COOKED);
 		add(combo(food(12), food(9)), ItemID.ANTELOPESUN_COOKED);
