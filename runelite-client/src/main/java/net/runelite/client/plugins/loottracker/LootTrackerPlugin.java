7f764ac30bca3ee740ff07d9f53a75726c295529
Jordan Atwood <nightfirecat@nightfirec.at>
2026-09-02 08:46:01 -0700

    loot tracker: code style cleanup

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
@@ -1274,7 +1274,7 @@ public class LootTrackerPlugin extends Plugin
 
 	private void countChangedItems(int itemId, Object metadata)
 	{
-		onInvChange((((invItems, groundItems, removedItems) ->
+		onInvChange((invItems, groundItems, removedItems) ->
 		{
 			int cnt = removedItems.count(itemId);
 			if (cnt > 0)
@@ -1285,7 +1285,7 @@ public class LootTrackerPlugin extends Plugin
 				combined.addAll(groundItems);
 				addLoot(name, -1, LootRecordType.EVENT, metadata, combined, cnt);
 			}
-		})));
+		});
 	}
 
 	@Subscribe
@@ -1424,7 +1424,7 @@ public class LootTrackerPlugin extends Plugin
 							put("HERBLORE", client.getBoostedSkillLevel(Skill.HERBLORE)).
 							put("HUNTER", client.getBoostedSkillLevel(Skill.HUNTER)).
 							build();
-						onInvChange((((invItems, groundItems, removedItems) ->
+						onInvChange((invItems, groundItems, removedItems) ->
 						{
 							int cnt = removedItems.count(itemId);
 							if (cnt > 0)
@@ -1432,7 +1432,7 @@ public class LootTrackerPlugin extends Plugin
 								String name = itemManager.getItemComposition(itemId).getMembersName();
 								addLoot(name, -1, LootRecordType.EVENT, levels, invItems, cnt);
 							}
-						})));
+						});
 						break;
 				}
 			}
