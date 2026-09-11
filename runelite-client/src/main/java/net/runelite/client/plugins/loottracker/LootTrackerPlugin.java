a2c4d7d262d56592e9efca625e8cc5ec738536ea
Blonks <40224407+Blonks-afk@users.noreply.github.com>
2026-09-11 12:32:20 -0400

    loottracker: Remove unused import
    loottracker: Remove unused import

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
@@ -47,7 +47,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
