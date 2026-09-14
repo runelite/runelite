044da3b73d876752efc81049ec51627de1f02800
Jordan Atwood <nightfirecat@nightfirec.at>
2026-09-14 16:15:18 -0700

    agility: update colossal wyrm basic course xp

--- runelite-client/src/main/java/net/runelite/client/plugins/agility/Courses.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/agility/Courses.java
@@ -46,7 +46,7 @@ enum Courses
 	APE_ATOLL(580.0, 11050, new WorldPoint(2770, 2747, 0)),
 	SHAYZIEN_ADVANCED(507.5, 5944, new WorldPoint(1522, 3625, 0)),
 	FALADOR(586, 12084, new WorldPoint(3029, 3332, 0), new WorldPoint(3029, 3333, 0), new WorldPoint(3029, 3334, 0), new WorldPoint(3029, 3335, 0)),
-	COLOSSAL_WYRM_BASIC(504),
+	COLOSSAL_WYRM_BASIC(601.6),
 	WILDERNESS(571.4, 11837, new WorldPoint(2993, 3933, 0), new WorldPoint(2994, 3933, 0), new WorldPoint(2995, 3933, 0)),
 	WEREWOLF(730.0, 14234, new WorldPoint(3528, 9873, 0)),
 	SEERS(570.0, 10806, new WorldPoint(2704, 3464, 0)),
