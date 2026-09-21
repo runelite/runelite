a5c2494fc913d5b6023edc7883c4933c948f852e
Ella Ruby <160190913+katsuninken@users.noreply.github.com>
2026-09-22 01:37:41 +1000

    skill calc: update Sepulchre floor 4 and 5 level requirements

--- runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/AgilityAction.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/skillcalculator/skills/AgilityAction.java
@@ -66,10 +66,10 @@ public enum AgilityAction implements NamedSkillAction
 	DORGESH_KAAN_AGILITY_COURSE("Dorgesh-Kaan Agility Course", 70, 2750, ItemID.DORGESH_POWERSTATION_POWERBOX),
 	HALLOWED_SEPULCHRE_FLOOR_3("Hallowed Sepulchre Floor 3", 72, 1600, ItemID.RING_OF_ENDURANCE),
 	PRIFDDINAS_AGILITY_COURSE("Prifddinas Agility Course", 75, 1337, ItemID.PRIF_CRYSTAL_SHARD),
+	HALLOWED_SEPULCHRE_FLOOR_4("Hallowed Sepulchre Floor 4", 77, 2875, ItemID.RING_OF_ENDURANCE),
 	RELLEKKA_ROOFTOP("Rellekka Rooftop", 80, 780, ItemID.GRACE),
-	HALLOWED_SEPULCHRE_FLOOR_4("Hallowed Sepulchre Floor 4", 82, 2875, ItemID.RING_OF_ENDURANCE),
+	HALLOWED_SEPULCHRE_FLOOR_5("Hallowed Sepulchre Floor 5", 87, 5725, ItemID.RING_OF_ENDURANCE),
 	ARDOUGNE_ROOFTOP("Ardougne Rooftop", 90, 889, ItemID.GRACE),
-	HALLOWED_SEPULCHRE_FLOOR_5("Hallowed Sepulchre Floor 5", 92, 5725, ItemID.RING_OF_ENDURANCE),
 	;
 
 	private final String name;
